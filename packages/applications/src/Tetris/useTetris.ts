7a9a813bb3aefb29d24fa38c32e2cc14ee91c093
bestlyg <1057966749@qq.com>
2021-04-20 13:45:30 +0800

    格式(配置文件): format

--- packages/applications/src/Tetris/useTetris.ts
+++ packages/applications/src/Tetris/useTetris.ts
@@ -5,17 +5,29 @@ import { useKeyPress, useInterval } from 'ahooks';
 import { message } from 'antd';
 
 const { random, cloneDeep } = lodash;
-/** 边框最小值 */
+/** 宽度最小值 */
 export const WIDTH_MIN = 10;
+/** 宽度最大值 */
 export const WIDTH_MAX = 20;
-/** 边框最大值 */
+/** 高度最小值 */
 export const HEIGHT_MIN = 20;
+/** 高度最大值 */
 export const HEIGHT_MAX = 40;
+/** 单方块的长宽 */
 export const POINT_SIZE = 20;
+/** 用于判断，多少块数后进行速度增长 */
+const SPEED_INCREASE_BLOCK_COUNT = 20;
+/** 速度提升的倍数 */
+const SPEED_INCREASE_MULTIPLE = 0.8;
+/** NEXT的宽度 */
 const NEXT_BLOCK_WIDTH = 8;
+/** NEXT的高度 */
 const NEXT_BLOCK_HEIGHT = 6;
+/** NEXT离画布顶端的格数 */
 const NEXT_BLOCK_START_LINE = 2;
+/** 块数 */
 const BLOCK_COUNT = 7;
+/** 块类型 */
 enum Block {
   S,
   Z,
@@ -25,6 +37,7 @@ enum Block {
   O,
   T,
 }
+/** 块颜色 */
 const BLOCK_COLOR: Record<Block, string> = {
   [Block.I]: '#5cb3cc',
   [Block.J]: '#15559a',
@@ -34,7 +47,9 @@ const BLOCK_COLOR: Record<Block, string> = {
   [Block.O]: '#e2d849',
   [Block.T]: '#8076a3',
 };
+/** 生成随机块 */
 const getRandomBlock = () => random(0, BLOCK_COUNT - 1) as Block;
+/** 分数比例 */
 enum Score {
   BOLCK = 10,
   LINE1 = 100,
@@ -42,14 +57,17 @@ enum Score {
   LINE3 = 400,
   LINE4 = 800,
 }
+/** 获取活动区域列表 */
 const getBlockList = (width: number, height: number) =>
   new Array(height).fill(0).map(_ => new Array(width).fill(null));
+/** 旋转后的方向 */
 const nextDirectionMap: Record<Direction, Direction> = {
   [Direction.UP]: Direction.RIGHT,
   [Direction.RIGHT]: Direction.DOWN,
   [Direction.DOWN]: Direction.LEFT,
   [Direction.LEFT]: Direction.UP,
 };
+/** 从当前块进行旋转 */
 const changeDirectionMap: Record<
   Block,
   (list: [number, number][], nextDirection: Direction) => [number, number][]
@@ -182,8 +200,6 @@ const changeDirectionMap: Record<
     return ans;
   },
 };
-const SPEED_INCREASE_BLOCK_COUNT = 20;
-const SPEED_INCREASE_MULTIPLE = 0.8;
 export const useTetris = () => {
   const { score, setScore, maxScore } = useScore('Tetris_');
   /** canvasDOM引用 */
@@ -192,17 +208,16 @@ export const useTetris = () => {
   const [width, setWidth] = useState(WIDTH_MIN + 2);
   /** 高度 */
   const [height, setHeight] = useState(HEIGHT_MIN + 1);
+  /** 块列表，包含颜色值 */
   const [blockList, setBlockList] = useState<(string | null)[][]>(
     getBlockList(WIDTH_MIN, HEIGHT_MIN)
   );
+  /** 在块列表进行变化后，判断是否可消行 */
   useEffect(() => {
     let newScore = score;
     const indexList: number[] = [];
-    for (let i = blockList.length - 1; i >= 0; i--) {
-      if (blockList[i].every(v => v !== null)) {
-        indexList.push(i);
-      }
-    }
+    for (let i = blockList.length - 1; i >= 0; i--)
+      if (blockList[i].every(v => v !== null)) indexList.push(i);
     const count = indexList.length;
     if (count === 0) return;
     indexList.forEach(i => blockList.splice(i, 1));
@@ -213,17 +228,25 @@ export const useTetris = () => {
   }, [blockList]);
   const canvasWidth = useMemo(() => (width + NEXT_BLOCK_WIDTH + 1) * POINT_SIZE, [width]);
   const canvasHeight = useMemo(() => height * POINT_SIZE, [height]);
+  /** 下一个块类型 */
   const [nextBlock, setNextBlock] = useState<Block>(Block.Z);
+  /** 当前块坐标 */
   const [curBlockList, setCurBlockList] = useState<[number, number][] | null>(null);
+  /** 当前块类型 */
   const [curBlock, setCurBlock] = useState(Block.I);
+  /** 速度 */
   const [speed, setSpeed] = useState<number | null>(null);
+  /** 块数 */
   const [blockCount, setBlockCount] = useState(0);
+  /** 块数变化时检测倍数提升 */
   useEffect(() => {
     if (speed !== null && blockCount % SPEED_INCREASE_BLOCK_COUNT === 0) {
       setSpeed(speed * SPEED_INCREASE_MULTIPLE);
     }
   }, [blockCount]);
+  /** 旋转方向 */
   const [direction, setDirection] = useState<Direction>(Direction.UP);
+  /** 初始化块位置 */
   const initBlockMap = useMemo(() => {
     const halfWidth = width / 2;
     const cache: Record<Block, [number, number][]> = {
@@ -272,18 +295,23 @@ export const useTetris = () => {
     };
     return cache;
   }, [width, height]);
+  /** 游戏状态 */
   const [state, setState] = useState(false);
+  /** 游戏结束状态 */
   const [gameOver, setGameOver] = useState(false);
+  /** 绘制点 */
   const drawPoint = useCallback((x: number, y: number) => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
     ctx.fillRect(x * POINT_SIZE, y * POINT_SIZE, POINT_SIZE, POINT_SIZE);
   }, []);
+  /** 清空画布 */
   const drawClear = useCallback(() => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
     ctx?.clearRect(0, 0, canvasWidth, canvasHeight);
   }, [canvasWidth, canvasHeight]);
+  /** 绘制文本 */
   const drawText = useCallback(() => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
@@ -293,6 +321,7 @@ export const useTetris = () => {
     ctx.fillText('NEXT', (width + 1) * POINT_SIZE, 24);
     ctx.closePath();
   }, [width]);
+  /** 绘制墙体和背景 */
   const drawWall = useCallback(() => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
@@ -309,6 +338,7 @@ export const useTetris = () => {
     for (let i = 0; i < width; i++) drawPoint(i, height - 1);
     ctx.closePath();
   }, [height, width]);
+  /** 绘制下一块 */
   const drawNextBlock = useCallback(() => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
@@ -422,6 +452,7 @@ export const useTetris = () => {
     }
     ctx.closePath();
   }, [width, nextBlock]);
+  /** 绘制分割线 */
   const drawLine = useCallback(() => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
@@ -440,6 +471,7 @@ export const useTetris = () => {
     }
     ctx.closePath();
   }, [height, width]);
+  /** 绘制列表 */
   const drawBlockList = useCallback(() => {
     const ctx = canvas.current?.getContext('2d');
     if (!ctx) return;
@@ -452,6 +484,7 @@ export const useTetris = () => {
           ctx.closePath();
         }
   }, [width, height, blockList]);
+  /** 绘制当前下落块 */
   const drawCurBlock = useCallback(() => {
     if (curBlockList === null) return;
     const ctx = canvas.current?.getContext('2d');
@@ -461,6 +494,7 @@ export const useTetris = () => {
     curBlockList.forEach(([x, y]) => drawPoint(x + 1, y));
     ctx.closePath();
   }, [curBlockList]);
+  /** 绘制主函数 */
   const draw = useCallback(() => {
     drawClear();
     drawWall();
@@ -470,6 +504,7 @@ export const useTetris = () => {
     drawLine();
     drawText();
   }, [drawClear, drawWall, drawBlockList, drawNextBlock, drawCurBlock, drawLine, drawText]);
+  /** 重置 */
   const reset = useCallback(() => {
     setBlockList(getBlockList(width - 2, height - 1));
     setNextBlock(getRandomBlock());
@@ -479,6 +514,7 @@ export const useTetris = () => {
     setSpeed(null);
     setScore(0);
   }, [drawPoint, height, width, setNextBlock, setState]);
+  /** 方向键移动触发，左右移动，下快速下落，上旋转 */
   const onMove = useCallback(
     (moveDirection: Direction) => {
       if (curBlockList === null) return;
@@ -540,6 +576,7 @@ export const useTetris = () => {
   }, [gameOver]);
   useEffect(draw, [blockList, nextBlock, curBlockList, curBlock]);
   useInterval(() => {
+    /** 当前块为null时触发 */
     const opNull = () => {
       const block = initBlockMap[nextBlock];
       if (block.some(([x, y]) => blockList[y][x] !== null)) {
