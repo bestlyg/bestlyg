c27fe807b9f8bffe142a8de032564e4293558775
bestlyg <1057966749@qq.com>
2021-05-14 10:18:51 +0800

    特性(力扣题解): add leetcode no.12

--- scripts/createLeetCodeMarkdown.ts
+++ scripts/createLeetCodeMarkdown.ts
@@ -33,8 +33,8 @@ interface Markdown {
   solutions: Solution[];
 }
 const md: Markdown = {
-  existMarkdown: false,
-  name: '1302. 层数最深叶子节点的和',
+  existMarkdown: true,
+  name: '12. 整数转罗马数字',
   url: 'https://leetcode-cn.com/problems/deepest-leaves-sum/',
   difficulty: Difficulty.中等,
   tag: [Tag.树, Tag.深度优先搜索],
@@ -42,45 +42,53 @@ const md: Markdown = {
   solutions: [
     {
       script: Script.TS,
-      time: 124,
-      memory: 48.3,
-      desc: '层序遍历',
-      code: `function deepestLeavesSum(root: TreeNode | null): number {
-        if (root === null) return 0;
-        const queue: TreeNode[] = [root];
-        let size = 1;
-        let ans = root.val;
-        while (queue.length !== 0) {
-          const node = queue.shift()!;
-          node.left && queue.push(node.left);
-          node.right && queue.push(node.right);
-          if (--size === 0) {
-            if (queue.length !== 0) ans = queue.reduce((total, node) => total + node.val, 0);
-            size = queue.length;
-          }
+      time: 212,
+      memory: 44.3,
+      desc: '分节处理',
+      code: `const config: Map<number, Map<number, string>> = new Map([
+        [
+          100,
+          new Map([
+            [1, 'C'],
+            [5, 'D'],
+            [10, 'M'],
+          ]),
+        ],
+        [
+          10,
+          new Map([
+            [1, 'X'],
+            [5, 'L'],
+            [10, 'C'],
+          ]),
+        ],
+        [
+          1,
+          new Map([
+            [1, 'I'],
+            [5, 'V'],
+            [10, 'X'],
+          ]),
+        ],
+      ]);
+      function intToRoman(num: number): string {
+        let ans = '';
+        if (num >= 1000) {
+          ans += 'M'.repeat(~~(num / 1000));
+          num = num % 1000;
         }
-        return ans;
-      }`,
-    },
-    {
-      script: Script.TS,
-      time: 116,
-      memory: 48.3,
-      desc: '中序遍历',
-      code: `function deepestLeavesSum(root: TreeNode | null): number {
-        if (root === null) return 0;
-        let maxDep = 1;
-        let ans = root.val;
-        const inorder = (node: TreeNode, dep = 1): void => {
-          if (dep > maxDep) {
-            ans = 0;
-            maxDep = dep;
-          }
-          node.left && inorder(node.left, dep + 1);
-          node.right && inorder(node.right, dep + 1);
-          if (!node.left && !node.right && dep===maxDep) ans += node.val;
-        };
-        inorder(root)
+        for (const [val, map] of config) {
+          if (num < val) continue;
+          const c1 = map.get(1)!;
+          const c5 = map.get(5)!;
+          const c10 = map.get(10)!;
+          const c = ~~(num / val);
+          num = num % val;
+          if (c === 9) ans += c1 + c10;
+          else if (c >= 5) ans += c5 + c1.repeat(c - 5);
+          else if (c === 4) ans += c1 + c5;
+          else ans += c1.repeat(c);
+        }
         return ans;
       }`,
     },
--- scripts/env.ts
+++ scripts/env.ts
@@ -6,21 +6,49 @@ const { TreeNode, UnionFind, ListNode } = structures;
 type TreeNode = structures.TreeNode;
 type ListNode = structures.ListNode;
 type UnionFind = structures.UnionFind;
-function deepestLeavesSum(root: TreeNode | null): number {
-  if (root === null) return 0;
-  let maxDep = 1;
-  let ans = root.val;
-  const inorder = (node: TreeNode, dep = 1): void => {
-    if (dep > maxDep) {
-      ans = 0;
-      maxDep = dep;
-    }
-    node.left && inorder(node.left, dep + 1);
-    node.right && inorder(node.right, dep + 1);
-    if (!node.left && !node.right) ans += node.val;
-  };
+const config: Map<number, Map<number, string>> = new Map([
+  [
+    100,
+    new Map([
+      [1, 'C'],
+      [5, 'D'],
+      [10, 'M'],
+    ]),
+  ],
+  [
+    10,
+    new Map([
+      [1, 'X'],
+      [5, 'L'],
+      [10, 'C'],
+    ]),
+  ],
+  [
+    1,
+    new Map([
+      [1, 'I'],
+      [5, 'V'],
+      [10, 'X'],
+    ]),
+  ],
+]);
+function intToRoman(num: number): string {
+  let ans = '';
+  if (num >= 1000) {
+    ans += 'M'.repeat(~~(num / 1000));
+    num = num % 1000;
+  }
+  for (const [val, map] of config) {
+    if (num < val) continue;
+    const c1 = map.get(1)!;
+    const c5 = map.get(5)!;
+    const c10 = map.get(10)!;
+    const c = ~~(num / val);
+    num = num % val;
+    if (c === 9) ans += c1 + c10;
+    else if (c >= 5) ans += c5 + c1.repeat(c - 5);
+    else if (c === 4) ans += c1 + c5;
+    else ans += c1.repeat(c);
+  }
   return ans;
 }
-console.log(
-  deepestLeavesSum(TreeNode.factory([1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, 8]))
-);
