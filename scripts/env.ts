8c0b40ad7e4094695386bc011acd1b78b8871fbf
bestlyg <1057966749@qq.com>
2021-05-20 10:26:16 +0800

    特性(力扣题解): add leetcode no.692

--- scripts/env.ts
+++ scripts/env.ts
@@ -7,32 +7,25 @@ type TreeNode = structures.TreeNode;
 type ListNode = structures.ListNode;
 type UnionFind = structures.UnionFind;
 type Heap = structures.Heap;
-function kthLargestValue(matrix: number[][], k: number): number {
-  const rowLen = matrix.length;
-  const colLen = matrix[0].length;
-  const prefixSumList: number[][] = new Array(rowLen + 1)
-    .fill(0)
-    .map(_ => new Array(colLen + 1).fill(0));
-  const list: number[] = [];
-  for (let row = 1; row <= rowLen; row++) {
-    for (let col = 1; col <= colLen; col++) {
-      list.push(
-        (prefixSumList[row][col] =
-          prefixSumList[row - 1][col] ^
-          prefixSumList[row][col - 1] ^
-          prefixSumList[row - 1][col - 1] ^
-          matrix[row - 1][col - 1])
-      );
-    }
-  }
-  return list.sort((a, b) => b - a)[k - 1];
+function topKFrequent(words: string[], k: number): string[] {
+  const map: Record<string, number> = {};
+  for (const word of words) map[word] = (map[word] ?? 0) + 1;
+  const chartToNumber = (char: string) => char.codePointAt(0)! - 'a'.codePointAt(0)!;
+  const heap = new Heap<[string, number]>(([k1, v1], [k2, v2]) => {
+    if (v1 !== v2) return v1 - v2;
+    let i1 = 0;
+    const end1 = k1.length;
+    let i2 = 0;
+    const end2 = k2.length;
+    for (; i1 < end1 && i2 < end2; i1++, i2++)
+      if (k1[i1] !== k2[i2]) return chartToNumber(k2[i2]) - chartToNumber(k1[i1]);
+    if (i1 === end1) return 1;
+    else if (i2 === end2) return -1;
+    else return 0;
+  });
+  for (const data of Object.entries(map)) heap.add(data);
+  const ans: string[] = [];
+  while (heap.size !== 0 && k--) ans.push(heap.remove()[0]);
+  return ans;
 }
-console.log(
-  kthLargestValue(
-    [
-      [5, 2],
-      [1, 6],
-    ],
-    1
-  )
-);
+console.log(topKFrequent(['a', 'aa', 'aaa'], 1));
