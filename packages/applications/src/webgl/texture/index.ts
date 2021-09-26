1456716ddc618b5834282585756391e191d91ba6
bestlyg <1057966749@qq.com>
2021-09-26 22:57:09 +0800

    特性(应用合集): 增加多点放射

--- packages/applications/src/webgl/texture/index.ts
+++ packages/applications/src/webgl/texture/index.ts
@@ -1,2 +1,3 @@
 export { default as SkinTexture } from './SkinTexture';
 export { default as RadiationTexture } from './RadiationTexture';
+export { default as Point2RadiationTexture } from './Point2RadiationTexture';
