20591f765f46ed3fb60afa5458697cd4a28cdd6c
bestlyg <1057966749@qq.com>
2021-09-26 23:09:27 +0800

    特性(应用合集): 增加正弦形放射

---
+++ packages/applications/src/webgl/texture/SinRadiationTexture.tsx
@@ -0,0 +1,50 @@
+import { WEBGL, angleToRad } from '@bestlyg/shared';
+import React, { useEffect, useRef, useState } from 'react';
+const vertexShaderSource = `
+attribute vec4 a_Position;
+void main(){
+  gl_Position=a_Position;
+}
+`;
+const fragmentShaderSource = `
+precision mediump float;
+uniform vec2 u_Center ;
+float sinRandom(float rad , float A , float omega , float phi){
+    return A * sin( omega * rad + phi ) ;
+}
+void main(){
+    vec2 v = gl_FragCoord.xy - u_Center;
+    float rad = atan( v.y , v.x );
+    float color = sinRandom( rad , 1.0 , 4.0 , 2.0 ) ;
+    gl_FragColor = vec4( color , color , color , 1.0) ;
+}
+`;
+const CANVAS_SIZE = 300;
+export default function SinRadiationTexture() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const webglRef = useRef<WEBGL.Webgl>();
+  const polyRef = useRef<WEBGL.Poly>();
+  useEffect(() => {
+    webglRef.current = new WEBGL.Webgl({
+      canvas: canvasRef.current!,
+      vertexShaderSource,
+      fragmentShaderSource,
+      canvasSize: [CANVAS_SIZE, CANVAS_SIZE],
+    });
+    polyRef.current = new WEBGL.Poly(
+      webglRef.current,
+      new Float32Array([-1, 1, -1, -1, 1, 1, 1, -1]),
+      'TRIANGLE_STRIP',
+      [{ name: 'a_Position', size: 2, index: 0, byteIndex: 0 }],
+      [
+        {
+          name: 'u_Center',
+          method: 'uniform2fv',
+          data: new Float32Array([CANVAS_SIZE / 2, CANVAS_SIZE / 2]),
+        },
+      ]
+    );
+    polyRef.current.draw();
+  }, []);
+  return <canvas ref={canvasRef} />;
+}
--- packages/applications/src/webgl/texture/index.ts
+++ packages/applications/src/webgl/texture/index.ts
@@ -1,3 +1,4 @@
 export { default as SkinTexture } from './SkinTexture';
 export { default as RadiationTexture } from './RadiationTexture';
 export { default as Point2RadiationTexture } from './Point2RadiationTexture';
+export { default as SinRadiationTexture } from './SinRadiationTexture';
