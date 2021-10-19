4ee4d189daca08be232491239ad867c781224f7f
bestlyg <1057966749@qq.com>
2021-10-19 10:07:14 +0800

    特性(力扣题解): add leetcode no.211

--- scripts/script.ts
+++ scripts/script.ts
@@ -2,14 +2,63 @@ import { structures, log } from './utils';
 import { HashMap, toHash } from '@bestlyg/data-structures/src';
 import { sequence } from '@bestlyg/algorithms/src';
 import { fill, find, first, merge, min, random, reverse, size, upperFirst } from 'lodash';
-const { TreeNode, UnionFind, ListNode, Heap, Trie } = structures;
+const { TreeNode, UnionFind, ListNode, Heap, Trie, TrieNode } = structures;
 type TreeNode = structures.TreeNode;
 type ListNode = structures.ListNode;
 type UnionFind = structures.UnionFind;
 type Heap = structures.Heap;
+type TrieNode = structures.TrieNode;
 
 /*
  */
-const map = new HashMap<number, string>((k1, k2) => k1 - k2);
-map.set(7, '');
-console.log(map.remove(103));
+class WordDictionary {
+  private trie = new Trie();
+  addWord(word: string): void {
+    this.trie.add(word);
+  }
+  search(word: string): boolean {
+    return this._search(0, word, this.trie.root);
+  }
+  private _search(idx: number, word: string, node: TrieNode): boolean {
+    const ch = word[idx];
+    if (idx === word.length - 1) {
+      if (ch === '.') return Array.from(node.children.values()).some(node => node.end);
+      const lastNode = node.children.get(ch);
+      return !!lastNode?.end;
+    }
+    if (ch === '.') {
+      for (const nextNode of node.children.values()) {
+        if (this._search(idx + 1, word, nextNode)) return true;
+      }
+      return false;
+    }
+    const nextNode = node.children.get(ch);
+    if (!nextNode) return false;
+    return this._search(idx + 1, word, nextNode);
+  }
+}
+const wordDictionary = new WordDictionary();
+// log([
+//   wordDictionary.addWord('bad'),
+//   wordDictionary.addWord('dad'),
+//   wordDictionary.addWord('mad'),
+//   wordDictionary.search('pad'), // return False
+//   wordDictionary.search('bad'), // return True
+//   wordDictionary.search('.ad'), // return True
+//   wordDictionary.search('b..'), // return True
+// ]);
+log([
+  wordDictionary.addWord('at'),
+  wordDictionary.addWord('and'),
+  wordDictionary.addWord('an'),
+  wordDictionary.addWord('add'),
+  wordDictionary.search('a'), // return False
+  wordDictionary.search('.at'), // return True
+  wordDictionary.addWord('bat'), // return True
+  wordDictionary.search('.at'), // return True
+  wordDictionary.search('an.'), // return True
+  wordDictionary.search('a.d.'), // return True
+  wordDictionary.search('b.'), // return True
+  wordDictionary.search('a.d'), // return True
+  wordDictionary.search('.'), // return True
+]);
