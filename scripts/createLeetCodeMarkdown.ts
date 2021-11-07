f084dfc75cc2d897274340fc8132d07d30a44c7e
bestlyg <1057966749@qq.com>
2021-11-07 20:38:18 +0800

    特性(力扣题解): update leetcode

--- scripts/createLeetCodeMarkdown.ts
+++ scripts/createLeetCodeMarkdown.ts
@@ -1,43 +1,11 @@
 import { leetcode, trimBlank, resolve, fs, moment, specStr, LOGO, markdown, chalk } from './utils';
+import { leetCodeMarkdowns } from './leetCodeMarkdown';
 
 const { backquote } = specStr;
-const { link } = markdown;
-const { Script, Difficulty, Tag, srcPath, solutionReg, getDirOrder, getFileOrder, getDirName } =
-  leetcode;
-type Script = leetcode.Script;
-type Difficulty = leetcode.Difficulty;
-type Tag = leetcode.Tag;
+const { srcPath, solutionReg, getDirOrder, getFileOrder, getDirName } = leetcode;
 type Solution = leetcode.Solution;
 type Markdown = leetcode.Markdown;
 
-const mds: Markdown[] = [
-  {
-    existMarkdown: !true,
-    name: '268. 丢失的数字',
-    url: 'https://leetcode-cn.com/problems/range-addition-ii/',
-    difficulty: Difficulty.简单,
-    tag: [Tag.数组, Tag.数学],
-    desc: `给定一个初始元素全部为 0，大小为 m*n 的矩阵 M 以及在 M 上的一系列更新操作。`,
-    solutions: [
-      {
-        script: Script.TS,
-        time: 80,
-        memory: 40.1,
-        desc: '取最小值',
-        code: `function maxCount(m: number, n: number, ops: number[][]): number {
-          if (ops.length === 0) return m * n;
-          let minA = Infinity;
-          let minB = Infinity;
-          ops.forEach(([a, b]) => {
-            minA = Math.min(minA, a);
-            minB = Math.min(minB, b);
-          });
-          return minA * minB;
-        }`,
-      },
-    ],
-  },
-];
 const descFormat = (str: string) => (str.endsWith('。') ? str : str + '。');
 let current: Markdown;
 let dirName!: string;
@@ -47,7 +15,7 @@ let dirPath!: string;
 function main() {
   console.log(chalk.blue(`正在生成LeetCode题解`));
   console.log(LOGO);
-  for (const md of mds) {
+  for (const md of leetCodeMarkdowns) {
     current = md;
     dirName = getDirName(md.name);
     dirPath = resolve(srcPath, dirName);
--- scripts/script.ts
+++ scripts/script.ts
@@ -18,30 +18,17 @@ type Heap = structures.Heap;
 
 /*
  */
-function maxCount(m: number, n: number, ops: number[][]): number {
-  if (ops.length === 0) return m * n;
-  let minA = Infinity;
-  let minB = Infinity;
-  ops.forEach(([a, b]) => {
-    minA = Math.min(minA, a);
-    minB = Math.min(minB, b);
-  });
-  return minA * minB;
+function grayCode(n: number): number[] {
+  if (n === 0) return [0];
+  const ans = [0, 1];
+  if (n === 1) return ans;
+  while (--n) {
+    ans.push(
+      ...Array.from(ans)
+        .reverse()
+        .map(v => v | (1 << n))
+    );
+  }
+  return ans;
 }
-log([
-  maxCount(26, 17, [
-    [20, 10],
-    [26, 11],
-    [2, 11],
-    [4, 16],
-    [2, 3],
-    [23, 13],
-    [7, 15],
-    [11, 11],
-    [25, 13],
-    [11, 13],
-    [13, 11],
-    [13, 16],
-    [26, 17],
-  ]),
-]);
+log([grayCode(2)]);
