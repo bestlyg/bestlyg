5f6f5efd38bd7842b70d420d78cc6923ae1f0103
bestlyg <1057966749@qq.com>
2021-11-16 10:39:00 +0800

    特性(力扣题解): add leetcode no.391

--- scripts/script.ts
+++ scripts/script.ts
@@ -30,7 +30,90 @@ type Heap = structures.Heap;
 
 /*
  */
-function bulbSwitch(n: number): number {
-  return Math.floor(Math.sqrt(n + 0.5));
+function isRectangleCover(rectangles: number[][]): boolean {
+  /**
+   完美矩形要满足:
+   1. 最左上、左下、右上、右下四个顶点只出现1次;
+   2. 重合顶点重合次数必须为2、4，不能出现一次。
+   3. 大矩形面积等于小矩形面积之和。
+   */
+  type Data = { cnt: number; point: number[] };
+  const set = new Set<string>();
+  const map: Record<string, Data> = {};
+  const format = (x: number, y: number) => `${x}:${y}`;
+  const map_add = (x: number, y: number) => {
+    const formatStr = format(x, y);
+    let data = map[formatStr];
+    if (!data) map[formatStr] = data = { cnt: 0, point: [x, y] };
+    data.cnt++;
+  };
+  let sum = 0;
+  const vertex: number[] = [];
+  const is_vertex = (x: number, y: number) =>
+    (x === vertex[0] && y === vertex[1]) ||
+    (x === vertex[2] && y === vertex[3]) ||
+    (x === vertex[0] && y === vertex[3]) ||
+    (x === vertex[2] && y === vertex[1]);
+  for (const [x, y, a, b] of rectangles) {
+    const formatStr = format(x, y) + ':' + format(a, b);
+    if (set.has(formatStr)) return false;
+    set.add(formatStr);
+    if (vertex[0] === undefined || vertex[0] > x || vertex[1] > y) {
+      vertex[0] = x;
+      vertex[1] = y;
+    }
+    if (vertex[2] === undefined || vertex[2] < a || vertex[3] < b) {
+      vertex[2] = a;
+      vertex[3] = b;
+    }
+    sum += (a - x) * (b - y);
+    map_add(x, y);
+    map_add(a, b);
+    map_add(x, b);
+    map_add(a, y);
+  }
+  log({ vertex, sum });
+  if ((vertex[2] - vertex[0]) * (vertex[3] - vertex[1]) !== sum) return false;
+  console.log('sum checked');
+  for (const {
+    cnt,
+    point: [x, y],
+  } of Object.values(map)) {
+    if ((cnt === 1 && !is_vertex(x, y)) || (cnt !== 1 && cnt !== 2 && cnt !== 4)) {
+      console.log('err data');
+      console.log(cnt, x, y);
+      return false;
+    }
+  }
+  return true;
 }
-log([6].map(v => bulbSwitch(v)));
+const arr = [
+  [0, 0, 4, 1],
+  [7, 0, 8, 2],
+  [6, 2, 8, 3],
+  [5, 1, 6, 3],
+  [4, 0, 5, 1],
+  [6, 0, 7, 2],
+  [4, 2, 5, 3],
+  [2, 1, 4, 3],
+  [0, 1, 2, 2],
+  [0, 2, 2, 3],
+  [4, 1, 5, 2],
+  [5, 0, 6, 1],
+];
+log([
+  isRectangleCover([
+    [0, 0, 4, 1],
+    [7, 0, 8, 2],
+    [6, 2, 8, 3],
+    [5, 1, 6, 3],
+    [4, 0, 5, 1],
+    [6, 0, 7, 2],
+    [4, 2, 5, 3],
+    [2, 1, 4, 3],
+    [0, 1, 2, 2],
+    [0, 2, 2, 3],
+    [4, 1, 5, 2],
+    [5, 0, 6, 1],
+  ]),
+]);
