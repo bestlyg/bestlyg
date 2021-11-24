7b090f420cc541a94e394cc4d2c8165b90af35ca
bestlyg <1057966749@qq.com>
2021-11-24 10:50:58 +0800

    特性(力扣题解): add leetcode no.423

--- scripts/script.ts
+++ scripts/script.ts
@@ -31,7 +31,40 @@ type Heap = structures.Heap;
 /*
 6 abcw baz foo bar xtfn abcdef
  */
-function maxDepth(root: Node | null): number {
-  if (root === null) return 0;
-  return Math.max(...root.children.map(node => maxDepth(node))) + 1;
+const dict: Record<number, string[]> = {
+  1: 'one'.split(''),
+  2: 'two'.split(''), // w
+  3: 'three'.split(''), //t
+  4: 'four'.split(''), // u
+  5: 'five'.split(''), // f
+  6: 'six'.split(''), // x
+  7: 'seven'.split(''), // v
+  8: 'eight'.split(''), // g
+  9: 'nine'.split(''), // i
+  0: 'zero'.split(''), // z
+};
+const checkList = [
+  { num: 2, key: 'w' },
+  { num: 6, key: 'x' },
+  { num: 0, key: 'z' },
+  { num: 8, key: 'g' },
+  { num: 3, key: 't' },
+  { num: 4, key: 'u' },
+  { num: 5, key: 'f' },
+  { num: 7, key: 'v' },
+  { num: 9, key: 'i' },
+  { num: 1, key: 'o' },
+];
+function originalDigits(s: string): string {
+  const map: Record<string, number> = {};
+  for (const c of s) map[c] = (map[c] ?? 0) + 1;
+  const list: number[] = new Array(10).fill(0);
+  for (const { num, key } of checkList) {
+    if (!map[key]) continue;
+    const cnt = map[key];
+    list[num] += cnt;
+    for (const ch of dict[num]) map[ch] -= cnt;
+  }
+  return list.reduce((ans, cnt, num) => ans + num.toString().repeat(cnt), '');
 }
+log([originalDigits('fviefuro')]);
