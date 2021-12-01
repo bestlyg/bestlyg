69235fd7397c53532c58092d1785fdf674906c9f
bestlyg <1057966749@qq.com>
2021-12-01 09:59:47 +0800

    特性(力扣题解): add leetcode no.1446

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -10,39 +10,50 @@ type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
     existMarkdown: !true,
-    name: '400. 第 N 位数字',
-    url: 'https://leetcode-cn.com/problems/nth-digit/',
-    difficulty: Difficulty.中等,
-    tag: [Tag.数学, Tag.二分查找],
-    desc: `给你一个整数 n ，请你在无限的整数序列 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...] 中找出并返回第 n 位数字。`,
+    name: '1446. 连续字符',
+    url: 'https://leetcode-cn.com/problems/consecutive-characters/',
+    difficulty: Difficulty.简单,
+    tag: [Tag.字符串],
+    desc: `给你一个字符串 s ，字符串的「能量」定义为：只包含一种字符的最长非空子字符串的长度。`,
     solutions: [
       {
         script: Script.TS,
-        time: 68,
-        memory: 39.4,
-        desc: '直接分区间查找',
-        code: `// 每一段具有相同位数的数字算一个区间
-        function findNthDigit(n: number): number {
-          // 当前数字占几位
-          let bit = 1;
-          // 当前区间最大值
-          let max = 9;
-          // 当前区间最小值
-          let min = 1;
-          // 前区间占用位数
-          let cnt = 0;
-          // 判断n是否大于下一个区间数量，大于则移动至下一区间
-          while (n > cnt + (max - Math.floor(max / 10)) * bit) {
-            cnt += (max - Math.floor(max / 10)) * bit++;
-            max = max * 10 + 9;
-            min *= 10;
+        time: 88,
+        memory: 39.9,
+        desc: '遍历',
+        code: `function maxPower(s: string): number {
+          let ans = 0;
+          for (let i = 0, n = s.length; i < n; i++) {
+            let cnt = 1;
+            const ch = s[i];
+            while (i + 1 < n && s[i + 1] === ch) {
+              i++;
+              cnt++;
+            }
+            ans = Math.max(ans, cnt);
           }
-          // 删除前一区间的量
-          n -= cnt;
-          // 计算当前区间中所指向的数字
-          const num = Math.floor((n - 1) / bit) + min;
-          return +num.toString()[(n - 1) % bit];
-        }`,
+          return ans;
+        }
+        `,
+      },
+      {
+        script: Script.TS,
+        time: 4,
+        memory: 5.9,
+        desc: '遍历',
+        code: `int maxPower(char * s){
+    int ans = 0;
+    for (int i = 0; i < strlen(s); i++) {
+        int cnt = 1;
+        char ch = s[i];
+        while (i + 1 < strlen(s) && s[i + 1] == ch) {
+            i++; 
+            cnt++;
+        }
+        if (cnt > ans) ans = cnt;
+    }
+    return ans;
+}`,
       },
     ],
   },
--- scripts/script.ts
+++ scripts/script.ts
@@ -31,26 +31,16 @@ type Heap = structures.Heap;
 /*
 6 abcw baz foo bar xtfn abcdef
  */
-// 每一段具有相同位数的数字算一个区间
-function findNthDigit(n: number): number {
-  // 当前数字占几位
-  let bit = 1;
-  // 当前区间最大值
-  let max = 9;
-  // 当前区间最小值
-  let min = 1;
-  // 前区间占用位数
-  let cnt = 0;
-  // 判断n是否大于下一个区间数量，大于则移动至下一区间
-  while (n > cnt + (max - Math.floor(max / 10)) * bit) {
-    cnt += (max - Math.floor(max / 10)) * bit++;
-    max = max * 10 + 9;
-    min *= 10;
+function maxPower(s: string): number {
+  let ans = 0;
+  for (let i = 0, n = s.length; i < n; i++) {
+    let cnt = 1;
+    const ch = s[i];
+    while (i + 1 < n && s[i + 1] === ch) {
+      i++;
+      cnt++;
+    }
+    ans = Math.max(ans, cnt);
   }
-  // 删除前一区间的量
-  n -= cnt;
-  // 计算当前区间中所指向的数字
-  const num = Math.floor((n - 1) / bit) + min;
-  return +num.toString()[(n - 1) % bit];
+  return ans;
 }
-log([findNthDigit(1000), findNthDigit(1001), findNthDigit(1002), findNthDigit(1003)]);
