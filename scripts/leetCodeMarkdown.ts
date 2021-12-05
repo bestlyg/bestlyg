1644f7dfa604f325494fb8537abfae97b15bbdef
bestlyg <1057966749@qq.com>
2021-12-05 11:23:02 +0800

    特性(力扣题解): add leetcode no.372

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -10,129 +10,30 @@ type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
     existMarkdown: !true,
-    name: '959. 由斜杠划分区域',
-    url: 'https://leetcode-cn.com/problems/regions-cut-by-slashes/',
+    name: '372. 超级次方',
+    url: 'https://leetcode-cn.com/problems/super-pow/',
     difficulty: Difficulty.中等,
-    tag: [Tag.深度优先搜索, Tag.广度优先搜索, Tag.并查集, Tag.图],
-    desc: `在由 1 x 1 方格组成的 N x N 网格 grid 中，每个 1 x 1 方块由 /、\\ 或空格构成。这些字符会将方块划分为一些共边的区域。`,
+    tag: [Tag.数学, Tag.分治],
+    desc: `你的任务是计算 ab 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。`,
     solutions: [
       {
-        script: Script.C,
-        time: 4,
-        memory: 6.6,
-        desc: '并查集',
-        code: `typedef struct unionfind
-{
-    int *father, len, size;
-} UnionFind;
-UnionFind *unionfind_create(int len)
-{
-    UnionFind *uf = (UnionFind *)malloc(sizeof(UnionFind));
-    uf->size = uf->len = len;
-    uf->father = (int *)malloc(sizeof(int) * len);
-    for (int i = 0; i < len; i++)
-        uf->father[i] = i;
-    return uf;
-}
-void unionfind_free(UnionFind *uf)
-{
-    free(uf->father);
-    free(uf);
-}
-int unionfind_find(UnionFind *uf, int data)
-{
-    return uf->father[data] = uf->father[data] == data ? data : unionfind_find(uf, uf->father[data]);
-}
-void unionfind_union(UnionFind *uf, int data1, int data2)
-{
-    int p1 = unionfind_find(uf, data1), p2 = unionfind_find(uf, data2);
-    if (p1 == p2) return ;
-    uf->size--;
-    uf->father[p1] = p2;
-}
-#define BLOCK 4
-int get_idx(int row, int col, int n) {
-    return row * n * BLOCK + col * BLOCK;
-}
-int regionsBySlashes(char ** grid, int gridSize){
-    UnionFind *uf = unionfind_create(gridSize * gridSize * BLOCK);
-    for (int row = 0; row < gridSize; row++) {
-        for (int col = 0; col < gridSize; col++) {
-            char ch = grid[row][col];
-            int idx1 = get_idx(row, col, gridSize), idx2 = idx1 + 1, idx3 = idx1 + 2, idx4 = idx1 + 3;
-            if (ch == ' ') {
-                unionfind_union(uf, idx1, idx2);
-                unionfind_union(uf, idx1, idx3);
-                unionfind_union(uf, idx1, idx4);
-            } else if (ch == '/') {
-                unionfind_union(uf, idx1, idx2);
-                unionfind_union(uf, idx3, idx4);
-            } else {
-                unionfind_union(uf, idx1, idx4);
-                unionfind_union(uf, idx2, idx3);
-            }
-            //if (row > 0) unionfind_union(uf, idx1, get_idx(row - 1, col, gridSize) + 2);
-            //if (col > 0) unionfind_union(uf, idx2, get_idx(row, col - 1, gridSize) + 3);
-            if (col < gridSize - 1) unionfind_union(uf, idx4, get_idx(row, col + 1, gridSize) + 1);
-            if (row < gridSize - 1) unionfind_union(uf, idx3, get_idx(row + 1, col, gridSize));
+        script: Script.TS,
+        time: 84,
+        memory: 39.7,
+        desc: '99^(2345) === (99^234)^10 * 99^5',
+        code: `const MOD = 1337;
+        function pow(a: number, b: number) {
+          let ans = 1;
+          while (b--) ans = (ans * a) % MOD;
+          return ans;
         }
-    }
-    return uf->size;
-}`,
-      },
-    ],
-  },
-  {
-    existMarkdown: true,
-    name: '547. 省份数量',
-    url: 'https://leetcode-cn.com/problems/regions-cut-by-slashes/',
-    difficulty: Difficulty.中等,
-    tag: [Tag.深度优先搜索, Tag.广度优先搜索, Tag.并查集, Tag.图],
-    desc: `在由 1 x 1 方格组成的 N x N 网格 grid 中，每个 1 x 1 方块由 /、\\ 或空格构成。这些字符会将方块划分为一些共边的区域。`,
-    solutions: [
-      {
-        script: Script.JS,
-        time: 36,
-        memory: 6.9,
-        desc: '并查集',
-        code: `typedef struct unionfind
-{
-    int *father, len, size;
-} UnionFind;
-UnionFind *unionfind_create(int len)
-{
-    UnionFind *uf = (UnionFind *)malloc(sizeof(UnionFind));
-    uf->size = uf->len = len;
-    uf->father = (int *)malloc(sizeof(int) * len);
-    for (int i = 0; i < len; i++)
-        uf->father[i] = i;
-    return uf;
-}
-void unionfind_free(UnionFind *uf)
-{
-    free(uf->father);
-    free(uf);
-}
-int unionfind_find(UnionFind *uf, int data)
-{
-    return uf->father[data] = uf->father[data] == data ? data : unionfind_find(uf, uf->father[data]);
-}
-void unionfind_union(UnionFind *uf, int data1, int data2)
-{
-    int p1 = unionfind_find(uf, data1), p2 = unionfind_find(uf, data2);
-    if (p1 == p2) return ;
-    uf->size--;
-    uf->father[p1] = p2;
-}
-int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize){
-    UnionFind *uf = unionfind_create(isConnectedSize);
-    for (int i = 0; i < isConnectedSize; i++) {
-        for (int j = 0; j < isConnectedSize; j++) {
-            if (isConnected[i][j]) unionfind_union(uf, i, j);
-        }
-    }
-    return uf->size;
-}`,
+        function superPow(a: number, b: number[]): number {
+          let ans = 1;
+          for (let i = 0; i < b.length; i++) {
+            ans = (pow(ans, 10) * pow(a, b[i])) % MOD;
+          }
+          return ans;
+        }`,
       },
     ],
   },
--- scripts/script.ts
+++ scripts/script.ts
@@ -31,12 +31,68 @@ type Heap = structures.Heap;
 /*
 6 abcw baz foo bar xtfn abcdef
  */
-function gcd(a: number, b: number) {
-  if (!b) return a;
-  return gcd(b, a % b);
+function ex_gcd(
+  a: number,
+  b: number
+): {
+  ans: number;
+  x: number;
+  y: number;
+} {
+  if (b === 0) {
+    return {
+      ans: a,
+      x: 1,
+      y: 0,
+    };
+  }
+  const res = ex_gcd(b, a % b);
+  [res.x, res.y] = [res.y, res.x - Math.floor(a / b) * res.y];
+  return res;
 }
-function gcdOfStrings(str1: string, str2: string): string {
-  if (str1 + str2 !== str2 + str1) return '';
-  return str1.substr(0, gcd(str1.length, str2.length));
+function phi(n: number) {
+  let ans = n;
+  for (let i = 2; i ** 2 < n; i++) {
+    if (n % i === 0) ans = ans * (1 - 1 / i);
+    while (n % i === 0) n /= i;
+  }
+  if (n !== 1) ans = ans * (1 - 1 / n);
+  return ans;
 }
-log([numPrimeArrangements(5)]);
+function comp(a: number, b: number, c: number): string {
+  for (let x = 0; x < Number.MAX_SAFE_INTEGER; x++) {
+    if (a ** x % b === c) return `${a} ^ ${x} mod ${b} === ${c}`;
+  }
+  return 'NOT FOUND';
+}
+// log(
+//   [
+//     ...[
+//       [4, 6],
+//       [12, 5],
+//     ].map(([a, b]: [number, number]) => {
+//       const { ans, x, y } = ex_gcd(a, b);
+//       return `${a} * ${x} + ${b} * ${y} = ${ans}`;
+//     }),
+//     phi(7),
+//     phi(12),
+//     phi(24),
+//     13 ** 6 % 7,
+//     comp(999, 7, 2),
+//   ],
+//   { chunkCount: 1 }
+// )
+const MOD = 1337;
+function pow(a: number, b: number) {
+  let ans = 1;
+  while (b--) ans = (ans * a) % MOD;
+  return ans;
+}
+function superPow(a: number, b: number[]): number {
+  let ans = 1;
+  for (let i = 0; i < b.length; i++) {
+    ans = (pow(ans, 10) * pow(a, b[i])) % MOD;
+  }
+  return ans;
+}
+log([superPow(2147483647, [2, 0, 0])]);
