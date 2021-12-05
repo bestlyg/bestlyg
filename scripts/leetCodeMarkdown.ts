cf338b1ea3edd7f00f50768c72b459a9aed0ecb3
bestlyg <1057966749@qq.com>
2021-12-06 00:14:08 +0800

    特性(力扣题解): add leetcode no.1816

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -10,30 +10,50 @@ type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
     existMarkdown: !true,
-    name: '372. 超级次方',
-    url: 'https://leetcode-cn.com/problems/super-pow/',
-    difficulty: Difficulty.中等,
-    tag: [Tag.数学, Tag.分治],
-    desc: `你的任务是计算 ab 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。`,
+    name: '1816. 截断句子',
+    url: 'https://leetcode-cn.com/problems/truncate-sentence/',
+    difficulty: Difficulty.简单,
+    tag: [Tag.数组, Tag.字符串],
+    desc: `给你一个句子 s​​​​​​ 和一个整数 k​​​​​​ ，请你将 s​​ 截断 ​，​​​使截断后的句子仅含 前 k​​​​​​ 个单词。返回 截断 s​​​​​​ 后得到的句子。`,
     solutions: [
       {
         script: Script.TS,
-        time: 84,
-        memory: 39.7,
-        desc: '99^(2345) === (99^234)^10 * 99^5',
-        code: `const MOD = 1337;
-        function pow(a: number, b: number) {
-          let ans = 1;
-          while (b--) ans = (ans * a) % MOD;
-          return ans;
-        }
-        function superPow(a: number, b: number[]): number {
-          let ans = 1;
-          for (let i = 0; i < b.length; i++) {
-            ans = (pow(ans, 10) * pow(a, b[i])) % MOD;
+        time: 72,
+        memory: 39.2,
+        desc: '分割后重组',
+        code: `function truncateSentence(s: string, k: number): string {
+          return s.split(' ').slice(0,k).join(' ')
+      };`,
+      },
+      {
+        script: Script.TS,
+        time: 72,
+        memory: 39.2,
+        desc: '遍历',
+        code: `function truncateSentence(s: string, k: number): string {
+          for(let cnt = 0,i = 0;i<s.length;i++){
+              if(s[i] === ' '){
+                  if(++cnt === k) return s.substring(0,i)
+              }
           }
-          return ans;
-        }`,
+          return s
+      };`,
+      },
+      {
+        script: Script.C,
+        time: 4,
+        memory: 6.1,
+        desc: '遍历',
+        code: `char * truncateSentence(char * s, int k){
+    for (int cnt = 0, i = 0; i < strlen(s); i++) {
+        if (s[i] == ' ' && ++cnt == k) {
+            char *ans = (char *)calloc(i + 1, sizeof(char));
+            strncpy(ans, s, i);
+            return ans;
+        }
+    }
+    return s;
+}`,
       },
     ],
   },
