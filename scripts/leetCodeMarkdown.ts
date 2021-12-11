a1174641ecbe0468463d76ca25f3799b2c79992b
bestlyg <1057966749@qq.com>
2021-12-11 09:28:41 +0800

    特性(力扣题解): add leetcode no.911

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -10,32 +10,38 @@ type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
     existMarkdown: !true,
-    name: '748. 最短补全词',
-    url: 'https://leetcode-cn.com/problems/shortest-completing-word/',
-    difficulty: Difficulty.简单,
-    tag: [Tag.数组, Tag.哈希表, Tag.字符串],
-    desc: `给你一个字符串 licensePlate 和一个字符串数组 words ，请你找出并返回 words 中的 最短补全词 。`,
+    name: '911. 在线选举',
+    url: 'https://leetcode-cn.com/problems/online-election/',
+    difficulty: Difficulty.中等,
+    tag: [Tag.设计, Tag.数组, Tag.哈希表, Tag.二分查找],
+    desc: `给你两个整数数组 persons 和 times 。在选举中，第 i 张票是在时刻为 times[i] 时投给候选人 persons[i] 的。对于发生在时刻 t 的每个查询，需要找出在 t 时刻在选举中领先的候选人的编号。`,
     solutions: [
       {
         script: Script.TS,
-        time: 88,
-        memory: 41.9,
-        desc: '哈希',
-        code: `function shortestCompletingWord(licensePlate: string, words: string[]): string {
-          const reg_lowchar = /[a-z]/;
-          const map: Record<string, number> = {};
-          for (const ch of licensePlate.toLowerCase()) {
-            if (reg_lowchar.test(ch)) map[ch] = (map[ch] ?? 0) + 1;
+        time: 292,
+        memory: 51.5,
+        desc: '初始化时记录每个时刻的获胜人数，遍历用二分',
+        code: `class TopVotedCandidate {
+          arr: number[] = [];
+          constructor(persons: number[], private times: number[]) {
+            const n = persons.length;
+            const temp = new Array(n).fill(0);
+            let max = 0;
+            for (const person of persons) {
+              if (++temp[person] >= temp[max]) max = person;
+              this.arr.push(max);
+            }
           }
-          return words
-            .filter(word => {
-              const wmap = { ...map };
-              for (const ch of word) {
-                if (wmap[ch]) wmap[ch]--;
-              }
-              return Object.values(wmap).every(v => v <= 0);
-            })
-            .sort((a, b) => a.length - b.length)[0];
+          q(t: number): number {
+            let l = 0;
+            let r = this.times.length - 1;
+            while (l < r) {
+              const mid = (l + r + 1) >> 1;
+              if (this.times[mid] <= t) l = mid;
+              else r = mid - 1;
+            }
+            return this.arr[l];
+          }
         }`,
       },
     ],
