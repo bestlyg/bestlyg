e11e8215a9b47175ee177557b3abc322a5a0800d
bestlyg <1057966749@qq.com>
2021-12-20 23:02:15 +0800

    特性(力扣题解): update leetcode

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -10,7 +10,7 @@ type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
     existMarkdown: true,
-    name: '13. 罗马数字转整数',
+    name: '38. 外观数列',
     url: 'https://leetcode-cn.com/problems/heaters/',
     difficulty: Difficulty.中等,
     tag: [Tag.数组, Tag.双指针, Tag.二分查找, Tag.排序],
@@ -18,53 +18,28 @@ export const leetCodeMarkdowns: Markdown[] = [
     solutions: [
       {
         script: Script.CPP,
-        time: 4,
-        memory: 5.9,
-        desc: '遍历模拟',
+        time: 0,
+        memory: 6.5,
+        desc: '遍历',
         code: `class Solution {
    public:
-    int romanToInt(string s) {
-        int ans = 0;
-        for (int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-            if (ch == 'M')
-                ans += 1000;
-            else if (ch == 'D')
-                ans += 500;
-            else if (ch == 'C') {
-                if (s[i + 1] == 'M') {
-                    ans += 900;
-                    i++;
-                } else if (s[i + 1] == 'D') {
-                    ans += 400;
-                    i++;
-                } else
-                    ans += 100;
-            } else if (ch == 'L')
-                ans += 50;
-            else if (ch == 'X') {
-                if (s[i + 1] == 'C') {
-                    ans += 90;
-                    i++;
-                } else if (s[i + 1] == 'L') {
-                    ans += 40;
-                    i++;
-                } else
-                    ans += 10;
-            } else if (ch == 'V')
-                ans += 5;
-            else if (ch == 'I') {
-                if (s[i + 1] == 'X') {
-                    ans += 9;
-                    i++;
-                } else if (s[i + 1] == 'V') {
-                    ans += 4;
-                    i++;
-                } else
-                    ans += 1;
+    string countAndSay(int n) {
+        string str = "1";
+        while (--n) {
+            string next = "";
+            for (int i = 0, n = str.size(); i < n; i++) {
+                char ch = str[i];
+                int cnt = 1;
+                while (i + 1 < n && str[i + 1] == ch) i++, cnt++;
+                while (cnt) {
+                    next += cnt % 10 + '0';
+                    cnt /= 10;
+                }
+                next += ch;
             }
+            str = next;
         }
-        return ans;
+        return str;
     }
 };`,
       },
