757e0efe6d78e3d97df0c2423a58381df4601e86
bestlyg <1057966749@qq.com>
2021-12-21 10:13:53 +0800

    特性(力扣题解): add leetcode no.1154

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -9,37 +9,36 @@ type Solution = leetcode.Solution;
 type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
-    existMarkdown: true,
-    name: '38. 外观数列',
-    url: 'https://leetcode-cn.com/problems/heaters/',
-    difficulty: Difficulty.中等,
-    tag: [Tag.数组, Tag.双指针, Tag.二分查找, Tag.排序],
-    desc: `现在，给出位于一条水平线上的房屋 houses 和供暖器 heaters 的位置，请你找出并返回可以覆盖所有房屋的最小加热半径。`,
+    existMarkdown: !true,
+    name: '1154. 一年中的第几天',
+    url: 'https://leetcode-cn.com/problems/day-of-the-year/',
+    difficulty: Difficulty.简单,
+    tag: [Tag.数学, Tag.字符串],
+    desc: `给你一个字符串 date ，按 YYYY-MM-DD 格式表示一个 现行公元纪年法 日期。请你计算并返回该日期是当年的第几天。`,
     solutions: [
       {
         script: Script.CPP,
-        time: 0,
-        memory: 6.5,
-        desc: '遍历',
+        time: 16,
+        memory: 5.8,
+        desc: '检测闰年和前面有几个月',
         code: `class Solution {
    public:
-    string countAndSay(int n) {
-        string str = "1";
-        while (--n) {
-            string next = "";
-            for (int i = 0, n = str.size(); i < n; i++) {
-                char ch = str[i];
-                int cnt = 1;
-                while (i + 1 < n && str[i + 1] == ch) i++, cnt++;
-                while (cnt) {
-                    next += cnt % 10 + '0';
-                    cnt /= 10;
-                }
-                next += ch;
-            }
-            str = next;
+    int monthDay[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    void checkLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            monthDay[2] = 29;
         }
-        return str;
+    }
+    int dayOfYear(string date) {
+        int year = 0, month = 0, day = 0;
+        for (int i = 0; i < 4; i++) year = year * 10 + date[i] - '0';
+        for (int i = 5; i < 7; i++) month = month * 10 + date[i] - '0';
+        for (int i = 8; i < 10; i++) day = day * 10 + date[i] - '0';
+        checkLeapYear(year);
+        // printf("year = %d, month = %d, day = %d", year, month, day);
+        int ans = day;
+        for (int i = 1; i < month; i++) ans += monthDay[i];
+        return ans;
     }
 };`,
       },
