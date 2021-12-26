1dc7cd0ee226f0f62ff5e9d180b68aa3de16c352
bestlyg <1057966749@qq.com>
2021-12-26 11:22:48 +0800

    特性(力扣题解): add leetcode no.1078

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -10,45 +10,37 @@ type Markdown = leetcode.Markdown;
 export const leetCodeMarkdowns: Markdown[] = [
   {
     existMarkdown: !true,
-    name: '剑指 Offer II 009. 乘积小于 K 的子数组',
-    url: 'https://leetcode-cn.com/problems/ZVAVXX/',
-    difficulty: Difficulty.中等,
-    tag: [Tag.数组, Tag.滑动窗口],
-    desc: `请找出该数组内乘积小于 k 的连续的子数组的个数。`,
+    name: '1078. Bigram 分词',
+    url: 'https://leetcode-cn.com/problems/occurrences-after-bigram/',
+    difficulty: Difficulty.简单,
+    tag: [Tag.字符串],
+    desc: `对于每种这样的情况，将第三个词 "third" 添加到答案中，并返回答案。`,
     solutions: [
       {
         script: Script.CPP,
-        time: 84,
-        memory: 59.7,
-        desc: '双指针',
+        time: 0,
+        memory: 6.5,
+        desc: '分割字符串',
         code: `class Solution {
    public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if (k == 0) return 0;
-        int n = nums.size(), l = 0, r = 0, num = 1, ans = 0;
-        while (l < n && r <= n) {
-            while (r < n && num < k) num *= nums[r++];
-            ans += r - l - (num >= k ? 1 : 0);
-            num /= nums[l++];
+    vector<string> split(string text) {
+        vector<string> ans;
+        for (int i = 0; i < text.size(); i++) {
+            int end = i;
+            while (end < text.size() && text[end] != ' ') end++;
+            ans.push_back(text.substr(i, end - i));
+            i = end;
         }
-        return max(ans, 0);
+        return ans;
     }
-};`,
-      },
-      {
-        script: Script.CPP,
-        time: 49,
-        memory: 59.8,
-        desc: '双指针',
-        code: `class Solution {
-   public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if (k == 0) return 0;
-        int ans = 0, num = 1, l = 0;
-        for (int r = 0; r < nums.size(); r++) {
-            num *= nums[r];
-            while (l <= r && num >= k) num /= nums[l++];
-            ans += r - l + 1;
+    vector<string> findOcurrences(string text, string first, string second) {
+        vector<string> ans;
+        vector<string> list = split(text);
+        for (int i = 0; i < list.size() - 2; i++) {
+            string str = list[i];
+            if (str == first && list[i + 1] == second) {
+                ans.push_back(list[i + 2]);
+            }
         }
         return ans;
     }
