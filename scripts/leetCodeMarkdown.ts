b7d7e9b5bf638ccdb0ae29e048c657640e63542e
bestlyg <1057966749@qq.com>
2021-12-30 19:59:00 +0800

    特性(力扣题解): add leetcode no.329

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -8,39 +8,55 @@ type Tag = leetcode.Tag;
 type Solution = leetcode.Solution;
 type Markdown = leetcode.Markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '1296. 划分数组为连续数字的集合',
-  url: 'https://leetcode-cn.com/problems/divide-array-in-sets-of-k-consecutive-numbers/',
+  exist: true,
+  name: '329. 矩阵中的最长递增路径',
+  url: 'https://leetcode-cn.com/problems/numbers-with-same-consecutive-differences/',
   difficulty: Difficulty.中等,
-  tag: [Tag.贪心, Tag.数组, Tag.哈希表, Tag.排序],
-  desc: `给你一个整数数组 nums 和一个正整数 k，请你判断是否可以把这个数组划分成一些由 k 个连续数字组成的集合。如果可以，请返回 true；否则，返回 false。`,
+  tag: [Tag.广度优先搜索, Tag.回溯],
+  desc: `返回所有长度为 n 且满足其每两个连续位上的数字之间的差的绝对值为 k 的 非负整数 。`,
   solutions: [
     {
       script: Script.CPP,
       time: 32,
-      memory: 19.9,
-      desc: '遍历后标记使用过的值',
+      memory: 14.3,
+      desc: '记忆化dfs',
       code: `class Solution {
    public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size(), ans = 0, *check = (int*)calloc(n, sizeof(int)),
-            *nums = (int*)calloc(groupSize, sizeof(int));
-        sort(hand.begin(), hand.end());
-        for (int i = 0; i < n; i++) {
-            if (check[i]) continue;
-            int len = 0, pre = hand[i];
-            nums[len++] = i;
-            for (int j = i + 1; j < n && len < groupSize && hand[j] <= pre + 1;
-                 j++) {
-                if (check[j] || pre == hand[j]) continue;
-                pre = hand[j];
-                nums[len++] = j;
-            }
-            if (len < groupSize) continue;
-            ans++;
-            for (int j = 0; j < groupSize; j++) check[nums[j]] = 1;
+    int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int dfs(int mmap[][200], vector<vector<int>>& matrix, int n, int m, int row,
+            int col) {
+        if (mmap[row][col]) return mmap[row][col];
+        int num = matrix[row][col], ans = 1;
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + dirs[i][0];
+            int ncol = col + dirs[i][1];
+            if (nrow < 0 || ncol < 0 || nrow >= n || ncol >= m ||
+                matrix[nrow][ncol] <= num)
+                continue;
+            ans = max(ans, dfs(mmap, matrix, n, m, nrow, ncol) + 1);
         }
-        return ans * groupSize == n;
+        return mmap[row][col] = ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size(), ans = 0, mmap[n][200];
+        memset(mmap, 0, sizeof(int) * n * 200);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int tag = 1, num = matrix[i][j];
+                for (int k = 0; k < 4; k++) {
+                    int nrow = i + dirs[k][0];
+                    int ncol = j + dirs[k][1];
+                    if (nrow < 0 || ncol < 0 || nrow >= n || ncol >= m)
+                        continue;
+                    if (matrix[nrow][ncol] < num) {
+                        tag = 0;
+                        break;
+                    }
+                }
+                if (tag) ans = max(ans, dfs(mmap, matrix, n, m, i, j));
+            }
+        }
+        return ans;
     }
 };`,
     },
