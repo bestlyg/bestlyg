728921d5431cab8c0743077895c30da73140a55c
bestlyg <1057966749@qq.com>
2021-12-30 10:16:46 +0800

    特性(力扣题解): add leetcode no.846

--- scripts/leetCodeMarkdown.ts
+++ scripts/leetCodeMarkdown.ts
@@ -9,88 +9,38 @@ type Solution = leetcode.Solution;
 type Markdown = leetcode.Markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1995. 统计特殊四元组',
-  url: 'https://leetcode-cn.com/problems/count-special-quadruplets/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组, Tag.枚举],
-  desc: `给你一个 下标从 0 开始 的整数数组 nums ，返回满足下述条件的 不同 四元组 (a, b, c, d) 的 数目。`,
+  name: '1296. 划分数组为连续数字的集合',
+  url: 'https://leetcode-cn.com/problems/divide-array-in-sets-of-k-consecutive-numbers/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.贪心, Tag.数组, Tag.哈希表, Tag.排序],
+  desc: `给你一个整数数组 nums 和一个正整数 k，请你判断是否可以把这个数组划分成一些由 k 个连续数字组成的集合。如果可以，请返回 true；否则，返回 false。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 136,
-      memory: 10.2,
-      desc: '内嵌四循环',
+      time: 32,
+      memory: 19.9,
+      desc: '遍历后标记使用过的值',
       code: `class Solution {
    public:
-    int countQuadruplets(vector<int>& nums) {
-        int n = nums.size(), ans = 0;
-        for (int i1 = 0; i1 < n; i1++) {
-            for (int i2 = i1 + 1; i2 < n; i2++) {
-                for (int i3 = i2 + 1; i3 < n; i3++) {
-                    for (int i4 = i3 + 1; i4 < n; i4++) {
-                        if (nums[i1] + nums[i2] + nums[i3] == nums[i4]) ans++;
-                    }
-                }
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size(), ans = 0, *check = (int*)calloc(n, sizeof(int)),
+            *nums = (int*)calloc(groupSize, sizeof(int));
+        sort(hand.begin(), hand.end());
+        for (int i = 0; i < n; i++) {
+            if (check[i]) continue;
+            int len = 0, pre = hand[i];
+            nums[len++] = i;
+            for (int j = i + 1; j < n && len < groupSize && hand[j] <= pre + 1;
+                 j++) {
+                if (check[j] || pre == hand[j]) continue;
+                pre = hand[j];
+                nums[len++] = j;
             }
+            if (len < groupSize) continue;
+            ans++;
+            for (int j = 0; j < groupSize; j++) check[nums[j]] = 1;
         }
-        return ans;
-    }
-};`,
-    },
-    {
-      script: Script.CPP,
-      time: 56,
-      memory: 13.9,
-      desc: '内嵌三循环',
-      code: `class Solution {
-   public:
-    int countQuadruplets(vector<int>& nums) {
-        unordered_map<int, int> m;
-        int n = nums.size(), ans = 0;
-        for (int i3 = n - 1; i3 >= 0; i3--) {
-            m.clear();
-            for (int i4 = i3 + 1; i4 < n; i4++) {
-                int key = nums[i4] - nums[i3];
-                if (m.count(key))
-                    m[key]++;
-                else
-                    m[key] = 1;
-            }
-            for (int i1 = 0; i1 < i3; i1++) {
-                for (int i2 = i1 + 1; i2 < i3; i2++) {
-                    int key = nums[i1] + nums[i2];
-                    if (m.count(key)) {
-                        ans += m[key];
-                    }
-                }
-            }
-        }
-        return ans;
-    }
-};`,
-    },
-    {
-      script: Script.CPP,
-      time: 60,
-      memory: 10.6,
-      desc: '背包问题，前i个数能和为j的所使用的个数为k',
-      code: `class Solution {
-   public:
-    int countQuadruplets(vector<int>& nums) {
-        int n = nums.size(), dp[n + 1][310][4], ans = 0;
-        memset(dp, 0, sizeof(int) * (n + 1) * 310 * 4);
-        dp[0][0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < 310; j++) {
-                for (int k = 0; k < 4; k++) {
-                    dp[i][j][k] += dp[i - 1][j][k];
-                    if (j >= nums[i - 1] && k >= 1)
-                        dp[i][j][k] += dp[i - 1][j - nums[i - 1]][k - 1];
-                }
-            }
-        }
-        for (int i = 3; i < n; i++) ans += dp[i][nums[i]][3];
-        return ans;
+        return ans * groupSize == n;
     }
 };`,
     },
