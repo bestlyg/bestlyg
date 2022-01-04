cd9c98ad69c8d12a934c64fc748224a2bdfd47e9
bestlyg <1057966749@qq.com>
2022-01-04 22:35:56 +0800

    feat: add leetcode no.2125

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,22 +5,31 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '913. 猫和老鼠',
-  url: 'https://leetcode-cn.com/problems/cat-and-mouse/',
-  difficulty: Difficulty.困难,
-  tag: [Tag.广度优先搜索, Tag.图, Tag.记忆化搜索, Tag.数学, Tag.动态规划, Tag.博弈],
-  desc: `两位玩家分别扮演猫和老鼠，在一张 无向 图上进行游戏，两人轮流行动。`,
+  name: '2125. 银行中的激光束数量',
+  url: 'https://leetcode-cn.com/problems/number-of-laser-beams-in-a-bank/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数组, Tag.数学, Tag.字符串, Tag.矩阵],
+  desc: `返回银行中激光束的总数量。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.9,
-      desc: '遍历求出距离第一天的天数并取模',
-      code: `string names[] = {"Friday",  "Saturday",  "Sunday",  "Monday",
-      "Tuesday", "Wednesday", "Thursday"};
-int isLeapYear(int year) {
-return year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
-}`,
+      time: 80,
+      memory: 22.5,
+      desc: '遍历并记录上一次有多少个守卫 ',
+      code: `class Solution {
+   public:
+    int numberOfBeams(vector<string>& bank) {
+        int pre = 0, ans = 0, cnt = 0;
+        for (string& str : bank) {
+            cnt = 0;
+            for (char& ch : str) if (ch == '1') cnt++;
+            if (!cnt) continue;
+            ans += pre * cnt;
+            pre = cnt;
+        }
+        return ans;
+    }
+};`,
     },
   ],
 };
