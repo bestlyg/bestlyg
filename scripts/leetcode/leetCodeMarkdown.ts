6a2f6a7700c2d37fe8f8f0c1dea4e41f9ed9a0ec
bestlyg <1057966749@qq.com>
2022-01-13 09:54:49 +0800

    feat: add leetcode no.747

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,27 +5,31 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '334. 递增的三元子序列',
-  url: 'https://leetcode-cn.com/problems/increasing-triplet-subsequence/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.贪心, Tag.数组],
-  desc: `给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。`,
+  name: '747. 至少是其他数字两倍的最大数',
+  url: 'https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.数组, Tag.排序],
+  desc: `给你一个整数数组 nums ，其中总是存在 唯一的 一个最大整数 。请你找出数组中的最大元素并检查它是否 至少是数组中每个其他数字的两倍 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 48,
-      memory: 60.1,
-      desc: '每遍历一个数值记录前面的值是否匹配',
+      time: 4,
+      memory: 10.5,
+      desc: '遍历两次，第一次记录最大值，第二次判断其他值的两倍是否超过最大值',
       code: `class Solution {
    public:
-    bool increasingTriplet(vector<int>& nums) {
-        int pre1 = INT_MAX, pre2 = INT_MAX;
-        for (auto& num : nums) {
-            if (num > pre2) return 1;
-            if (num > pre1) pre2 = min(pre2, num);
-            pre1 = min(pre1, num);
+    int dominantIndex(vector<int>& nums) {
+        int maxn = INT_MIN, maxi;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > maxn) {
+                maxn = nums[i];
+                maxi = i;
+            }
         }
-        return 0;
+        for (auto& num : nums) {
+            if (num != maxn && num * 2 > maxn) return -1;
+        }
+        return maxi;
     }
 };`,
     },
