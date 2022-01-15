1af047b29cfb9eed52369a64ec0d1aa9bbe04031
bestlyg <1057966749@qq.com>
2022-01-15 10:28:53 +0800

    feat: add leetcode no.1716

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,58 +4,24 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '373. 查找和最小的K对数字',
-  url: 'https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/',
+  exist: !true,
+  name: '1716. 计算力扣银行的钱',
+  url: 'https://leetcode-cn.com/problems/calculate-money-in-leetcode-bank/',
   difficulty: Difficulty.简单,
-  tag: [Tag.数组, Tag.排序],
-  desc: `给你一个整数数组 nums ，其中总是存在 唯一的 一个最大整数 。请你找出数组中的最大元素并检查它是否 至少是数组中每个其他数字的两倍 。`,
+  tag: [Tag.数学],
+  desc: `给你 n ，请你返回在第 n 天结束的时候他在力扣银行总共存了多少块钱。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 924,
-      memory: 62.6,
-      desc: '堆',
+      time: 0,
+      memory: 5.8,
+      desc: '等差数列',
       code: `class Solution {
    public:
-    struct node {
-        int num1, num2, sum;
-        bool operator<(const node& obj) const {
-            return sum == obj.sum
-                       ? num2 == obj.num2 ? num1 < obj.num1 : num2 < obj.num2
-                       : sum < obj.sum;
-        }
-    };
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
-                                       int k) {
-        priority_queue<node> q;
-        int isBreak = 0, len = min(k, (int)(nums1.size() * nums2.size()));
-        // cout << "len = " << len << endl;
-        vector<vector<int>> ans(len, vector<int>(2));
-        for (auto& num1 : nums1) {
-            if (isBreak) break;
-            for (auto& num2 : nums2) {
-                if (q.size() >= len && num1 > 0 && num2 > 0 &&
-                    num1 > q.top().sum && num2 > q.top().sum)
-                    isBreak = 1;
-                if (isBreak) break;
-                if (q.size() < len) {
-                    q.push((node){num1, num2, num1 + num2});
-                } else if (q.top().sum > num1 + num2) {
-                    // cout << q.top().num1 << ',' << q.top().num2;
-                    // if (q.top() )
-                    q.pop();
-                    q.push((node){num1, num2, num1 + num2});
-                }
-            }
-        }
-        while (q.size()) {
-            node n = q.top();
-            q.pop();
-            ans[len - 1][0] = n.num1;
-            ans[len - 1][1] = n.num2;
-            len--;
-        }
+    int totalMoney(int n) {
+        int ans = 0, start = 1;
+        for (; start <= n / 7; start++) ans += (start + start + 6) * 7 / 2;
+        ans += (start + start + n % 7 - 1) * (n % 7) / 2;
         return ans;
     }
 };`,
