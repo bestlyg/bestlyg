32658287a644ba1cd7435b1f5c7ff86798c45205
bestlyg <1057966749@qq.com>
2022-01-16 21:49:41 +0800

    feat: update scripts

--- scripts/leetcode/script.ts
+++ scripts/leetcode/script.ts
@@ -1,4 +1,3 @@
-import { log } from '../utils';
 import { HashMap, toHash } from '@bestlyg/data-structures/src';
 import { encryption, common } from '@bestlyg/algorithms/src';
 import {
@@ -13,62 +12,47 @@ import {
   takeWhile,
   upperFirst,
 } from 'lodash';
+import { Logger } from '../utils';
 // type TrieNode = structures.TrieNode;
 // type Trie = structures.Trie;
 
-/*
-6 abcw baz foo bar xtfn abcdef
- */
-/*
-function ex_gcd(
-  a: number,
-  b: number
-): {
-  ans: number;
-  x: number;
-  y: number;
-} {
-  if (b === 0) return { ans: a, x: 1, y: 0 };
-  const res = ex_gcd(b, a % b);
-  [res.x, res.y] = [res.y, res.x - Math.floor(a / b) * res.y];
-  return res;
+const logger = new Logger();
+function main() {
+  const a = 1;
+  const b = '2';
+  const c = false;
+  const d = { e: 'e1', f: 'f2', aaa: { sad: 2 } };
+  const g = new Set([1, 2, 3]);
+  const f = new Map([
+    ['a', 1],
+    ['b', 2],
+    ['c', 3],
+  ]);
+  logger.log({
+    a,
+    b,
+    c,
+    d,
+    g,
+    f,
+  });
+  logger.visible = false;
+  logger.log({
+    a,
+    b,
+    c,
+    d,
+    g,
+    f,
+  });
+  logger.visible = true;
+  logger.log({
+    a,
+    b,
+    c,
+    d,
+    g,
+    f,
+  });
 }
-function phi(n: number) {
-  let ans = n;
-  for (let i = 2; i ** 2 < n; i++) {
-    if (n % i === 0) ans = ans * (1 - 1 / i);
-    while (n % i === 0) n /= i;
-  }
-  if (n !== 1) ans = ans * (1 - 1 / n);
-  return ans;
-}
-function pow(x: bigint, y: bigint): bigint {
-  let ans = 1n;
-  while (y--) ans *= x;
-  return ans;
-}
-function formual(a: number, b: number, c: number): string {
-  const debug = (...args: any[]) => false && console.log(...args);
-  const error = (str: string) => `ERROR : ${str}`;
-  const { ans: gcd, x, y } = ex_gcd(a, b);
-  if (gcd !== 1) return error('AB互质');
-  if (c >= b) return error('c < b');
-  debug(`phi(${b}) = ${phi(b)}`);
-  debug(`gcd(${a}, ${b}) = gcd:${gcd}, x:${x}, y:${y}`);
-  const formula2Str = (a: bigint, b: bigint, c: bigint, x: bigint) =>
-    `${a} ^ ${x} mod ${b} === ${c}`;
-  const a_big = BigInt(a);
-  const b_big = BigInt(b);
-  const c_big = BigInt(c);
-  for (let x = 0n; x < phi(b); x++) {
-    debug(formula2Str(a_big, b_big, pow(a_big, x) % b_big, x));
-  }
-  for (let x = 0n; x < phi(b); x++) {
-    if (pow(a_big, x) % b_big === c_big) {
-      return `${a} ^ ${x} mod ${b} === ${c}`;
-    }
-  }
-  return error('NOT FOUND');
-}
-*/
-console.log(common.phi(4));
+main();
