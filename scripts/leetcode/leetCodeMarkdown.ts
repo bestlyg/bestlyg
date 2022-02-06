90e195f6cf3ea324a57a6ea47fbaaf96f793631f
bestlyg <1057966749@qq.com>
2022-02-06 19:29:58 +0800

    feat: add leetcode no.1748

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,24 +5,28 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1725. 可以形成最大正方形的矩形数目',
-  url: 'https://leetcode-cn.com/problems/number-of-rectangles-that-can-form-the-largest-square/',
+  name: '1748. 唯一元素的和',
+  url: 'https://leetcode-cn.com/problems/sum-of-unique-elements/',
   difficulty: Difficulty.简单,
-  tag: [Tag.数组],
-  desc: `请你统计有多少个矩形能够切出边长为 maxLen 的正方形，并返回矩形 数目 。`,
+  tag: [Tag.数组, Tag.哈希表, Tag.计数],
+  desc: `给你一个整数数组 nums 。数组中唯一元素是那些只出现 恰好一次 的元素。请你返回 nums 中唯一元素的 和 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 36,
-      memory: 17.9,
+      time: 4,
+      memory: 7.6,
       desc: '遍历',
       code: `class Solution {
    public:
-    int countGoodRectangles(vector<vector<int>>& rectangles) {
-        int len = INT_MIN, ans = 0;
-        for (auto& data : rectangles) len = max(len, min(data[0], data[1]));
-        for (auto& data : rectangles)
-            if (len <= data[0] && len <= data[1]) ans++;
+    int sumOfUnique(vector<int>& nums) {
+        int check[110] = {0}, ans = 0;
+        for (int i = 0, n = nums.size(); i < n; ++i) {
+            ++check[nums[i]];
+            if (check[nums[i]] == 2)
+                ans -= nums[i];
+            else if (check[nums[i]] == 1)
+                ans += nums[i];
+        }
         return ans;
     }
 };`,
