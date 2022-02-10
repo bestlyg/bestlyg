2c7bf9c26ccf55e76e93c4111173cf440a5f9034
bestlyg <1057966749@qq.com>
2022-02-10 10:01:54 +0800

    feat: add leetcode no.1447

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,8 +4,8 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '2006. 差的绝对值为 K 的数对数目',
+  exist: true,
+  name: '1447. 最简分数',
   url: 'https://leetcode-cn.com/problems/count-number-of-pairs-with-absolute-difference-k/',
   difficulty: Difficulty.简单,
   tag: [Tag.数组, Tag.哈希表, Tag.计数],
@@ -13,34 +13,39 @@ const leetCodeMarkdown: Markdown = {
   solutions: [
     {
       script: Script.CPP,
-      time: 4,
-      memory: 12.1,
-      desc: '遍历',
+      time: 116,
+      memory: 32.5,
+      desc: '判断最大公约数',
       code: `class Solution {
    public:
-    int countKDifference(vector<int>& nums, int k) {
-        int m[300] = {0}, ans = 0;
-        for (auto& num : nums) {
-            ans += m[num + k + 100] + m[num - k + 100];
-            m[num + 100]++;
+    vector<string> simplifiedFractions(int n) {
+        unordered_set<string> s;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                int num = gcd(i, j);
+                s.insert(to_string(j / num) + "/" + to_string(i / num));
+            }
         }
+        vector<string> ans;
+        for (auto& str : s) ans.push_back(str);
         return ans;
     }
 };`,
     },
     {
       script: Script.CPP,
-      time: 8,
-      memory: 14,
-      desc: '遍历',
+      time: 48,
+      memory: 21.3,
+      desc: '判断最大公约数',
       code: `class Solution {
    public:
-    int countKDifference(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
-        int ans = 0;
-        for (auto& num : nums) {
-            ans += m[num + k] + m[num - k];
-            m[num]++;
+    vector<string> simplifiedFractions(int n) {
+        vector<string> ans;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (j == 1 || gcd(i, j) == 1)
+                    ans.push_back(to_string(j) + "/" + to_string(i));
+            }
         }
         return ans;
     }
