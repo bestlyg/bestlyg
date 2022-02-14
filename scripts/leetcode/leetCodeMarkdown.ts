9a56c8800f63e3328eb9be62d720a131877c7f7b
bestlyg <1057966749@qq.com>
2022-02-14 10:20:00 +0800

    feat: add leetcode no.540

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,24 +5,52 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1189. “气球” 的最大数量',
-  url: 'https://leetcode-cn.com/problems/maximum-number-of-balloons/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.哈希表, Tag.字符串, Tag.计数],
-  desc: `字符串 text 中的每个字母最多只能被使用一次。请你返回最多可以拼凑出多少个单词 "balloon"。`,
+  name: '540. 有序数组中的单一元素',
+  url: 'https://leetcode-cn.com/problems/single-element-in-a-sorted-array/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数组, Tag.二分查找],
+  desc: `给你一个仅由整数组成的有序数组，其中每个元素都会出现两次，唯有一个数只会出现一次。请你找出并返回只出现一次的那个数。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 6.5,
-      desc: '遍历',
+      time: 16,
+      memory: 21.9,
+      desc: 'bs',
       code: `class Solution {
    public:
-    int maxNumberOfBalloons(string text) {
-        int m[26] = {0};
-        for (auto &ch : text) m[ch - 'a']++;
-        return min(min(min(m[1], m[0]), m['n' - 'a']),
-                   min(m['l' - 'a'], m['o' - 'a']) / 2);
+    int singleNonDuplicate(vector<int>& nums) {
+        return bs(nums, 0, nums.size() - 1);
+    }
+    int bs(vector<int>& nums, int l, int r) {
+        int n = r - l + 1, m = (l + r) >> 1;
+        if (n == 1 || nums[l] != nums[l + 1]) return nums[l];
+        if (nums[r] != nums[r - 1]) return nums[r];
+        if (nums[m] != nums[m - 1] && nums[m] != nums[m + 1]) return nums[m];
+        if (nums[m] == nums[m - 1]) m--;
+        if ((m - l) & 1)
+            return bs(nums, l, m - 1);
+        else
+            return bs(nums, m + 2, r);
+    }
+};`,
+    },
+    {
+      script: Script.CPP,
+      time: 20,
+      memory: 21.9,
+      desc: 'bs, 如果是偶数下标与后面的比，奇数下标与前面的比',
+      code: `class Solution {
+   public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1, m;
+        while (l < r) {
+            m = (l + r) >> 1;
+            if (nums[m] == nums[m ^ 1])
+                l = m + 1;
+            else
+                r = m;
+        }
+        return nums[l];
     }
 };`,
     },
