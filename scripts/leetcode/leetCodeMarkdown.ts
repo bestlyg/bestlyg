39e62e116cf2835ed870a5873f13c86b6a2a42c2
bestlyg <1057966749@qq.com>
2022-02-18 10:02:13 +0800

    feat: add leetcode no.1791

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,49 +5,24 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '688. 骑士在棋盘上的概率',
-  url: 'https://leetcode-cn.com/problems/knight-probability-in-chessboard/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.动态规划],
-  desc: `返回 骑士在棋盘停止移动后仍留在棋盘上的概率 。`,
+  name: '1791. 找出星型图的中心节点',
+  url: 'https://leetcode-cn.com/problems/find-center-of-star-graph/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.图],
+  desc: `给你一个二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示在节点 ui 和 vi 之间存在一条边。请你找出并返回 edges 所表示星型图的中心节点。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.8,
-      desc: '动态规划，统计每个点的概率',
-      code: `int dirs[8][2] = {{-1, -2}, {-1, 2}, {-2, -1}, {-2, 1},
-          {1, 2},   {1, -2}, {2, 1},   {2, -1}};
-class Solution {
-  public:
-   double knightProbability(int n, int k, int row, int column) {
-       double table[2][n][n], ans = 0;
-       memset(table, 0, sizeof(double) * 2 * n * n);
-       table[0][row][column] = 1;
-       for (int i = 0; i < k; i++) {
-           int idx = i & 1, nidx = (i + 1) & 1;
-           memset(table[nidx], 0, sizeof(double) * n * n);
-           for (int row = 0; row < n; row++) {
-               for (int col = 0; col < n; col++) {
-                   if (table[idx][row][col] == 0) continue;
-                   for (int next = 0; next < 8; next++) {
-                       int nrow = row + dirs[next][0],
-                           ncol = col + dirs[next][1];
-                       if (nrow < 0 || nrow >= n || ncol < 0 || ncol >= n)
-                           continue;
-                       table[nidx][nrow][ncol] +=
-                           1.0 / 8 * table[idx][row][col];
-                   }
-               }
-           }
-       }
-       for (int row = 0; row < n; row++) {
-           for (int col = 0; col < n; col++) {
-               if (table[k & 1][row][col]) ans += table[k & 1][row][col];
-           }
-       }
-       return ans;
-   }
+      time: 144,
+      memory: 65.7,
+      desc: '直接判断前两个边中，存在相同的点',
+      code: `class Solution {
+   public:
+    int findCenter(vector<vector<int>>& edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]
+                   ? edges[0][0]
+                   : edges[0][1];
+    }
 };`,
     },
   ],
