8936e955be0f7b27a56488d0aa0fa76b9184c709
bestlyg <1057966749@qq.com>
2022-02-20 11:15:11 +0800

    feat: add leetcode no.717

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,43 +4,29 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '969. 煎饼排序',
-  url: 'https://leetcode-cn.com/problems/XltzEq/',
+  exist: !true,
+  name: '717. 1比特与2比特字符',
+  url: 'https://leetcode-cn.com/problems/1-bit-and-2-bit-characters/',
   difficulty: Difficulty.简单,
-  tag: [Tag.双指针, Tag.字符串],
-  desc: `给定一个字符串 s ，验证 s 是否是 回文串 ，只考虑字母和数字字符，可以忽略字母的大小写。`,
+  tag: [Tag.数组],
+  desc: `给定一个以 0 结尾的二进制数组 bits ，如果最后一个字符必须是一位字符，则返回 true 。`,
   solutions: [
     {
       script: Script.CPP,
       time: 4,
-      memory: 11,
-      desc: '每次翻转把最大值翻转到首位，再翻转到结尾',
+      memory: 9.3,
+      desc: '遍历， 判断最后的下标是否等于末尾',
       code: `class Solution {
    public:
-    vector<int> pancakeSort(vector<int>& arr) {
-        vector<int> ans;
-        _pancakeSort(ans, arr, 0, arr.size() - 1);
-        return ans;
-    }
-    void _pancakeSort(vector<int>& ans, vector<int>& arr, int start, int end) {
-        if (end == 0) return;
-        int vmax = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[i] > arr[vmax]) vmax = i;
+    bool isOneBitCharacter(vector<int>& bits) {
+        int idx = 0;
+        while (idx < bits.size() - 1) {
+            if (bits[idx] == 0)
+                idx += 1;
+            else
+                idx += 2;
         }
-        if (vmax != end) {
-            reverse(arr, 0, vmax);
-            ans.push_back(vmax + 1);
-            reverse(arr, 0, end);
-            ans.push_back(end + 1);
-        }
-        _pancakeSort(ans, arr, start, end - 1);
-    }
-    void reverse(vector<int>& arr, int start, int end) {
-        for (int l = start, r = end; l < r; l++, r--) {
-            swap(arr[l], arr[r]);
-        }
+        return idx == bits.size() - 1;
     }
 };`,
     },
