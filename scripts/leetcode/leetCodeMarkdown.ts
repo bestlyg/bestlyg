d24fbf8e0a90757905d95dc452cf5c10c08624a1
bestlyg <1057966749@qq.com>
2022-02-26 09:51:58 +0800

    feat: add leetcode no.2016

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,32 +5,28 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '537. 复数乘法',
-  url: 'https://leetcode-cn.com/problems/complex-number-multiplication/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.数学, Tag.字符串, Tag.模拟],
-  desc: `给你两个字符串表示的复数 num1 和 num2 ，请你遵循复数表示形式，返回表示它们乘积的字符串。`,
+  name: '2016. 增量元素之间的最大差值',
+  url: 'https://leetcode-cn.com/problems/maximum-difference-between-increasing-elements/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.数组],
+  desc: `给你一个下标从 0 开始的整数数组 nums ，该数组的大小为 n ，请你计算 nums[j] - nums[i] 能求得的 最大差值 ，其中 0 <= i < j < n 且 nums[i] < nums[j] 。返回 最大差值 。如果不存在满足要求的 i 和 j ，返回 -1 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.7,
-      desc: '模拟',
+      time: 4,
+      memory: 8,
+      desc: '遍历，记录最小值',
       code: `class Solution {
    public:
-    void analysis(string num, int& snum, int& fnum) {
-        int add = num.find("+");
-        snum = atoi(num.substr(0, add).c_str());
-        fnum = atoi(num.substr(add + 1, num.size() - add - 1).c_str());
-    }
-    string complexNumberMultiply(string num1, string num2) {
-        int snum1, fnum1, snum2, fnum2;
-        analysis(num1, snum1, fnum1);
-        analysis(num2, snum2, fnum2);
-        ostringstream ostr;
-        ostr << snum1 * snum2 + fnum1 * fnum2 * -1 << "+"
-             << snum1 * fnum2 + snum2 * fnum1 << "i";
-        return ostr.str();
+    int maximumDifference(vector<int>& nums) {
+        int minnum = nums[0], ans = INT_MIN;
+        for (auto& num : nums) {
+            if (num > minnum) {
+                ans = max(ans, num - minnum);
+            }
+            minnum = min(minnum, num);
+        }
+        return ans == INT_MIN ? -1 : ans;
     }
 };`,
     },
