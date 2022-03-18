34454c743ee3080c14885e1795a8246da98fbbd0
bestlyg <1057966749@qq.com>
2022-03-18 10:03:55 +0800

    feat: update dep

--- scripts/leetcode/script.js
+++ scripts/leetcode/script.js
@@ -1,27 +1,8 @@
-function format(curBoard) {
-  let flag = false;
-  let n = curBoard.length;
-  do {
-    flag = false;
-    for (let i = 0; i < n - 1; i++) {
-      const ball = curBoard[i];
-      let start = i;
-      let end = i;
-      let cnt = 1;
-      while (end < n - 1 && ball === curBoard[end + 1]) {
-        end++;
-        cnt++;
-      }
-      console.log(i, start, end, curBoard);
-      if (cnt < 3) {
-        i = end;
-        continue;
-      }
-      curBoard = curBoard.substring(0, start) + curBoard.substring(end + 1);
-      n = curBoard.length;
-      flag = true;
-    }
-  } while (flag);
-  return curBoard;
+const tmplate = `
+setTimeout(()=>{
+throw new Error('');
+},1000);
+for(let i = 0; i < 10; i++) {
 }
-console.log(format('RBBBRRB'));
+`;
+// eval(tmplate);
