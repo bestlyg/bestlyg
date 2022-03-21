6b608defb4cdc9bc19a1c66e5c7f00efe4635363
bestlyg <1057966749@qq.com>
2022-03-21 11:28:08 +0800

    feat: add leetcode no.386

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,48 +5,28 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '821. 字符的最短距离',
-  url: 'https://leetcode-cn.com/problems/shortest-distance-to-a-character/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组, Tag.双指针, Tag.字符串],
-  desc: `给你一个字符串 s 和一个字符 c ，且 c 是 s 中出现过的字符。返回一个整数数组 answer ，其中 answer.length == s.length 且 answer[i] 是 s 中从下标 i 到离它 最近 的字符 c 的 距离 。`,
+  name: '386. 字典序排数',
+  url: 'https://leetcode-cn.com/problems/lexicographical-numbers/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.深度优先搜索, Tag.字典树],
+  desc: `给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 4,
-      memory: 6.7,
-      desc: 'bfs',
+      time: 16,
+      memory: 11.2,
+      desc: 'dfs遍历每一层',
       code: `class Solution {
    public:
-    vector<int> shortestToChar(string s, char c) {
-        unordered_set<string> sset;
-        int n = s.size();
-        vector<int> ans(n, -1);
-        queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == c) {
-                q.push(i);
-                ans[i] = 0;
-            }
-        }
-        int level = 1, size = q.size();
-        while (q.size()) {
-            int idx = q.front();
-            q.pop();
-            if (idx < n - 1 && ans[idx + 1] == -1) {
-                q.push(idx + 1);
-                ans[idx + 1] = level;
-            }
-            if (idx > 0 && ans[idx - 1] == -1) {
-                q.push(idx - 1);
-                ans[idx - 1] = level;
-            }
-            if (--size == 0) {
-                size = q.size();
-                level++;
-            }
-        }
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for (int i = 1; i <= 9; i++) dfs(ans, n, i);
         return ans;
+    }
+    void dfs(vector<int> &ans, int &max, int num) {
+        if (num > max) return;
+        ans.push_back(num);
+        for (int i = 0; i <= 9; i++) dfs(ans, max, num * 10 + i);
     }
 };`,
     },
