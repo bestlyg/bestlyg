f94b3f521a550923af8a554e1bb7fdeb0cb9f10f
bestlyg <1057966749@qq.com>
2022-04-07 09:45:58 +0800

    feat: add leetcode no.796

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,7 +5,7 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: true,
-  name: '310. 最小高度树',
+  name: '796. 旋转字符串',
   url: 'https://leetcode-cn.com/problems/array-of-doubled-pairs/',
   difficulty: Difficulty.中等,
   tag: [Tag.贪心, Tag.数组, Tag.哈希表, Tag.排序],
@@ -13,50 +13,28 @@ const leetCodeMarkdown: Markdown = {
   solutions: [
     {
       script: Script.CPP,
-      time: 172,
-      memory: 77.7,
-      desc: '从外向内遍历',
+      time: 0,
+      memory: 6,
+      desc: '遍历',
       code: `class Solution {
    public:
-    struct node {
-        int val, cnt;
-        unordered_set<int> children;
-    };
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if (n == 1) return {0};
-        vector<node> list(n);
+    int n;
+    bool rotateString(string s, string goal) {
+        n = s.size();
+        vector<int> list;
         for (int i = 0; i < n; i++) {
-            list[i].val = i;
-            list[i].cnt = 0;
+            if (s[i] == goal[0]) list.push_back(i);
         }
-        for (auto& edge : edges) {
-            int n0 = edge[0], n1 = edge[1];
-            list[n0].children.insert(n1);
-            list[n1].children.insert(n0);
-            list[n0].cnt++;
-            list[n1].cnt++;
+        for (auto &start : list) {
+            if (check(s, goal, start)) return true;
         }
-        queue<int> q;
-        for (auto& node : list) {
-            if (node.cnt == 1) q.push(node.val);
+        return false;
+    }
+    bool check(string &s, string &goal, int start) {
+        for (int i = 0; i < n; i++, start = (start + 1) % n) {
+            if (s[start] != goal[i]) return false;
         }
-        vector<int> ans;
-        int size = q.size();
-        while (q.size()) {
-            int idx = q.front();
-            ans.push_back(idx);
-            q.pop();
-            list[idx].cnt--;
-            for (auto& child : list[idx].children) {
-                if (--list[child].cnt != 1) continue;
-                q.push(child);
-            }
-            if (--size == 0) {
-                size = q.size();
-                if (size) ans.clear();
-            }
-        }
-        return ans;
+        return true;
     }
 };`,
     },
