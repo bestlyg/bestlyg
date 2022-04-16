1bc2a56a16b8978928c2ef3ad374cb0f4479d0ca
bestlyg <1057966749@qq.com>
2022-04-16 23:37:11 +0800

    feat: add leetcode no.479

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,43 +4,23 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '385. 迷你语法分析器',
-  url: 'https://leetcode-cn.com/problems/richest-customer-wealth/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组, Tag.矩阵],
-  desc: `客户的 资产总量 就是他们在各家银行托管的资产数量之和。最富有客户就是 资产总量 最大的客户。`,
+  exist: !true,
+  name: '479. 最大回文数乘积',
+  url: 'https://leetcode-cn.com/problems/largest-palindrome-product/',
+  difficulty: Difficulty.困难,
+  tag: [Tag.数学],
+  desc: `给定一个整数 n ，返回 可表示为两个 n 位整数乘积的 最大回文整数 。因为答案可能非常大，所以返回它对 1337 取余 。  `,
   solutions: [
     {
       script: Script.CPP,
-      time: 8,
-      memory: 12.3,
-      desc: '递归遍历',
-      code: `class Solution {
-   public:
-    NestedInteger deserialize(string s) {
-        NestedInteger res;
-        if (s == "[]")
-            return res;
-        else if (s[0] == '[')
-            split(res, s.substr(1, s.size() - 2));
-        else
-            res.setInteger(stoi(s));
-        return res;
-    }
-    void split(NestedInteger &obj, string s) {
-        int level = 0, start = 0, n = s.size();
-        for (int i = 0; i < n; i++) {
-            char ch = s[i];
-            if (ch == '[')
-                level++;
-            else if (ch == ']')
-                level--;
-            else if (ch == ',' && level == 0)
-                obj.add(deserialize(s.substr(start, i - start))), start = i + 1;
-        }
-        obj.add(deserialize(s.substr(start, n - start)));
-    }
+      time: 0,
+      memory: 5.7,
+      desc: '枚举',
+      code: `long long arr[8] = {9,          9009,         906609,         99000099,
+                    9966006699, 999000000999, 99956644665999, 9999000000009999};
+class Solution {
+    public:
+     int largestPalindrome(int n) { return arr[n - 1] % 1337; }
 };`,
     },
   ],
--- scripts/leetcode/script.ts
+++ scripts/leetcode/script.ts
@@ -5,7 +5,6 @@ import {
   find,
   first,
   merge,
-  min,
   random,
   remove,
   reverse,
@@ -16,14 +15,31 @@ import {
 import { Logger, resolve, fs } from '../utils';
 import { ListNode } from './structures';
 
-function duplicateZeros(arr: number[]): void {
-  const list = new Array(arr.length)
-    .fill(0)
-    .map((_, i) => i)
-    .filter(v => arr[v] === 0)
-    .reverse();
-  for (const idx of list) {
-    arr.splice(idx, 0, 0);
-    arr.pop();
-  }
+let min = 1n;
+let max = 9n;
+function check(num: bigint) {
+  return num.toString().split('').reverse().join('') === num.toString();
 }
+const ans: bigint[] = [];
+for (let i = 1; i <= 8; i++) {
+  const startStr = String('9').repeat(i >> 1);
+  let arr: bigint[] = [];
+  for (let n1 = max; n1 >= min && n1.toString().startsWith(startStr); n1--) {
+    for (
+      let n2 = max;
+      n2 >= min &&
+      (arr.length === 0 || n1 >= arr[0] || n2 >= arr[1]) &&
+      n2.toString().startsWith(startStr);
+      n2--
+    ) {
+      if (check(n1 * n2) && (arr.length === 0 || n1 * n2 > arr[2])) {
+        arr = [n1, n2, n1 * n2];
+      }
+    }
+  }
+  console.log(`${arr[0]} * ${arr[1]} == ${arr[2]}`);
+  ans.push(arr[2]);
+  min *= 10n;
+  max = max * 10n + 9n;
+}
+console.log(ans);
