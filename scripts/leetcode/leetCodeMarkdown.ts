f2f7d03c79d5c8bd8511cbe17b5d13d5366b499b
bestlyg <1057966749@qq.com>
2022-04-17 09:53:44 +0800

    feat: add leetcode no.819

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,8 +4,8 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '479. 最大回文数乘积',
+  exist: true,
+  name: '819. 最常见的单词',
   url: 'https://leetcode-cn.com/problems/largest-palindrome-product/',
   difficulty: Difficulty.困难,
   tag: [Tag.数学],
@@ -13,14 +13,30 @@ const leetCodeMarkdown: Markdown = {
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.7,
-      desc: '枚举',
-      code: `long long arr[8] = {9,          9009,         906609,         99000099,
-                    9966006699, 999000000999, 99956644665999, 9999000000009999};
-class Solution {
-    public:
-     int largestPalindrome(int n) { return arr[n - 1] % 1337; }
+      time: 4,
+      memory: 7.6,
+      desc: '遍历',
+      code: `class Solution {
+   public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        int n = paragraph.size();
+        string ans = "";
+        unordered_map<string, int> m;
+        unordered_set<string> s;
+        for (auto& str : banned) s.insert(str);
+        for (int i = 0; i < n; i++) {
+            while (i < n && !isalpha(paragraph[i])) i++;
+            if (i == n) break;
+            int end = i;
+            string next = "";
+            do {
+                next += tolower(paragraph[end++]);
+            } while (end < n && isalpha(paragraph[end]));
+            if (!s.count(next) && m[ans] < ++m[next]) ans = next;
+            i = end;
+        }
+        return ans;
+    }
 };`,
     },
   ],
