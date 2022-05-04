f1efe9417f471f20331cdea6985d93743d5ff687
bestlyg <1057966749@qq.com>
2022-05-04 22:52:09 +0800

    feat: update c code

--- packages/c/kmp.c
+++ packages/c/kmp.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int *getNext(const char *t) {}
+#define MAX 26
+
+void printNext(int **next, int len) {
+    printf("===[PRINT NEXT START]===\n");
+    for (int i = 0; i < len; i++) {
+        printf("i = %d : ", i);
+        for (int j = 0; j < MAX / 2; j++) {
+            if (j != 0) printf(", ");
+            printf("%c = %d", j + 'a', next[i][j]);
+        }
+        printf("\n        ");
+        for (int j = MAX / 2; j < MAX; j++) {
+            if (j != MAX / 2) printf(", ");
+            printf("%c = %d", j + 'a', next[i][j]);
+        }
+        printf("\n-------------------------------------\n");
+    }
+    printf("===[PRINT NEXT END]===\n");
+}
+int **getJump(const char *t, int len) {
+    int **res = (int **)malloc(sizeof(int *) * len);
+    for (int i = 0; i < len; i++) {
+        res[i] = (int *)malloc(sizeof(int) * MAX);
+        memset(res[i], -1, sizeof(int) * MAX);
+    }
+    int j = -1;
+    for (int i = 0; i < len; i++) {
+        while (j + 1 < len && t[j + 1] != t[i]) j =
+    }
+    printNext(res, len);
+    return res;
+}
 
 int kmp(const char *s, const char *t) {
-    int *next = getNext(t);
+    int tlen = strlen(t);
+    printf("tlen = %d\n", tlen);
+    int **next = getNext(t, tlen);
     for (int i = 0, j = 0; s[i]; i++) {
-        j = next[j][s[i] - 'a'];
+        // j = next[j][s[i] - 'a'];
     }
     return -1;
 }
