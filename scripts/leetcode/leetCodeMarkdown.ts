e40fbbcac3ae19e9a04ad1a3ef9ef1a9fb831c06
bestlyg <1057966749@qq.com>
2022-05-05 08:37:06 +0800

    feat: add leetcode no.713

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,8 +4,8 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '1823. 找出游戏的获胜者',
+  exist: true,
+  name: '713. 乘积小于 K 的子数组',
   url: 'https://leetcode-cn.com/problems/find-the-winner-of-the-circular-game/',
   difficulty: Difficulty.中等,
   tag: [Tag.递归, Tag.队列, Tag.数组, Tag.数学, Tag.模拟],
@@ -13,41 +13,22 @@ const leetCodeMarkdown: Markdown = {
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.4,
-      desc: '遍历',
-      code: `int findTheWinner(int n, int k) {
-    int list[n], current = n - 1;
-    for (int i = 0; i < n; i++) list[i] = i + 1;
-    list[n - 1] = 0;
-    for (int jump = 0; n > 1; n--) {
-        jump = (k - 1) % n;
-        while (jump > 0) current = list[current], jump--;
-        list[current] = list[list[current]];
-    }
-    return current + 1;
-}`,
-    },
-    {
-      script: Script.GO,
-      time: 0,
-      memory: 1.9,
-      desc: '遍历',
-      code: `func findTheWinner(n int, k int) int {
-    list := make([]int, n)
-    for i := 0; i < n; i++ {
-        list[i] = i + 1
-    }
-    list[n-1] = 0
-    current := n - 1
-    for jump := 0; n > 1; n-- {
-        jump = (k - 1) % n
-        for ; jump > 0; jump-- {
-            current = list[current]
+      time: 76,
+      memory: 9.4,
+      desc: '滑动窗口',
+      code: `int numSubarrayProductLessThanK(int *nums, int numsSize, int k) {
+    int l = 0, r = 0, sum = 1, ans = 0;
+    while (l < numsSize) {
+        while (r < numsSize && sum * nums[r] < k) sum *= nums[r++];
+        if (l == r) {
+            l++;
+            r++;
+        } else {
+            ans += r - l;
+            sum /= nums[l++];
         }
-        list[current] = list[list[current]]
     }
-    return current + 1
+    return ans;
 }`,
     },
   ],
