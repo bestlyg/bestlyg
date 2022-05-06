aa423b79fa7376df8929687d1b5835d5d01bbede
bestlyg <1057966749@qq.com>
2022-05-06 10:26:01 +0800

    feat: add leetcode no.933

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,30 +5,47 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: true,
-  name: '713. 乘积小于 K 的子数组',
-  url: 'https://leetcode-cn.com/problems/find-the-winner-of-the-circular-game/',
+  name: '933. 最近的请求次数',
+  url: 'https://leetcode-cn.com/problems/construct-quad-tree/',
   difficulty: Difficulty.中等,
-  tag: [Tag.递归, Tag.队列, Tag.数组, Tag.数学, Tag.模拟],
-  desc: `给你参与游戏的小伙伴总数 n ，和一个整数 k ，返回游戏的获胜者。`,
+  tag: [Tag.树, Tag.数组, Tag.分治, Tag.矩阵],
+  desc: `你需要返回能表示矩阵的 四叉树 的根结点。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 76,
-      memory: 9.4,
-      desc: '滑动窗口',
-      code: `int numSubarrayProductLessThanK(int *nums, int numsSize, int k) {
-    int l = 0, r = 0, sum = 1, ans = 0;
-    while (l < numsSize) {
-        while (r < numsSize && sum * nums[r] < k) sum *= nums[r++];
-        if (l == r) {
-            l++;
-            r++;
-        } else {
-            ans += r - l;
-            sum /= nums[l++];
-        }
+      time: 132,
+      memory: 56,
+      desc: 'queue',
+      code: `class RecentCounter {
+   public:
+    queue<int> q;
+    RecentCounter() {}
+    int ping(int t) {
+        q.push(t);
+        while (t - q.front() > 3000) q.pop();
+        return q.size();
     }
-    return ans;
+};`,
+    },
+    {
+      script: Script.GO,
+      time: 136,
+      memory: 8.1,
+      desc: 'queue',
+      code: `type RecentCounter struct {
+    queue []int
+}
+
+func Constructor() RecentCounter {
+    res := RecentCounter{}
+    return res
+}
+func (this *RecentCounter) Ping(t int) int {
+    this.queue = append(this.queue, t)
+    for t-this.queue[0] > 3000 {
+        this.queue = this.queue[1:]
+    }
+    return len(this.queue)
 }`,
     },
   ],
