34f7e32a6d81f2ab3a4f76782f8a10a23d4466fb
bestlyg <1057966749@qq.com>
2022-05-24 13:57:29 +0800

    feat: add leetcode no.965

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,8 +4,8 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '675. 为高尔夫比赛砍树',
+  exist: true,
+  name: '965. 单值二叉树',
   url: 'https://leetcode.cn/problems/cut-off-trees-for-golf-event/',
   difficulty: Difficulty.困难,
   tag: [Tag.广度优先搜索, Tag.数组, Tag.矩阵, Tag.堆_优先队列],
@@ -13,62 +13,32 @@ const leetCodeMarkdown: Markdown = {
   solutions: [
     {
       script: Script.CPP,
-      time: 760,
-      memory: 97.5,
-      desc: 'bfs, 每次从当前值寻找下一个目标',
-      code: `int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-class Solution {
+      time: 0,
+      memory: 9.5,
+      desc: 'dfs',
+      code: `class Solution {
    public:
-    typedef pair<int, int> node;
-    int rowLen, colLen;
-    int cutOffTree(vector<vector<int>>& forest) {
-        rowLen = forest.size(), colLen = forest[0].size();
-        vector<int> list;
-        for (int row = 0; row < rowLen; row++) {
-            for (int col = 0; col < colLen; col++) {
-                if (forest[row][col] > 1) list.emplace_back(forest[row][col]);
-            }
-        }
-        sort(list.begin(), list.end(),
-             [&](int a, int b) -> bool { return a < b; });
-        int ans = 0;
-        node prev = make_pair(0, 0);
-        for (int i = 0; i < list.size(); i++) {
-            int step = findNext(forest, prev, list[i]);
-            if (step == -1) return -1;
-            ans += step;
-        }
-        return ans;
+    bool isUnivalTree(TreeNode* root) { return _isUnivalTree(root, root->val); }
+    bool _isUnivalTree(TreeNode* node, int val) {
+        if (node == nullptr) return true;
+        return node->val == val && _isUnivalTree(node->left, val) &&
+               _isUnivalTree(node->right, val);
     }
-    int findNext(vector<vector<int>>& forest, node& start, int target) {
-        int step = 0, size = 1;
-        queue<node> q;
-        vector<vector<bool>> used(rowLen, vector(colLen, false));
-        used[start.first][start.second] = true;
-        q.push(start);
-        while (q.size()) {
-            node item = q.front();
-            q.pop();
-            if (forest[item.first][item.second] == target) {
-                start.first = item.first;
-                start.second = item.second;
-                return step;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nrow = item.first + dirs[i][0],
-                    ncol = item.second + dirs[i][1];
-                if (nrow < 0 || nrow == rowLen || ncol < 0 || ncol == colLen ||
-                    forest[nrow][ncol] == 0 || used[nrow][ncol])
-                    continue;
-                q.push(make_pair(nrow, ncol));
-                used[nrow][ncol] = true;
-            }
-            if (--size == 0) {
-                size = q.size();
-                step++;
-            }
-        }
-        return -1;
+};`,
+    },
+    {
+      script: Script.CPP,
+      time: 0,
+      memory: 9.6,
+      desc: 'dfs',
+      code: `class Solution {
+   public:
+    int val = 0;
+    bool isUnivalTree(TreeNode* root) {
+        if (!root) return true;
+        if (val == 0) val = root->val;
+        return root->val == val && isUnivalTree(root->left) &&
+               isUnivalTree(root->right);
     }
 };`,
     },
