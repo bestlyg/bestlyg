44ffc8d997435ad00553fbb90fb43eb3d8dc8857
liyigang <liyigang@bytedance.com>
2022-06-07 10:20:52 +0800

    feat: add leetcode no.875

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,33 +5,41 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '478. 在圆内随机生成点',
-  url: 'https://leetcode.cn/problems/generate-random-point-in-a-circle/',
+  name: '875. 爱吃香蕉的珂珂',
+  url: 'https://leetcode.cn/problems/koko-eating-bananas/',
   difficulty: Difficulty.中等,
-  tag: [Tag.几何, Tag.数学, Tag.拒绝采样, Tag.随机化],
-  desc: `给定圆的半径和圆心的位置，实现函数 randPoint ，在圆中产生均匀随机点。`,
+  tag: [Tag.数组, Tag.二分查找],
+  desc: `返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。`,
   solutions: [
     {
       script: Script.TS,
-      time: 128,
-      memory: 58.3,
-      desc: '忽略边上的点',
+      time: 52,
+      memory: 18.2,
+      desc: 'bs',
       code: `class Solution {
-        constructor(public radius: number, public x_center: number, public y_center: number) { }
-        randPoint(): number[] {
-            while (true) {
-                const [x, y] = [
-                    2 * Math.random() * this.radius - this.radius,
-                    2 * Math.random() * this.radius - this.radius,
-                ]
-                if (x ** 2 + y ** 2 <= this.radius * this.radius) 
-                return [
-                    x + this.x_center,
-                    y + this.y_center
-                ];
-            }
+   public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int nmin = 1, nmax = INT_MIN;
+        for (auto& pile : piles) nmax = max(nmax, pile);
+        while (nmin < nmax) {
+            int m = (nmin + nmax) >> 1;
+            int c = check(piles, m);
+            if (c > h)
+                nmin = m + 1;
+            else
+                nmax = m;
         }
-    }`,
+        return nmin;
+    }
+    int check(vector<int>& piles, int target) {
+        int ans = 0;
+        for (auto pile : piles) {
+            ans += pile / target;
+            if (pile % target) ans += 1;
+        }
+        return ans;
+    }
+};`,
     },
   ],
 };
