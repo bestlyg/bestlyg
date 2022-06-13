7650308facfb70de8f2e09c96a35531358532bee
liyigang <liyigang@bytedance.com>
2022-06-13 10:06:34 +0800

    feat: add leetcode no.1051

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,41 +4,30 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '890. 查找和替换模式',
+  exist: true,
+  name: '1051. 高度检查器',
   url: 'https://leetcode.cn/problems/find-and-replace-pattern/',
   difficulty: Difficulty.中等,
   tag: [Tag.数组, Tag.哈希表, Tag.字符串],
   desc: `返回 words 中与给定模式匹配的单词列表。`,
   solutions: [
     {
-      script: Script.TS,
+      script: Script.CPP,
       time: 4,
-      memory: 7.6,
-      desc: '遍历比较',
+      memory: 7.9,
+      desc: '排序后比较',
       code: `class Solution {
    public:
-    int n;
-    vector<string> findAndReplacePattern(vector<string> &words,
-                                         string pattern) {
-        vector<string> ans;
-        n = pattern.size();
-        for (auto &word : words) {
-            if (check(pattern, word)) ans.push_back(word);
+    int heightChecker(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> list(n);
+        for (int i = 0; i < n; i++) list[i] = heights[i];
+        sort(list.begin(), list.end());
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (list[i] != heights[i]) ans++;
         }
         return ans;
-    }
-    bool check(string &pattern, string &word) {
-        char list[26] = {0};
-        bool check[26] = {0};
-        for (int i = 0; i < n; i++) {
-            char wc = word[i], pc = pattern[i];
-            if (list[pc - 'a'] && list[pc - 'a'] != wc) return false;
-            if (list[pc - 'a'] == '\\0' && check[wc - 'a']) return false;
-            check[wc - 'a'] = true;
-            list[pc - 'a'] = wc;
-        }
-        return true;
     }
 };`,
     },
