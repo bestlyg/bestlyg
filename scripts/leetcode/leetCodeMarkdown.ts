9149e3b12f3086d4f70e1265bf6907e488f1d25a
liyigang <liyigang@bytedance.com>
2022-06-16 13:20:40 +0800

    feat: add leetcode no.532

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,43 +5,40 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '719. 找出第 K 小的数对距离',
-  url: 'https://leetcode.cn/problems/find-k-th-smallest-pair-distance/',
-  difficulty: Difficulty.困难,
-  tag: [Tag.数组, Tag.双指针, Tag.二分查找,Tag.排序],
-  desc: `返回 所有数对距离中 第 k 小的数对距离。`,
+  name: '532. 数组中的 k-diff 数对',
+  url: 'https://leetcode.cn/problems/k-diff-pairs-in-an-array/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数组, Tag.哈希表, Tag.双指针, Tag.二分查找, Tag.排序],
+  desc: `给你一个整数数组 nums 和一个整数 k，请你在数组中找出 不同的 k-diff 数对，并返回不同的 k-diff 数对 的数目。`,
   solutions: [
     {
       script: Script.CPP,
       time: 16,
-      memory: 9.7,
-      desc: '排序后，对于每一种差值，计算可能的数量',
+      memory: 13.9,
+      desc: '排序后，针对于每个点查找差值为k的点的数量',
       code: `class Solution {
    public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), l = 0, r = nums[n - 1] - nums[0], m;
-        while (l < r) {
-            m = (l + r) >> 1;
-            int cnt = 0;
-            for (int i = 0; i < n; i++) cnt += i - bs(nums, i, nums[i] - m);
-            if (cnt >= k)
-                r = m;
-            else
-                l = m + 1;
+    int findPairs(vector<int> &nums, int k) {
+        map<int, int> m;
+        for (auto &num : nums) m[num]++;
+        if (k == 0) return check0(m);
+        int ans = 0;
+        vector<int> list;
+        for (auto &item : m) list.push_back(item.first);
+        int n = list.size(), l1 = 0, l2 = 0;
+        for (int r = 0; r < n; r++) {
+            while (l2 < r && list[r] - list[l2] >= k) l2++;
+            while (l1 < l2 && list[r] - list[l1] > k) l1++;
+            ans += l2 - l1;
         }
-        return l;
+        return ans;
     }
-    int bs(vector<int>& nums, int r, int target) {
-        int l = 0, m;
-        while (l < r) {
-            m = (l + r) >> 1;
-            if (nums[m] >= target)
-                r = m;
-            else
-                l = m + 1;
+    int check0(map<int, int> &m) {
+        int ans = 0;
+        for (auto &item : m) {
+            if (item.second > 1) ans++;
         }
-        return l;
+        return ans;
     }
 };`,
     },
