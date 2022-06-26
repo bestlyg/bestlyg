8fbd8b31d712b04628bc5dba8edde8bdf6e0b45d
liyigang <liyigang@bytedance.com>
2022-06-26 23:44:50 +0800

    feat: add leetcode no.710

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,31 +4,40 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '剑指 Offer II 091. 粉刷房子',
-  url: 'https://leetcode.cn/problems/find-largest-value-in-each-tree-row/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.树, Tag.深度优先搜索, Tag.广度优先搜索, Tag.二叉树],
-  desc: `给定一棵二叉树的根节点 root ，请找出该二叉树中每一层的最大值。`,
+  exist: !true,
+  name: '710. 黑名单中的随机数',
+  url: 'https://leetcode.cn/problems/random-pick-with-blacklist/',
+  difficulty: Difficulty.困难,
+  tag: [Tag.哈希表, Tag.数学, Tag.二分查找, Tag.排序, Tag.随机化],
+  desc: `给定一个整数 n 和一个 无重复 黑名单整数数组 blacklist 。设计一种算法，从 [0, n - 1] 范围内的任意整数中选取一个 未加入 黑名单 blacklist 的整数。任何在上述范围内且不在黑名单 blacklist 中的整数都应该有 同等的可能性 被返回。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 8,
-      memory: 9.6,
-      desc: 'dp',
+      time: 112,
+      memory: 68.6,
+      desc: '修改随机范围把范围内不可能取到的值映射出去',
       code: `class Solution {
    public:
-    int minCost(vector<vector<int>>& costs) {
-        int n = costs.size(), ans = INT_MAX;
-        vector<vector<int>> dp(3, vector<int>(n, 0));
-        for (int i = 0; i < 3; i++) dp[i][0] = costs[0][i];
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = min(dp[1][j - 1], dp[2][j - 1]) + costs[j][0];
-            dp[1][j] = min(dp[0][j - 1], dp[2][j - 1]) + costs[j][1];
-            dp[2][j] = min(dp[0][j - 1], dp[1][j - 1]) + costs[j][2];
+    int n;
+    unordered_map<int, int> m;
+    Solution(int n, vector<int> &blacklist) {
+        srand(time(0));
+        int size = blacklist.size(), nextN = n - size;
+        unordered_set<int> s;
+        for (auto &num : blacklist) {
+            if (num >= nextN) s.emplace(num);
         }
-        for (int i = 0; i < 3; i++) ans = min(ans, dp[i][n - 1]);
-        return ans;
+        int i = nextN;
+        for (auto &num : blacklist) {
+            if (num >= nextN) continue;
+            while (s.count(i)) i++;
+            m[num] = i++;
+        }
+        this->n = nextN;
+    }
+    int pick() {
+        int num = floor(random() % n);
+        return m.count(num) ? m[num] : num;
     }
 };`,
     },
