4663f867c5b46d4c6173a421211125744df94d1a
liyigang <liyigang@bytedance.com>
2022-06-27 16:01:39 +0800

    feat: 🎸 add leetcode no.522

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,39 +5,39 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '710. 黑名单中的随机数',
-  url: 'https://leetcode.cn/problems/random-pick-with-blacklist/',
-  difficulty: Difficulty.困难,
-  tag: [Tag.哈希表, Tag.数学, Tag.二分查找, Tag.排序, Tag.随机化],
-  desc: `给定一个整数 n 和一个 无重复 黑名单整数数组 blacklist 。设计一种算法，从 [0, n - 1] 范围内的任意整数中选取一个 未加入 黑名单 blacklist 的整数。任何在上述范围内且不在黑名单 blacklist 中的整数都应该有 同等的可能性 被返回。`,
+  name: '522. 最长特殊序列 II',
+  url: 'https://leetcode.cn/problems/longest-uncommon-subsequence-ii/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数组, Tag.哈希表, Tag.双指针, Tag.字符串, Tag.排序],
+  desc: `给定字符串列表 strs ，返回其中 最长的特殊序列 。如果最长特殊序列不存在，返回 -1 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 112,
-      memory: 68.6,
-      desc: '修改随机范围把范围内不可能取到的值映射出去',
+      time: 4,
+      memory: 8,
+      desc: '对于每个字符串，查找是否有父串',
       code: `class Solution {
    public:
-    int n;
-    unordered_map<int, int> m;
-    Solution(int n, vector<int> &blacklist) {
-        srand(time(0));
-        int size = blacklist.size(), nextN = n - size;
-        unordered_set<int> s;
-        for (auto &num : blacklist) {
-            if (num >= nextN) s.emplace(num);
+    int findLUSlength(vector<string> &strs) {
+        int ans = -1, n = strs.size();
+        for (int i = 0; i < n; i++) {
+            string s1 = strs[i];
+            bool check = true;
+            for (int j = 0; j < n; j++) {
+                if (i != j && isSubStr(s1, strs[j])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) ans = max(ans, (int)s1.size());
         }
-        int i = nextN;
-        for (auto &num : blacklist) {
-            if (num >= nextN) continue;
-            while (s.count(i)) i++;
-            m[num] = i++;
-        }
-        this->n = nextN;
+        return ans;
     }
-    int pick() {
-        int num = floor(random() % n);
-        return m.count(num) ? m[num] : num;
+    bool isSubStr(const string &s1, const string &s2) {
+        int i1 = 0, i2 = 0, n1 = s1.size(), n2 = s2.size();
+        for (; i1 < n1 && i2 < n2; i2++)
+            if (s1[i1] == s2[i2]) i1++;
+        return i1 == n1;
     }
 };`,
     },
