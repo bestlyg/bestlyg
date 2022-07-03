e89b2860c290dadc893d5f0296b8deaf0651ac4b
bestlyg <1057966749@qq.com>
2022-07-03 15:13:53 +0800

    feat: 🎸 update leetcode

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,65 +4,30 @@ import { Markdown, Difficulty, Tag, Script } from './leetcode';
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '241. 为运算表达式设计优先级',
+  exist: !true,
+  name: '556. 下一个更大元素 III',
   url: 'https://leetcode.cn/problems/wiggle-sort-ii/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.数组, Tag.哈希表, Tag.双指针, Tag.字符串, Tag.排序],
-  desc: `给你一个整数数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]... 的顺序。`,
+  difficulty: Difficulty.困难,
+  tag: [Tag.数学, Tag.双指针, Tag.字符串],
+  desc: `给你一个正整数 n ，请你找出符合条件的最小整数，其由重新排列 n 中存在的每位数字组成，并且其值大于 n 。如果不存在这样的正整数，则返回 -1 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 8,
-      memory: 12.4,
-      desc: '分治',
+      time: 0,
+      memory: 5.8,
+      desc: '从小到大找到第一个逆序的数，再找到第一个逆序数大的数，进行交换，然后翻转后半段',
       code: `class Solution {
    public:
-    unordered_set<char> opset;
-    Solution() {
-        opset.insert('+');
-        opset.insert('-');
-        opset.insert('*');
-    }
-    vector<int> diffWaysToCompute(string expression) {
-        vector<int> ans, oplist;
-        int n = expression.size();
-        for (int i = 0; i < n; i++) {
-            if (opset.count(expression[i])) oplist.push_back(i);
-        }
-        if (oplist.size() == 0)
-            ans.push_back(toNum(expression));
-        else
-            dfs(expression, oplist, ans);
-        return ans;
-    }
-    int toNum(string &expression) {
-        int num = 0, n = expression.size(), i = 0;
-        while (i < n && !opset.count(expression[i]))
-            num = num * 10 + expression[i++] - '0';
-        return num;
-    }
-    void dfs(string &expression, vector<int> &oplist, vector<int> &ans) {
-        for (auto &idx : oplist) {
-            vector<int> llist = diffWaysToCompute(expression.substr(0, idx));
-            vector<int> rlist = diffWaysToCompute(
-                expression.substr(idx + 1, expression.size() - idx));
-            for (auto &num1 : llist) {
-                for (auto &num2 : rlist) {
-                    switch (expression[idx]) {
-                        case '+':
-                            ans.push_back(num1 + num2);
-                            break;
-                        case '-':
-                            ans.push_back(num1 - num2);
-                            break;
-                        case '*':
-                            ans.push_back(num1 * num2);
-                            break;
-                    }
-                }
-            }
-        }
+    int nextGreaterElement(int n) {
+        string str = to_string(n);
+        int len = str.size(), i = len - 2, j = len - 1;
+        while (i >= 0 && str[i] - '0' >= str[i + 1] - '0') i--;
+        if (i == -1) return -1;
+        while (j > i && str[j] - '0' <= str[i] - '0') j--;
+        swap(str[i], str[j]);
+        reverse(str.begin() + i + 1, str.end());
+        long num = stol(str);
+        return num > INT_MAX ? -1 : num;
     }
 };`,
     },
