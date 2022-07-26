c82092d338386ea7c1b28057c544012ab1155271
liyigang <liyigang@bytedance.com>
2022-07-26 14:43:37 +0800

    feat: 🎸 add leetcode no.1206

--- scripts/leetcode/script.ts
+++ scripts/leetcode/script.ts
@@ -7,12 +7,24 @@ function getNext(needle: string): number[] {
   }
   return next;
 }
+const getIdx = (char: string) => char.codePointAt(0)! - 'a'.codePointAt(0)!;
+function getJump(needle: string, next: number[]): number[][] {
+  const COUNT = 26;
+  const jump = new Array(needle.length + 1).fill(0).map(_ => new Array(COUNT).fill(-1));
+  jump[0][getIdx(needle[0])] = 0;
+  for (let i = 1; i < needle.length; i++) {
+    jump[i] = [...jump[next[i - 1] + 1]];
+    jump[i][getIdx(needle[i])] = i;
+  }
+  return jump;
+}
 function strStr(haystack: string, needle: string): number {
   const next = getNext(needle);
+  const jump = getJump(needle, next);
   for (let i = 0, j = -1; i < haystack.length; i++) {
-    while (j != -1 && needle[j + 1] != haystack[i]) j = next[j];
-    if (needle[j + 1] === haystack[i]) j++;
+    j = jump[j+1][getIdx(haystack[i])];
     if (j === needle.length - 1) return i - j;
   }
   return -1;
 }
+console.log(strStr('mississippi', 'issip'));
