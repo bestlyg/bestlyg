69db901b285e2da24b6ae83a11993f59c4d456e8
liyigang <liyigang@bytedance.com>
2022-08-04 10:13:54 +0800

    feat: 🎸 add leetcode no.1403

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,38 +5,31 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '899. 有序队列',
-  url: 'https://leetcode.cn/problems/orderly-queue/',
-  difficulty: Difficulty.困难,
-  tag: [Tag.数学, Tag.字符串, Tag.排序],
-  desc: `给定一个字符串 s 和一个整数 k 。你可以从 s 的前 k 个字母中选择一个，并把它加到字符串的末尾。  `,
+  name: '1403. 非递增顺序的最小子序列',
+  url: 'https://leetcode.cn/problems/minimum-subsequence-in-non-increasing-order/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.贪心, Tag.数组, Tag.排序],
+  desc: `和 严格 大于未包含在该子序列中的各元素之和。`,
   solutions: [
     {
       script: Script.RUST,
       time: 0,
-      memory: 2.3,
-      desc: '如果k大于1，说明可以任意调换顺序，如果k等于1说明只能把头部放入尾部',
-      code: `use std::cmp::Ordering;
-use std::collections::VecDeque;
-impl Solution {
-    pub fn orderly_queue(s: String, k: i32) -> String {
-        if k == 1 {
-            let mut s = s.chars().collect::<VecDeque<char>>();
-            let mut ans = s.clone();
-            let mut temp = s.clone();
-            for i in 0..s.len() {
-                temp.pop_front();
-                temp.push_back(s[i]);
-                if ans.cmp(&temp) == Ordering::Greater {
-                    ans = temp.clone();
-                }
-            }
-            ans.into_iter().collect()
-        } else {
-            let mut s = s.chars().collect::<Vec<char>>();
-            s.sort();
-            s.into_iter().collect()
+      memory: 2.1,
+      desc: '排序后，从后往前取值',
+      code: `impl Solution {
+    pub fn min_subsequence(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums = nums;
+        nums.sort();
+        let sum: i32 = nums.iter().sum();
+        let mut cur = 0;
+        let mut i = nums.len() - 1;
+        let mut ans = Vec::new();
+        while cur * 2 <= sum {
+            cur += nums[i];
+            ans.push(nums[i]);
+            i -= 1;
         }
+        ans
     }
 }`,
     },
