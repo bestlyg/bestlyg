5b137924e206a9707c9042187213af7ac225a637
liyigang <liyigang@bytedance.com>
2022-08-10 10:36:55 +0800

    feat: 🎸 add leetcode no.640

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -1,5 +1,58 @@
 mod preclude;
+
 use preclude::*;
 fn main() {
     println!("Hello, world!");
+}
+
+use std::collections::VecDeque;
+impl Solution {
+    pub fn solve_equation(equation: String) -> String {
+        let (mut l, mut r) = (0, 0);
+        let equation = equation.split('=').collect::<Vec<&str>>();
+        Solution::analysis(&mut l, &mut r, Solution::format(equation[0]), 1);
+        Solution::analysis(&mut l, &mut r, Solution::format(equation[1]), -1);
+        if l == 0 && r == 0 {
+            String::from("Infinite solutions")
+        } else if l == 0 {
+            String::from("No solution")
+        } else {
+            format!("x={}", r / l)
+        }
+    }
+    fn format(s: &str) -> String {
+        let mut ans = s.to_string().chars().collect::<VecDeque<char>>();
+        let front = ans.front().unwrap();
+        if *front != '+' && *front != '-' {
+            ans.push_front('+');
+        }
+        ans.push_front('0');
+        String::from_utf8(ans.iter().map(|c| *c as u8).collect::<Vec<u8>>()).unwrap()
+    }
+    fn analysis(l: &mut i32, r: &mut i32, s: String, flag: i32) {
+        println!("analysis");
+        let s = s.chars().collect::<Vec<char>>();
+        let mut i = 0;
+        while i < s.len() {
+            let mut num = String::new();
+            if i != 0 {
+                num.push(s[i - 1]);
+            }
+            while i < s.len() && s[i] != '+' && s[i] != '-' && s[i] != 'x' {
+                num.push(s[i]);
+                i += 1;
+            }
+            println!("num = {:?}", num);
+            if i < s.len() && s[i] == 'x' {
+                if num.len() == 0 || (num.len() == 1 && (num.eq("+") || num.eq("-"))) {
+                    num.push('1');
+                }
+                *l += flag * num.parse::<i32>().unwrap();
+                i += 1;
+            } else {
+                *r += -1 * flag * num.parse::<i32>().unwrap();
+            }
+            i += 1;
+        }
+    }
 }
--- packages/rust/packages/leetcode/src/preclude.rs
+++ packages/rust/packages/leetcode/src/preclude.rs
@@ -1,35 +1,34 @@
-pub use std::{rc::Rc, cell::RefCell};
+pub use std::{cell::RefCell, rc::Rc};
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
 }
+
+pub struct Solution;
