2d4bed119cc1aedb0d77e9d1c4ecbd1344729bee
liyigang <liyigang@bytedance.com>
2022-08-18 14:33:23 +0800

    feat: 🎸 add leetcode no.1224

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -1,39 +1,37 @@
 mod preclude;
+
 use preclude::*;
 fn main() {}
 
-use std::borrow::Borrow;
-use std::cell::RefCell;
-use std::collections::VecDeque;
-use std::rc::Rc;
 impl Solution {
-    pub fn deepest_leaves_sum(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let root = root.unwrap();
-        let mut q = VecDeque::<Rc<RefCell<TreeNode>>>::new();
-        q.push_back(root.clone());
-        let mut ans = root.as_ref().borrow().val;
-        let mut cur = 0;
-        let mut size = 1;
-        while !q.is_empty() {
-            let node = q.pop_front().unwrap();
-            let node = node.as_ref().borrow();
-            if node.left.is_some() {
-                cur += node.left.as_ref().unwrap().as_ref().borrow().val;
-                q.push_back(node.left.as_ref().unwrap().clone());
+    pub fn max_equal_freq(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+        let (mut freq_map, mut cnt_map) =
+            (HashMap::<usize, usize>::new(), HashMap::<i32, usize>::new());
+        let mut ans = 0;
+        let mut max_freq = 0;
+        for i in 0..nums.len() {
+            let num = nums[i];
+            let cnt = cnt_map.get(&num).unwrap_or(&0);
+            let prev_freq = freq_map.get_mut(cnt);
+            if prev_freq.is_some() {
+                *prev_freq.unwrap() -= 1;
             }
-            if node.right.is_some() {
-                cur += node.right.as_ref().unwrap().as_ref().borrow().val;
-                q.push_back(node.right.as_ref().unwrap().clone());
-            }
-            size -= 1;
-            if size == 0 {
-                size = q.len();
-                if size != 0 {
-                    ans = cur;
-                }
-                cur = 0;
+            let cnt = *cnt + 1;
+            cnt_map.insert(num, cnt);
+            freq_map.insert(cnt, freq_map.get(&cnt).unwrap_or(&0) + 1);
+            max_freq = max_freq.max(cnt);
+            if max_freq == 1
+                || freq_map.get(&max_freq).unwrap_or(&0) * max_freq
+                    + freq_map.get(&(max_freq - 1)).unwrap_or(&0) * (max_freq - 1)
+                    == i + 1
+                    && *freq_map.get(&max_freq).unwrap_or(&0) == 1
+                || freq_map.get(&max_freq).unwrap_or(&0) * max_freq == i
+                    && *freq_map.get(&1).unwrap_or(&0) == 1
+            {
+                ans = i + 1;
             }
         }
-        ans
+        ans as i32
     }
 }
