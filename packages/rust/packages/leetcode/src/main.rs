14a87622a4c0c3a64c6700a12b5689c508ee3c2d
liyigang <liyigang@bytedance.com>
2022-08-22 10:23:13 +0800

    feat: 🎸 update leetcode

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -2,36 +2,3 @@ mod preclude;
 
 use preclude::*;
 fn main() {}
-
-impl Solution {
-    pub fn max_equal_freq(nums: Vec<i32>) -> i32 {
-        use std::collections::HashMap;
-        let (mut freq_map, mut cnt_map) =
-            (HashMap::<usize, usize>::new(), HashMap::<i32, usize>::new());
-        let mut ans = 0;
-        let mut max_freq = 0;
-        for i in 0..nums.len() {
-            let num = nums[i];
-            let cnt = cnt_map.get(&num).unwrap_or(&0);
-            let prev_freq = freq_map.get_mut(cnt);
-            if prev_freq.is_some() {
-                *prev_freq.unwrap() -= 1;
-            }
-            let cnt = *cnt + 1;
-            cnt_map.insert(num, cnt);
-            freq_map.insert(cnt, freq_map.get(&cnt).unwrap_or(&0) + 1);
-            max_freq = max_freq.max(cnt);
-            if max_freq == 1
-                || freq_map.get(&max_freq).unwrap_or(&0) * max_freq
-                    + freq_map.get(&(max_freq - 1)).unwrap_or(&0) * (max_freq - 1)
-                    == i + 1
-                    && *freq_map.get(&max_freq).unwrap_or(&0) == 1
-                || freq_map.get(&max_freq).unwrap_or(&0) * max_freq == i
-                    && *freq_map.get(&1).unwrap_or(&0) == 1
-            {
-                ans = i + 1;
-            }
-        }
-        ans as i32
-    }
-}
--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,28 +5,34 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1450. 在既定时间做作业的学生人数',
-  url: 'https://leetcode.cn/problems/number-of-students-doing-homework-at-a-given-time/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组],
-  desc: `请返回在查询时间 queryTime 时正在做作业的学生人数。`,
+  name: '654. 最大二叉树',
+  url: 'https://leetcode.cn/problems/maximum-binary-tree/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.栈, Tag.树, Tag.数组, Tag.分治, Tag.二叉树, Tag.单调栈],
+  desc: `返回 nums 构建的 最大二叉树 。`,
   solutions: [
     {
-      script: Script.RUST,
-      time: 0,
-      memory: 2.1,
-      desc: '遍历',
-      code: `impl Solution {
-    pub fn busy_student(start_time: Vec<i32>, end_time: Vec<i32>, query_time: i32) -> i32 {
-        let mut ans = 0;
-        for i in 0..start_time.len() {
-            if start_time[i] <= query_time && end_time[i] >= query_time {
-                ans += 1;
+      script: Script.CPP,
+      time: 64,
+      memory: 41.2,
+      desc: 'dfs',
+      code: `class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return _constructMaximumBinaryTree(nums, 0, nums.size());
+    }
+    TreeNode* _constructMaximumBinaryTree(vector<int>& nums, int l, int r) {
+        if (l >= r) return nullptr;
+        int max_num = INT_MIN, max_idx;
+        for (int i = l; i < r; i++) {
+            if (nums[i] > max_num) {
+                max_num = nums[i];
+                max_idx = i;
             }
         }
-        ans
+        return new TreeNode(max_num, _constructMaximumBinaryTree(nums, l, max_idx), _constructMaximumBinaryTree(nums, max_idx + 1, r));
     }
-}`,
+};`,
     },
   ],
 };
