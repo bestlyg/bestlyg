7ed83f904435b800d26a074178c214adc0f38987
liyigang <liyigang@bytedance.com>
2022-08-24 10:18:55 +0800

    feat: 🎸 add leetcode no.1460

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -2,3 +2,19 @@ mod preclude;
 
 use preclude::*;
 fn main() {}
+
+impl Solution {
+    pub fn can_be_equal(target: Vec<i32>, arr: Vec<i32>) -> bool {
+        let mut map = [5; 1005];
+        for num in arr {
+            map[num as usize] += 1;
+        }
+        for num in target {
+            if map[num as usize] == 0 {
+                return false;
+            }
+            map[num as usize] -= 1;
+        }
+        true
+    }
+}
--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,34 +5,49 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '654. 最大二叉树',
-  url: 'https://leetcode.cn/problems/maximum-binary-tree/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.栈, Tag.树, Tag.数组, Tag.分治, Tag.二叉树, Tag.单调栈],
-  desc: `返回 nums 构建的 最大二叉树 。`,
+  name: '1460. 通过翻转子数组使两个数组相等',
+  url: 'https://leetcode.cn/problems/make-two-arrays-equal-by-reversing-sub-arrays/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.数组, Tag.哈希表, Tag.排序],
+  desc: `给你两个长度相同的整数数组 target 和 arr 。每一步中，你可以选择 arr 的任意 非空子数组 并将它翻转。你可以执行此过程任意次。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 64,
-      memory: 41.2,
-      desc: 'dfs',
+      time: 12,
+      memory: 13.9,
+      desc: '只要数的数量相同就可以匹配',
       code: `class Solution {
 public:
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return _constructMaximumBinaryTree(nums, 0, nums.size());
-    }
-    TreeNode* _constructMaximumBinaryTree(vector<int>& nums, int l, int r) {
-        if (l >= r) return nullptr;
-        int max_num = INT_MIN, max_idx;
-        for (int i = l; i < r; i++) {
-            if (nums[i] > max_num) {
-                max_num = nums[i];
-                max_idx = i;
-            }
-        }
-        return new TreeNode(max_num, _constructMaximumBinaryTree(nums, l, max_idx), _constructMaximumBinaryTree(nums, max_idx + 1, r));
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+       int map[1005] = {0};
+       for (auto &num : arr) map[num]++;
+       for (auto &num : target) {
+           if (map[num]-- == 0) return false;
+       } 
+       return true;
     }
 };`,
+    },
+    {
+      script: Script.RUST,
+      time: 0,
+      memory: 2,
+      desc: '只要数的数量相同就可以匹配',
+      code: `impl Solution {
+    pub fn can_be_equal(target: Vec<i32>, arr: Vec<i32>) -> bool {
+        let mut map = [0; 1005];
+        for num in arr {
+            map[num as usize] += 1;
+        }
+        for num in target {
+            if map[num as usize] == 0 {
+                return false;
+            }
+            map[num as usize] -= 1;
+        }
+        true
+    }
+}`,
     },
   ],
 };
