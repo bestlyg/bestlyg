c918e3945e4eedb517af444aedf61e224da17537
liyigang <liyigang@bytedance.com>
2022-08-25 10:34:50 +0800

    feat: 🎸 add leetcode no.658

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -4,17 +4,7 @@ use preclude::*;
 fn main() {}
 
 impl Solution {
-    pub fn can_be_equal(target: Vec<i32>, arr: Vec<i32>) -> bool {
-        let mut map = [5; 1005];
-        for num in arr {
-            map[num as usize] += 1;
-        }
-        for num in target {
-            if map[num as usize] == 0 {
-                return false;
-            }
-            map[num as usize] -= 1;
-        }
-        true
+    pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
+        
     }
 }
