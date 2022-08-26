89be0d2d4ec60d7b7e61f3da0924786819c2b17e
liyigang <liyigang@bytedance.com>
2022-08-26 10:52:46 +0800

    feat: 🎸 add leetcode no.1464

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -4,7 +4,19 @@ use preclude::*;
 fn main() {}
 
 impl Solution {
-    pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
-        
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let (mut num1, mut num2) = (nums[0], nums[1]);
+        if num2 > num1 {
+            (num1, num2) = (num2, num1);
+        }
+        let mut i = 2;
+        while i < nums.len() {
+            if nums[i] > num1 {
+                num1 = nums[i];
+            } else if nums[i] > num2 {
+                num2 = nums[i];
+            }
+        }
+        (num1 - 1) * (num2 - 1)
     }
 }
--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,52 +5,49 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '658. 找到 K 个最接近的元素',
-  url: 'https://leetcode.cn/problems/find-k-closest-elements/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.数组, Tag.双指针, Tag.二分查找, Tag.排序, Tag.堆],
-  desc: `给定一个 排序好 的数组 arr ，两个整数 k 和 x ，从数组中找到最靠近 x（两数之差最小）的 k 个数。返回的结果必须要是按升序排好的。`,
+  name: '1464. 数组中两元素的最大乘积',
+  url: 'https://leetcode.cn/problems/maximum-product-of-two-elements-in-an-array/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.数组, Tag.排序, Tag.堆_优先队列],
+  desc: `给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。`,
   solutions: [
     {
       script: Script.RUST,
-      time: 16,
-      memory: 2.2,
-      desc: '只要数的数量相同就可以匹配',
-      code: `use std::cmp::Ordering;
-use std::collections::{BinaryHeap, VecDeque};
-#[derive(PartialEq, Eq, Debug)]
-struct Item(i32, i32);
-impl PartialOrd for Item {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let ord = other.1.cmp(&self.1);
-        if ord == Ordering::Equal {
-            Some(other.0.cmp(&self.0))
-        } else {
-            Some(ord)
-        }
+      time: 0,
+      memory: 2,
+      desc: '排序',
+      code: `impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort();
+        (nums[nums.len() - 1] - 1) * (nums[nums.len() - 2] - 1)
     }
-}
-impl Ord for Item {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-impl Solution {
-    pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
-        let mut ans = VecDeque::<i32>::new();
-        let mut heap = BinaryHeap::<Item>::new();
-        for num in arr {
-            heap.push(Item(num, (num - x).abs()));
+}`,
+    },
+    {
+      script: Script.RUST,
+      time: 0,
+      memory: 2,
+      desc: '遍历',
+      code: `impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let (mut num1, mut num2) = (nums[0], nums[1]);
+        if num2 > num1 {
+            num1 ^= num2;
+            num2 ^= num1;
+            num1 ^= num2;
         }
-        for _ in 0..k {
-            let num = heap.pop().unwrap().0;
-            if ans.len() == 0 || *ans.back().unwrap() <= num {
-                ans.push_back(num);
-            } else {
-                ans.push_front(num);
+        let mut i = 2;
+        while i < nums.len() {
+            if nums[i] > num1 {
+                num2 = num1;
+                num1 = nums[i];
+            } else if nums[i] > num2 {
+                num2 = nums[i];
             }
+            i += 1;
         }
-        Vec::from(ans)
+        (num1 - 1) * (num2 - 1)
     }
 }`,
     },
