10407b620fa8675ac19a814182b7eaf0e6384ca5
liyigang <liyigang@bytedance.com>
2022-08-29 10:52:22 +0800

    feat: 🎸 add leetcode no.1470

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,40 +5,26 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '793. 阶乘函数后 K 个零',
-  url: 'https://leetcode.cn/problems/preimage-size-of-factorial-zeroes-function/',
-  difficulty: Difficulty.困难,
-  tag: [Tag.数学, Tag.二分查找],
-  desc: `给定 k，找出返回能满足 f(x) = k 的非负整数 x 的数量。`,
+  name: '1470. 重新排列数组',
+  url: 'https://leetcode.cn/problems/shuffle-the-array/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.数组],
+  desc: `请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.8,
+      time: 12,
+      memory: 6.9,
       desc: '层序遍历',
-      code: `class Solution {
-public:
-    int preimageSizeFZF(int k) {
-        return help(k + 1) - help(k);
+      code: `int* shuffle(int* nums, int numsSize, int n, int* returnSize){
+     int *ans = (int *)malloc(sizeof(int) * n * 2);
+    for (int i = 0, j = n, idx = 0; idx < n * 2; i++, j++) {
+        ans[idx++] = nums[i];
+        ans[idx++] = nums[j];
     }
-    long long help(int k) {
-        long long l = 0, r = 5LL * k;
-        while (l < r) {
-            long long mid = (l + r) / 2;
-            if (get_cnt(mid) >= k) r = mid;
-            else l = mid + 1;
-        }
-        return l;
-    }
-    int get_cnt(int num) {
-        int cnt = 0;
-        while (num) {
-            cnt += num / 5;
-            num /= 5;
-        }
-        return cnt;
-    }
-};`,
+    *returnSize = 2 * n;
+    return ans;
+}`,
     },
   ],
 };
