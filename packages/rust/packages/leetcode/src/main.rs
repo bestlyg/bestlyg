475ba8fcbad619b663f6ae9632125bf0bbaaec1e
liyigang <liyigang@bytedance.com>
2022-09-01 10:29:58 +0800

    feat: 🎸 add leetcode no.1475

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -3,45 +3,17 @@ mod preclude;
 use preclude::*;
 fn main() {}
 
-use std::cell::RefCell;
-use std::collections::VecDeque;
-use std::rc::Rc;
 impl Solution {
-    pub fn width_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let root = root.unwrap();
-        let mut q = VecDeque::<(i32, Rc<RefCell<TreeNode>>)>::new();
-        q.push_back((0, root.clone()));
-        let mut ans = 1_i32;
-        let mut size = 1;
-        while !q.is_empty() {
-            let (idx, node) = q.pop_front().unwrap();
-            if node.as_ref().borrow().left.is_some() {
-                q.push_back((
-                    idx * 2 + 1,
-                    node.as_ref().borrow().left.as_ref().unwrap().clone(),
-                ));
+    pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
+        let mut s = Vec::<usize>::new();
+        let mut prices = prices;
+        for i in 0..prices.len() {
+            while !s.is_empty() && prices[*s.last().unwrap()] >= prices[i] {
+                let prev = s.pop().unwrap();
+                prices[prev] -= prices[i];
             }
-            if node.as_ref().borrow().right.is_some() {
-                q.push_back((
-                    idx * 2 + 2,
-                    node.as_ref().borrow().right.as_ref().unwrap().clone(),
-                ));
-            }
-            size -= 1;
-            if size == 0 {
-                size = q.len();
-                if !q.is_empty() {
-                    println!(
-                        "size = {}, front_idx = {}, back_idx = {}, ans = {}",
-                        q.len(),
-                        q.front().unwrap().0,
-                        q.back().unwrap().0,
-                        ans
-                    );
-                    ans = ans.max(q.back().unwrap().0 - q.front().unwrap().0);
-                }
-            }
+            s.push(i);
         }
-        ans
+        prices
     }
 }
--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,33 +4,54 @@ const { specStr, markdown } = utils;
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '946. 验证栈序列',
-  url: 'https://leetcode.cn/problems/maximum-binary-tree-ii/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.树, Tag.二叉树],
-  desc: `返回 Construct(b) 。`,
+  exist: !true,
+  name: '1475. 商品折扣后的最终价格',
+  url: 'https://leetcode.cn/problems/final-prices-with-a-special-discount-in-a-shop/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.栈, Tag.数组, Tag.单调栈],
+  desc: `请你返回一个数组，数组中第 i 个元素是折扣后你购买商品 i 最终需要支付的价格。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 16,
-      memory: 14.8,
-      desc: 'stack',
+      time: 4,
+      memory: 9.8,
+      desc: '单调栈',
       code: `class Solution {
 public:
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int len = popped.size();
+    vector<int> finalPrices(vector<int>& prices) {
         stack<int> s;
-        for (int i1 = 0, i2 = 0; i1 < len; i1++) {
-            s.push(pushed[i1]);
-            while (i2 < len && s.size() && s.top() == popped[i2]) {
+        vector<int> ans(prices.begin(), prices.end());
+        for (int i = 0; i < prices.size(); i++) {
+            while (s.size() && prices[s.top()] >= prices[i]) {
+                int prev = s.top();
                 s.pop();
-                i2++;
+                ans[prev] -= prices[i];
             }
+            s.push(i);
         }
-        return s.empty();
+        return ans;
     }
 };`,
+    },
+    {
+      script: Script.CPP,
+      time: 0,
+      memory: 2.1,
+      desc: '单调栈',
+      code: `impl Solution {
+    pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
+        let mut s = Vec::<usize>::new();
+        let mut prices = prices;
+        for i in 0..prices.len() {
+            while !s.is_empty() && prices[*s.last().unwrap()] >= prices[i] {
+                let prev = s.pop().unwrap();
+                prices[prev] -= prices[i];
+            }
+            s.push(i);
+        }
+        prices
+    }
+}`,
     },
   ],
 };
