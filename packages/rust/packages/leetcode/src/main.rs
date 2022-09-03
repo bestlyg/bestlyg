2fec410f93ae929ca7663926ab9bd757f9bad922
liyigang <liyigang@bytedance.com>
2022-09-03 16:31:47 +0800

    feat: 🎸 add leetcode no.646

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -4,16 +4,27 @@ use preclude::*;
 fn main() {}
 
 impl Solution {
-    pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
-        let mut s = Vec::<usize>::new();
-        let mut prices = prices;
-        for i in 0..prices.len() {
-            while !s.is_empty() && prices[*s.last().unwrap()] >= prices[i] {
-                let prev = s.pop().unwrap();
-                prices[prev] -= prices[i];
+    pub fn find_longest_chain(pairs: Vec<Vec<i32>>) -> i32 {
+        let mut pairs = pairs;
+        pairs.sort_by(|a, b| {
+            if a[0] != b[0] {
+                a[0].cmp(&b[0])
+            } else {
+                a[1].cmp(&b[1])
             }
-            s.push(i);
+        });
+        let len = pairs.len();
+        let mut dp = vec![1; len];
+        let mut ans = 0;
+        for i in 0..len {
+            for j in 0..i {
+                if pairs[j][1] < pairs[i][1] {
+                    dp[i] = dp[i].max(dp[j] + 1)
+                }
+            }
+            ans = ans.max(dp[i]);
         }
-        prices
+        println!("{dp:?}");
+        ans
     }
 }
--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,34 +5,40 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '687. 最长同值路径',
-  url: 'https://leetcode.cn/problems/longest-univalue-path/',
+  name: '646. 最长数对链',
+  url: 'https://leetcode.cn/problems/maximum-length-of-pair-chain/',
   difficulty: Difficulty.中等,
-  tag: [Tag.树, Tag.深度优先搜索, Tag.二叉树],
-  desc: `给定一个二叉树的 root ，返回 最长的路径的长度 ，这个路径中的 每个节点具有相同值 。 这条路径可以经过也可以不经过根节点。`,
+  tag: [Tag.贪心, Tag.数组, Tag.动态规划, Tag.排序],
+  desc: `给定一个数对集合，找出能够形成的最长数对链的长度。你不需要用到所有的数对，你可以以任何顺序选择其中的一些数对来构造。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 84,
-      memory: 49.9,
-      desc: '递归,每次记录以根结点起的最长链路和子节点的最长内部链路',
-      code: `#define MAX(a, b) ((a) > (b) ? (a) : (b))
-int longestUnivaluePath(struct TreeNode *root){
-    if (!root) return 0;
-    int ans = 0;
-    _longestUnivaluePath(root, &ans);
-    return ans - 1;
-}
-int _longestUnivaluePath(struct TreeNode *root, int *ans) {
-    if (!root) return 0;
-    int cnt1 = 1, cnt2 = 1,
-        left = _longestUnivaluePath(root->left, ans),
-        right = _longestUnivaluePath(root->right, ans);
-    if (root->left && root->left->val == root->val) cnt1 = MAX(cnt1, 1 + left), cnt2 += left;
-    if (root->right && root->right->val == root->val) cnt1 = MAX(cnt1, 1 + right), cnt2 += right;
-    *ans = MAX(*ans, cnt1);
-    *ans = MAX(*ans, cnt2);
-    return cnt1;
+      time: 32,
+      memory: 2.2,
+      desc: 'dp记录当前点为结尾的最大链路',
+      code: `impl Solution {
+    pub fn find_longest_chain(pairs: Vec<Vec<i32>>) -> i32 {
+        let mut pairs = pairs;
+        pairs.sort_by(|a, b| {
+            if a[0] != b[0] {
+                a[0].cmp(&b[0])
+            } else {
+                a[1].cmp(&b[1])
+            }
+        });
+        let len = pairs.len();
+        let mut dp = vec![1; len];
+        let mut ans = 0;
+        for i in 0..len {
+            for j in 0..i {
+                if pairs[j][1] < pairs[i][0] {
+                    dp[i] = dp[i].max(dp[j] + 1)
+                }
+            }
+            ans = ans.max(dp[i]);
+        }
+        ans
+    }
 }`,
     },
   ],
