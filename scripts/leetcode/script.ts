afa9b55a8add403fe8086ccae57af599269bcd87
bestlyg <1057966749@qq.com>
2022-09-11 22:21:01 +0800

    feat: 🎸 add leetcode no.857

--- scripts/leetcode/script.ts
+++ scripts/leetcode/script.ts
@@ -1,60 +1,74 @@
-// class TicTacToe {
-//   arr: number[][];
-//   constructor(public n: number) {
-//     this.arr = new Array(n).fill(0).map(_ => new Array(n).fill(0));
-//   }
-//   move(row: number, col: number, player: number): number {
-//     this.arr[row][col] = player;
-//     return this.check(row, col);
-//   }
-//   check(row: number, col: number): number {
-//     const player = this.arr[row][col];
-//     let check1 = true;
-//     let check2 = true;
-//     for (let i = 0; i < this.n; i++) {
-//       if (this.arr[i][col] !== player) check1 = false;
-//       if (this.arr[row][i] !== player) check2 = false;
-//     }
-//     console.log(check1, check2, this.checkX1(row, col) === 1, this.checkX2(row, col) === 1);
-//     if (check1 || check2 || this.checkX1(row, col) === 1 || this.checkX2(row, col) === 1) return 1;
-//     return 0;
-//   }
-//   checkX1(row: number, col: number) {
-//     const player = this.arr[row][col];
-//     let nrow = 0;
-//     let ncol = 0;
-//     for (; nrow < this.n && ncol < this.n && this.arr[nrow][ncol] === player; nrow++, col++) {}
-//     return nrow === this.n && ncol === this.n ? 1 : 0;
-//   }
-//   checkX2(row: number, col: number) {
-//     const player = this.arr[row][col];
-//     let nrow = 0;
-//     let ncol = this.n - 1;
-//     for (; nrow < this.n && ncol >= 0 && this.arr[nrow][ncol] === player; nrow++, col++) {}
-//     return nrow === this.n && ncol === -1 ? 1 : 0;
-//   }
-// }
-// const obj = new TicTacToe(3);
-// console.log(obj.move(0, 0, 1));
-
-function findMissingRanges(nums: number[], lower: number, upper: number): string[] {
-  if (nums.length === 0) return [];
-  const ans: string[] = [];
-  if (nums[0] !== lower) {
-    if (nums[0] - 1 === lower) ans.push(`${lower}`);
-    else ans.push(`${lower}->${nums[0] - 1}`);
+class Heap<T = number> {
+  private arr: T[] = [];
+  get isEmpty() {
+    return this.size === 0;
   }
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] - 1 === nums[i - 1]) continue;
-    add(nums[i - 1], nums[i]);
+  get size() {
+    return this.arr.length;
   }
-  if (nums[nums.length - 1] !== upper) {
-    if (nums[nums.length - 1] + 1 === lower) ans.push(`${upper}`);
-    else ans.push(`${nums[nums.length - 1] + 1}->${upper}`);
+  get top() {
+    return this.arr[0];
+  }
+  constructor(private compare: (t1: T, t2: T) => number) {}
+  add(num: T): void {
+    this.arr.push(num);
+    this.shiftUp(this.size - 1);
+  }
+  remove(): T {
+    const num = this.arr.shift()!;
+    if (this.size) {
+      this.arr.unshift(this.arr.pop()!);
+      this.shiftDown(0);
+    }
+    return num;
+  }
+  private shiftUp(index: number): void {
+    if (index === 0) return;
+    const parentIndex = (index - 1) >> 1;
+    if (this.compare(this.arr[index], this.arr[parentIndex]) > 0) {
+      [this.arr[index], this.arr[parentIndex]] = [this.arr[parentIndex], this.arr[index]];
+      this.shiftUp(parentIndex);
+    }
+  }
+  private shiftDown(index: number): void {
+    let childrenIndex = index * 2 + 1;
+    if (childrenIndex > this.size - 1) return;
+    if (
+      childrenIndex + 1 <= this.size - 1 &&
+      this.compare(this.arr[childrenIndex + 1], this.arr[childrenIndex]) > 0
+    ) {
+      childrenIndex++;
+    }
+    if (this.compare(this.arr[childrenIndex], this.arr[index]) > 0) {
+      [this.arr[childrenIndex], this.arr[index]] = [this.arr[index], this.arr[childrenIndex]];
+      this.shiftDown(childrenIndex);
+    }
+  }
+  *[Symbol.iterator](): IterableIterator<T> {
+    for (const t of this.arr) {
+      yield t;
+    }
+  }
+}
+function mincostToHireWorkers(quality: number[], wage: number[], k: number): number {
+  const len = quality.length;
+  const arr = new Array(len)
+    .fill(0)
+    .map((_, i) => i)
+    .sort((i1, i2) => wage[i1] / quality[i1] - wage[i2] / quality[i2]);
+  const heap = new Heap<number>((t1, t2) => t2 - t1);
+  let i = 0;
+  let sum = 0;
+  let ans = Infinity;
+  for (; i < k - 1; i++) {
+    sum += quality[arr[i]];
+    heap.add(quality[arr[i]]);
+  }
+  for (; i < len; i++) {
+    sum += quality[arr[i]];
+    heap.add(quality[arr[i]]);
+    ans = Math.min(ans, (wage[arr[i]] / quality[arr[i]]) * sum);
+    sum -= heap.remove();
   }
   return ans;
-  function add(before: number, after: number) {
-    if (before + 1 === after - 1) ans.push(`${before + 1}`);
-    else ans.push(`${before + 1}->${after - 1}`);
-  }
 }
