a86d9d869eebbb65b5838445d5a597867444a50c
liyigang <liyigang@bytedance.com>
2022-09-14 10:03:59 +0800

    feat: 🎸 add leetcode no.1619

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,26 +5,25 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1608. 特殊数组的特征值',
-  url: 'https://leetcode.cn/problems/special-array-with-x-elements-greater-than-or-equal-x/',
+  name: '1619. 删除某些元素后的数组均值',
+  url: 'https://leetcode.cn/problems/mean-of-array-after-removing-some-elements/',
   difficulty: Difficulty.简单,
-  tag: [Tag.数组, Tag.二分查找, Tag.排序],
-  desc: `如果存在一个数 x ，使得 nums 中恰好有 x 个元素 大于或者等于 x ，那么就称 nums 是一个 特殊数组 ，而 x 是该数组的 特征值 。`,
+  tag: [Tag.数组, Tag.排序],
+  desc: `给你一个整数数组 arr ，请你删除最小 5% 的数字和最大 5% 的数字后，剩余数字的平均值。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 8.1,
+      time: 8,
+      memory: 9.1,
       desc: '排序后遍历',
       code: `class Solution {
 public:
-    int specialArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), cnt = n;
-        for (int i = 0; i < n; i++, cnt--) {
-            if (nums[i] >= cnt && (i > 0 && cnt > nums[i - 1] || i == 0)) return cnt;
-        }
-        return -1;
+    double trimMean(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size(), v = n / 20;
+        double ans = 0;
+        for (int i = v; i < n - v; i++) ans += arr[i];
+        return ans / (n - 2 * v);
     }
 };`,
     },
