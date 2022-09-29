b15be89950b270d24ef09efa273d0f0a2f9e441f
liyigang <liyigang@bytedance.com>
2022-09-29 10:36:26 +0800

    feat: 🎸 add leetcode 面试题 01.09. 字符串轮转

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,32 +4,50 @@ const { specStr, markdown } = utils;
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '面试题 17.09. 第 k 个数',
-  url: 'https://leetcode.cn/problems/check-permutation-lcci/',
+  exist: !true,
+  name: '面试题 01.09. 字符串轮转',
+  url: 'https://leetcode.cn/problems/string-rotation-lcci/',
   difficulty: Difficulty.简单,
-  tag: [Tag.哈希表, Tag.字符串, Tag.排序],
-  desc: `给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。`,
+  tag: [Tag.字符串, Tag.字符串匹配],
+  desc: `字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 6.1,
+      time: 4,
+      memory: 7.5,
       desc: '遍历',
       code: `class Solution {
 public:
-    int getKthMagicNumber(int k) {
-        vector<int> list(k);
-        list[0] = 1;
-        int p[3] = {0};
-        for (int i = 1; i < k; i++) {
-            int next = min(list[p[0]] * 3, min(list[p[1]] * 5, list[p[2]] * 7));
-            list[i] = next;
-            if (next % 3 == 0) p[0]++;
-            if (next % 5 == 0) p[1]++;
-            if (next % 7 == 0) p[2]++;
+    int n;
+    bool isFlipedString(string s1, string s2) {
+        if (s1.size() != s2.size()) return false;
+        n = s1.size();
+        if (n == 0) return true;
+        vector<int> list;
+        char start = s2[0];
+        for (int i = 0; i < n; i++) if (s1[i] == start) list.push_back(i);
+        for (auto &start : list) if (comp(s1, start, s2, 0)) return true;
+        return false;
+    }
+    bool comp(string &s1, int i1, string &s2, int i2) {
+        while (i2 < n) {
+            if (s1[i1] != s2[i2]) return false;
+            i1 = (i1 + 1) % n;
+            i2++;
         }
-        return list.back();
+        return true;
+    }
+};`,
+    },
+    {
+      script: Script.CPP,
+      time: 4,
+      memory: 7.7,
+      desc: '拼接两个s1，如果是旋转，s2一定是子串',
+      code: `class Solution {
+public:
+    bool isFlipedString(string s1, string s2) {
+        return s1.size() == s2.size() && (s1 + s1).find(s2) != string::npos;
     }
 };`,
     },
