dc43a16e16e8f43d61ae071305380e85945b2f1e
liyigang <liyigang@bytedance.com>
2022-09-30 10:58:01 +0800

    feat: 🎸 add leetcode 面试题 01.08. 零矩阵

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,49 +5,36 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '面试题 01.09. 字符串轮转',
-  url: 'https://leetcode.cn/problems/string-rotation-lcci/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.字符串, Tag.字符串匹配],
-  desc: `字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成。`,
+  name: '面试题 01.08. 零矩阵',
+  url: 'https://leetcode.cn/problems/zero-matrix-lcci/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数组, Tag.哈希表, Tag.矩阵],
+  desc: `编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 4,
-      memory: 7.5,
+      time: 12,
+      memory: 11.9,
       desc: '遍历',
       code: `class Solution {
 public:
-    int n;
-    bool isFlipedString(string s1, string s2) {
-        if (s1.size() != s2.size()) return false;
-        n = s1.size();
-        if (n == 0) return true;
-        vector<int> list;
-        char start = s2[0];
-        for (int i = 0; i < n; i++) if (s1[i] == start) list.push_back(i);
-        for (auto &start : list) if (comp(s1, start, s2, 0)) return true;
-        return false;
-    }
-    bool comp(string &s1, int i1, string &s2, int i2) {
-        while (i2 < n) {
-            if (s1[i1] != s2[i2]) return false;
-            i1 = (i1 + 1) % n;
-            i2++;
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        unordered_set<int> rows, cols;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+            }
         }
-        return true;
-    }
-};`,
-    },
-    {
-      script: Script.CPP,
-      time: 4,
-      memory: 7.7,
-      desc: '拼接两个s1，如果是旋转，s2一定是子串',
-      code: `class Solution {
-public:
-    bool isFlipedString(string s1, string s2) {
-        return s1.size() == s2.size() && (s1 + s1).find(s2) != string::npos;
+        for (auto &row : rows) {
+            for (int i = 0; i < m; i++) matrix[row][i] = 0;
+        }
+        for (auto &col : cols) {
+            for (int i = 0; i < n; i++) matrix[i][col] = 0;
+        }
     }
 };`,
     },
