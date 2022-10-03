7e950972dc48be9580241ba1d98c5b4da93081fc
liyigang <liyigang@bytedance.com>
2022-10-03 09:30:23 +0800

    feat: 🎸 add leetcode no.1784

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,36 +5,25 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '777. 在LR字符串中交换相邻字符',
-  url: 'https://leetcode.cn/problems/swap-adjacent-in-lr-string/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.双指针, Tag.字符串],
-  desc: `现给定起始字符串start和结束字符串end，请编写代码，当且仅当存在一系列移动操作使得start可以转换成end时， 返回True。`,
+  name: '1784. 检查二进制字符串字段',
+  url: 'https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.字符串],
+  desc: `给你一个二进制字符串 s ，该字符串 不含前导零 。如果 s 包含 零个或一个由连续的 '1' 组成的字段 ，返回 true​​​ 。否则，返回 false 。如果 s 中 由连续若干个 '1' 组成的字段 数量不超过 1，返回 true​​​ 。否则，返回 false 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 8,
-      memory: 9.2,
-      desc: 'L字符可以左移，R字符可以右移，确保LR在字符串中数量相等，相对位置一样',
+      time: 0,
+      memory: 6,
+      desc: '遍历',
       code: `class Solution {
 public:
-    typedef pair<int, int> node;
-    bool canTransform(string start, string end) {
-        int n = start.size(), s_l = 0, s_r = 0, e_l = 0, e_r = 0;
-        vector<node> s_list, e_list;
+    bool checkOnesSegment(string s) {
+        int cnt = 0, n = s.size();
         for (int i = 0; i < n; i++) {
-            if (start[i] == 'L') s_list.push_back(make_pair(i, 0)), s_l++;
-            else if (start[i] == 'R') s_list.push_back(make_pair(i, 1)), s_r++;
-            if (end[i] == 'L') e_list.push_back(make_pair(i, 0)), e_l++;
-            else if (end[i] == 'R') e_list.push_back(make_pair(i, 1)), e_r++;
-        }
-        if (s_l != e_l || s_r != e_r) return false;
-        n = s_list.size();
-        for (int i = 0; i < n; i++) {
-            node s_node = s_list[i], e_node = e_list[i];
-            if (s_node.second != e_node.second) return false;
-            if (s_node.second == 0 && s_node.first < e_node.first) return false;
-            if (s_node.second == 1 && s_node.first > e_node.first) return false;
+            if (s[i] == '0') continue;
+            while (i + 1 < n && s[i + 1] == '1') i++;
+            if (++cnt > 1) return false;
         }
         return true;
     }
