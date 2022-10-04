3ad0b2e92c8baeb1dceedbbfba1ff635583cf0a4
liyigang <liyigang@bytedance.com>
2022-10-04 14:53:08 +0800

    feat: 🎸 add leetcode no.921

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,11 +5,11 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1784. 检查二进制字符串字段',
-  url: 'https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.字符串],
-  desc: `给你一个二进制字符串 s ，该字符串 不含前导零 。如果 s 包含 零个或一个由连续的 '1' 组成的字段 ，返回 true​​​ 。否则，返回 false 。如果 s 中 由连续若干个 '1' 组成的字段 数量不超过 1，返回 true​​​ 。否则，返回 false 。`,
+  name: '921. 使括号有效的最少添加',
+  url: 'https://leetcode.cn/problems/minimum-add-to-make-parentheses-valid/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.栈, Tag.贪心, Tag.字符串],
+  desc: `返回 为使结果字符串 s 有效而必须添加的最少括号数。`,
   solutions: [
     {
       script: Script.CPP,
@@ -18,14 +18,14 @@ const leetCodeMarkdown: Markdown = {
       desc: '遍历',
       code: `class Solution {
 public:
-    bool checkOnesSegment(string s) {
-        int cnt = 0, n = s.size();
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '0') continue;
-            while (i + 1 < n && s[i + 1] == '1') i++;
-            if (++cnt > 1) return false;
+    int minAddToMakeValid(string s) {
+        int cnt = 0, l = 0;
+        for (auto &c : s) {
+            if (c == '(') l++;
+            else if (l == 0) cnt++;
+            else l--;
         }
-        return true;
+        return cnt + l;
     }
 };`,
     },
