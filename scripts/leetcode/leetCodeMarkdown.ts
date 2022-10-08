6630b2396e89996a8259eea559054f81184cf245
liyigang <liyigang@bytedance.com>
2022-10-08 10:28:17 +0800

    feat: 🎸 add leetcode no.870

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,25 +5,30 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1800. 最大升序子数组和',
-  url: 'https://leetcode.cn/problems/maximum-ascending-subarray-sum/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组],
-  desc: `给你一个正整数组成的数组 nums ，返回 nums 中一个 升序 子数组的最大可能元素和。`,
+  name: '870. 优势洗牌',
+  url: 'https://leetcode.cn/problems/advantage-shuffle/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.贪心, Tag.数组, Tag.双指针, Tag.排序],
+  desc: `返回 nums1 的任意排列，使其相对于 nums2 的优势最大化。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 8.1,
-      desc: '遍历',
+      time: 164,
+      memory: 58.3,
+      desc: '排序下标数组后，从大往小判断',
       code: `class Solution {
 public:
-    int maxAscendingSum(vector<int>& nums) {
-        int ans = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = nums[i];
-            while (i + 1 < nums.size() && nums[i + 1] > nums[i]) sum += nums[++i];
-            ans = max(ans, sum);
+    vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<int> ans(n), inums1(n), inums2(n);
+        for (int i = 0; i < n; i++) inums1[i] = inums2[i] = i;
+        sort(inums1.begin(), inums1.end(), [&](int i1, int i2){ return nums1[i1] < nums1[i2]; });
+        sort(inums2.begin(), inums2.end(), [&](int i1, int i2){ return nums2[i1] < nums2[i2]; });
+        int e1 = n - 1, s1 = 0, i2 = n - 1;
+        while (e1 >= s1) {
+            if (nums1[inums1[e1]] > nums2[inums2[i2]]) ans[inums2[i2]] = nums1[inums1[e1--]];
+            else ans[inums2[i2]] = nums1[inums1[s1++]];
+            i2--;
         }
         return ans;
     }
