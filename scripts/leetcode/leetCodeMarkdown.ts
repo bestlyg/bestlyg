f2a6fbe8daa7a815d00092ddb989f8d8f506c5fe
liyigang <liyigang@bytedance.com>
2022-10-10 12:03:44 +0800

    feat: 🎸 add leetcode no.801

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,39 +4,36 @@ const { specStr, markdown } = utils;
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '856. 括号的分数',
-  url: 'https://leetcode.cn/problems/advantage-shuffle/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.贪心, Tag.数组, Tag.双指针, Tag.排序],
-  desc: `给定一个平衡括号字符串 S，按下述规则计算该字符串的分数`,
+  exist: !true,
+  name: '801. 使序列递增的最小交换次数',
+  url: 'https://leetcode.cn/problems/minimum-swaps-to-make-sequences-increasing/',
+  difficulty: Difficulty.困难,
+  tag: [Tag.数组, Tag.动态规划],
+  desc: `返回 使 nums1 和 nums2 严格递增 所需操作的最小次数 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 5.9,
-      desc: '栈',
+      time: 236,
+      memory: 117.2,
+      desc: 'dp[i][0|1]表示i为结尾下标时，交换和不交换的最小交换次数',
       code: `class Solution {
 public:
-    int scoreOfParentheses(string s) {
-        vector<int> st;
-        for (auto &c : s) {
-            if (c == '(') {
-                st.push_back(-1);
-            } else if (st.back() == -1) {
-                st.pop_back();
-                st.push_back(1);
-            } else {
-                int num = 0;
-                while (st.back() != -1) {
-                    num += st.back();
-                    st.pop_back();
-                }
-                st.pop_back();
-                st.push_back(num * 2);
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<vector<int>> dp(n, vector<int>(2, n));
+        dp[0][0] = 0;
+        dp[0][1] = 1; 
+        for (int i = 1; i < n; i++) {
+            if (nums1[i - 1] < nums1[i] && nums2[i - 1] < nums2[i]) {
+                dp[i][0] = min(dp[i][0], dp[i - 1][0]);
+                dp[i][1] = min(dp[i][1], dp[i - 1][1] + 1);
+            }
+            if (nums1[i - 1] < nums2[i] && nums2[i - 1] < nums1[i]) {
+                dp[i][0] = min(dp[i][0], dp[i - 1][1]);
+                dp[i][1] = min(dp[i][1], dp[i - 1][0] + 1);
             }
         }
-        return accumulate(st.begin(), st.end(), 0);
+        return min(dp[n - 1][0], dp[n - 1][1]);
     }
 };`,
     },
