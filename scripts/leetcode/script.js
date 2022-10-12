682472160cd3a40a8b29644d0a3ff93cf1677d9f
liyigang <liyigang@bytedance.com>
2022-10-12 15:45:16 +0800

    feat: 🎸 update dep

--- scripts/leetcode/script.js
+++ scripts/leetcode/script.js
@@ -51,3 +51,4 @@ async function sum(...data) {
   });
 }
 */
+
--- scripts/leetcode/script.ts
+++ scripts/leetcode/script.ts
@@ -1,48 +1,98 @@
-class NNode {
-  next = new Set<NNode>();
-  constructor(public v: number, public idx: number) {}
+enum PRCPromotionTemplate {
+  TEMPLATE_DIRECT_DISCOUNT = 10000,
+  TEMPLATE_FREE_SHIPPING = 20000,
+  TEMPLATE_FREE_SHIPPING_PLATFORM = 20010,
+  TEMPLATE_FREE_SHIPPING_HIGH_COMMISSION = 20020,
+  TEMPLATE_FIXED_PRICE = 30000,
+  TEMPLATE_PRODUCT_DIRECT_SUBSIDY = 40000,
+  TEMPLATE_FLASH_SALE = 50000,
+  TEMPLATE_NEW_USER_PRICE = 60000,
+  TEMPLATE_CASHBACK_COUPON = 70000,
+  TEMPLATE_CAMPAIGN_PRICE = 80000,
+  TEMPLATE_BUY_MORE_SAVE_MORE_PLATFORM = 90000,
+  TEMPLATE_VOUCHER_PLATFROM_NEW_USER = 1010000,
+  TEMPLATE_VOUCHER_SELLER_SKU_PRICE = 1020000,
+  TEMPLATE_VOUCHER_PLATFORM_FREE_SHIPPING = 1030000,
 }
-function numberOfGoodPaths(vals: number[], edges: number[][]): number {
-  const n = vals.length;
-  const map: Record<number, number> = {};
-  for (const v of vals) map[v] = (map[v] ?? 0) + 1;
-  const nodes = new Array(n).fill(0).map((_, i) => new NNode(vals[i], i));
-  for (const [a, b] of edges) {
-    const node1 = nodes[a];
-    const node2 = nodes[b];
-    node1.next.add(node2);
-    node2.next.add(node1);
-  }
-  const linkSet = new Set<NNode>();
-  const set = new Set<string>();
-  const cntList = new Array(n).fill(0);
-  for (const node of nodes) {
-    if (map[node.v] === 1) continue;
-    linkSet.add(node);
-    for (const next of node.next) {
-      if (next.v <= node.v) {
-        dfs(next, [node]);
-      }
-    }
-    linkSet.delete(node);
-  }
-  return set.size / 2 + n;
-  function dfs(node: NNode, list: NNode[]) {
-    if (node.v === list[0].v) {
-      const format1 = list[0].idx + ',' + node.idx;
-      const format2 = node.idx + ',' + list[0].idx;
-      set.add(format1);
-      set.add(format2);
-      cntList[list[0].idx]++;
-      cntList[node.idx]++;
-    }
-    for (const item of node.next) {
-      if (item.v > list[0].v || linkSet.has(item) || cntList[item.idx] === 2) continue;
-      linkSet.add(item);
-      list.push(item);
-      dfs(item, list);
-      list.pop();
-      linkSet.delete(item);
-    }
-  }
-}
+const promotionTemplateOptions = [
+  {
+    // 10000
+    label: 'Direct Discount',
+    value: PRCPromotionTemplate.TEMPLATE_DIRECT_DISCOUNT,
+  },
+  {
+    // 20000
+    label: 'Free Shipping',
+    value: PRCPromotionTemplate.TEMPLATE_FREE_SHIPPING,
+  },
+  {
+    // 20010
+    label: 'Platform Free Shipping',
+    value: PRCPromotionTemplate.TEMPLATE_FREE_SHIPPING_PLATFORM,
+  },
+  {
+    // 20020
+    label: 'Free Shipping High Commission',
+    value: PRCPromotionTemplate.TEMPLATE_FREE_SHIPPING_HIGH_COMMISSION,
+  },
+  {
+    // 30000
+    label: 'Fixed Price',
+    value: PRCPromotionTemplate.TEMPLATE_FIXED_PRICE,
+  },
+  {
+    // 40000
+    label: 'Platform Direct Subsidy Product',
+    value: PRCPromotionTemplate.TEMPLATE_PRODUCT_DIRECT_SUBSIDY,
+  },
+  {
+    // 50000
+    label: 'Flash Deal',
+    value: PRCPromotionTemplate.TEMPLATE_FLASH_SALE,
+  },
+  {
+    // 60000
+    label: 'New User Price',
+    value: PRCPromotionTemplate.TEMPLATE_NEW_USER_PRICE,
+  },
+  {
+    // 70000
+    label: 'Cashback Coupon',
+    value: PRCPromotionTemplate.TEMPLATE_CASHBACK_COUPON,
+  },
+  {
+    // 80000
+    label: 'Campaign Price',
+    value: PRCPromotionTemplate.TEMPLATE_CAMPAIGN_PRICE,
+  },
+  {
+    // 90000
+    label: 'Buy More Save More',
+    value: PRCPromotionTemplate.TEMPLATE_BUY_MORE_SAVE_MORE_PLATFORM,
+  },
+  {
+    // 1010000
+    label: 'Voucher Platform New User',
+    value: PRCPromotionTemplate.TEMPLATE_VOUCHER_PLATFROM_NEW_USER,
+  },
+  {
+    // 1020000
+    label: 'Seller Coupon',
+    value: PRCPromotionTemplate.TEMPLATE_VOUCHER_SELLER_SKU_PRICE,
+  },
+  {
+    // 1030000
+    label: 'Platform Shipping Fee Voucher',
+    value: PRCPromotionTemplate.TEMPLATE_VOUCHER_PLATFORM_FREE_SHIPPING,
+  },
+];
+const promotion = new Array(promotionTemplateOptions.length).fill(0).map((_, i) => ({
+  id: '704108410973254631' + i,
+  title: promotionTemplateOptions[i].label,
+  type: promotionTemplateOptions[i].value,
+  promotion_sponsor: 2,
+  begin_time: 1007608888,
+  end_time: -2025918408,
+  promotion_time_status: 1,
+}));
+console.log(JSON.stringify(promotion));
