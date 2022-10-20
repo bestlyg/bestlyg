e6cf148f5af3e14fd76a0f7d08fa6574391becc6
liyigang <liyigang@bytedance.com>
2022-10-20 14:26:05 +0800

    feat: 🎸 add leetcode no.779

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,46 +5,26 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1700. 无法吃午餐的学生数量',
-  url: 'https://leetcode.cn/problems/number-of-students-unable-to-eat-lunch/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.栈, Tag.队列, Tag.数组, Tag.模拟],
-  desc: `请你返回无法吃午餐的学生数量。`,
+  name: '779. 第K个语法符号',
+  url: 'https://leetcode.cn/problems/k-th-symbol-in-grammar/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.位运算, Tag.递归, Tag.数学],
+  desc: `给定行数 n 和序数 k，返回第 n 行中第 k 个字符。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 8,
-      desc: '模拟',
-      code: `class Solution {
-public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int n = sandwiches.size(), list[2] = {0};
-        queue<int> q1, q2;
-        for (auto &s : students) q1.push(s), list[s]++;;
-        for (auto &s : sandwiches) q2.push(s);
-        while (q2.size()) {
-            int size = q2.size();
-            while (q1.size() && q1.front() == q2.front()) list[q1.front()]--, q1.pop(), q2.pop();
-            if (q2.size()) q1.push(q1.front()), q1.pop();
-            if (q2.size() && list[q2.front()] == 0) break;
-        }
-        return q1.size();
-    }
-};`,
-    },
-    {
-      script: Script.CPP,
       time: 4,
-      memory: 8.4,
-      desc: '模拟',
+      memory:5.9,
+      desc: '当前层前半段和上层一样，第三段和第二段相同，第一段和第四段相同',
       code: `class Solution {
 public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int list[2] = {0};
-        for (auto &s : students) list[s]++;
-        for (int i = 0; i < sandwiches.size() && list[sandwiches[i]]; i++) list[sandwiches[i]]--;
-        return list[0] + list[1];
+    int kthGrammar(int n, int k) {
+        if (n == 1) return 0;
+        else if (n == 2) return k == 1 ? 0 : 1;
+        int nmax = pow(2, n - 1), nextK = k;
+        if (k > nmax / 2 && k <= nmax / 4 * 3) nextK = k - nmax / 2 + nmax / 4;
+        else if (k > nmax / 4 * 3) nextK = k - nmax / 2 - nmax / 4;
+        return kthGrammar(n - 1, nextK);
     }
 };`,
     },
--- scripts/leetcode/script.ts
+++ scripts/leetcode/script.ts
@@ -1,33 +1,13 @@
-function countSubarrays(nums: number[], minK: number, maxK: number): number {
-  const n = nums.length;
-  const emptyList: number[] = [];
-  for (let i = 0; i < n; i++) {
-    if (nums[i] < minK || nums[i] > maxK) emptyList.push(i);
+const list: number[][] = [[0]];
+const n = 7;
+for (let i = 1; i <= n; i++) {
+  const cur: number[] = [];
+  for (const num of list[i - 1]) {
+    if (num == 0) cur.push(0, 1);
+    else cur.push(1, 0);
   }
-  if (emptyList.length === 0) return check(0, n - 1);
-  let ans = check(0, emptyList[0] - 1) + check(emptyList[emptyList.length - 1] + 1, n - 1);
-  for (let i = 1; i < emptyList.length; i++) ans += check(emptyList[i - 1] + 1, emptyList[i] - 1);
-  return ans;
-  function check(l: number, r: number): number {
-    if (l > r) return 0;
-    const minList :number[]=[]
-    const maxList :number[]=[]
-    for (let i = l;i <= r;i++) {
-        if (minK === nums[i]) minList++;
-    }
-    return ans;
-  }
+  list.push(cur);
 }
-
-console.log(
-  countSubarrays(
-    [
-      35054, 398719, 945315, 945315, 820417, 945315, 35054, 945315, 171832, 945315, 35054, 109750,
-      790964, 441974, 552913,
-    ],
-    35054,
-    945315
-  )
-);
-console.log(countSubarrays([1, 3, 5, 2, 7, 5], 1, 5));
-console.log(countSubarrays([1, 1, 1, 1], 1, 1));
+for (let i = 0; i < list.length; i++) {
+  console.log(`line = ${i + 1}, data = ${list[i].join('')}`);
+}
