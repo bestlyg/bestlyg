5828e88ebd3d75dc8c15c114ddf2e66d5f42fc3b
liyigang <liyigang@bytedance.com>
2022-10-24 10:56:01 +0800

    feat: 🎸 add leetcode no.915

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,37 +5,54 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '6224. 最大公因数等于 K 的子数组数目',
-  url: 'https://leetcode.cn/problems/number-of-subarrays-with-gcd-equal-to-k/',
+  name: '915. 分割数组',
+  url: 'https://leetcode.cn/problems/partition-array-into-disjoint-intervals/',
   difficulty: Difficulty.中等,
-  tag: [],
-  desc: `数组的最大公因数 是能整除数组中所有元素的最大整数。`,
+  tag: [Tag.数组],
+  desc: `在完成这样的分组后返回 left 的 长度 。`,
   solutions: [
     {
-      script: Script.TS,
-      time: 88,
-      memory: 42.4,
-      desc: 'gcd后暴力',
-      code: `function gcd(a: number, b: number): number {
-        if (a < b) return gcd(b, a);
-        if (b) return gcd(b, a % b);
-        return a;
-      }
-      function subarrayGCD(nums: number[], k: number): number {
-        let ans = 0;
-        const n = nums.length;
-        for (let i = 0; i < n; i++) {
-          if (nums[i] === k) {
-            ans++;
-          }
-          let num = nums[i];
-          for (let j = i - 1; j >= 0; j--) {
-            num = gcd(nums[j], num);
-            if (num === k) ans++;
-          }
+      script: Script.CPP,
+      time: 732,
+      memory: 136.2,
+      desc: '维护有序序列',
+      code: `class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int> m1, m2;
+        m1[nums[0]]++;
+        for (int i = 1; i < n; i++) m2[nums[i]]++;
+        int idx = 1;
+        while (m1.crbegin()->first > m2.cbegin()->first) {
+            m1[nums[idx]]++;
+            m2[nums[idx]]--;
+            if (m2[nums[idx]] == 0) m2.erase(nums[idx]);
+            ++idx;
+        }
+        return idx;
+    }
+};`,
+    },
+    {
+      script: Script.CPP,
+      time: 140,
+      memory: 91.9,
+      desc: '一次遍历，维护左边的最大值',
+      code: `class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size(), cur_max = nums[0], ans = 1, ans_max = nums[0];
+        for (int i = 1; i < n; i++) {
+            cur_max = max(cur_max, nums[i]);
+            if (nums[i] < ans_max) {
+                ans_max = cur_max;
+                ans = i + 1;
+            }
         }
         return ans;
-      }`,
+    }
+};`,
     },
   ],
 };
