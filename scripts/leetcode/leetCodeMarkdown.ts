24a18934d2f6ee5b09a876c41764679e09baeb25
liyigang <liyigang@bytedance.com>
2022-10-27 10:14:55 +0800

    feat: 🎸 add leetcode no.1822

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,34 +4,27 @@ const { specStr, markdown } = utils;
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '862. 和至少为 K 的最短子数组',
-  url: 'https://leetcode.cn/problems/shortest-subarray-with-sum-at-least-k/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.深度优先搜索, Tag.广度优先搜索, Tag.数组, Tag.矩阵],
-  desc: `返回必须翻转的 0 的最小数目。`,
+  exist: !true,
+  name: '1822. 数组元素积的符号',
+  url: 'https://leetcode.cn/problems/sign-of-the-product-of-an-array/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.数组, Tag.数学],
+  desc: `给你一个整数数组 nums 。令 product 为数组 nums 中所有元素值的乘积。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 204,
-      memory: 102.3,
-      desc: '前缀和+单调递增队列，遍历到一个值时，可以快速知道前面的前缀和',
+      time: 4,
+      memory: 9.9,
+      desc: '遍历检查有几个负数',
       code: `class Solution {
 public:
-    int shortestSubarray(vector<int>& nums, int k) {
-        int n = nums.size(), ans = 0x7fffffff;
-        vector<long long> sums(1 + n, 0);
-        for (int i = 0; i < n; i++) sums[i + 1] = sums[i] + nums[i];
-        deque<int> q;
-        q.push_back(0);
-        for (int i = 0; i < n; i++) {
-            int idx = -1;
-            while (q.size() && sums[i + 1] - sums[q.front()] >= k) idx = q.front(), q.pop_front();
-            if (idx != -1) ans = min(ans, i + 1 - idx);
-            while (q.size() && sums[q.back()] > sums[i + 1]) q.pop_back();
-            q.push_back(i + 1);
+    int arraySign(vector<int>& nums) {
+        int cnt = 0;
+        for (auto &num : nums) {
+            if (num == 0) return 0;
+            else if (num < 0) cnt ^= 1;
         }
-        return ans == 0x7fffffff ? -1 : ans;
+        return cnt ? -1 : 1;
     }
 };`,
     },
