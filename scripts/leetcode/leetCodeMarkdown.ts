53878fc27c11ef389fbe1132db97f872dac7ff37
bestlyg <1057966749@qq.com>
2022-10-30 10:26:02 +0800

    feat: 🎸 add leetcode no.784

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,25 +5,33 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1773. 统计匹配检索规则的物品数量',
-  url: 'https://leetcode.cn/problems/count-items-matching-a-rule/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组, Tag.字符串],
-  desc: `统计并返回 匹配检索规则的物品数量 。`,
+  name: '784. 字母大小写全排列',
+  url: 'https://leetcode.cn/problems/letter-case-permutation/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.位运算, Tag.字符串, Tag.回溯],
+  desc: `返回 所有可能得到的字符串集合 。以 任意顺序 返回输出。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 68,
-      memory: 30.2,
+      time: 4,
+      memory: 9.8,
       desc: '模拟',
       code: `class Solution {
 public:
-    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        return accumulate(items.begin(), items.end(), 0, [&](int sum, vector<string> &s){
-            int idx = ruleKey == "type" ? 0 : ruleKey == "color" ? 1 : 2;
-            if (s[idx] == ruleValue) sum += 1;
-            return sum;
-        });
+    vector<string> letterCasePermutation(string s) {
+        vector<string> list;
+        dfs(list, s, 0, "");
+        return list;
+    }
+    void dfs(vector<string> &list, string &s, int idx, string &&cur) {
+        if (idx == s.size()) {
+            list.push_back(cur);
+            return;
+        }
+        dfs(list, s, idx + 1, cur + s[idx]);
+        if (s[idx] >= 'A' && s[idx] <= 'Z' || s[idx] >= 'a' && s[idx] <= 'z') {
+            dfs(list, s, idx + 1, cur + static_cast<char>(s[idx] ^ 0b100000));
+        }
     }
 };`,
     },
