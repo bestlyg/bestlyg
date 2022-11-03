40d1583815c76aa692e3d2dae9d72a0a8589371d
liyigang <liyigang@bytedance.com>
2022-11-03 20:21:35 +0800

    feat: 🎸 update haizei linux-program

--- haizeix/linux-program/ls.c
+++ haizeix/linux-program/ls.c
@@ -1,14 +1,34 @@
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "unistd.h"
 #include "dirent.h"
 #include "sys/types.h"
 #include "string.h"
+#include "sys/ioctl.h"
+#include "math.h"
 
 #define FILEMAX 1024
 #define NAMEMAX 256
 
 int flag_a = 0, flag_l = 0;
+
+void show_files(char names[][FILEMAX], int cnt, int row, int col) {
+    int wide_file[cnt];
+    memset(wide_file, 0, sizeof(int) * cnt);
+    for (int i = 0; i < col; i++) {
+        for (int j = i * row; j < (i + 1) * row && j < cnt; j++) {
+            if (wide_file[i] < strlen(names[j])) wide_file[i] = strlen(names[j]);
+        }
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = i; j < i + (row * col) && j < cnt; j += row) {
+            int tmp = j / row;
+            printf("%-*s", wide_file[tmp] + 1, names[j]);
+        }
+        printf("\n");
+    }
+}
 
 void do_stat(const char *filename) {
     printf("Doing with stat %s.\n", filename);
@@ -17,6 +37,60 @@ void do_stat(const char *filename) {
 int cmp_name(const void *a, const void *b) {
     // printf("cmp : %s & %s = %d\n", (const char *)a, (const char *)b, strcmp((const char*)a, (const char *)b));
     return strcmp((const char*)a, (const char *)b);
+}
+
+void size_window(char names[][FILEMAX], int cnt, int *row, int *col) {
+    struct winsize size;
+    int len[cnt], max = 0, total = 0;
+    memset(len, 0, sizeof(int) * cnt);
+    if (isatty(STDOUT_FILENO) == 0) {
+        exit(1);
+    }
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &size) < 0) {
+        perror("ioctl");
+        exit(1);
+    }
+    printf("winsize row = %d, col = %d\n", size.ws_row, size.ws_col);
+    for (int i = 0; i < cnt; i++) {
+        len[i] = strlen(names[i]);
+        if (max < len[i]) max = len[i];
+        total += len[i] + 1;
+    }
+    if (max + 1 >= size.ws_col) {
+        *row = cnt;
+        *col = 1;
+        return;
+    }
+    if (total <= size.ws_col) {
+        *row = 1;
+        *col = cnt;
+        return;
+    }
+    int try_begin = 0;
+    for (int i = 0, tmp = 0; i < cnt; i++) {
+        tmp += len[i] + 1;
+        if (tmp >= size.ws_col) {
+            try_begin = i;
+            break;
+        }
+    }
+    for (int i = try_begin; ; i--) {
+        int *wide = (int *)malloc(sizeof(int) * i);
+        memset(wide, 0, sizeof(int) * i);
+        *row = (int)ceil(1.0 * cnt / i);
+        int try_sum = 0;
+        for (int x = 0; x < i; x++) {
+            for (int y = x * *row; y < (x + 1) * *row && y < cnt; y++) {
+                if (wide[x] < len[y]) wide[x] = len[y];
+            }
+            try_sum += wide[x] + 1;
+        }
+        if (try_sum > size.ws_col) continue;
+        if (try_sum <= size.ws_col) {
+            *col = i;
+            break;
+        }
+    }
 }
 
 void do_ls(const char *dirname) {
@@ -54,7 +128,11 @@ void do_ls(const char *dirname) {
                 do_stat(names[i]);
             }
         } else {
-
+            printf("Print all file.\n");
+            int row, col;
+            size_window(names, cnt, &row, &col);
+            printf("row = %d, col = %d\n", row, col);
+            show_files(names, cnt, row, col);
         }
     }
     printf("Doing with dir %s.\n", dirname);
