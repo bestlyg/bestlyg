28302c5081c6f85e275da1ecf4c13155b7f0f967
bestlyg <1057966749@qq.com>
2022-11-13 13:00:20 +0800

    feat: 🎸 update c++ contest header

---
+++ haizeix/contest.h
@@ -0,0 +1,9 @@
+# include <bits/stdc++.h>
+# define X first
+# define Y second
+# define lb(x) ((x) & (-x))
+# define mem(a,b) memset(a,b,sizeof(a))
+# define debug freopen("r.txt","r",stdin)
+# define pi pair<int,int>
+using namespace std;
+typedef long long ll;
