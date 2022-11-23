a8102bc0179ba624317a27054e57b38908af4ea3
liyigang <liyigang@bytedance.com>
2022-11-23 10:13:27 +0800

    feat: 🎸 add leetcode no.1742

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,40 +4,31 @@ const { specStr, markdown } = utils;
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '878. 第 N 个神奇数字',
-  url: 'https://leetcode.cn/problems/soup-servings/',
-  difficulty: Difficulty.中等,
-  tag: [Tag.数学, Tag.动态规划, Tag.概率与统计],
-  desc: `有 A 和 B 两种类型 的汤。当两种类型的汤都分配完时，停止操作。`,
+  exist: !true,
+  name: '1742. 盒子中小球的最大数量',
+  url: 'https://leetcode.cn/problems/maximum-number-of-balls-in-a-box/',
+  difficulty: Difficulty.简单,
+  tag: [Tag.哈希表, Tag.数学, Tag.计数],
+  desc: `给你两个整数 lowLimit 和 highLimit ，返回放有最多小球的盒子中的小球数量。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 4,
-      memory: 5.7,
-      desc: '二分，判断当前数是第几个数 = n / a + n / b - n / lcm',
+      time: 8,
+      memory: 5.8,
+      desc: '模拟',
       code: `class Solution {
-public:
-    const int mod = 1e9 + 7;
-    int gcd(int a, int b) {
-        if (b > a) return gcd(b, a);
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
-    int lcm(int a, int b) {
-        return a * b / gcd(a, b);
-    }
-    int nthMagicalNumber(int n, int a, int b) {
-        long long l = min(a, b), r = n * l, m, nlcm = lcm(a, b);
-        while (l < r) {
-            m = (l + r) >> 1;
-            int num = m / a + m / b - m / nlcm;
-            if (num >= n) r = m;
-            else l = m + 1;
-        }
-        return l % mod;
-    }
-};`,
+ public:
+     int countBalls(int lowLimit, int highLimit) {
+         int ans = 0, list[46] = {0};
+         for (int num = lowLimit; num <= highLimit; num++) ans = max(ans, ++list[t(num)]);
+         return ans;
+     }
+     int t(int num) {
+         int ans = 0;
+         for (; num; num /= 10) ans += num % 10;
+         return ans;
+     }
+ };`,
     },
   ],
 };
