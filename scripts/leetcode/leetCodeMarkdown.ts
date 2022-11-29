bd750fae4facf9d78ea0939a33dbbca6189fed94
liyigang <liyigang@bytedance.com>
2022-11-29 10:08:19 +0800

    feat: 🎸 add leetcode no.1758

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,40 +5,43 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '813. 最大平均值和的分组',
-  url: 'https://leetcode.cn/problems/largest-sum-of-averages',
-  difficulty: Difficulty.中等,
-  tag: [],
-  desc: `给定数组 nums 和一个整数 k 。我们将给定的数组 nums 分成 最多 k 个相邻的非空子数组 。 分数 由每个子数组内的平均值的总和构成。`,
+  name: '1758. 生成交替二进制字符串的最少操作数',
+  url: 'https://leetcode.cn/problems/minimum-changes-to-make-alternating-binary-string',
+  difficulty: Difficulty.简单,
+  tag: [Tag.字符串],
+  desc: `给你一个仅由字符 '0' 和 '1' 组成的字符串 s 。一步操作中，你可以将任一 '0' 变成 '1' ，或者将 '1' 变成 '0' 。返回使 s 变成 交替字符串 所需的 最少 操作数。`,
   solutions: [
     {
       script: Script.CPP,
       time: 12,
       memory: 7.7,
-      desc: 'dp[i][j] = 最多分成i组，只用到前j个字符串的最大平均值',
+      desc: '遍历',
       code: `class Solution {
 public:
-    double largestSumOfAverages(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<vector<double>> dp(k + 1, vector<double>(n + 1, 0));
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            dp[1][i + 1] = 1.0 * sum / (i + 1);
+    int minOperations(string s) {
+        int cnt1 = 0, cnt2 = 0, cur = 1;
+        for (auto &c : s) {
+            if (c - '0' == cur) cnt1++;
+            else cnt2++;
+            cur ^= 1;
         }
-        double ans = dp[1][n];
-        for (int knum = 2; knum <= k; knum++) {
-            for (int i = knum; i <= n; i++) {
-                int sum = 0, cnt = 0;
-                for (int j = i; j >= knum; j--) {
-                    sum += nums[j - 1];
-                    cnt += 1;
-                    dp[knum][i] = max(dp[knum][i], dp[knum - 1][j - 1] + 1.0 * sum / cnt);
-                }
-            }
-            ans = max(ans, dp[knum][n]);
+        return min(cnt1, cnt2);
+    }
+};`,
+    },
+    {
+      script: Script.CPP,
+      time: 0,
+      memory: 6.8,
+      desc: '遍历',
+      code: `class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.size(), cnt = 0, cur = 1;
+        for (int i = 0; i < n; i++, cur^=1) {
+            if (s[i] - '0' == cur) cnt++;
         }
-        return ans;
+        return min(cnt, n - cnt);
     }
 };`,
     },
