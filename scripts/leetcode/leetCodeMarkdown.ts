6183980911c59006fcb40de4c80f9ded1581ff6a
liyigang <liyigang@bytedance.com>
2022-12-02 00:51:18 +0800

    feat: 🎸 add leetcode no.1769

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,29 +5,40 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1779. 找到最近的有相同 X 或 Y 坐标的点',
-  url: 'https://leetcode.cn/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数组],
-  desc: `请返回距离你当前位置 曼哈顿距离 最近的 有效 点的下标（下标从 0 开始）。`,
+  name: '1769. 移动所有球到每个盒子所需的最小操作数',
+  url: 'https://leetcode.cn/problems/minimum-number-of-operations-to-move-all-balls-to-each-box',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数组, Tag.字符串],
+  desc: `返回一个长度为 n 的数组 answer ，其中 answer[i] 是将所有小球移动到第 i 个盒子所需的 最小 操作数。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 132,
-      memory: 57.8,
-      desc: '枚举所有点',
+      time: 76,
+      memory: 8.6,
+      desc: '先统计右侧所有的点和数量，每次移动时快速计算左侧1数量和右侧1数量',
       code: `class Solution {
-public:
-    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
-        int ans = -1, dans = 0x3f3f3f3f;
-        for (int i = 0; i < points.size(); i++) {
-            int ix = points[i][0], iy = points[i][1], d = abs(ix - x) + abs(iy - y);
-            if (ix != x && iy != y) continue;
-            if (dans > d) ans = i, dans = d;
-        }
-        return ans;
-    }
-};`,
+        public:
+            vector<int> minOperations(string boxes) {
+                int n = boxes.size(), 
+                    lsum = 0, lcnt = 0, 
+                    rsum = 0, rcnt = 0;
+                vector<int> list(n);
+                for (int i = 0; i < n; i++) {
+                    if (boxes[i] == '1') {
+                        rsum += i;
+                        rcnt += 1;
+                    }
+                }
+                list[0] = rsum;
+                for (int i = 1; i < n; i++) {
+                    if (boxes[i - 1] == '1') rcnt--, lcnt++;
+                    rsum -= rcnt;
+                    lsum += lcnt;
+                    list[i] = lsum + rsum;
+                }
+                return list;
+            }
+        };`,
     },
   ],
 };
