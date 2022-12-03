eb59ac50bb1c8d7eb766b54b6c3f90151a24c605
liyigang <liyigang@bytedance.com>
2022-12-03 15:03:25 +0800

    feat: 🎸 add leetcode no.1796

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,40 +5,33 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1769. 移动所有球到每个盒子所需的最小操作数',
-  url: 'https://leetcode.cn/problems/minimum-number-of-operations-to-move-all-balls-to-each-box',
-  difficulty: Difficulty.中等,
-  tag: [Tag.数组, Tag.字符串],
-  desc: `返回一个长度为 n 的数组 answer ，其中 answer[i] 是将所有小球移动到第 i 个盒子所需的 最小 操作数。`,
+  name: '1796. 字符串中第二大的数字',
+  url: 'https://leetcode.cn/problems/second-largest-digit-in-a-string',
+  difficulty: Difficulty.简单,
+  tag: [Tag.哈希表, Tag.字符串],
+  desc: `给你一个混合字符串 s ，请你返回 s 中 第二大 的数字，如果不存在第二大的数字，请你返回 -1 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 76,
-      memory: 8.6,
-      desc: '先统计右侧所有的点和数量，每次移动时快速计算左侧1数量和右侧1数量',
+      time: 0,
+      memory: 6.5,
+      desc: '遍历',
       code: `class Solution {
-        public:
-            vector<int> minOperations(string boxes) {
-                int n = boxes.size(), 
-                    lsum = 0, lcnt = 0, 
-                    rsum = 0, rcnt = 0;
-                vector<int> list(n);
-                for (int i = 0; i < n; i++) {
-                    if (boxes[i] == '1') {
-                        rsum += i;
-                        rcnt += 1;
-                    }
-                }
-                list[0] = rsum;
-                for (int i = 1; i < n; i++) {
-                    if (boxes[i - 1] == '1') rcnt--, lcnt++;
-                    rsum -= rcnt;
-                    lsum += lcnt;
-                    list[i] = lsum + rsum;
-                }
-                return list;
-            }
-        };`,
+public:
+    int secondHighest(string s) {
+        int n1 = -1, n2 = -1;
+        for (auto &c : s) {
+            if (!isdigit(c)) continue;
+            int num = c - '0';
+            if (n1 == -1) n1 = num;
+            else if (num > n1) n2 = n1, n1 = num;
+            else if (num == n1) continue;
+            else if (n2 == -1) n2 = num;
+            else if (num > n2) n2 = num;
+        }
+        return n2;
+    }
+};`,
     },
   ],
 };
