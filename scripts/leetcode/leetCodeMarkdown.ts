efef56cda724d7698cdb3b7989075a51d38a3ce1
liyigang <liyigang@bytedance.com>
2022-12-09 10:19:29 +0800

    feat: 🎸 add leetcode no.1780

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,33 +5,40 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1812. 判断国际象棋棋盘中一个格子的颜色',
-  url: 'https://leetcode.cn/problems/determine-color-of-a-chessboard-square/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.数学, Tag.字符串],
-  desc: `如果所给格子的颜色是白色，请你返回 true，如果是黑色，请返回 false 。`,
+  name: '1780. 判断一个数字是否可以表示成三的幂的和',
+  url: 'https://leetcode.cn/problems/check-if-number-is-a-sum-of-powers-of-three/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.数学],
+  desc: `给你一个整数 n ，如果你可以将 n 表示成若干个不同的三的幂之和，请你返回 true ，否则请返回 false 。`,
   solutions: [
     {
       script: Script.CPP,
       time: 0,
-      memory: 5.8,
-      desc: '判断行列',
+      memory: 5.7,
+      desc: '如果是由3的幂组成，那一定是可以模3等于0或者减一模3等于0',
       code: `class Solution {
 public:
-    bool squareIsWhite(string coordinates) {
-        return (coordinates[1] - '0' - 1 ^ ((coordinates[0] - 'a') & 1)) & 1;
+    bool checkPowersOfThree(int n) {
+        if (n % 3 != 0) n--;
+        for (; n && n % 3 == 0; n--) 
+            while (n % 3 == 0) n /= 3;
+        return !n;
     }
 };`,
     },
     {
       script: Script.CPP,
       time: 0,
-      memory: 5.8,
-      desc: '判断行列',
+      memory: 5.7,
+      desc: '优化后为模3为0或1',
       code: `class Solution {
 public:
-    bool squareIsWhite(string coordinates) {
-        return ((coordinates[0] - 'a') & 1) == ((coordinates[1] - '0') & 1);
+    bool checkPowersOfThree(int n) {
+        while (n) {
+            if (n % 3 == 2) return false;
+            n /= 3;
+        }
+        return true;
     }
 };`,
     },
