ebf4f87cf6fe8900ccb7892377fd712bfb3a1bc5
bestlyg <1057966749@qq.com>
2022-12-11 16:53:32 +0800

    feat: 🎸 update contest

--- static/contest.h
+++ static/contest.h
@@ -49,14 +49,13 @@
 #include <valarray>
 #include <vector>
 
-
 // bestlyg
 # define X first
 # define Y second
 # define lb(x) ((x) & (-x))
 # define mem(a,b) memset(a,b,sizeof(a))
-# define debug freopen("r.txt","r",stdin)
-# define pi pair<int,int>
+# define debug freopen("input","r",stdin)
+# define PII pair<int,int>
 
 #ifdef DEBUG
 #define log(frm, args...) {\
@@ -67,9 +66,44 @@
 #endif
 
 typedef long long ll;
-
 using namespace std;
 
+class UnionFind {
+public:
+    int n;
+    vector<int> data, cnt;
+    UnionFind(int n): n(n), data(vector<int>(n, 0)), cnt(vector<int>(n, 1)) {
+        for (int i = 0; i < n; i++) data[i] = i;
+    } 
+    int size(int v) {
+        return cnt[find(v)];
+    }
+    int find(int v) {
+        if (data[v] == v) return v;
+        return data[v] = find(data[v]);
+    }
+    void uni(int v1, int v2) {
+        int p1 = find(v1), p2 = find(v2);
+        if (p1 != p2) {
+            cnt[p1] += cnt[p2];
+            data[p2] = p1;
+        }
+    }
+};
+int pos2Idx(int x, int y, int size) { 
+    return x * size + y; 
+}
+void idx2Pos(int idx, int size, int &x, int &y) {
+    x = idx / size;
+    y = idx % size;
+}
+vector<vector<int>> dirs = {
+    {0, 1}, {0, -1},
+    {1, 0}, {-1, 0}
+};
+// START
+
+// END
 int main() {
     return 0;
 }
