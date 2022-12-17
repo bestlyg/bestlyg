a3336290d30cadd111978707e79961951ddb50d8
bestlyg <1057966749@qq.com>
2022-12-17 16:38:24 +0800

    feat: 🎸 add leetcode no.1764

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,35 +5,35 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1785. 构成特定和需要添加的最少元素',
-  url: 'https://leetcode.cn/problems/minimum-elements-to-add-to-form-a-given-sum/',
+  name: '1764. 通过连接另一个数组的子数组得到一个数组',
+  url: 'https://leetcode.cn/problems/form-array-by-concatenating-subarrays-of-another-array/',
   difficulty: Difficulty.中等,
-  tag: [Tag.贪心, Tag.数组],
-  desc: `返回使数组元素总和等于 goal 所需要向数组中添加的 最少元素数量 ，添加元素 不应改变 数组中 abs(nums[i]) <= limit 这一属性。`,
+  tag: [Tag.贪心, Tag.数组, Tag.字符串匹配],
+  desc: `给你一个长度为 n 的二维整数数组 groups ，同时给你一个整数数组 nums 。如果你可以找出这样的 n 个子数组，请你返回 true ，否则返回 false 。`,
   solutions: [
     {
       script: Script.CPP,
       time: 100,
       memory: 71.7,
-      desc: '向上取整',
+      desc: '遍历',
       code: `class Solution {
 public:
-    int minElements(vector<int>& nums, int limit, int goal) {
-        long long v = goal;
-        for (auto &num : nums) v -= num;
-        v = abs(v);
-        return ceil(1.0 * v / limit);
+    bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+        auto check = [&](int igroup, int inum) -> bool {
+            int i = 0;
+            for (; i < groups[igroup].size() && inum < nums.size(); i++, inum++)
+                if (groups[igroup][i] != nums[inum]) return false;
+            return i == groups[igroup].size();
+        };
+        int j = 0;
+        for (int i = 0; i < groups.size(); i++) {
+            while (j < nums.size() && (nums[j] != groups[i][0] || !check(i, j))) j++;
+            if (j == nums.size()) return false;
+            j += groups[i].size();
+        }
+        return true;
     }
 };`,
-    },
-    {
-      script: Script.TS,
-      time: 92,
-      memory: 50.5,
-      desc: '向上取整',
-      code: `function minElements(nums: number[], limit: number, goal: number): number {
-        return Math.ceil(Math.abs(nums.reduce((sum, num) => sum - num, goal)) / limit);
-    };`,
     },
   ],
 };
