b0f39f1d6a39ddd2460a3a60104363d2ad368d5e
liyigang <liyigang@bytedance.com>
2022-12-25 16:01:56 +0800

    feat: 🎸 update leetcode

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,27 +5,34 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1739. 放置盒子',
-  url: '  desc: `https://leetcode.cn/problems/building-boxes/',
+  name: '6272. 好分区的数目',
+  url: 'https://leetcode.cn/problems/number-of-great-partitions/',
   difficulty: Difficulty.困难,
-  tag: [Tag.贪心, Tag.数学, Tag.二分查找],
-  desc: `给你一个整数 n ，返回接触地面的盒子的 最少 可能数量。`,
+  tag: [],
+  desc: `返回 不同 的好分区的数目。由于答案可能很大，请返回对 109 + 7 取余 后的结果。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 4,
-      memory: 5.9,
-      desc: '找规律',
+      time: 112,
+      memory: 36.7,
+      desc: '逆向统计，统计出有多少组是少于k的，res  = sum - 2 * val',
       code: `class Solution {
 public:
-    int minimumBoxes(int n) {
-        int sum = 0, cur = 1, ans = 0, prev = 0;
-        while (sum + prev + cur < n) sum = sum + prev + cur, prev += cur, ans += cur++;
-        return ans + comp(n - sum, cur);
-    }
-    int comp(int num, int cur) {
-        int ans = 1;
-        for (int sum = 0, i = 1; sum + i < num; i++) sum += i, ans++;
+    int countPartitions(vector<int>& nums, int k) {
+        if (accumulate(nums.begin(), nums.end(), 0LL) < 2 * k) return 0;
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), mod = 1e9 + 7, ans = 1;
+        vector<vector<int>> dp(n + 1, vector<int>(k, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = (ans * 2) % mod;
+            dp[i][0] = 1;
+            for (int j = 1; j < k; j++) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % mod;
+                if (j >= nums[i - 1]) dp[i][j] = (dp[i][j] + dp[i - 1][j - nums[i - 1]]) % mod;
+            }
+        }
+        for (auto &num : dp[n]) ans = (ans - 2 * num % mod + mod) % mod;
         return ans;
     }
 };`,
