d868c963c0bc624e31d95f5547104c93f9f0191a
liyigang <liyigang@bytedance.com>
2022-12-28 11:20:35 +0800

    feat: 🎸 add leetcode no.1750

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -5,24 +5,27 @@ const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '2027. 转换字符串的最少操作次数',
-  url: 'https://leetcode.cn/problems/minimum-moves-to-convert-string/',
-  difficulty: Difficulty.简单,
-  tag: [Tag.贪心, Tag.字符串],
-  desc: `返回将 s 中所有字符均转换为 'O' 需要执行的 最少 操作次数。`,
+  name: '1750. 删除字符串两端相同字符后的最短长度',
+  url: 'https://leetcode.cn/problems/minimum-length-of-string-after-deleting-similar-ends/',
+  difficulty: Difficulty.中等,
+  tag: [Tag.双指针, Tag.字符串],
+  desc: `请你返回对字符串 s 执行上面操作任意次以后（可能 0 次），能得到的 最短长度 。`,
   solutions: [
     {
       script: Script.CPP,
-      time: 0,
-      memory: 6.2,
+      time: 16,
+      memory: 12.5,
       desc: '遍历',
       code: `class Solution {
 public:
-    int minimumMoves(string s) {
-        int ans = 0;
-        for (int i = 0; i < s.size(); i++) 
-            if (s[i] == 'X') ans++, i += 2;
-        return ans;
+    int minimumLength(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r && s[l] == s[r]) {
+            auto c = s[l];
+            while (l <= r && s[l] == c) l++;
+            while (l <= r && s[r] == c) r--;
+        }
+        return r - l + 1;
     }
 };`,
     },
