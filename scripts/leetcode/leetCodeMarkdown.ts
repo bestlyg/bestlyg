c08b4e5875fbaf08e729e9961d2fc0e6cf012e77
liyigang <liyigang@bytedance.com>
2023-01-02 13:37:32 +0800

    feat: 🎸 add leetcode no.1801

--- scripts/leetcode/leetCodeMarkdown.ts
+++ scripts/leetcode/leetCodeMarkdown.ts
@@ -4,8 +4,8 @@ const { specStr, markdown } = utils;
 const { backquote } = specStr;
 const { link } = markdown;
 const leetCodeMarkdown: Markdown = {
-  exist: !true,
-  name: '6280. 范围内最接近的两个质数',
+  exist: true,
+  name: '1801. 积压订单中的订单总数',
   url: 'https://leetcode.cn/problems/closest-prime-numbers-in-range/',
   difficulty: Difficulty.中等,
   tag: [],
@@ -13,87 +13,203 @@ const leetCodeMarkdown: Markdown = {
   solutions: [
     {
       script: Script.CPP,
-      time: 248,
-      memory: 10.2,
-      desc: '计算质数表后统计',
-      code: `class Solution {
-public:
-    int dp[1000005] = {0};
-    int left, right;
-    void load() {
-        for (int i = 2; i <= right; i++) {
-            if (dp[i] == 0) dp[++dp[0]] = i;
-            for (int j = 1; j <= dp[0] && i * dp[j] < 1000005; j++) {
-                dp[i * dp[j]] = 1;
-                if (i % dp[j] == 0) break;
-            }
-        }
+      time: 188,
+      memory: 57.2,
+      desc: '模拟',
+      code: `typedef pair<int, int> node;
+#define X              first
+#define Y              second
+struct cmp {
+    int type;
+    cmp(int type): type(type) {}
+    bool operator()(node &a, node &b) {
+        if (type) return a.X < b.X;
+        else return a.X > b.X;
     }
-    vector<int> closestPrimes(int left, int right) {
-        this->left = left;
-        this->right = right;
-        load();
-        int start = 1;
-        while (start <= dp[0] && dp[start] < left) start++;
-        start++;
-        vector<int> ans(2, -1);
-        if (start > dp[0]) return ans;
-        ans[0] = dp[start - 1]; ans[1] = dp[start];
-        for (int i = start + 1; i <= dp[0] && dp[i] <= right; i++) {
-            if (dp[i] - dp[i - 1] < ans[1] - ans[0]) {
-                ans[0] = dp[i - 1];
-                ans[1] = dp[i];
+};
+int mod = 1e9 + 7;
+class Solution {
+public:
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
+        priority_queue<node, vector<node>, cmp> buyq(cmp(1));
+        priority_queue<node, vector<node>, cmp> sellq(cmp(0));
+        for (auto &order : orders) {
+            if (order[2] == 0) {
+                node cur = make_pair(order[0], order[1]);
+                while (cur.Y && sellq.size() && sellq.top().X <= cur.X) {
+                    if (sellq.top().Y > cur.Y) {
+                        auto v = sellq.top();
+                        sellq.pop();
+                        v.Y -= cur.Y;
+                        sellq.push(v);
+                        cur.Y = 0;
+                    } else if (sellq.top().Y < cur.Y) {
+                        cur.Y -= sellq.top().Y;
+                        sellq.pop();
+                    } else {
+                        cur.Y = 0;
+                        sellq.pop();
+                    }
+                }
+                if (cur.Y) buyq.push(cur);
+            } else {
+                node cur = make_pair(order[0], order[1]);
+                while (cur.Y && buyq.size() && buyq.top().X >= cur.X) {
+                    if (buyq.top().Y > cur.Y) {
+                        auto v = buyq.top();
+                        buyq.pop();
+                        v.Y -= cur.Y;
+                        buyq.push(v);
+                        cur.Y = 0;
+                    } else if (buyq.top().Y < cur.Y) {
+                        cur.Y -= buyq.top().Y;
+                        buyq.pop();
+                    } else {
+                        cur.Y = 0;
+                        buyq.pop();
+                    }
+                }
+                if (cur.Y) sellq.push(cur);
             }
         }
+        int ans = 0;
+        while (buyq.size()) ans = (ans + buyq.top().Y) % mod, buyq.pop();
+        while (sellq.size()) ans = (ans + sellq.top().Y) % mod, sellq.pop();
+        return ans;
+    }
+};`,
+    },
+    {
+      script: Script.CPP,
+      time: 200,
+      memory: 57.2,
+      desc: '同上',
+      code: `typedef pair<int, int> node;
+#define X              first
+#define Y              second
+struct cmp {
+    int type;
+    cmp(int type): type(type) {}
+    bool operator()(node &a, node &b) {
+        if (type) return a.X < b.X;
+        else return a.X > b.X;
+    }
+};
+int mod = 1e9 + 7;
+class Solution {
+public:
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
+        priority_queue<node, vector<node>, cmp> buyq(cmp(1));
+        priority_queue<node, vector<node>, cmp> sellq(cmp(0));
+        for (auto &order : orders) {
+            auto &q1 = order[2] == 0 ? sellq : buyq,
+                 &q2 = order[2] == 0 ? buyq  : sellq;
+            node cur = make_pair(order[0], order[1]);
+            while (cur.Y && q1.size() && (order[2] == 0 ? q1.top().X <= cur.X : q1.top().X >= cur.X)) {
+                if (q1.top().Y > cur.Y) {
+                    auto v = q1.top();
+                    q1.pop();
+                    v.Y -= cur.Y;
+                    q1.push(v);
+                    cur.Y = 0;
+                } else if (q1.top().Y < cur.Y) {
+                    cur.Y -= q1.top().Y;
+                    q1.pop();
+                } else {
+                    cur.Y = 0;
+                    q1.pop();
+                }
+            }
+            if (cur.Y) q2.push(cur);/
+        }
+        int ans = 0;
+        while (buyq.size()) ans = (ans + buyq.top().Y) % mod, buyq.pop();
+        while (sellq.size()) ans = (ans + sellq.top().Y) % mod, sellq.pop();
         return ans;
     }
 };`,
     },
     {
       script: Script.RUST,
-      time: 180,
-      memory: 9.6,
+      time: 24,
+      memory: 9.5,
       desc: '同上',
-      code: `impl Solution {
-    pub fn closest_primes(left: i32, right: i32) -> Vec<i32> {
-        let primes = Solution::get_primes(right as usize);
-        let mut start = 1;
-        while start <= primes[0] && primes[start] < left as usize {
-            start += 1;
-        }
-        let mut ans: Vec<i32> = vec![-1; 2];
-        if start + 1 <= primes[0] {
-            start += 1;
-            ans[0] = primes[start - 1] as i32;
-            ans[1] = primes[start] as i32;
-            while start + 1 <= primes[0] {
-                start += 1;
-                if ((primes[start] - primes[start - 1]) as i32) < ans[1] - ans[0] {
-                    ans[0] = primes[start - 1] as i32;
-                    ans[1] = primes[start] as i32;
-                }
-            }
-        }
-        ans
+      code: `use std::{cmp::Ordering, collections::BinaryHeap};
+struct Node(i32, i32, bool);
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
     }
-    fn get_primes(max: usize) -> Vec<usize> {
-        let mut ans = vec![0; 1000005];
-        for i in 2..=max {
-            if ans[i] == 0 {
-                ans[0] += 1;
-                let idx = ans[0];
-                ans[idx] = i;
-            }
-            for j in 1..=ans[0] {
-                if ans[j] * i >= 1000005 {
-                    break;
+}
+impl Eq for Node {}
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.2 {
+            self.0.cmp(&other.0)
+        } else {
+            other.0.cmp(&self.0)
+        }
+    }
+}
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.2 {
+            self.0.partial_cmp(&other.0)
+        } else {
+            other.0.partial_cmp(&self.0)
+        }
+    }
+}
+const mode: i32 = 1000000000 + 7;
+impl Solution {
+    pub fn get_number_of_backlog_orders(orders: Vec<Vec<i32>>) -> i32 {
+        let mut buy_heap = BinaryHeap::<Node>::new();
+        let mut sell_heap = BinaryHeap::<Node>::new();
+        for order in orders {
+            if order[2] == 0 {
+                let mut cur = Node(order[0], order[1], order[2] == 0);
+                while cur.1 > 0 && !buy_heap.is_empty() && buy_heap.peek().unwrap().0 <= cur.0 {
+                    if buy_heap.peek().unwrap().1 > cur.1 {
+                        let mut node = buy_heap.pop().unwrap();
+                        node.1 -= cur.1;
+                        buy_heap.push(node);
+                        cur.1 = 0;
+                    } else if buy_heap.peek().unwrap().1 < cur.1 {
+                        cur.1 -= buy_heap.pop().unwrap().1;
+                    } else {
+                        cur.1 = 0;
+                        buy_heap.pop();
+                    }
                 }
-                let idx = ans[j] * i;
-                ans[idx] = 1;
-                if i % ans[j] == 0 {
-                    break;
+                if cur.1 > 0 {
+                    sell_heap.push(cur);
+                };
+            } else {
+                let mut cur = Node(order[0], order[1], order[2] == 0);
+                while cur.1 > 0 && !sell_heap.is_empty() && sell_heap.peek().unwrap().0 >= cur.0 {
+                    if sell_heap.peek().unwrap().1 > cur.1 {
+                        let mut node = sell_heap.pop().unwrap();
+                        node.1 -= cur.1;
+                        sell_heap.push(node);
+                        cur.1 = 0;
+                    } else if sell_heap.peek().unwrap().1 < cur.1 {
+                        cur.1 -= sell_heap.pop().unwrap().1;
+                    } else {
+                        cur.1 = 0;
+                        sell_heap.pop();
+                    }
                 }
+                if cur.1 > 0 {
+                    buy_heap.push(cur);
+                };
             }
+        }
+        let mut ans = 0;
+        while !buy_heap.is_empty() {
+            ans = (ans + buy_heap.pop().unwrap().1) % mode;
+        }
+        while !sell_heap.is_empty() {
+            ans = (ans + sell_heap.pop().unwrap().1) % mode;
         }
         ans
     }
