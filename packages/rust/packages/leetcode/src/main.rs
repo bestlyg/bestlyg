ce51b612073948784d5e2eaad407849b6607c519
liyigang <liyigang@bytedance.com>
2023-01-05 13:03:49 +0800

    feat: 🎸 add leetcode no.1803

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -1,16 +1,24 @@
 mod preclude;
 
+use std::borrow::Borrow;
+
 use preclude::*;
-fn main() {}
+fn main() {
+    let nums = vec![1, 4, 2, 7];
+    let low = 2;
+    let high = 6;
+    println!("{}", Solution::count_pairs(nums, low, high));
+}
 
 impl Solution {
-    pub fn min_operations(logs: Vec<String>) -> i32 {
-        let mut ans = 0_i32;
-        for log in logs {
-            if log.eq("../") {
-                ans = (ans - 1).max(0);
-            } else if log.ne("./") {
-                ans += 1;
+    pub fn count_pairs(nums: Vec<i32>, low: i32, high: i32) -> i32 {
+        let mut ans = 0;
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                let val = nums[i] ^ nums[j];
+                if val >= low && val <= high {
+                    ans += 1;
+                }
             }
         }
         ans
