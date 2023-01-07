1035b9bc189bdf88f556d5ef098a5318488e7896
liyigang <liyigang@bytedance.com>
2023-01-07 11:02:00 +0800

    feat: 🎸 add leetcode no.1658

--- packages/rust/packages/leetcode/src/main.rs
+++ packages/rust/packages/leetcode/src/main.rs
@@ -3,29 +3,55 @@ mod preclude;
 use std::borrow::Borrow;
 
 use preclude::*;
-fn main() {
-    let nums = vec![1, 4, 2, 7];
-    let low = 2;
-    let high = 6;
-    println!("{}", Solution::count_pairs(nums, low, high));
-}
+fn main() {}
 
 impl Solution {
-    pub fn count_even(num: i32) -> i32 {
-        return Solution::dfs(num, 0, 0) - 1;
-    }
-    fn dfs(num: i32, cur: i32, sum: i32) -> i32 {
-        if cur > num {
-            0
-        } else {
-            let mut ans = if sum % 2 == 0 { 1 } else { 0 };
-            for i in 0..=9 {
-                if cur * 10 + i == cur {
-                    continue;
-                }
-                ans += Solution::dfs(num, cur * 10 + i, sum + i);
+    pub fn min_operations(nums: Vec<i32>, x: i32) -> i32 {
+        let mut ans = i32::MAX;
+        let n = nums.len();
+        let (mut r, mut rsum) = (0, nums.iter().fold(0, |sum, cur| sum + cur));
+        if rsum < x {
+            return -1;
+        }
+        while r < n && rsum > x {
+            rsum -= nums[r];
+            r += 1;
+        }
+        if rsum == x {
+            ans = (n - r) as i32;
+        }
+        let (mut l, mut lsum) = (0, 0);
+        while l < n {
+            lsum += nums[l];
+            while r < n && (l + 1 + n - r > n || lsum + rsum > x) {
+                rsum -= nums[r];
+                r += 1;
             }
+            if lsum + rsum == x {
+                ans = ans.min((l + 1 + n - r) as i32);
+            }
+            l += 1;
+        }
+        if ans == i32::MAX {
+            -1
+        } else {
             ans
         }
     }
 }
+
+// class Solution {
+//     public:
+//         int minOperations(vector<int>& nums, int x) {
+//             int ans = 0x7fffffff, n = nums.size(), r = 0, rsum = accumulate(nums.begin(), nums.end(), 0);
+//             if (rsum < x) return -1;
+//             while (r < n && rsum > x) rsum -= nums[r++];
+//             if (rsum == x) ans = n - r;
+//             for (int l = 0, lsum = 0; l < n; l++) {
+//                 lsum += nums[l];
+//                 while (r < n && (l + 1 + n - r > n || lsum + rsum > x)) rsum -= nums[r++];
+//                 if (lsum + rsum == x) ans = min(ans, l + 1 + n - r);
+//             }
+//             return ans == 0x7fffffff ? -1 : ans;
+//         }
+//     };
