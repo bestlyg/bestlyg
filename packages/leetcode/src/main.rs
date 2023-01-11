cfcc3201d8dad844fb15aa210df56e6b04ec7d5c
liyigang <liyigang@bytedance.com>
2023-01-11 12:51:38 +0800

    feat: 🎸 refactor leetcode

--- packages/leetcode/src/main.rs
+++ packages/leetcode/src/main.rs
@@ -1,15 +1,14 @@
 mod preclude;
-use rand::Rng;
 
 use preclude::*;
 fn main() {
-    let res = Solution::crack_safe(2, 2);
+    let res = Solution::digit_count("123".to_string());
     println!("res = {res:#?}");
 }
 
 impl Solution {
     pub fn digit_count(num: String) -> bool {
-        let l = [0; 10];
+        let mut l = [0; 10];
         let n = num.len();
         let num = num.chars().collect::<Vec<char>>();
         for c in num.iter() {
