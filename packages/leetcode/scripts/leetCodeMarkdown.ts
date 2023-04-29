a00bd0701a7c9bdafdc578bc9e8a4369f50c3fcb
bestlyg <1057966749@qq.com>
2023-04-29 10:49:09 +0800

    feat: 🎸 add leetcode no.2423

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-  exist: true,
-  name: '1172. 餐盘栈',
-  url: 'https://leetcode.cn/problems/dinner-plate-stacks/',
+  exist: !true,
+  name: '2423. 删除字符使频率相同',
+  url: 'https://leetcode.cn/problems/remove-letter-to-equalize-frequency/',
   difficulty: Difficulty.中等,
   tag: [Tag.广度优先搜索, Tag.数组, Tag.矩阵],
-  desc: `我们把无限数量 ∞ 的栈排成一行，按从左到右的次序从 0 开始编号。每个栈的的最大容量 capacity 都相同。实现一个叫「餐盘」的类 DinnerPlates。`,
+  desc: `给你一个下标从 0 开始的字符串 word ，字符串只包含小写英文字母。你需要选择 一个 下标并 删除 下标处的字符，使得 word 中剩余每个字母出现 频率 相同。如果删除一个字母后，word 中剩余所有字母的出现频率都相同，那么返回 true ，否则返回 false 。`,
   solutions: [
     //     {
     //       script: Script.TS,
@@ -23,59 +23,39 @@ const leetCodeMarkdown: Markdown = {
     // }`,
     //       date: new Date('2022/09/25').getTime(),
     //     },
-//     {
-//       script: Script.CPP,
-//       time: 448,
-//       memory: 205.7,
-//       desc: '模拟栈，用优先队列和哈希表存储从左往右空着的元素，末尾为空时删除末尾的栈',
-//       code: `class DinnerPlates {
-// public:
-//     int capacity;
-//     vector<vector<int>> ss;
-//     unordered_set<int> used;
-//     priority_queue<int, vector<int>, greater<int>> q;
-
-//     DinnerPlates(int capacity): capacity(capacity) {}
-
-//     int load_stack() {
-//         ss.push_back(vector<int>());
-//         return ss.size() - 1;
-//     }
-
-//     void clear_last() {
-//         while (ss.size() && ss.back().size() == 0) ss.pop_back();
-//     }
-    
-//     void push(int val) {
-//         while (q.size() && q.top() >= ss.size()) q.pop();
-//         if (q.empty()) {
-//             int idx = ss.size() - 1;
-//             if (ss.empty() || ss[idx].size() == capacity) idx = load_stack();
-//             ss[idx].push_back(val);
-//         } else {
-//             int idx = q.top();
-//             ss[idx].push_back(val);
-//             if (ss[idx].size() == capacity) q.pop(), used.erase(idx);
-//         }
-//     }
-    
-//     int pop() {
-//         clear_last();
-//         if (ss.empty()) return -1;
-//         int back = ss.back().back();
-//         ss.back().pop_back();
-//         return back;
-//     }
-    
-//     int popAtStack(int index) {
-//         if (index >= ss.size() || ss[index].size() == 0) return -1;
-//         int back = ss[index].back();
-//         ss[index].pop_back();
-//         clear_last();
-//         if (index < ss.size() && !used.count(index)) q.push(index), used.insert(index);
-//         return back;
-//     }
-// };`,
+    {
+      script: Script.CPP,
+      time: 0,
+      memory:6,
+      desc: '模拟',
+      code: `class Solution {
+public:
+    bool equalFrequency(string word) {
+        int cnt[26] = {0};
+        for (auto &c : word) cnt[c - 'a']++;
+        map<int, vector<int>> m;
+        for (int i = 0; i < 26; i++) 
+            if (cnt[i]) m[cnt[i]].push_back(i);
+        // 如果都是一个频率，只有在频率都是1或者数量为1的时候才可以
+        if (m.size() == 1) return m.begin()->first == 1 || m.begin()->second.size() == 1;
+        // 超过两个频率就报错
+        if (m.size() != 2) return false;
+        int pi = -1;
+        vector<int> pl;
+        for (auto &item : m) {
+            int ci = item.first;
+            vector<int> cl = item.second;
+            if (pi == -1) pi = ci, pl = cl;
+            // 如果有一个频率是1且数量是1， 可以直接删
+            else if (pi == 1 && pl.size() == 1 || ci == 1 && cl.size() == 1) return true;
+            // 如果两个频率不差1， 就报错
+            else if (pi + 1 != ci) return false;
+            // 如果高频率不是只有1个，就报错
+            else if (cl.size() != 1) return false;
+        }
+        return true;
+    }
+};`,
 //     },
 //     {
 //       script: Script.PY3,
@@ -130,98 +110,98 @@ const leetCodeMarkdown: Markdown = {
 //             self.used.add(index)
 //         return back`,
 //     },
-    {
-      script: Script.RUST,
-      time: 116,
-      memory: 76.6,
-      desc: '同上',
-      code: `use std::cmp::Ordering;
+//     {
+//       script: Script.RUST,
+//       time: 116,
+//       memory: 76.6,
+//       desc: '同上',
+//       code: `use std::cmp::Ordering;
 
-#[derive(PartialEq)]
-struct RevUnsize(usize);
-impl Eq for RevUnsize {}
+// #[derive(PartialEq)]
+// struct RevUnsize(usize);
+// impl Eq for RevUnsize {}
 
-impl PartialOrd for RevUnsize {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        other.0.partial_cmp(&self.0)
-    }
-}
-impl Ord for RevUnsize {
-    fn cmp(&self, other: &RevUnsize) -> Ordering {
-        other.0.partial_cmp(&self.0).unwrap()
-    }
-}
+// impl PartialOrd for RevUnsize {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         other.0.partial_cmp(&self.0)
+//     }
+// }
+// impl Ord for RevUnsize {
+//     fn cmp(&self, other: &RevUnsize) -> Ordering {
+//         other.0.partial_cmp(&self.0).unwrap()
+//     }
+// }
 
-struct DinnerPlates {
-    capacity: usize,
-    last: usize,
-    ss: Vec<Vec<i32>>,
-    used: std::collections::HashSet<usize>,
-    q: std::collections::BinaryHeap<RevUnsize>,
-}
+// struct DinnerPlates {
+//     capacity: usize,
+//     last: usize,
+//     ss: Vec<Vec<i32>>,
+//     used: std::collections::HashSet<usize>,
+//     q: std::collections::BinaryHeap<RevUnsize>,
+// }
 
-impl DinnerPlates {
-    fn new(capacity: i32) -> Self {
-        Self {
-            capacity: capacity as usize,
-            last: 0,
-            ss: vec![vec![]],
-            used: Default::default(),
-            q: Default::default(),
-        }
-    }
+// impl DinnerPlates {
+//     fn new(capacity: i32) -> Self {
+//         Self {
+//             capacity: capacity as usize,
+//             last: 0,
+//             ss: vec![vec![]],
+//             used: Default::default(),
+//             q: Default::default(),
+//         }
+//     }
 
-    fn format_last(&mut self) {
-        if self.ss[self.last].len() == self.capacity {
-            self.last += 1;
-        }
-        if self.last == self.ss.len() {
-            self.ss.push(vec![]);
-        }
-    }
+//     fn format_last(&mut self) {
+//         if self.ss[self.last].len() == self.capacity {
+//             self.last += 1;
+//         }
+//         if self.last == self.ss.len() {
+//             self.ss.push(vec![]);
+//         }
+//     }
 
-    fn push(&mut self, val: i32) {
-        while !self.q.is_empty() && (*self.q.peek().unwrap()).0 > self.last {
-            self.q.pop();
-        }
-        if self.q.is_empty() {
-            self.format_last();
-            self.ss[self.last].push(val);
-        } else {
-            let idx = (*self.q.peek().unwrap()).0;
-            self.ss[idx].push(val);
-            if self.ss[idx].len() == self.capacity {
-                self.q.pop();
-                self.used.remove(&idx);
-            }
-        }
-    }
+//     fn push(&mut self, val: i32) {
+//         while !self.q.is_empty() && (*self.q.peek().unwrap()).0 > self.last {
+//             self.q.pop();
+//         }
+//         if self.q.is_empty() {
+//             self.format_last();
+//             self.ss[self.last].push(val);
+//         } else {
+//             let idx = (*self.q.peek().unwrap()).0;
+//             self.ss[idx].push(val);
+//             if self.ss[idx].len() == self.capacity {
+//                 self.q.pop();
+//                 self.used.remove(&idx);
+//             }
+//         }
+//     }
 
-    fn pop(&mut self) -> i32 {
-        while self.last > 0 && self.ss[self.last].len() == 0 {
-            self.last -= 1;
-        }
-        if self.last == 0 && self.ss[self.last].len() == 0 {
-            -1
-        } else {
-            self.ss[self.last].pop().unwrap()
-        }
-    }
+//     fn pop(&mut self) -> i32 {
+//         while self.last > 0 && self.ss[self.last].len() == 0 {
+//             self.last -= 1;
+//         }
+//         if self.last == 0 && self.ss[self.last].len() == 0 {
+//             -1
+//         } else {
+//             self.ss[self.last].pop().unwrap()
+//         }
+//     }
 
-    fn pop_at_stack(&mut self, index: i32) -> i32 {
-        let index = index as usize;
-        if index > self.last || self.ss[index].len() == 0 {
-            -1
-        } else {
-            let back = self.ss[index].pop().unwrap();
-            if !self.used.contains(&index) {
-                self.q.push(RevUnsize(index));
-                self.used.insert(index);
-            }
-            back
-        }
-    }
-}`,
+//     fn pop_at_stack(&mut self, index: i32) -> i32 {
+//         let index = index as usize;
+//         if index > self.last || self.ss[index].len() == 0 {
+//             -1
+//         } else {
+//             let back = self.ss[index].pop().unwrap();
+//             if !self.used.contains(&index) {
+//                 self.q.push(RevUnsize(index));
+//                 self.used.insert(index);
+//             }
+//             back
+//         }
+//     }
+// }`,
     },
   ],
 };
