edfee69389299ec8c4b70635eef96a810929bb62
bestlyg <1057966749@qq.com>
2023-05-21 23:48:58 +0800

    feat: 🎸 add leetcode LCP33

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
   exist: !true,
-  name: '1373. 二叉搜索子树的最大键值和',
-  url: 'https://leetcode.cn/problems/maximum-sum-bst-in-binary-tree/',
+  name: 'LCP 33. 蓄水',
+  url: 'https://leetcode.cn/problems/o8SXZn/',
   difficulty: Difficulty.简单,
   tag: [],
-  desc: `给你一棵以 root 为根的 二叉树 ，请你返回 任意 二叉搜索子树的最大键值和`,
+  desc: `每个水缸对应最低蓄水量记作 vat[i]，返回小扣至少需要多少次操作可以完成所有水缸蓄水要求。`,
   solutions: [
     //         {
     //           script: Script.TS,
@@ -27,193 +27,56 @@ const leetCodeMarkdown: Markdown = {
     //         },
     {
       script: Script.CPP,
-      time: 128,
-      memory: 110.6,
-      desc: '遍历左右子树的同时记录最大值，最小值和总和',
-      code: `struct Node {
-    int l, r, sum;
-    Node(int l, int r, int sum): l(l), r(r), sum(sum) {}
-};
-
-class Solution {
+      time: 84,
+      memory: 8,
+      desc: '利用堆获取需要次数最多的桶',
+      code: `class Solution {
 public:
-    int maxSumBST(TreeNode* root) {
-        int res = 0;
-        Node no(INT_MIN, -1, -1);
-        function<Node(TreeNode*)> dfs = [&](TreeNode *node) -> Node {
-            if (!node) return no;
-            int val = node->val;
-            auto lv = dfs(node->left), rv = dfs(node->right);
-            if (node->left == nullptr && node->right == nullptr) {
-                res = max(res, val);
-                return Node(val, val, val);
-            } else if (node->left == nullptr) {
-                if (rv.l == no.l) return no;
-                if (val >= rv.l) return no;
-                rv.l = val;
-                rv.sum += val;
-                res = max(res, rv.sum);
-                return rv;
-            } else if (node->right == nullptr) {
-                if (lv.l == no.l) return no;
-                if (lv.r >= val) return no;
-                lv.r = val;
-                lv.sum += val;
-                res = max(res, lv.sum);
-                return lv;
-            } else {
-                if (lv.l == no.l || rv.l == no.l) return no;
-                if (lv.r >= val) return no;
-                if (val >= rv.l) return no;
-                Node next(lv.l, rv.r, lv.sum + rv.sum + val);
-                res = max(res, next.sum);
-                return next;
+    int storeWater(vector<int>& bucket, vector<int>& vat) {
+        auto get_cnt = [&](int idx) {
+            if (bucket[idx] == 0) {
+                if (vat[idx] == 0) return 0;
+                return 0x3f3f3f3f;
             }
+            return (int)ceil(1.0 * vat[idx] / bucket[idx]);
         };
-        dfs(root);
+        auto cmp = [&](int x, int y) -> bool { 
+            return get_cnt(x) < get_cnt(y);
+        };
+        priority_queue<int, vector<int>, decltype(cmp)> q(cmp);
+        for (int i = 0; i < bucket.size(); i++) q.push(i);
+        int res = get_cnt(q.top()), add = 0;
+        while (get_cnt(q.top()) > 1) {
+            int cur_cnt = get_cnt(q.top());
+            while (get_cnt(q.top()) == cur_cnt) {
+                int idx = q.top();
+                q.pop();
+                do {
+                    bucket[idx] += 1;
+                    add += 1;
+                } while(get_cnt(idx) == cur_cnt);
+                q.push(idx);
+            }
+            res = min(res, get_cnt(q.top()) + add);
+        }
         return res;
     }
 };`,
     },
-    {
-      script: Script.PY3,
-      time: 304,
-      memory: 65.5,
-      desc: '同上',
-      code: `class Node:
-    def __init__(self, l: int, r: int, sum: int):
-        self.l = l
-        self.r = r
-        self.sum = sum
-
-
-class Solution:
-    def maxSumBST(self, root: Optional[TreeNode]) -> int:
-        res = 0
-        no = Node(-inf, -1, -1)
-
-        def dfs(node: Optional[TreeNode]) -> Node:
-            nonlocal res
-            if not node:
-                return no
-            val = node.val
-            lv, rv = dfs(node.left), dfs(node.right)
-            if node.left == None and node.right == None:
-                res = max(res, val)
-                return Node(val, val, val)
-            elif node.left == None:
-                if rv.l == no.l:
-                    return no
-                if val >= rv.l:
-                    return no
-                rv.l = val
-                rv.sum += val
-                res = max(res, rv.sum)
-                return rv
-            elif node.right == None:
-                if lv.l == no.l:
-                    return no
-                if lv.r >= val:
-                    return no
-                lv.r = val
-                lv.sum += val
-                res = max(res, lv.sum)
-                return lv
-            else:
-                if lv.l == no.l or rv.l == no.l:
-                    return no
-                if lv.r >= val:
-                    return no
-                if val >= rv.l:
-                    return no
-                next = Node(lv.l, rv.r, lv.sum + rv.sum + val)
-                res = max(res, next.sum)
-                return next
-        dfs(root)
-        return res`,
-    },
-    {
-      script: Script.RUST,
-      time: 24,
-      memory: 9.8,
-      desc: '同上',
-      code: `static NoVal: i32 = i32::MIN;
-#[derive(Debug, Clone)]
-struct Node {
-    l: i32,
-    r: i32,
-    sum: i32,
-}
-impl Node {
-    fn new(l: i32, r: i32, sum: i32) -> Node {
-        Node { l, r, sum }
-    }
-    fn no() -> Node {
-        Node {
-            l: NoVal,
-            r: 0,
-            sum: 0,
-        }
-    }
-}
-use std::cell::RefCell;
-use std::rc::Rc;
-impl Solution {
-    pub fn max_sum_bst(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut res = 0;
-        dfs(&mut res, &root);
-        res
-    }
-}
-fn dfs(res: &mut i32, node: &Option<Rc<RefCell<TreeNode>>>) -> Node {
-    match node {
-        Some(node) => {
-            let nodeRef = node.as_ref().borrow();
-            let val = nodeRef.val;
-            let (mut lv, mut rv) = (dfs(res, &nodeRef.left), dfs(res, &nodeRef.right));
-            if nodeRef.left.is_none() && nodeRef.right.is_none() {
-                *res = (*res).max(val);
-                Node::new(val, val, val)
-            } else if nodeRef.left.is_none() {
-                if rv.l == NoVal {
-                    Node::no()
-                } else if val >= rv.l {
-                    Node::no()
-                } else {
-                    rv.l = val;
-                    rv.sum += val;
-                    *res = (*res).max(rv.sum);
-                    rv
-                }
-            } else if nodeRef.right.is_none() {
-                if lv.l == NoVal {
-                    Node::no()
-                } else if lv.r >= val {
-                    Node::no()
-                } else {
-                    lv.r = val;
-                    lv.sum += val;
-                    *res = (*res).max(lv.sum);
-                    lv
-                }
-            } else {
-                if lv.l == NoVal || rv.l == NoVal {
-                    Node::no()
-                } else if lv.r >= val {
-                    Node::no()
-                } else if val >= rv.l {
-                    Node::no()
-                } else {
-                    let next = Node::new(lv.l, rv.r, lv.sum + rv.sum + val);
-                    *res = (*res).max(next.sum);
-                    next
-                }
-            }
-        }
-        None => Node::no(),
-    }
-}`,
-    },
+    // {
+    //   script: Script.PY3,
+    //   time: 304,
+    //   memory: 65.5,
+    //   desc: '同上',
+    //   code: ``,
+    // },
+    // {
+    //   script: Script.RUST,
+    //   time: 24,
+    //   memory: 9.8,
+    //   desc: '同上',
+    //   code: ``,
+    // },
   ],
 };
 
