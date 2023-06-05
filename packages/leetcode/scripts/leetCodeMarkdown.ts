8a3a02cc27148ebee5adda32ce770dd6005cbe34
liyigang <liyigang@bytedance.com>
2023-06-05 10:20:08 +0800

    feat: 🎸 update leetcode

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,131 +2,56 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: !true,
-    name: '1156. 单字符重复子串的最大长度',
-    url: 'https://leetcode.cn/problems/swap-for-longest-repeated-character-substring/',
+    exist: true,
+    name: '2460. 对数组执行操作',
+    url: 'https://leetcode.cn/problems/apply-operations-to-an-array/',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个字符串 text，你只能交换其中两个字符一次或者什么都不做，然后得到一些单字符重复的子串。返回其中最长的子串的长度。`,
+    desc: `给你一个下标从 0 开始的数组 nums ，数组大小为 n ，且由 非负 整数组成。返回结果数组。`,
     solutions: [
-        //     {
-        //       script: Script.TS,
-        //       time: 76,
-        //       memory: 45.3,
-        //       desc: 'dfs',
-        //       code: `// 特殊标识符，在左右相等时返回
-        // }`,
-        //     },
         {
-            script: Script.CPP,
-            time: 4,
-            memory: 7.9,
-            desc: '按字符统计数据',
-            code: `#define pii pair<int, int>
-#define X first
-#define Y second
-class Solution {
+            script: Script.TS,
+            time: 0,
+            memory: 8.6,
+            desc: '遍历',
+            code: `class Solution {
 public:
-    int maxRepOpt1(string text) {
-        vector<vector<pii>> m(26);
-        int res = 0, n = text.size();
-        for (int i = 0; i < n; i++) {
-            int start = i;
-            while (i + 1 < n && text[i + 1] == text[start]) i++;
-            m[text[start] - 'a'].push_back(make_pair(start, i));
-            res = max(res, i - start + 1);
-        }
-        for (auto &list : m) {
-            int n = list.size();
-            for (int i = 0; i < n; i++) {
-                if (n != 1) res = max(res, list[i].Y - list[i].X + 2);
-                if (i + 1 < n && list[i].Y + 1 == list[i + 1].X - 1) {
-                    int val = list[i].Y - list[i].X + 1 + list[i + 1].Y - list[i + 1].X + 1;
-                    if (!(i == 0 && i + 2 == n)) val += 1;
-                    res = max(res, val);
-                }
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size(), start = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
+            if (nums[i] != 0) nums[start++] = nums[i];
         }
-        return res;
+        if (nums[n - 1]) nums[start++] = nums[n - 1];
+        while (start < n) nums[start++] = 0;
+        return nums;
     }
 };`,
         },
-        {
-            script: Script.PY3,
-            time: 100,
-            memory: 18.1,
-            desc: '同上',
-            code: `class Solution:
-    def maxRepOpt1(self, text: str) -> int:
-        m = [[] for _ in range(26)]
-        res = 0
-        n = len(text)
-        i = 0
-        while i < n:
-            start = i
-            while i + 1 < n and text[i + 1] == text[start]:
-                i += 1
-            m[ord(text[start]) - ord('a')].append((start, i))
-            res = max(res, i - start + 1)
-            i += 1
-        for list in m:
-            n = len(list)
-            for i in range(n):
-                if n != 1:
-                    res = max(res, list[i][1] - list[i][0] + 2)
-                if i + 1 < n and list[i][1] + 1 == list[i + 1][0] - 1:
-                    val = list[i][1] - list[i][0] + 1 + \
-                        list[i + 1][1] - list[i + 1][0] + 1
-                    if not (i == 0 and i + 2 == n):
-                        val += 1
-                    res = max(res, val)
-        return res`,
-        },
-                {
-                    script: Script.RUST,
-                    time: 0,
-                    memory: 2.3,
-                    desc: '同上',
-                    code: `pub fn str_to_vec(s: &String) -> Vec<char> {
-    s.chars().collect()
-}
-impl Solution {
-    pub fn max_rep_opt1(text: String) -> i32 {
-        let text = str_to_vec(&text);
-        let mut m = vec![vec![]; 26];
-        let mut res = 0;
-        let mut n = text.len();
-        {
-            let mut i = 0;
-            while i < n {
-                let start = i;
-                while i + 1 < n && text[i + 1] == text[start] {
-                    i += 1;
-                }
-                m[text[start] as usize - 'a' as usize].push((start, i));
-                res = res.max(i - start + 1);
-                i += 1;
-            }
-        }
-        for list in m {
-            let n = list.len();
-            for i in 0..n {
-                if n != 1 {
-                    res = res.max(list[i].1 - list[i].0 + 2);
-                }
-                if i + 1 < n && list[i].1 + 1 == list[i + 1].0 - 1 {
-                    let mut val = list[i].1 - list[i].0 + 1 + list[i + 1].1 - list[i + 1].0 + 1;
-                    if !(i == 0 && i + 2 == n) {
-                        val += 1;
-                    }
-                    res = res.max(val);
-                }
-            }
-        }
-        res as i32
-    }
-}`,
-                },
+        // {
+        //     script: Script.CPP,
+        //     time: 4,
+        //     memory: 7.9,
+        //     desc: '按字符统计数据',
+        //     code: ``,
+        // },
+        // {
+        //     script: Script.PY3,
+        //     time: 100,
+        //     memory: 18.1,
+        //     desc: '同上',
+        //     code: ``,
+        // },
+        //         {
+        //             script: Script.RUST,
+        //             time: 0,
+        //             memory: 2.3,
+        //             desc: '同上',
+        //             code: ``,
+        //         },
     ],
 };
 
--- packages/leetcode/scripts/script.ts
+++ packages/leetcode/scripts/script.ts
@@ -104,48 +104,16 @@
 //   ? unknown
 //   : _DivideBy<M, N, []>;
 
-// type B = Subtract<200, 13>;
-// 特殊标识符，在左右相等时返回
-const same = Symbol('same');
-// 存储所有已经存在的key
-function mergeKey(key1: string[], key2: string[]) {
-    const set1 = new Set(key1);
-    const set2 = new Set(key2);
-    const res = new Set<string>();
-    for (const k of set1) {
-        if (set2.has(k)) res.add(k);
-    }
-    return res;
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+function throwError(msg: string): never {
+    throw new Error(msg);
 }
-
-function objDiff(obj1: any, obj2: any, topLevel = true): any {
-    const t1 = typeof obj1;
-    const t2 = typeof obj2;
-    // 类型不等，肯定不等
-    if (t1 !== t2) return [obj1, obj2];
-    // 如果不是对象，直接判断是否相等
-    if (t1 !== 'object') return obj1 === obj2 ? same : [obj1, obj2];
-    // 如果是null或undefined，直接判断防止下面出错
-    if (obj1 === null || obj1 === undefined || obj2 === null || obj2 === undefined) return same;
-    // 如果一个是数组一个不是数组，那就不等
-    if (
-        (!Array.isArray(obj1) && Array.isArray(obj2)) ||
-        (Array.isArray(obj1) && !Array.isArray(obj2))
-    )
-        return [obj1, obj2];
-    // 此时肯定是对象或数组
-    // 拿所有共存的key
-    const keys = mergeKey(Object.keys(obj1), Object.keys(obj2));
-    const res = {};
-    // 遍历obj1中所有的kv
-    for (const [k, v] of Object.entries(obj1).filter(([k]) => keys.has(k))) {
-        // 递归比较，利用topLevel记录是不是顶层
-        const diff = objDiff(v, obj2[k], false);
-        // 如果不同就存储
-        if (diff != same) res[k] = diff;
-    }
-    // 如果是空的，但是是顶层的，那就返回相等
-    if (Object.keys(res).length === 0 && !topLevel) return same;
-    // 否则顶层要返回控对象
-    return res;
+function expect(val1: any): ToBeOrNotToBe {
+    return {
+        toBe: val2 => (val1 === val2 ? true : throwError('Not Equal')),
+        notToBe: val2 => (val1 === val2 ? true : throwError('Not Equal')),
+    };
 }
