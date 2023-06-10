e013ae605b4bbe51828d9039bd7ba93a14f59817
liyigang <liyigang@bytedance.com>
2023-06-10 17:01:44 +0800

    feat: move scripts

---
+++ scripts/init.sh
@@ -0,0 +1,2 @@
+git remote add gitee git@gitee.com:bestlyg/bestlyg.git
+git remote add github git@github.com:bestlyg/bestlyg.git
--- scripts/site.sh
+++ scripts/site.sh
@@ -4,3 +4,4 @@ cp -rf ./packages/leetcode/dist ./packages/site/docs/leetcode
 pnpm --filter site build
 zip -vr ./packages/site/build/site.zip ./packages/site/build
 scp -r ./packages/site/build/site.zip ubuntu@101.34.26.179:~/site.zip 
+# sudo unzip -o -d /root/projects/bestlyg /home/ubuntu/site.zip
