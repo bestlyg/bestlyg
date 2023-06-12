ffacb860cd9814a74143e33f3df10bf0fd448053
liyigang <liyigang@bytedance.com>
2023-06-12 10:04:09 +0800

    feat: 🎸 add leetcode no.1483

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,7 +2,7 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: !true,
+    exist: true,
     name: '1483. 树节点的第 K 个祖先',
     url: 'https://leetcode.cn/problems/kth-ancestor-of-a-tree-node/',
     difficulty: Difficulty.简单,
@@ -30,9 +30,9 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.CPP,
-            time: 520,
-            memory: 132.2,
-            desc: '倍增法',
+            time: 392,
+            memory: 132.1,
+            desc: '当前节点的第n项等于第n-1个父节点的第n-1项',
             code: `class TreeAncestor {
 public:
     vector<vector<int>> list;
@@ -40,9 +40,9 @@ public:
     TreeAncestor(int n, vector<int>& parent): list(vector<vector<int>>(n)) {
         for (int i = 1; i < parent.size(); i++) {
             list[i].push_back(parent[i]);
-            for (int j = 1, res = 1; res != -1; j++) {
-                res = getKthAncestor(i, pow(2, j));
-                if (res != -1) list[i].push_back(res);
+            for (int j = 1; ; j++) {
+                if (list[list[i].back()].size() > j - 1) list[i].push_back(list[list[i].back()][j - 1]);
+                else break;
             }
         }
     }
@@ -60,91 +60,91 @@ public:
     }
 };`,
         },
-        {
-            script: Script.PY3,
-            time: 7080,
-            memory: 48.2,
-            desc: '同上',
-            code: `class TreeAncestor:
+//         {
+//             script: Script.PY3,
+//             time: 7080,
+//             memory: 48.2,
+//             desc: '同上',
+//             code: `class TreeAncestor:
 
-    def __init__(self, n: int, parent: List[int]):
-        self.list = [[] for _ in range(n)]
-        for i in range(1, len(parent)):
-            self.list[i].append(parent[i])
-            j = res = 1
-            while res != -1:
-                res = self.getKthAncestor(i, pow(2, j))
-                if res != -1:
-                    self.list[i].append(res)
-                j += 1
+//     def __init__(self, n: int, parent: List[int]):
+//         self.list = [[] for _ in range(n)]
+//         for i in range(1, len(parent)):
+//             self.list[i].append(parent[i])
+//             j = res = 1
+//             while res != -1:
+//                 res = self.getKthAncestor(i, pow(2, j))
+//                 if res != -1:
+//                     self.list[i].append(res)
+//                 j += 1
 
-    def getKthAncestor(self, node: int, k: int) -> int:
-        if k == 0:
-            return node
-        l = -1
-        r = len(self.list[node]) - 1
-        while l < r:
-            m = (l+r+1)//2
-            if k >= pow(2, m):
-                l = m
-            else:
-                r = m-1
-        if l == -1:
-            return l
-        return self.getKthAncestor(self.list[node][l], k-pow(2, l))
-`,
-        },
-        {
-            script: Script.RUST,
-            time: 204,
-            memory: 34.9,
-            desc: '同上',
-            code: `struct TreeAncestor {
-    list: Vec<Vec<usize>>,
-}
-impl TreeAncestor {
-    fn new(n: i32, parent: Vec<i32>) -> Self {
-        let mut list = vec![vec![]; n as usize];
-        for i in 1..parent.len() {
-            list[i].push(parent[i] as usize);
-            let mut res = 1;
-            for j in 1.. {
-                res = TreeAncestor::_get_kth_ancestor(&list, i as i32, 2i32.pow(j as u32));
-                if res != -1 {
-                    list[i].push(res as usize);
-                } else {
-                    break;
-                }
-            }
-        }
-        Self { list }
-    }
-    fn _get_kth_ancestor(list: &Vec<Vec<usize>>, node: i32, k: i32) -> i32 {
-        if k == 0 {
-            node
-        } else {
-            let mut l = -1 as i32;
-            let mut r = (list[node as usize].len() - 1) as i32;
-            while l < r {
-                let m = (l + r + 1) / 2;
-                if k >= 2i32.pow(m as u32) {
-                    l = m;
-                } else {
-                    r = m - 1;
-                }
-            }
-            if l == -1 {
-                l
-            } else {
-                TreeAncestor::_get_kth_ancestor(list, list[node as usize][l as usize] as i32, k - (2i32.pow(l as u32)))
-            }
-        }
-    }
-    fn get_kth_ancestor(&self, node: i32, k: i32) -> i32 {
-        TreeAncestor::_get_kth_ancestor(&self.list, node, k)
-    }
-}`,
-        },
+//     def getKthAncestor(self, node: int, k: int) -> int:
+//         if k == 0:
+//             return node
+//         l = -1
+//         r = len(self.list[node]) - 1
+//         while l < r:
+//             m = (l+r+1)//2
+//             if k >= pow(2, m):
+//                 l = m
+//             else:
+//                 r = m-1
+//         if l == -1:
+//             return l
+//         return self.getKthAncestor(self.list[node][l], k-pow(2, l))
+// `,
+//         },
+//         {
+//             script: Script.RUST,
+//             time: 204,
+//             memory: 34.9,
+//             desc: '同上',
+//             code: `struct TreeAncestor {
+//     list: Vec<Vec<usize>>,
+// }
+// impl TreeAncestor {
+//     fn new(n: i32, parent: Vec<i32>) -> Self {
+//         let mut list = vec![vec![]; n as usize];
+//         for i in 1..parent.len() {
+//             list[i].push(parent[i] as usize);
+//             let mut res = 1;
+//             for j in 1.. {
+//                 res = TreeAncestor::_get_kth_ancestor(&list, i as i32, 2i32.pow(j as u32));
+//                 if res != -1 {
+//                     list[i].push(res as usize);
+//                 } else {
+//                     break;
+//                 }
+//             }
+//         }
+//         Self { list }
+//     }
+//     fn _get_kth_ancestor(list: &Vec<Vec<usize>>, node: i32, k: i32) -> i32 {
+//         if k == 0 {
+//             node
+//         } else {
+//             let mut l = -1 as i32;
+//             let mut r = (list[node as usize].len() - 1) as i32;
+//             while l < r {
+//                 let m = (l + r + 1) / 2;
+//                 if k >= 2i32.pow(m as u32) {
+//                     l = m;
+//                 } else {
+//                     r = m - 1;
+//                 }
+//             }
+//             if l == -1 {
+//                 l
+//             } else {
+//                 TreeAncestor::_get_kth_ancestor(list, list[node as usize][l as usize] as i32, k - (2i32.pow(l as u32)))
+//             }
+//         }
+//     }
+//     fn get_kth_ancestor(&self, node: i32, k: i32) -> i32 {
+//         TreeAncestor::_get_kth_ancestor(&self.list, node, k)
+//     }
+// }`,
+//         },
     ],
 };
 
