7e3f71bcc20d693f5ef1552df84058f0a0eec14e
liyigang <liyigang@bytedance.com>
2023-06-16 23:15:50 +0800

    feat: 🎸 update static

--- scripts/site.sh
+++ scripts/site.sh
@@ -1,7 +1,7 @@
 pnpm --filter leetcode build
-rm -rf ../packages/site/docs/leetcode
-cp -rf ../packages/leetcode/dist ../packages/site/docs/leetcode
+rm -rf .packages/site/docs/leetcode
+cp -rf .packages/leetcode/dist .packages/site/docs/leetcode
 pnpm --filter site build
-zip -vr ../packages/site/build/site.zip ../packages/site/build
-scp -r ../packages/site/build/site.zip ubuntu@101.34.26.179:~/site.zip 
+zip -vr .packages/site/build/site.zip .packages/site/build
+scp -r .packages/site/build/site.zip ubuntu@101.34.26.179:~/site.zip 
 # sudo unzip -o -d /root/projects/bestlyg /home/ubuntu/site.zip
