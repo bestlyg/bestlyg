0543a32588fdc175b21e97df91021e107f22e611
liyigang <liyigang@bytedance.com>
2023-06-19 10:34:44 +0800

    feat: 🎸 add leetcode no.1262

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '6890. 找出分区值',
-    url: 'https://leetcode.cn/problems/find-the-value-of-the-partition/',
+    name: '1262. 可被三整除的最大和',
+    url: 'https://leetcode.cn/problems/greatest-sum-divisible-by-three/',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个 正 整数数组 nums 。返回表示分区值的整数。`,
+    desc: `给你一个整数数组 nums，请你找出并返回能被三整除的元素最大和。`,
     solutions: [
         //         {
         //             script: Script.TS,
@@ -30,47 +30,78 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.CPP,
-            time: 104,
-            memory: 57.8,
-            desc: '排序后左差比较',
+            time: 24,
+            memory: 26,
+            desc: '求出总和，模3判断多了的数字',
             code: `class Solution {
 public:
-    int findValueOfPartition(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int res = INT_MAX;
-        for (int i = 1; i < nums.size(); i++) {
-            res = min(res, nums[i] - nums[i - 1]);
+    int maxSumDivThree(vector<int>& nums) {
+        int sum = 0;
+        vector<int> v1, v2;
+        for (auto &num : nums) {
+            sum += num;
+            switch (num % 3) {
+                case 1: v1.push_back(num); break;
+                case 2: v2.push_back(num); break;
+            }
         }
+        if (sum % 3 == 0) return sum;
+        sort(v1.begin(), v1.end());
+        sort(v2.begin(), v2.end());
+        if (sum % 3 == 2) swap(v1, v2);
+        int res = v1.size() ? sum - v1[0] : 0;
+        if (v2.size() > 1) res = max(res, sum - v2[0] - v2[1]);
         return res;
     }
 };`,
         },
         {
-            script: Script.PY3,
-            time: 132,
-            memory: 27.8,
-            desc: '同上',
-            code: `class Solution:
-    def findValueOfPartition(self, nums: List[int]) -> int:
-        nums.sort()
-        return min(nums[i] - nums[i - 1] for i in range(1, len(nums)))`,
+            script: Script.CPP,
+            time: 72,
+            memory: 36,
+            desc: 'dp表示余i个数的时候的最大和',
+            code: `class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        vector<int> dp = {0, INT_MIN, INT_MIN};
+        for (auto &num : nums) {
+            auto nextDp = dp;
+            for (int i = 0; i < 3; i++) {
+                int idx = (i + num) % 3;
+                nextDp[idx] = max(nextDp[idx], dp[i] + num);
+            }
+            dp = nextDp;
+        }
+        return dp[0];
+    }
+};`,
         },
-        {
-            script: Script.RUST,
-            time: 24,
-            memory: 3.5,
-            desc: '同上',
-            code: `impl Solution {
-pub fn find_value_of_partition(mut nums: Vec<i32>) -> i32 {
-    nums.sort();
-    (1..nums.len())
-        .into_iter()
-        .map(|i| nums[i] - nums[i - 1])
-        .min()
-        .unwrap()
-}
-}`,
-        },
+//         {
+//             script: Script.PY3,
+//             time: 132,
+//             memory: 27.8,
+//             desc: '同上',
+//             code: `class Solution:
+//     def findValueOfPartition(self, nums: List[int]) -> int:
+//         nums.sort()
+//         return min(nums[i] - nums[i - 1] for i in range(1, len(nums)))`,
+//         },
+//         {
+//             script: Script.RUST,
+//             time: 24,
+//             memory: 3.5,
+//             desc: '同上',
+//             code: `impl Solution {
+// pub fn find_value_of_partition(mut nums: Vec<i32>) -> i32 {
+//     nums.sort();
+//     (1..nums.len())
+//         .into_iter()
+//         .map(|i| nums[i] - nums[i - 1])
+//         .min()
+//         .unwrap()
+// }
+// }`,
+//         },
     ],
 };
 
