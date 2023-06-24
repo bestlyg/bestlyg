3761c1a09bfe01e0cc6ce50ecaaa16c1a33cac94
liyigang <liyigang@bytedance.com>
2023-06-24 15:18:50 +0800

    feat: 🎸 site update image2shadow

--- packages/site/src/components/Applications/image2shadow/index.tsx
+++ packages/site/src/components/Applications/image2shadow/index.tsx
@@ -1,57 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { Upload, Button, Space } from 'antd';
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
+import { Upload, Button, Space, message, InputNumber } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import { useBoolean } from 'ahooks';
+
+function getImageData(image: HTMLImageElement) {
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+    const imageData = ctx.getImageData(0, 0, image.width, image.height).data;
+    return imageData;
+}
 
 export function Image2Shadow() {
     const [file, setFile] = useState<File | null>(null);
-    const [loading, loadingActions] = useBoolean(false);
+    const shadowRef = useRef<HTMLDivElement>();
+    const [options, setOptions] = useState<{
+        imageData: number[];
+        image: HTMLImageElement;
+        blurPx: number;
+    }>({
+        imageData: [],
+        image: new Image(),
+        blurPx: 0,
+    });
     useEffect(() => {
         console.log('Input File', file);
-        if (file) {
-            loadingActions.setTrue();
-            const url = URL.createObjectURL(file);
-            const image = new Image();
-            image.src = url;
-            new Promise<void>((resolve, reject) => {
-                image.onload = () => {
-                    console.log('image load success', image, image.width, image.height);
-                    resolve();
-                };
-                image.onerror = e => {
-                    console.log('image load error', e);
-                    reject();
-                    loadingActions.setFalse();
-                };
-            }).then(() => {
-                const shadowDom = document.getElementById('shadow') as HTMLDivElement;
-                shadowDom.style.marginBottom = image.height + 'px';
-                const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-                canvas.width = image.width;
-                canvas.height = image.height;
-                const ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-                const imageData = ctx.getImageData(0, 0, image.width, image.height).data;
-                const rgbaList: string[] = [];
-                for (let i = 0; i < imageData.length; i += 4) {
-                    const [r, g, b, a] = [
-                        imageData[i],
-                        imageData[i + 1],
-                        imageData[i + 2],
-                        imageData[i + 3],
-                    ];
-                    rgbaList.push(
-                        `${(i / 4) % image.width}px ${Math.floor(
-                            i / 4 / image.width
-                        )}px  0px 1px rgba(${r}, ${g}, ${b}, ${a / 255})`
-                    );
-                }
-                console.log('RGBA List', rgbaList);
-                shadowDom.style.boxShadow = rgbaList.join(',');
-                loadingActions.setFalse();
+        if (!file || !shadowRef.current) return;
+        const url = URL.createObjectURL(file);
+        const image = new Image();
+        image.src = url;
+        new Promise<void>((resolve, reject) => {
+            image.onload = () => {
+                console.log('image load success', image, image.width, image.height);
+                message.success(
+                    `image load success, width : ${image.width} height : ${image.height}`
+                );
+                resolve();
+            };
+            image.onerror = e => {
+                console.log('image load error', e);
+                message.error(`image load error, ${e.toString()}`);
+                reject();
+            };
+        }).then(() => {
+            setOptions({
+                image,
+                imageData: Array.from(getImageData(image)),
+                blurPx: 0,
             });
-        }
+        });
     }, [file]);
+
+    useLayoutEffect(() => {
+        if (!shadowRef.current) return;
+        console.log('options', options);
+        const { image, imageData } = options;
+        const dom: HTMLDivElement = shadowRef.current;
+        dom.style.marginRight = image.width + 'px';
+        dom.style.marginBottom = image.height + 'px';
+        const shadowlist: string[] = [];
+        for (let i = 0; i < imageData.length; i += 4) {
+            const [r, g, b, a] = [
+                imageData[i],
+                imageData[i + 1],
+                imageData[i + 2],
+                imageData[i + 3],
+            ];
+            const [widthOffset, heightOffset] = [
+                (i / 4) % image.width,
+                Math.floor(i / 4 / image.width),
+            ];
+            shadowlist.push(
+                `${widthOffset}px ${heightOffset}px  ${
+                    options.blurPx
+                }px 1px rgba(${r}, ${g}, ${b}, ${a / 255})`
+            );
+        }
+        dom.style.boxShadow = shadowlist.join(',');
+    }, [options]);
+
     return (
         <Space direction="vertical">
             <Upload
@@ -65,10 +93,16 @@ export function Image2Shadow() {
             >
                 <Button icon={<UploadOutlined />}>Select File</Button>
             </Upload>
-            <div>{loading ? 'loading' : 'done'}</div>
-
-            <canvas id="canvas"></canvas>
-            <div id="shadow" style={{ width: 0, height: 0 }}></div>
+            <InputNumber
+                min={0}
+                addonBefore="blurPx"
+                addonAfter="px"
+                value={options.blurPx}
+                onChange={blurPx => {
+                    setOptions({ ...options, blurPx });
+                }}
+            />
+            <div ref={shadowRef} id="image2Shadow" style={{ width: 0, height: 0 }}></div>
         </Space>
     );
 }
