0f00edacbff9b118f76e2235b914f9d8aa487555
liyigang <bestlyg@foxmail.com>
2023-07-09 10:48:30 +0800

    feat: 🎸 add leetcode no.15

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,7 +3,7 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: true,
-    name: '167. 两数之和 II - 输入有序数组',
+    name: '15. 三数之和',
     url: 'https://leetcode.cn/problems/maximum-split-of-positive-even-integers/',
     difficulty: Difficulty.简单,
     tag: [],
@@ -30,8 +30,8 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.CPP,
-            time: 8,
-            memory: 15.2,
+            time: 336,
+            memory: 23.3,
             desc: '二分',
             code: `class Solution {
 public:
@@ -50,56 +50,56 @@ public:
     }
 };`,
         },
-        {
-            script: Script.PY,
-            time: 112,
-            memory: 17.1,
-            desc: '同上',
-            code: `class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        n = len(numbers)
-        for i in range(n):
-            l = i + 1
-            r = n
-            while l < r:
-                m = (l + r) // 2
-                val = numbers[i] + numbers[m]
-                if val < target:
-                    l = m + 1
-                else:
-                    r = m
-            if l != n and numbers[i] + numbers[l] == target:
-                return [i+1, l+1]
-        return []`,
-        },
-        {
-            script: Script.RUST,
-            time: 4,
-            memory: 2.1,
-            desc: '同上',
-            code: `impl Solution {
-    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let n = numbers.len();
-        for i in 0..n {
-            let mut l = i + 1;
-            let mut r = n;
-            while l < r {
-                let m = (l + r) / 2;
-                let val = numbers[i] + numbers[m];
-                if val < target {
-                    l = m + 1;
-                } else {
-                    r = m;
-                }
-            }
-            if l != n && numbers[i] + numbers[l] == target {
-                return vec![(i as i32) + 1, (l as i32) + 1];
-            }
-        }
-        return vec![];
-    }
-}`,
-        },
+//         {
+//             script: Script.PY,
+//             time: 112,
+//             memory: 17.1,
+//             desc: '同上',
+//             code: `class Solution:
+//     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+//         n = len(numbers)
+//         for i in range(n):
+//             l = i + 1
+//             r = n
+//             while l < r:
+//                 m = (l + r) // 2
+//                 val = numbers[i] + numbers[m]
+//                 if val < target:
+//                     l = m + 1
+//                 else:
+//                     r = m
+//             if l != n and numbers[i] + numbers[l] == target:
+//                 return [i+1, l+1]
+//         return []`,
+//         },
+//         {
+//             script: Script.RUST,
+//             time: 4,
+//             memory: 2.1,
+//             desc: '同上',
+//             code: `impl Solution {
+//     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+//         let n = numbers.len();
+//         for i in 0..n {
+//             let mut l = i + 1;
+//             let mut r = n;
+//             while l < r {
+//                 let m = (l + r) / 2;
+//                 let val = numbers[i] + numbers[m];
+//                 if val < target {
+//                     l = m + 1;
+//                 } else {
+//                     r = m;
+//                 }
+//             }
+//             if l != n && numbers[i] + numbers[l] == target {
+//                 return vec![(i as i32) + 1, (l as i32) + 1];
+//             }
+//         }
+//         return vec![];
+//     }
+// }`,
+//         },
     ],
 };
 
