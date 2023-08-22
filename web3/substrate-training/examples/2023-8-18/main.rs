f470b8c24cd68ea87e2d7643328233fe5f0fe07c
liyigang <bestlyg@foxmail.com>
2023-08-22 22:39:11 +0800

    feat: 🎸 update web3/substrate-training

--- web3/substrate-training/examples/2023-8-18/main.rs
+++ web3/substrate-training/examples/2023-8-18/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 /// # cargo run --example 2023-8-18
 /// 基础要求：固定类型（比如i32）的数组排序
 /// 提高部分：能够使用template和PartialOrd实现对任意类型的排序
@@ -14,10 +16,8 @@ fn sort1(list: &mut Vec<i32>) {
 }
 
 /// 对任意结实现了 比较的结构体 进行冒泡排序
-fn sort<T: PartialOrd>(
-    /// 用切片适配各种 数组 （Vec， 固定数组等）
-    list: &mut [T],
-) {
+/// 用切片适配各种 数组 （Vec， 固定数组等）
+fn sort<T: PartialOrd>(list: &mut [T]) {
     for i in 0..list.len() {
         for j in 0..list.len() - 1 - i {
             if list[j + 1].partial_cmp(&list[j]).unwrap().is_lt() {
@@ -27,16 +27,56 @@ fn sort<T: PartialOrd>(
     }
 }
 
-fn main() {
-    let mut list = vec![];
-    for _ in 0..20 {
-        let num = rand::random::<i32>() % 1000;
-        list.push(num);
+#[derive(Debug, PartialEq)]
+struct Object {
+    val: i32,
+}
+impl Object {
+    fn new_random() -> Self {
+        let val = rand::random::<i32>() % 1000;
+        Self { val }
     }
-    println!("List: {list:?}");
-    // sort1(&mut list);
-    // println!("Sorted List: {list:?}");
+}
+impl PartialOrd for Object {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.val.partial_cmp(&other.val)
+    }
+}
 
-    sort(&mut list);
-    println!("Sorted List: {list:?}");
+impl Display for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "O({})", self.val)
+    }
+}
+
+fn log<T: Display>(title: &str, list: &[T]) {
+    print!("{:16}: [", title);
+    for num in list {
+        print!("{},", num);
+    }
+    println!("]");
+}
+
+fn main() {
+    let split = "=".to_string().repeat(100);
+    {
+        let mut list = vec![];
+        for _ in 0..20 {
+            let num = rand::random::<i32>() % 1000;
+            list.push(num);
+        }
+        log("List", &list);
+        sort1(&mut list);
+        log("Sorted List", &list);
+    }
+    println!("{}", split);
+    {
+        let mut list = vec![];
+        for _ in 0..20 {
+            list.push(Object::new_random());
+        }
+        log("List", &list);
+        sort(&mut list);
+        log("Sorted List", &list);
+    }
 }
