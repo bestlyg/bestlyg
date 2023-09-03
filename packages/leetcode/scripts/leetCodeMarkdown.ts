8abfee31ede517ffea29911fe6c1a55264ab4948
liyigang <bestlyg@foxmail.com>
2023-09-03 13:18:51 +0800

    feat: 🎸 add leetcode no.1921

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2511. 最多可以摧毁的敌人城堡数目',
-    url: 'https://leetcode.cn/problems/maximum-enemy-forts-that-can-be-captured',
+    name: '1921. 消灭怪物的最大数量',
+    url: 'https://leetcode.cn/problems/eliminate-maximum-number-of-monsters',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你返回 最多 可以摧毁的敌人城堡数目。`,
+    desc: `返回在你输掉游戏前可以消灭的怪物的 最大 数量。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -27,72 +27,53 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.CPP,
-            time: 4,
-            memory: 7.41,
-            desc: '遍历',
+            time: 104,
+            memory: 86.8,
+            desc: '排序',
             code: `class Solution {
 public:
-    int captureForts(vector<int>& forts) {
-        int res = 0, p0 = -1, p1 = -1;
-        for (int i = 0; i < forts.size(); i++) {
-            int fort = forts[i];
-            if (fort == 1) {
-                if (p0 != -1 && p0 > p1) res = max(res, i - 1 - p0);
-                p1 = i;
-            } else if (fort == -1) {
-                if (p1 != -1 && p1 > p0) res = max(res, i - 1 - p1);
-                p0 = i;
-            }
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        vector<double> arr;
+        for (int i = 0; i < dist.size(); i++) arr.push_back(1.0 * dist[i] / speed[i]);
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < dist.size(); i++) {
+            if (i >= arr[i]) return i;
         }
-        return res;
+        return dist.size();
     }
 };`,
         },
         {
             script: Script.PY,
-            time: 60,
-            memory: 15.82,
+            time: 168,
+            memory: 30.3,
             desc: '同上',
             code: `class Solution:
-    def captureForts(self, forts: List[int]) -> int:
-        res = 0
-        p0 = p1 = -1
-        for i in range(len(forts)):
-            fort = forts[i]
-            if fort == 1:
-                if p0 != -1 and p0 > p1:
-                    res = max(res, i - p0 - 1)
-                p1 = i
-            elif fort == -1:
-                if p1 != -1 and p1 > p0:
-                    res = max(res, i - p1 - 1)
-                p0 = i
-        return res`,
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        arr = [dist[i] / speed[i] for i in range(len(dist))]
+        arr.sort()
+        for i in range(len(dist)):
+            if i >= arr[i]: return i
+        return len(dist)`,
         },
         {
             script: Script.RUST,
-            time: 0,
-            memory: 1.93,
+            time: 32,
+            memory: 4.1,
             desc: '同上',
             code: `impl Solution {
-    pub fn capture_forts(forts: Vec<i32>) -> i32 {
-        let mut res = 0i32;
-        let (mut p0, mut p1) = (-1i32, -1i32);
-        for i in 0..forts.len() {
-            let fort = forts[i];
-            if fort == 1 {
-                if p0 != -1 && p0 > p1 {
-                    res = res.max((i as i32) - 1 - p0);
-                }
-                p1 = i as i32;
-            } else if fort == -1 {
-                if p1 != -1 && p1 > p0 {
-                    res = res.max((i as i32) - 1 - p1);
-                }
-                p0 = i as i32;
+    pub fn eliminate_maximum(dist: Vec<i32>, speed: Vec<i32>) -> i32 {
+        let mut arr = vec![];
+        for i in 0..dist.len() {
+            arr.push(dist[i] as f64 / speed[i] as f64);
+        }
+        arr.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        for i in 0..dist.len() {
+            if i as f64 >= arr[i] {
+                return i as i32;
             }
         }
-        res as i32
+        dist.len() as i32
     }
 }`,
         },
