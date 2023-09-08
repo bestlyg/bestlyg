b9e46c0c6ace83927e52d6f8d54636e0474b1d6c
liyigang <bestlyg@foxmail.com>
2023-09-08 10:09:38 +0800

    feat: 🎸 add leetcode no.2651

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist:! true,
-    name: '2594. 修车的最少时间',
-    url: 'https://leetcode.cn/problems/minimum-time-to-repair-cars/',
+    name: '2651. 计算列车到站时间',
+    url: 'https://leetcode.cn/problems/calculate-delayed-arrival-time',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个整数数组 ranks ，表示一些机械工的 能力值 。ranksi 是第 i 位机械工的能力值。能力值为 r 的机械工可以在 r * n2 分钟内修好 n 辆车。同时给你一个整数 cars ，表示总共需要修理的汽车数目。请你返回修理所有汽车 最少 需要多少时间。`,
+    desc: `给你一个正整数 arrivalTime 表示列车正点到站的时间（单位：小时），另给你一个正整数 delayedTime 表示列车延误的小时数。返回列车实际到站的时间。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -27,71 +27,33 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.CPP,
-            time: 112,
-            memory: 51.34,
-            desc: '二分答案',
+            time: 0,
+            memory: 2.1,
+            desc: '相加后取模',
             code: `class Solution {
 public:
-    typedef long long ll;
-    ll repairCars(vector<int>& ranks, int cars) {
-        auto comp = [&](ll t) {
-            ll res = 0;
-            for (auto &rank : ranks) {
-                res += floor(sqrt(1.0 * t / rank));
-            }
-            return res;
-        };
-        ll l = 0, r = LLONG_MAX;
-        while (l < r) {
-            ll m = (r - l) / 2 + l;
-            if (comp(m) >= cars) r = m;
-            else l = m + 1;
-        }
-        return l;
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
     }
 };`,
         },
         {
             script: Script.PY,
-            time: 1292,
-            memory: 20.16,
+            time: 48,
+            memory: 16.1,
             desc: '同上',
             code: `class Solution:
-    def repairCars(self, ranks: List[int], cars: int) -> int:
-        l = 0
-        r = 2 ** 63 - 1
-        while l < r:
-            m = (r - l) // 2 + l
-            if sum(floor(sqrt(m / rank)) for rank in ranks) >= cars:
-                r = m
-            else:
-                l = m + 1
-        return l`,
+    def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
+        return (arrivalTime + delayedTime) % 24`,
         },
         {
             script: Script.RUST,
-            time: 56,
+            time: 0,
             memory: 2.98,
             desc: '同上',
             code: `impl Solution {
-    pub fn repair_cars(ranks: Vec<i32>, cars: i32) -> i64 {
-        let cars = cars as i64;
-        let mut l = 0;
-        let mut r = i64::MAX;
-        while l < r {
-            let m = (r - l) / 2 + l;
-            if ranks
-                .iter()
-                .map(|rank| (m as f64 / *rank as f64).sqrt().floor() as i64)
-                .sum::<i64>()
-                >= cars
-            {
-                r = m;
-            } else {
-                l = m + 1;
-            }
-        }
-        l
+    pub fn find_delayed_arrival_time(arrival_time: i32, delayed_time: i32) -> i32 {
+        (arrival_time + delayed_time) % 24
     }
 }`,
         },
