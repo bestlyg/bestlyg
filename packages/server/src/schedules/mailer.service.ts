0e4c3e060b65d53f1ea4df4ebfb6ba89442fe3e0
liyigang <bestlyg@foxmail.com>
2023-09-11 15:08:24 +0800

    feat: 🎸 server update schedule

--- packages/server/src/schedules/mailer.service.ts
+++ packages/server/src/schedules/mailer.service.ts
@@ -37,13 +37,6 @@ export class MailerTaskService {
   async yzx_mailerTask(name: string, content: string) {
     await this.mailer.send(['2428047022@qq.com'], `定时提醒-${name}`, content);
   }
-
-  // @Cron('* * * * * *')
-  // async test() {
-  //   console.log('test mail')
-  //   await this.lyg_mailerTask('test', 'test content');
-  // }
-
   @Cron('0 0 8,20 * * *')
   async ownerDaily() {
     const now = dayjs();
@@ -73,57 +66,23 @@ export class MailerTaskService {
       ),
     );
   }
-  @Cron('0 30 20 * * *')
-  async ownerXXYX() {
-    await this.lyg_mailerTask('定时提醒-晓晓优选', '晓晓优选记得提交');
+  @Cron('0 55 9 * * 3')
+  async coupon3() {
+    this.yzx_mailerTask('抢劵', '抢劵');
   }
-  //交行抢5折券 每周五提醒
-  @Cron('0 55 9 * * *')
-  async yzx_jh() {
-    if (dayjs().day() !== 5) return;
-    await this.yzx_mailerTask('10点钟交行抢5折券', '10点钟交行抢5折券');
+  @Cron('0 55 10 * * 5')
+  async coupon5() {
+    this.yzx_mailerTask('抢劵', '抢劵');
   }
-  // 建行生活 每日
-  @Cron('0 0 12 * * *')
-  async yzx_jhsh() {
-    await this.yzx_mailerTask('建行生活', '建行生活记得签到');
-    await this.lyg_mailerTask('建行生活', '建行生活记得签到');
+  @Cron('0 30 7 * * *')
+  async daily() {
+    this.yzx_mailerTask(
+      '建行/番茄/支付宝小鸡/淘金币',
+      '建行/番茄/支付宝小鸡/淘金币',
+    );
   }
-  // 每日大姨妈提醒
   @Cron('0 0 22 * * *')
-  async yzx_menses() {
-    const format = 'YYYY-MM-DD';
-    const date_now = dayjs();
-    const cnt = date_now.diff(this.menses, 'day');
-    const set = new Set([21, 24, 28]);
-    if (set.has(cnt)) {
-      await this.yzx_mailerTask(
-        '大姨妈提醒',
-        `距离上一次大姨妈(${this.menses.format(format)})已有${cnt}天。`,
-      );
-    }
-  }
-  // 腾讯视频 每个月1号早上8点
-  @Cron('0 0 8 1 * *')
-  async yzx_txsp() {
-    await this.yzx_mailerTask('腾讯视频提醒', '腾讯视频签到');
-  }
-  // 腾讯视频 每个月最后一天晚上8点
-  @Cron('0 0 20 * * *')
-  async yzx_txsp_last() {
-    const day = dayjs().date();
-    const lastDay = dayjs().daysInMonth();
-    if (lastDay - day <= 2) {
-      await this.yzx_mailerTask('腾讯视频提醒', '腾讯视频签到');
-    }
-  }
-  // 维生素，早上8.50 晚上7.00
-  @Cron('0 50 8 * * *')
-  async yzx_wss1() {
-    await this.yzx_mailerTask('维生素提醒', '补充钙片维生素');
-  }
-  @Cron('0 0 19 * * *')
-  async yzx_wss2() {
-    await this.yzx_mailerTask('维生素提醒', '补充钙片维生素');
+  async medicine() {
+    this.yzx_mailerTask('吃药', '吃药');
   }
 }
