f39c243ab630fa66b26e22ee491401d09971b356
liyigang <bestlyg@foxmail.com>
2023-09-23 11:23:13 +0800

    feat: 🎸 add leetcode no.1993

--- packages/leetcode/python3/main.py
+++ packages/leetcode/python3/main.py
@@ -2,26 +2,61 @@ from math import *
 from preclude import *
 
 
-class Solution:
-    def distMoney(self, money: int, children: int) -> int:
-        if money < children:
-            return -1
-        cnt = money // 8
-        surplus_money = money % 8
-        surplus_children = children - cnt
-        if cnt == children:
-            if surplus_money == 0:
-                return children
-            return children - 1
-        if cnt > children:
-            return children - 1
-        if surplus_money == surplus_children:
-            return cnt
-        if surplus_money > surplus_children:
-            if surplus_children == 1 and surplus_money == 4:
-                return cnt - 1
-            return cnt
-        return cnt - ceil((surplus_children - surplus_money) / 7)
+class Node:
+    def __init__(self, val: int):
+        self.val = val
+        self.parent = None
+        self.children = []
+        self.lock_state = 0
+
+    def lock(self, user: int) -> bool:
+        if self.lock_state:
+            return False
+        self.lock_state = user
+        return True
+
+    def unlock(self, user: int) -> bool:
+        if self.lock_state != user:
+            return False
+        self.lock_state = 0
+        return True
+
+    def unlock_children(self) -> bool:
+        for node in self.children:
+            node.lock_state = 0
+            node.unlock_children()
+
+    def is_lock(self) -> bool:
+        return self.lock_state != 0
+
+    def is_parent_unlock(self) -> bool:
+        return not self.parent or not self.parent.is_lock() and self.parent.is_parent_unlock()
+
+    def exist_children_lock(self) -> bool:
+        return any(child.is_lock() or child.exist_children_lock() for child in self.children)
+
+
+class LockingTree:
+    def __init__(self, parent: List[int]):
+        self.nodes = [Node(i) for i in range(len(parent))]
+        self.root = self.nodes[0]
+        for i in range(1, len(parent)):
+            self.nodes[i].parent = self.nodes[parent[i]]
+            self.nodes[parent[i]].children.append(self.nodes[i])
+
+    def lock(self, num: int, user: int) -> bool:
+        return self.nodes[num].lock(user)
+
+    def unlock(self, num: int, user: int) -> bool:
+        return self.nodes[num].unlock(user)
+
+    def upgrade(self, num: int, user: int) -> bool:
+        node = self.nodes[num]
+        if not node.is_lock() and node.is_parent_unlock() and node.exist_children_lock():
+            node.lock(user)
+            node.unlock_children()
+            return True
+        return False
 
 
 def main():
--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2591. 将钱分给最多的儿童',
-    url: 'https://leetcode.cn/problems/distribute-money-to-maximum-children',
+    name: '1993. 树上的操作',
+    url: 'https://leetcode.cn/problems/operations-on-tree',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你按照上述规则分配金钱，并返回 最多 有多少个儿童获得 恰好 8 美元。如果没有任何分配方案，返回 -1 。`,
+    desc: `给你一棵 n 个节点的树，编号从 0 到 n - 1 ，以父节点数组 parent 的形式给出，其中 parent[i] 是第 i 个节点的父节点。树的根节点为 0 号节点，所以 parent[0] = -1 ，因为它没有父节点。你想要设计一个数据结构实现树里面对节点的加锁，解锁和升级操作。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -25,87 +25,82 @@ const leetCodeMarkdown: Markdown = {
         //     desc: '归并排序',
         //     code: ``,
         // },
-        {
-            script: Script.CPP,
-            time: 4,
-            memory: 6.05,
-            desc: '贪心计算',
-            code: `class Solution {
-public:
-    int distMoney(int money, int children) {
-        if (money < children) return -1;
-        int cnt = money / 8, surplus_money = money % 8, surplus_children = children - cnt;
-        if (cnt == children) return surplus_money == 0 ? children : children - 1;
-        if (cnt > children) return children - 1;
-        if (surplus_money == surplus_children) return cnt;
-        if (surplus_money > surplus_children) return surplus_children == 1 and surplus_money == 4 ? cnt - 1 : cnt;
-        return cnt - ceil(1.0 * (surplus_children - surplus_money) / 7.0);
-    }
-};`,
-        },
+        // {
+        //     script: Script.CPP,
+        //     time: 4,
+        //     memory: 6.05,
+        //     desc: '贪心计算',
+        //     code: ``,
+        // },
         {
             script: Script.PY,
             time: 52,
             memory: 15.61,
-            desc: '同上',
-            code: `class Solution:
-    def distMoney(self, money: int, children: int) -> int:
-        if money < children:
-            return -1
-        cnt = money // 8
-        surplus_money = money % 8
-        surplus_children = children - cnt
-        if cnt == children:
-            if surplus_money == 0:
-                return children
-            return children - 1
-        if cnt > children:
-            return children - 1
-        if surplus_money == surplus_children:
-            return cnt
-        if surplus_money > surplus_children:
-            if surplus_children == 1 and surplus_money == 4:
-                return cnt - 1
-            return cnt
-        return cnt - ceil((surplus_children - surplus_money) / 7)
-`,
+            desc: '模拟',
+            code: `class Node:
+    def __init__(self, val: int):
+        self.val = val
+        self.parent = None
+        self.children = []
+        self.lock_state = 0
+
+    def lock(self, user: int) -> bool:
+        if self.lock_state:
+            return False
+        self.lock_state = user
+        return True
+
+    def unlock(self, user: int) -> bool:
+        if self.lock_state != user:
+            return False
+        self.lock_state = 0
+        return True
+
+    def unlock_children(self) -> bool:
+        for node in self.children:
+            node.lock_state = 0
+            node.unlock_children()
+
+    def is_lock(self) -> bool:
+        return self.lock_state != 0
+
+    def is_parent_unlock(self) -> bool:
+        return not self.parent or not self.parent.is_lock() and self.parent.is_parent_unlock()
+
+    def exist_children_lock(self) -> bool:
+        return any(child.is_lock() or child.exist_children_lock() for child in self.children)
+
+
+class LockingTree:
+    def __init__(self, parent: List[int]):
+        self.nodes = [Node(i) for i in range(len(parent))]
+        self.root = self.nodes[0]
+        for i in range(1, len(parent)):
+            self.nodes[i].parent = self.nodes[parent[i]]
+            self.nodes[parent[i]].children.append(self.nodes[i])
+
+    def lock(self, num: int, user: int) -> bool:
+        return self.nodes[num].lock(user)
+
+    def unlock(self, num: int, user: int) -> bool:
+        return self.nodes[num].unlock(user)
+
+    def upgrade(self, num: int, user: int) -> bool:
+        node = self.nodes[num]
+        if not node.is_lock() and node.is_parent_unlock() and node.exist_children_lock():
+            node.lock(user)
+            node.unlock_children()
+            return True
+        return False`,
         },
-        {
-            script: Script.RUST,
-            time: 0,
-            memory: 2.58,
-            desc: '同上',
-            code: `impl Solution {
-    pub fn dist_money(money: i32, children: i32) -> i32 {
-        if money < children {
-            -1
-        } else {
-            let (cnt, surplus_money) = (money / 8, money % 8);
-            let surplus_children = children - cnt;
-            if cnt == children {
-                if surplus_money == 0 {
-                    children
-                } else {
-                    children - 1
-                }
-            } else if cnt > children {
-                children - 1
-            } else if surplus_money == surplus_children {
-                cnt
-            } else if surplus_money > surplus_children {
-                if surplus_children == 1 && surplus_money == 4 {
-                    cnt - 1
-                } else {
-                    cnt
-                }
-            } else {
-                cnt - ((surplus_children - surplus_money) as f64 / 7.0).ceil() as i32
-            }
-        }
-    }
-}
-`,
-        },
+//         {
+//             script: Script.RUST,
+//             time: 0,
+//             memory: 2.58,
+//             desc: '同上',
+//             code: `
+// `,
+//         },
     ],
 };
 
