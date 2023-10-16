7df8988830251e31c0cf0e867ca996510b632019
liyigang <bestlyg@foxmail.com>
2023-10-16 22:54:38 +0800

    feat: 🎸 update best-tools

--- packages/best/packages/tools/src/build/update-package-json.ts
+++ packages/best/packages/tools/src/build/update-package-json.ts
@@ -3,33 +3,41 @@ import fs from 'fs-extra';
 import { merge } from 'lodash';
 
 const TAG = 'best-tools';
+interface BestToolsPackageJson {
+    extends?: string | string[];
+    vars?: any;
+    template?: Record<string, Record<string, any>>;
+}
 
-function dfs({
-    vars,
-    path,
-    pathSet,
-}: {
-    vars: Record<string, any>;
-    path: string;
-    pathSet: Set<string>;
-}) {
-    const info = require(path);
-    if (!info[TAG]) return info;
-    const nextVars = {
-        ...info[TAG],
-        ...vars,
-    };
+function loadExtends(fn: (p: string) => void, info: BestToolsPackageJson['extends']) {
+    if (typeof info === 'string') {
+        return [fn(info)];
+    } else if (Array.isArray(info)) {
+        return info.map(p => fn(p));
+    }
+}
+
+function loadFile(map: Record<string, Record<string, any>>, path: string) {
+    if (!map[path]) return;
+    const json = require(path);
+    map[path] = json;
+    const fn = loadFile.bind(null, map);
+    loadExtends(fn, json?.[TAG]?.['extends'] ?? []);
+}
+
+function loadVars(map: Record<string, Record<string, any>>, path: string) {
+    const res: BestToolsPackageJson = map[path][TAG] ?? {};
+    const vars: Record<string, any> = {};
+    // const vars = map[path][TAG]?.vars;
+    const fn = loadVars.bind(null, map);
 }
 
 export function updatePackageJson(path: string) {
-    const vars: Record<string, any> = {};
     const pathSet = new Set<string>([path]);
+    const map: Record<string, Record<string, any>> = {};
     try {
-        return dfs({
-            path,
-            vars,
-            pathSet,
-        });
+        loadFile(map, path);
+        const vars: Record<string, any> = {};
     } catch (err) {
         error('Update package json error.', err);
     }
