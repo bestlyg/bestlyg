35254fb2903120adc21cb99353ea08e8bc654c53
liyigang <bestlyg@foxmail.com>
2023-10-18 11:29:00 +0800

    feat: 🎸 add leetcode no.2530

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,9 +2,9 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: !true,
-    name: '2652. 倍数求和',
-    url: 'https://leetcode.cn/problems/sum-multiples',
+    exist: true,
+    name: '2530. 执行 K 次操作后的最大分数',
+    url: 'https://leetcode.cn/problems/maximal-score-after-applying-k-operations',
     difficulty: Difficulty.简单,
     tag: [],
     desc: `给你一个正整数 n ，请你计算在 [1，n] 范围内能被 3、5、7 整除的所有整数之和。返回一个整数，用于表示给定范围内所有满足约束条件的数字之和。`,
@@ -34,19 +34,42 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 92,
-            memory: 16,
-            desc: '遍历',
+            time: 1604,
+            memory: 44.59,
+            desc: '堆存储',
+            code: `class Node:
+        def __init__(self, val):
+            self.val = val
+        def __lt__(self, o):
+            return o.val < self.val
+    
+    class Solution:
+        def maxKelements(self, nums: List[int], k: int) -> int:
+            q = [Node(num) for num in nums]
+            heapify(q)
+            res = 0
+            while k:
+                res += q[0].val
+                heappush(q, Node(ceil(heappop(q).val / 3)))
+                k -= 1
+            return res
+    `,
+        },
+        {
+            script: Script.PY,
+            time: 344,
+            memory: 29.59,
+            desc: '堆存储',
             code: `class Solution:
-    def sumOfMultiples(self, n: int) -> int:
-        return sum(
-            list(
-                filter(
-                    lambda num: num % 3 == 0 or num % 5 == 0 or num % 7 == 0, 
-                    [num for num in range(1, n + 1)]
-                )
-            )
-        )`,
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        for i in range(len(nums)): nums[i] *= -1
+        heapify(nums)
+        res = 0
+        for _ in range(k):
+            res += -nums[0]
+            heappush(nums, -ceil(heappop(nums) / -3))
+        return res
+`,
         },
         // {
         //     script: Script.RUST,
