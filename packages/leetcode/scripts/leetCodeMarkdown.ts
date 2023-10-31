6b3e9ebf9c66df42083fc69e9c3d6c5a1e9d969e
liyigang <bestlyg@foxmail.com>
2023-10-31 10:42:19 +0800

    feat: 🎸 add leetcode no.2003

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '275. H 指数 II',
-    url: 'https://leetcode.cn/problems/h-index-ii',
+    exist: !true,
+    name: '2003. 每棵子树内缺失的最小基因值',
+    url: 'https://leetcode.cn/problems/smallest-missing-genetic-value-in-each-subtree',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个整数数组 citations ，其中 citations[i] 表示研究者的第 i 篇论文被引用的次数，citations 已经按照 升序排列 。计算并返回该研究者的 h 指数。`,
+    desc: `请你返回一个数组 ans ，长度为 n ，其中 ans[i] 是以节点 i 为根的子树内 缺失 的 最小 基因值。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -34,20 +34,59 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 40,
-            memory: 21.5,
-            desc: '二分搜索',
+            time: 812,
+            memory: 172.67,
+            desc: 'dfs时用set存储所有值',
             code: `class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        l = 0
-        r = len(citations)
-        while l < r:
-            m = (l + r) // 2
-            if citations[m] >= len(citations) - m:
-                r = m
-            else:
-                l = m + 1
-        return len(citations) - l`,
+    def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
+        n = len(parents)
+        nodes = [[] for i in range(n)]
+        for i in range(1, n): nodes[parents[i]].append(i)
+        ans = [1 for _ in range(n)]
+        def dfs(idx: int) -> (int, Set[int]):
+            last = 1
+            s = set([nums[idx]])
+            for child in nodes[idx]:
+                resLast, resSet = dfs(child)
+                last = max(last, resLast)
+                if len(resSet) > len(s):
+                    resSet |= s
+                    s = resSet
+                else:
+                    s |= resSet
+            while last in s: last += 1
+            ans[idx] = last
+            return last, s
+        dfs(0)
+        return ans`,
+        },
+        {
+            script: Script.PY,
+            time: 452,
+            memory: 66.3,
+            desc: '自底向上，只遍历存在1的树',
+            code: `class Solution:
+    def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
+        n = len(parents)
+        nodes = [[] for i in range(n)]
+        for i in range(1, n): nodes[parents[i]].append(i)
+        ans = [1 for _ in range(n)]
+        used = [False for _ in range(n)]
+        s = set()
+        def dfs(idx: int):
+            if used[idx]: return
+            used[idx] = True
+            s.add(nums[idx])
+            for child in nodes[idx]: dfs(child)
+        
+        cur = nums.index(1) if 1 in nums else -1
+        last = 1
+        while cur != -1:
+            dfs(cur)
+            while last in s: last += 1
+            ans[cur] = last
+            cur = parents[cur]
+        return ans`,
         },
         // {
         //     script: Script.RUST,
