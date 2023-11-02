630749fd99514ef44ddc15c2a0ef72e6e225c6d7
liyigang <bestlyg@foxmail.com>
2023-11-02 14:01:41 +0800

    feat: 🎸 add leetcode no.2103

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2127. 参加会议的最多员工数',
-    url: 'https://leetcode.cn/problems/maximum-employees-to-be-invited-to-a-meeting',
+    name: '2103. 环和杆',
+    url: 'https://leetcode.cn/problems/rings-and-rods',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个下标从 0 开始的整数数组 favorite ，其中 favorite[i] 表示第 i 位员工喜欢的员工。请你返回参加会议的 最多员工数目 。`,
+    desc: `找出所有集齐 全部三种颜色 环的杆，并返回这种杆的数量。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -34,40 +34,16 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 552,
-            memory: 137.49,
-            desc: '判断环的数量，如果环内超过2个，找最大数量的环，如果只有2个，找两个点的最大延伸链',
+            time: 40,
+            memory: 15.66,
+            desc: '遍历',
             code: `class Solution:
-    def maximumInvitations(self, favorite: List[int]) -> int:
-        n = len(favorite)
-        deg = [0 for _ in range(n)]
-        for i in range(n): deg[favorite[i]] += 1
-        q = deque(i for i in range(n) if deg[i] == 0)
-        nexts = [[] for _ in range(n)]
-        while q:
-            i = q.popleft()
-            nexts[favorite[i]].append(i)
-            deg[favorite[i]] -= 1
-            if deg[favorite[i]] == 0: q.append(favorite[i])
-        def dfs(idx: int) -> int:
-            res = 1
-            for next_i in nexts[idx]:
-                res = max(res, dfs(next_i) + 1)
-            return res
-
-        max_ring = sum_chain = 0
-        for i in range(n):
-            if deg[i] == 0: continue
-            deg[i] = 0
-            ring = 1
-            next_i = favorite[i]
-            while next_i != i:
-                deg[next_i] = 0
-                ring += 1
-                next_i = favorite[next_i]
-            if ring == 2: sum_chain += dfs(i) + dfs(favorite[i])
-            else: max_ring = max(max_ring, ring)
-        return max(sum_chain, max_ring)`,
+    def countPoints(self, rings: str) -> int:
+        map = { 'R': 0, 'G': 1, 'B': 2}
+        arr = [0] * 10
+        for i in range(0, len(rings), 2):
+            arr[int(rings[i + 1])] |= 1 << map[rings[i]]
+        return sum(v == 0b111 for v in arr)`,
         },
         // {
         //     script: Script.RUST,
