5d3afb699df48bf3c83d9489cd97319172be4252
liyigang <bestlyg@foxmail.com>
2023-11-12 16:09:02 +0800

    feat: 🎸 add leetcode no.715

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '765. 情侣牵手',
-    url: 'https://leetcode.cn/problems/successful-pairs-of-spells-and-potions',
+    exist: !true,
+    name: '715. Range 模块',
+    url: 'https://leetcode.cn/problems/range-module',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你返回一个长度为 n 的整数数组 pairs，其中 pairs[i] 是能跟第 i 个咒语成功组合的 药水 数目。`,
+    desc: `Range模块是跟踪数字范围的模块。设计一个数据结构来跟踪表示为 半开区间 的范围并查询它们。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -25,29 +25,125 @@ const leetCodeMarkdown: Markdown = {
         //     desc: 'dp',
         //     code: ``,
         // },
-        // {
-        //     script: Script.CPP,
-        //     time: 716,
-        //     memory: 172.36,
-        //     desc: '同上',
-        //     code: ``,
-        // },
+        {
+            date: new Date('2022.06.20').getTime(),
+            script: Script.CPP,
+            time: 200,
+            memory: 66.95,
+            desc: '有序集合',
+            code: `class RangeModule {
+public:
+    RangeModule() {}
+    
+    void addRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it != intervals.begin()) {
+            auto start = prev(it);
+            if (start->second >= right) {
+                return;
+            }
+            if (start->second >= left) {
+                left = start->first;
+                intervals.erase(start);
+            }
+        }
+        while (it != intervals.end() && it->first <= right) {
+            right = max(right, it->second);
+            it = intervals.erase(it);
+        }
+        intervals[left] = right;
+    }
+    
+    bool queryRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it == intervals.begin()) {
+            return false;
+        }
+        it = prev(it);
+        return right <= it->second;
+    }
+    
+    void removeRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it != intervals.begin()) {
+            auto start = prev(it);
+            if (start->second >= right) {
+                int ri = start->second;
+                if (start->first == left) {
+                    intervals.erase(start);
+                }
+                else {
+                    start->second = left;
+                }
+                if (right != ri) {
+                    intervals[right] = ri;
+                }
+                return;
+            }
+            else if (start->second > left) {
+                start->second = left;
+            }
+        }
+        while (it != intervals.end() && it->first < right) {
+            if (it->second <= right) {
+                it = intervals.erase(it);
+            }
+            else {
+                intervals[right] = it->second;
+                intervals.erase(it);
+                break;
+            }
+        }
+    }
+
+private:
+    map<int, int> intervals;
+};`,
+        },
         {
             script: Script.PY,
-            time: 48,
-            memory: 15.68,
-            desc: '贪心的每次没有匹配上去重置',
-            code: `class Solution:
-    def minSwapsCouples(self, row: List[int]) -> int:
-        n = len(row)
-        map = {row[i]: i for i in range(n)}
-        ans = 0
-        for i in range(0, n, 2):
-            if row[i] ^ 1 != row[i + 1]:
-                map[row[i + 1]] = map[row[i] ^ 1]
-                row[map[row[i] ^ 1]], row[i + 1] = row[i + 1], row[map[row[i] ^ 1]]
-                ans += 1
-        return ans`,
+            time: 480,
+            memory: 20.4,
+            desc: '有序数组，每次合并数组',
+            code: `from sortedcontainers import SortedList
+class RangeModule:
+    def __init__(self):
+        self.arr = SortedList([(-1, -1), (inf, inf)])
+
+    def addRange(self, left: int, right: int) -> None:
+        arr = self.arr
+        item = (left, right)
+        idx = arr.bisect_left(item)
+        if arr[idx - 1][1] >= left:
+            item = (arr[idx - 1][0], max(arr[idx - 1][1], item[1]))
+            arr.pop(idx - 1)
+            idx -= 1
+        while arr[idx][0] <= item[1]:
+            item = (item[0], max(arr[idx][1], item[1]))
+            arr.pop(idx)
+        arr.add(item)
+
+    def queryRange(self, left: int, right: int) -> bool:
+        arr = self.arr
+        idx = arr.bisect_right((left, inf))
+        return arr[idx - 1][0] <= left and arr[idx - 1][1] >= right
+
+    def removeRange(self, left: int, right: int) -> None:
+        arr = self.arr
+        idx = arr.bisect_left((left, right))
+        if arr[idx - 1][1] > left:
+            if arr[idx - 1][1] > right:
+                arr.add((right, arr[idx - 1][1]))
+            item = (arr[idx - 1][0], left)
+            arr.pop(idx - 1)
+            arr.add(item)
+        while arr[idx][1] <= right:
+            arr.pop(idx)
+        if arr[idx][0] <= right:
+            item = (right, arr[idx][1])
+            arr.pop(idx)
+            if item[0] != item[1]:
+                arr.add(item)`,
         },
         // {
         //     script: Script.RUST,
