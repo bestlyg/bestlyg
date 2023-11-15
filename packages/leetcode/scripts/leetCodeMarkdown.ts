a74616061b7d8676c39f5533724d64fb98fca4b5
liyigang <bestlyg@foxmail.com>
2023-11-15 09:45:14 +0800

    feat: 🎸 add leetco.2656

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '1334. 阈值距离内邻居最少的城市',
-    url: 'https://leetcode.cn/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance',
+    exist:! true,
+    name: '2656. K 个元素的最大和',
+    url: 'https://leetcode.cn/problems/maximum-sum-with-exactly-k-elements',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `有 n 个城市，按从 0 到 n-1 编号。给你一个边数组 edges，其中 edges[i] = [fromi, toi, weighti] 代表 fromi 和 toi 两个城市之间的双向加权边，距离阈值是一个整数 distanceThreshold。返回能通过某些路径到达其他城市数目最少、且路径距离 最大 为 distanceThreshold 的城市。如果有多个这样的城市，则返回编号最大的城市。`,
+    desc: `给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。请你返回执行以上操作恰好 k 次后的最大得分。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -35,33 +35,12 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 268,
-            memory: 17,
-            desc: '迪杰特斯拉短路算法',
+            time: 52,
+            memory: 15.9,
+            desc: '等差数列',
             code: `class Solution:
-    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        nodes = [[] for _ in range(n)]
-        for edge in edges:
-            nodes[edge[0]].append((edge[1], edge[2]))
-            nodes[edge[1]].append((edge[0], edge[2]))
-        ans = 0
-        cnt = inf
-        for i in range(n):
-            q = [(0, i)]
-            used = [False] * n
-            cur = -1
-            while q:
-                d, node = heappop(q)
-                if used[node]: continue
-                used[node] = True
-                cur += 1
-                for next_node, next_d in nodes[node]:
-                    if not used[next_node] and d + next_d <= distanceThreshold:
-                        heappush(q, (d + next_d, next_node))
-            if cur <= cnt:
-                cnt = cur
-                ans = i
-        return ans`,
+    def maximizeSum(self, nums: List[int], k: int) -> int:
+        return max(nums) * k + (k - 1) * k // 2`,
         },
         // {
         //     script: Script.RUST,
