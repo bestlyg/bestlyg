5347d7bf4b6a4cac3be32a329d35ead56eb7bd9c
liyigang <bestlyg@foxmail.com>
2023-11-21 09:46:41 +0800

    feat: 🎸 add leetcode no.2216

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '53. 最大子数组和',
-    url: 'https://leetcode.cn/problems/maximum-subarray',
+    exist: !true,
+    name: '2216. 美化数组的最少删除数',
+    url: 'https://leetcode.cn/problems/minimum-deletions-to-make-array-beautiful',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个下标从 0 开始的数组 nums ，数组中的元素都是 正 整数。请你选出两个下标 i 和 j（i != j），且 nums[i] 的数位和 与  nums[j] 的数位和相等。请你找出所有满足条件的下标 i 和 j ，找出并返回 nums[i] + nums[j] 可以得到的 最大值 。`,
+    desc: `返回使 nums 变为美丽数组所需删除的 最少 元素数目。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -35,19 +35,15 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 168,
-            memory: 30.34,
-            desc: '遍历后记录前面的最小前缀和',
+            time: 216,
+            memory: 26.28,
+            desc: '一次遍历',
             code: `class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        ans = -inf
-        prev = 0
-        sums = 0
-        for num in nums:
-            sums += num
-            ans = max(ans, sums - prev)
-            prev = min(prev, sums)
-        return ans`,
+    def minDeletion(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(0, len(nums) - 1):
+            if (i - ans) % 2 == 0 and nums[i] == nums[i + 1]: ans += 1
+        return ans + (len(nums) - ans) % 2`,
         },
         // {
         //     script: Script.RUST,
