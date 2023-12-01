0f006a210d5bbcbf300b11bebd971606751aaa5a
liyigang <bestlyg@foxmail.com>
2023-12-01 10:21:32 +0800

    feat: 🎸 add leetcode no.2661

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '1657. 确定两个字符串是否接近',
-    url: 'https://leetcode.cn/problems/determine-if-two-strings-are-close',
+    name: '2661. 找出叠涂元素',
+    url: 'https://leetcode.cn/problems/first-completely-painted-row-or-column',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你两个字符串，word1 和 word2 。如果 word1 和 word2 接近 ，就返回 true ；否则，返回 false 。`,
+    desc: `请你找出 arr 中在 mat 的某一行或某一列上都被涂色且下标最小的元素，并返回其下标 i 。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -35,22 +35,51 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 128,
-            memory: 16.86,
-            desc: '排序',
+            time: 192,
+            memory: 49.78,
+            desc: '哈希计数',
             code: `class Solution:
-    def closeStrings(self, word1: str, word2: str) -> bool:
-        c1 = Counter(word1)
-        c2 = Counter(word2)
-        return sorted(c1.keys()) == sorted(c2.keys()) and sorted(c1.values()) == sorted(c2.values())`,
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        n = len(mat)
+        m = len(mat[0])
+        map = {mat[i][j]: (i, j) for i in range(n) for j in range(m)}
+        rows = [0] * n
+        cols = [0] * m
+        for idx, num in enumerate(arr):
+            i, j = map[num]
+            rows[i] += 1
+            cols[j] += 1
+            if rows[i] == m or cols[j] == n: return idx`,
         },
-        // {
-        //     script: Script.RUST,
-        //     time: 564,
-        //     memory: 85,
-        //     desc: '同上',
-        //     code: ``,
-        // },
+        {
+            script: Script.RUST,
+            time: 40,
+            memory: 11.39,
+            desc: '同上',
+            code: `impl Solution {
+    pub fn first_complete_index(arr: Vec<i32>, mat: Vec<Vec<i32>>) -> i32 {
+        let n = mat.len();
+        let m = mat[0].len();
+        let mut map = std::collections::HashMap::<i32, (usize, usize)>::new();
+        for i in 0..n {
+            for j in 0..m {
+                map.insert(mat[i][j], (i, j));
+            }
+        }
+        let mut rows = vec![0; n];
+        let mut cols = vec![0; m];
+        for (idx, num) in arr.into_iter().enumerate() {
+            let (i, j) = map.get(&num).unwrap();
+            rows[*i] += 1;
+            cols[*j] += 1;
+            if rows[*i] == m || cols[*j] == n {
+                return idx as i32;
+            }
+        }
+        0
+    }
+}`,
+        },
     ],
 };
 
--- packages/leetcode/src/main.rs
+++ packages/leetcode/src/main.rs
@@ -14,78 +14,26 @@ fn main() {
     // println!("res = {res:#?}");
 }
 
-struct TrieNode {
-    left: Option<Box<TrieNode>>,
-    right: Option<Box<TrieNode>>,
-}
-impl TrieNode {
-    fn new() -> Self {
-        Self {
-            left: None,
-            right: None,
-        }
-    }
-}
-
-fn add(mut node: &mut Box<TrieNode>, num: i32) {
-    let mut pos = 30;
-    while pos >= 0 {
-        let v = (num >> pos) & 1;
-        if v != 0 {
-            if node.left.is_none() {
-                node.left = Some(Box::new(TrieNode::new()));
-            }
-            node = node.left.as_mut().unwrap()
-        } else {
-            if node.right.is_none() {
-                node.right = Some(Box::new(TrieNode::new()));
-            }
-            node = node.right.as_mut().unwrap()
-        }
-        pos -= 1;
-    }
-}
-
-fn find(mut node: &Box<TrieNode>, num: i32) -> i32 {
-    let mut pos = 30;
-    let mut ans = 0;
-    let mut node = Some(node);
-    while pos >= 0 && node.is_some() {
-        let node_ref = node.unwrap();
-        let v = (num >> pos) & 1;
-        if v != 0 {
-            if node_ref.right.is_some() {
-                ans |= 1 << pos;
-                node = Some(node_ref.right.as_ref().unwrap());
-            } else if node_ref.left.is_some() {
-                node = Some(node_ref.left.as_ref().unwrap());
-            } else {
-                node = None;
-            }
-        } else {
-            if node_ref.left.is_some() {
-                ans |= 1 << pos;
-                node = Some(node_ref.left.as_ref().unwrap());
-            } else if node_ref.right.is_some() {
-                node = Some(node_ref.right.as_ref().unwrap());
-            } else {
-                node = None;
-            }
-        }
-    }
-    ans
-}
-
 impl Solution {
-    pub fn find_maximum_xor(nums: Vec<i32>) -> i32 {
-        let mut root = Box::new(TrieNode::new());
-        let mut ans = 0;
-        for num in &nums {
-            add(&mut root, *num);
+    pub fn first_complete_index(arr: Vec<i32>, mat: Vec<Vec<i32>>) -> i32 {
+        let n = mat.len();
+        let m = mat[0].len();
+        let mut map = std::collections::HashMap::<i32, (usize, usize)>::new();
+        for i in 0..n {
+            for j in 0..m {
+                map.insert(mat[i][j], (i, j));
+            }
         }
-        for num in &nums {
-            ans = ans.max(find(&root, *num));
+        let mut rows = vec![0; n];
+        let mut cols = vec![0; m];
+        for (idx, num) in arr.into_iter().enumerate() {
+            let (i, j) = map.get(&num).unwrap();
+            rows[*i] += 1;
+            cols[*j] += 1;
+            if rows[*i] == m || cols[*j] == n {
+                return idx as i32;
+            }
         }
-        ans
+        0
     }
 }
