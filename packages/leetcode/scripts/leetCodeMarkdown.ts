cff8572b99217c02390015d105fc3cb45e3d96f6
liyigang <bestlyg@foxmail.com>
2023-12-09 08:45:01 +0800

    feat: 🎸 add leetcode no.2048

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: !true,
-    name: '1466. 重新规划路线',
-    url: 'https://leetcode.cn/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero',
+    exist:!true,
+    name: '2048. 下一个更大的数值平衡数',
+    url: 'https://leetcode.cn/problems/next-greater-numerically-balanced-number',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你帮助重新规划路线方向，使每个城市都可以访问城市 0 。返回需要变更方向的最小路线数。`,
+    desc: `给你一个整数 n ，请你返回 严格大于 n 的 最小数值平衡数 。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -25,52 +25,76 @@ const leetCodeMarkdown: Markdown = {
         //     desc: 'dp',
         //     code: ``,
         // },
+
         {
-            date: new Date('2023-12-7').getTime(),
-            script: Script.PY,
-            time: 272,
-            memory: 36.8,
-            desc: '从0点开始向外bfs',
-            code: `class Solution:
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        froms = [[] for _ in range(n)]
-        tos = [[] for _ in range(n)]
-        for a, b in connections:
-            froms[a].append(b)
-            tos[b].append(a)
-        q = deque()
-        q.append((0, 0, -1))
-        ans = 0
-        while q:
-            cur, f, p = q.popleft()
-            ans += len(froms[cur]) - f
-            for next in froms[cur]:
-                if next != p: q.append((next, 0, cur))
-            for next in tos[cur]:
-                if next != p: q.append((next, 1, cur))
-        return ans`,
+            date:new Date('2022.05.06').getTime(),
+            script: Script.CPP,
+            time: 4,
+            memory: 7.03,
+            desc: '打表',
+            code: `class Solution {
+    public:
+        void getNumber(int d, int ind, vector<int>& buff, vector<int>& arr) {
+            if (d == 0) {
+                vector<int> temp;
+                for (auto x : buff) {
+                    for (int i = 0; i < x; i++) {
+                        temp.push_back(x);
+                    }
+                }
+                do {
+                    int num = 0;
+                    for (auto x : temp) num = num * 10 + x;
+                    arr.push_back(num);
+                } while (next_permutation(temp.begin(), temp.end()));
+                return;
+            }
+            for (int i = ind; i <= d; i++) {
+                if (d - i > i || i == d) {
+                    buff.push_back(i);
+                    getNumber(d - i, i + 1, buff, arr);
+                    buff.pop_back();
+                }
+            }
+        }
+        void getAllNumber(int d, vector<int>& arr) {
+            vector<int> buff;
+            getNumber(d, 1, buff, arr);
+        }
+        int nextBeautifulNumber(int n) {
+            if (n == 0) return 1;
+            int d = floor(log10(n)) + 1;
+            vector<int> arr;
+            getAllNumber(d, arr);
+            getAllNumber(d + 1, arr);
+            int ans = INT_MAX;
+            for (auto x : arr) {
+                if (x > n) ans = min(ans, x);
+            }
+            return ans;
+        }
+};`,
         },
-        //         {
-        //             script: Script.CPP,
-        //             time: 0,
-        //             memory: 8.3,
-        //             desc: '同上',
-        //             code: `class Solution {
-        // public:
-        //     TreeNode* bstToGst(TreeNode* root) {
-        //         int sums = 0;
-        //         function<void(TreeNode*)> dfs = [&](TreeNode *node) {
-        //             if (!node) return;
-        //             dfs(node->right);
-        //             sums += node->val;
-        //             node->val = sums;
-        //             dfs(node->left);
-        //         };
-        //         dfs(root);
-        //         return root;
-        //     }
-        // };`,
-        //         },
+        {
+            script: Script.PY,
+            time: 3380,
+            memory: 15.98,
+            desc: '枚举',
+            code: `class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        while True:
+            n = n + 1
+            arr = [0] * 10
+            for c in str(n):
+                arr[int(c)] += 1
+            f = True
+            for i in range(10):
+                if arr[i] != 0 and arr[i] != i:
+                    f = False
+                    break
+            if f:
+                return n`,
+        },
         //         {
         //             script: Script.RUST,
         //             time: 0,
