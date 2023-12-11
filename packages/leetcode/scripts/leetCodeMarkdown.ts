c8364308820d3757900a558b1b696e9cdcb019d2
liyigang <bestlyg@foxmail.com>
2023-12-11 10:23:43 +0800

    feat: 🎸 add leetcode no.1631

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist:true,
-    name: '70. 爬楼梯',
-    url: 'https://leetcode.cn/problems/next-greater-numerically-balanced-number',
+    exist: !true,
+    name: '1631. 最小体力消耗路径',
+    url: 'https://leetcode.cn/problems/path-with-minimum-effort/',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个整数 n ，请你返回 严格大于 n 的 最小数值平衡数 。`,
+    desc: `一条路径耗费的 体力值 是路径上相邻格子之间 高度差绝对值 的 最大值 决定的。请你返回从左上角走到右下角的最小 体力消耗值 。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -17,14 +17,43 @@ const leetCodeMarkdown: Markdown = {
         //     desc: '归并排序',
         //     code: ``,
         // },
-        // {
-        //     date: new Date('2020.12.28').getTime(),
-        //     script: Script.TS,
-        //     time: 120,
-        //     memory: 44.6,
-        //     desc: 'dp',
-        //     code: ``,
-        // },
+        {
+            date: new Date('2021.01.29').getTime(),
+            script: Script.TS,
+            time: 352,
+            memory: 46.7,
+            desc: '二分',
+            code: `var minimumEffortPath = function(heights) {
+const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+const m = heights.length, n = heights[0].length;
+let left = 0, right = 999999, ans = 0;
+while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const queue = [[0, 0]];
+    const seen = new Array(m * n).fill(0);
+    seen[0] = 1;
+    while (queue.length) {
+        const [x, y] = queue.shift();
+        for (let i = 0; i < 4; i++) {
+            const nx = x + dirs[i][0];
+            const ny = y + dirs[i][1];
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !seen[nx * n + ny] && Math.abs(heights[x][y] - heights[nx][ny]) <= mid) {
+                queue.push([nx, ny]);
+                seen[nx * n + ny] = 1;
+            }
+        }
+    }
+    if (seen[m * n - 1]) {
+        ans = mid;
+        right = mid - 1;
+    } else {
+        left = mid + 1;
+    }
+}
+return ans;
+};`,
+        },
 
         // {
         //     script: Script.CPP,
@@ -37,17 +66,32 @@ const leetCodeMarkdown: Markdown = {
             script: Script.PY,
             time: 36,
             memory: 16.67,
-            desc: 'dp',
-            code: `class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
-        dp = [0] * (n + 1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        print(dp)
-        return dp[n]`,
+            desc: '二分',
+            code: `dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        n, m = len(heights), len(heights[0])
+        def check(val: int) -> bool:
+            q = deque([(0, 0)])
+            used = [[False] * m for _ in range(n)]
+            used[0][0] = True
+            while q:
+                i, j = q.popleft()
+                if i == n - 1 and j == m - 1: return True
+                for dir in dirs:
+                    ni = i + dir[0]
+                    nj = j + dir[1]
+                    if 0 <= ni < n and 0 <= nj < m and not used[ni][nj] and abs(heights[ni][nj] - heights[i][j]) <= val:
+                        q.append((ni, nj))
+                        used[ni][nj] = True
+            return False
+
+        l, r = 0, 10 ** 6
+        while l < r:
+            mid = (l + r) // 2
+            if check(mid): r = mid
+            else: l = mid + 1
+        return l`,
         },
         //         {
         //             script: Script.RUST,
