308b46f366d7407e933a14cc0a0f8e132c37a523
liyigang <bestlyg@foxmail.com>
2024-01-07 12:45:12 +0800

    feat: 🎸 add leetcode no.383

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -61,34 +61,34 @@ const leetCodeMarkdown: Markdown = {
         //     }
         // };`,
         //         },
-        {
-            script: Script.RUST,
-            time: 8,
-            memory: 2.83,
-            desc: '同上',
-            code: `fn gcd(a: i32, b: i32) -> i32 {
-    if a < b {
-        gcd(b, a)
-    } else if b == 0 {
-        a
-    } else {
-        gcd(b, a % b)
-    }
-}
-impl Solution {
-    pub fn insert_greatest_common_divisors(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut head = head.unwrap();
-        let mut p = &mut head;
-        while let Some(mut next) = p.next.take() {
-            let mut new_next = Box::new(ListNode::new(gcd(p.val, next.val)));
-            new_next.next = Some(next);
-            p.next = Some(new_next);
-            p = p.next.as_mut().unwrap().next.as_mut().unwrap();
-        }
-        Some(head)
-    }
-}`,
-        },
+//         {
+//             script: Script.RUST,
+//             time: 8,
+//             memory: 2.83,
+//             desc: '同上',
+//             code: `fn gcd(a: i32, b: i32) -> i32 {
+//     if a < b {
+//         gcd(b, a)
+//     } else if b == 0 {
+//         a
+//     } else {
+//         gcd(b, a % b)
+//     }
+// }
+// impl Solution {
+//     pub fn insert_greatest_common_divisors(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//         let mut head = head.unwrap();
+//         let mut p = &mut head;
+//         while let Some(mut next) = p.next.take() {
+//             let mut new_next = Box::new(ListNode::new(gcd(p.val, next.val)));
+//             new_next.next = Some(next);
+//             p.next = Some(new_next);
+//             p = p.next.as_mut().unwrap().next.as_mut().unwrap();
+//         }
+//         Some(head)
+//     }
+// }`,
+//         },
     ],
 };
 
