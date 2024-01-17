4dab3ae4031debe3c3afdf049e202b6c44aca70d
liyigang <bestlyg@foxmail.com>
2024-01-17 10:05:27 +0800

    feat: 🎸 add leetcode no.2744

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2719. 统计整数数目',
-    url: 'https://leetcode.cn/problems/count-of-integers/description/',
+    name: '2744. 最大字符串配对数目',
+    url: 'https://leetcode.cn/problems/find-maximum-number-of-string-pairs',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你返回好整数的数目。`,
+    desc: `请你返回数组 words 中的 最大 匹配数目。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -27,26 +27,12 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 232,
-            memory: 18.31,
-            desc: '数位dp',
-            code: `def digit_dp(n: int, min_num: str, max_num: str, min_sum: int, max_sum: int):
-    @cache
-    def dfs(i: int, val: int, limit_low: bool, limit_high: bool) -> int:
-        if val > max_sum: return 0
-        if i == n: return val >= min_sum
-        lo = int(min_num[i]) if limit_low else 0
-        hi = int(max_num[i]) if limit_high else 9
-        return sum(
-            dfs(i + 1, val + d, limit_low and lo == d, limit_high and hi == d)
-            for d in range(lo, hi + 1)
-        )
-    return  dfs
-    
-class Solution:
-    def count(self, num1: str, num2: str, min_sum: int, max_sum: int) -> int:
-        dfs = digit_dp(len(num2), num1.zfill(len(num2)), num2, min_sum, max_sum)
-        return dfs(0, 0, True, True) % (10 ** 9 + 7)`,
+            time: 44,
+            memory: 16.88,
+            desc: '遍历',
+            code: `class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        return sum(words[i1][::-1] == words[i2] for i1 in range(len(words)) for i2 in range(0, i1))`,
         },
 
         //         {
