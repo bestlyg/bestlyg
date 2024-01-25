b5608ea884594a3904a0200e99497fdcb88085a3
liyigang <bestlyg@foxmail.com>
2024-01-25 10:36:52 +0800

    feat: 🎸 add leetcode no.2859

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2865. 美丽塔 I',
-    url: 'https://leetcode.cn/problems/beautiful-towers-i',
+    name: '2859. 计算 K 置位下标对应元素的和',
+    url: 'https://leetcode.cn/problems/sum-of-values-at-indices-with-k-set-bits',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你返回满足 美丽塔 要求的方案中，高度和的最大值 。`,
+    desc: `请你用整数形式返回 nums 中的特定元素之 和 ，这些特定元素满足：其对应下标的二进制表示中恰存在 k 个置位。`,
     solutions: [
         // {
         //     date: new Date('2020.04.26').getTime(),
@@ -27,26 +27,12 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 41,
-            memory: 16.82,
-            desc: '单调栈',
+            time: 42,
+            memory: 16.55,
+            desc: '遍历',
             code: `class Solution:
-    def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
-        n = len(maxHeights)
-        s = []
-        larr = [-1] * n
-        rarr = [n] * n
-        for i in range(n):
-            while s and maxHeights[s[-1]] >= maxHeights[i]: rarr[s.pop()] = i
-            if s: larr[i] = s[-1]
-            s.append(i)
-        lh = [0] * (n + 2)
-        rh = [0] * (n + 2)
-        for i in range(n):
-            lh[i + 1] = maxHeights[i] * (i - larr[i]) + lh[larr[i] + 1]
-        for i in range(n - 1, -1, -1):
-            rh[i + 1] = maxHeights[i] * (rarr[i] - i) + rh[rarr[i] + 1]
-        return max(lh[i + 1] + rh[i + 1] - maxHeights[i] for i in range(n))`,
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        return sum(nums[i] if bin(i).count('1') == k else 0 for i in range(len(nums)))`,
         },
 
         //         {
