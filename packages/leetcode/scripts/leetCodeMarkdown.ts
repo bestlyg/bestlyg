7ec641dbbe0afdd6365bb40aba74a0ba4db34597
liyigang <bestlyg@foxmail.com>
2024-01-29 10:47:57 +0800

    feat: 🎸 add leetcode no.514

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,21 +2,40 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '365. 水壶问题',
-    url: 'https://leetcode.cn/problems/maximum-number-of-alloys',
+    exist: !true,
+    name: '514. 自由之路',
+    url: 'https://leetcode.cn/problems/freedom-trail',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你整数 n、k、budget，下标从 1 开始的二维数组 composition，两个下标从 1 开始的数组 stock 和 cost，请你在预算不超过 budget 金钱的前提下，最大化 公司制造合金的数量。`,
+    desc: `给定一个字符串 ring ，表示刻在外环上的编码；给定另一个字符串 key ，表示需要拼写的关键词。您需要算出能够拼写关键词中所有字符的最少步数。`,
     solutions: [
-        // {
-        //     date: new Date('2020.04.26').getTime(),
-        //     script: Script.JS,
-        //     time: 384,
-        //     memory: 44.78,
-        //     desc: '归并排序',
-        //     code: ``,
-        // },
+        {
+            date: new Date('2020.11.11').getTime(),
+            script: Script.JS,
+            time: 140,
+            memory: 45.82,
+            desc: 'dp',
+            code: `const getIdx = (str, v) => str.codePointAt(v) - 'a'.codePointAt(0);
+var findRotateSteps = function(ring, key) {
+    const n = ring.length, m = key.length;
+    const pos = new Array(26).fill(0).map(v => []);
+    for (let i = 0; i < n; ++i) {
+        pos[getIdx(ring, i)].push(i);
+    }
+    const dp = new Array(m).fill(0).map(v => new Array(n).fill(Number.MAX_SAFE_INTEGER));
+    for (const i of pos[getIdx(key, 0)]) {
+        dp[0][i] = Math.min(i, n - i) + 1;
+    }
+    for (let i = 1; i < m; ++i) {
+        for (const j of pos[getIdx(key, i)]) {
+            for (const k of pos[getIdx(key, i - 1)]) {
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + Math.min(Math.abs(j - k), n - Math.abs(j - k)) + 1);
+            }
+        }
+    }
+    return dp[m - 1].reduce((pre, cur) => Math.min(pre, cur), Number.MAX_SAFE_INTEGER);
+};`,
+        },
         // {
         //     date: new Date('2020.07.25').getTime(),
         //     script: Script.TS,
@@ -27,32 +46,23 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 2181,
-            memory: 67.66,
-            desc: 'bfs',
+            time: 108,
+            memory: 17.4,
+            desc: 'dfs',
             code: `class Solution:
-    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
-        used = defaultdict(defaultdict)
-        q = deque()
-        q.append((0, 0))
-        used[0][0] = True
-        while q:
-            jug1, jug2 = q.popleft()
-            if jug1 == targetCapacity or jug2 == targetCapacity or jug1 + jug2 == targetCapacity:
-                return True
-            arr =[
-                [jug1Capacity, jug2],
-                [0, jug2],
-                [min(jug1Capacity, jug1 + jug2), jug2 - (min(jug1Capacity, jug1 + jug2) - jug1)],
-                [jug1, jug2Capacity],
-                [jug1, 0],
-                [jug1 - (min(jug2Capacity, jug1 + jug2) - jug2), min(jug2Capacity, jug1 + jug2)]
-            ]
-            for jug1, jug2 in arr:
-                if jug2 not in used[jug1]:
-                    q.append((jug1, jug2))
-                    used[jug1][jug2] = True
-        return False`,
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        arr = defaultdict(list)
+        for i in range(len(ring)):
+            arr[ring[i]].append(i)
+        @cache
+        def dfs(i1: int, i2: int) -> int:
+            if i2 == len(key): return 0
+            if ring[i1] == key[i2]: return dfs(i1, i2 + 1) + 1
+            return min(
+                dfs(next_i, i2 + 1) + min(abs(i1 - next_i), len(ring) - abs(i1 - next_i)) + 1
+                for next_i in arr[key[i2]]
+            )
+        return dfs(0, 0)`,
         },
 
         //         {
@@ -76,50 +86,13 @@ const leetCodeMarkdown: Markdown = {
         //     }
         // };`,
         //         },
-        {
-            script: Script.RUST,
-            time: 441,
-            memory: 21.37,
-            desc: '同上',
-            code: `impl Solution {
-    pub fn can_measure_water(jug1_capacity: i32, jug2_capacity: i32, target_capacity: i32) -> bool {
-        use std::cmp::min;
-        use std::collections::{HashMap, VecDeque};
-        let mut used: HashMap<i32, HashMap<i32, bool>> = Default::default();
-        let mut q: VecDeque<(i32, i32)> = Default::default();
-        q.push_back((0, 0));
-        used.entry(0).or_default().entry(0).or_insert(true);
-        while let Some((jug1, jug2)) = q.pop_front() {
-            if jug1 == target_capacity || jug2 == target_capacity || jug1 + jug2 == target_capacity
-            {
-                return true;
-            }
-            let next = [
-                [jug1_capacity, jug2],
-                [0, jug2],
-                [
-                    min(jug1_capacity, jug1 + jug2),
-                    jug2 - (min(jug1_capacity, jug1 + jug2) - jug1),
-                ],
-                [jug1, jug2_capacity],
-                [jug1, 0],
-                [
-                    jug1 - (min(jug2_capacity, jug1 + jug2) - jug2),
-                    min(jug2_capacity, jug1 + jug2),
-                ],
-            ];
-            for [jug1, jug2] in next {
-                let item = used.entry(jug1).or_default().entry(jug2).or_default();
-                if !*item {
-                    q.push_back((jug1, jug2));
-                    *item = true;
-                }
-            }
-        }
-        false
-    }
-}`,
-        },
+        // {
+        //     script: Script.RUST,
+        //     time: 441,
+        //     memory: 21.37,
+        //     desc: '同上',
+        //     code: ``,
+        // },
     ],
 };
 
