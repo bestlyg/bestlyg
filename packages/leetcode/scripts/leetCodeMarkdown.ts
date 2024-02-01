d316eb5aca88e64b014c663a0d59ad84e5d0da34
liyigang <bestlyg@foxmail.com>
2024-02-01 14:03:43 +0800

    feat: 🎸 add leetcode LCP 24. 数字游戏

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '2670. 找出不同元素数目差数组',
-    url: 'https://leetcode.cn/problems/minimum-seconds-to-equalize-a-circular-array',
+    exist: !true,
+    name: 'LCP 24. 数字游戏',
+    url: 'https://leetcode.cn/problems/5TxKeK/',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你返回将数组 nums 中所有元素变成相等元素所需要的 最少 秒数。`,
+    desc: `主办方请小扣回答出一个长度为 N 的数组，第 i 个元素(0 <= i < N)表示将 0~i 号计数器 初始 所示数字操作成满足所有条件 nums[a]+1 == nums[a+1],(0 <= a < i) 的最小操作数。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -27,22 +27,42 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 300,
-            memory: 48.3,
-            desc: '记录下标，判断下标之间的最大距离',
+            time: 248,
+            memory:34.91,
+            desc: '依次递增转换为使每个数都保持一致的最小步数',
             code: `class Solution:
-    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
-        rdict = defaultdict(int)
-        for num in nums: rdict[num] += 1
-        cur = len(rdict)
+    def numsGame(self, nums: List[int]) -> List[int]:
+        lq = []
+        rq = []
         res = []
-        ldict = defaultdict(int)
-        for num in nums:
-            rdict[num] -= 1
-            if rdict[num] == 0: cur -= 1
-            ldict[num] += 1
-            if ldict[num] == 1: cur -= 1
-            res.append(-cur)
+        rsum = lsum = 0
+        mod = 10 ** 9 + 7
+        for i in range(len(nums)):
+            num = nums[i] - i
+            if lq and -lq[0] >= num:
+                lsum += num
+                heappush(lq, -num)
+            else:
+                rsum += num
+                heappush(rq, num)
+            # print(f'lq = {lq}, lsum = {lsum}')
+            # print(f'rq = {rq}, rsum = {rsum}')
+            if len(lq) > len(rq):
+                num = -heappop(lq)
+                lsum -= num
+                rsum += num
+                heappush(rq, num)            
+            elif len(lq) < len(rq) - 1:
+                num = heappop(rq)
+                lsum += num
+                rsum -= num
+                heappush(lq, -num)
+            # print(f'lq = {lq}, lsum = {lsum}')
+            # print(f'rq = {rq}, rsum = {rsum}')
+            if (i + 1) % 2 == 0:
+                res.append((rsum - lsum) % mod)
+            else:
+                res.append((rsum - lsum - rq[0]) % mod)
         return res`,
         },
 
