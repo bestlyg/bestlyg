27e3b0b2e53ece38ba7f1a2226dbca2ae11b742d
liyigang <bestlyg@foxmail.com>
2024-02-02 10:50:23 +0800

    feat: 🎸 add leetcode no.1686

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: 'LCP 24. 数字游戏',
-    url: 'https://leetcode.cn/problems/5TxKeK/',
+    name: '1686. 石子游戏 VI',
+    url: 'https://leetcode.cn/problems/stone-game-vi',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `主办方请小扣回答出一个长度为 N 的数组，第 i 个元素(0 <= i < N)表示将 0~i 号计数器 初始 所示数字操作成满足所有条件 nums[a]+1 == nums[a+1],(0 <= a < i) 的最小操作数。`,
+    desc: `所有石子都被取完后，得分较高的人为胜者。如果两个玩家得分相同，那么为平局。两位玩家都会采用 最优策略 进行游戏。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -27,43 +27,17 @@ const leetCodeMarkdown: Markdown = {
         // },
         {
             script: Script.PY,
-            time: 248,
-            memory:34.91,
-            desc: '依次递增转换为使每个数都保持一致的最小步数',
+            time: 241,
+            memory: 27.69,
+            desc: '优先拿分数总和最大的',
             code: `class Solution:
-    def numsGame(self, nums: List[int]) -> List[int]:
-        lq = []
-        rq = []
-        res = []
-        rsum = lsum = 0
-        mod = 10 ** 9 + 7
-        for i in range(len(nums)):
-            num = nums[i] - i
-            if lq and -lq[0] >= num:
-                lsum += num
-                heappush(lq, -num)
-            else:
-                rsum += num
-                heappush(rq, num)
-            # print(f'lq = {lq}, lsum = {lsum}')
-            # print(f'rq = {rq}, rsum = {rsum}')
-            if len(lq) > len(rq):
-                num = -heappop(lq)
-                lsum -= num
-                rsum += num
-                heappush(rq, num)            
-            elif len(lq) < len(rq) - 1:
-                num = heappop(rq)
-                lsum += num
-                rsum -= num
-                heappush(lq, -num)
-            # print(f'lq = {lq}, lsum = {lsum}')
-            # print(f'rq = {rq}, rsum = {rsum}')
-            if (i + 1) % 2 == 0:
-                res.append((rsum - lsum) % mod)
-            else:
-                res.append((rsum - lsum - rq[0]) % mod)
-        return res`,
+    def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
+        values = sorted((av + bv, av, bv) for av, bv in zip(aliceValues, bobValues))
+        asum = sum(item[1] for item in values[-1::-2])
+        bsum = sum(item[2] for item in values[-2::-2])
+        if asum > bsum: return 1
+        if asum < bsum: return -1
+        return 0`,
         },
 
         //         {
