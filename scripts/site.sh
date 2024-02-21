1172f5bd9b7389c2512e738bca27c0c8e7e7ddff
liyigang <bestlyg@foxmail.com>
2024-02-21 19:54:21 +0800

    feat: 🎸 update script

--- scripts/site.sh
+++ scripts/site.sh
@@ -3,5 +3,5 @@ rm -rf ./packages/site/docs/leetcode
 cp -rf ./packages/leetcode/dist ./packages/site/docs/leetcode
 pnpm --filter site build
 zip -vr ./packages/site/build/site.zip ./packages/site/build
-scp -r ./packages/site/build/site.zip ubuntu@101.34.26.179:~/site.zip 
-# sudo unzip -o -d /root/projects/bestlyg /home/ubuntu/site.zip
+scp -r ./packages/site/build/site.zip ubuntu@106.54.220.193:~/site.zip 
+# sudo unzip -o -d /root/bestlyg /home/ubuntu/site.zip
