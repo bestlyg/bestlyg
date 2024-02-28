7986f5fa77b1858354e530b8d66da6b536bd1186
liyigang <bestlyg@foxmail.com>
2024-02-28 10:58:20 +0800

    feat: 🎸 add leetcode no.2673

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,8 +2,8 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: !true,
-    name: '2867. 统计树中的合法路径数目',
+    exist: true,
+    name: '2673. 使二叉树所有路径值相等的最小代价',
     url: 'https://leetcode.cn/problems/count-valid-paths-in-a-tree/',
     difficulty: Difficulty.简单,
     tag: [],
@@ -28,52 +28,20 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 322,
-            memory: 58.63,
-            desc: '预处理好质数表，通过遍历所有质数，找到以当前质数为根结点的时候，所有子树的长度，进行两两相乘',
-            code: `def get_primes2(n: int) -> List[bool]:
-        n += 1
-        primes = [True for _ in range(n)]
-        primes[0] = primes[1] = False
-        for i in range(2, n):
-            if primes[i]:
-                j = 2
-                while i * j < n:
-                    primes[i*j] = False
-                    j += 1
-        return primes
-    primes = get_primes2(10 ** 5 + 1)
-    
-    class Solution:
-        def countPaths(self, n: int, edges: List[List[int]]) -> int:
-            nodes = [[] for _ in range(n + 1)]
-            for n1, n2 in edges:
-                nodes[n1].append(n2)
-                nodes[n2].append(n1)
-            ans = 0
-    
-            cache = defaultdict(int)
-            def dfs(arr: List[int], node: int, parent: int):
-                if primes[node]: return
-                arr.append(node)
-                ans = 1
-                for child in nodes[node]:
-                    if not primes[child] and child != parent:
-                        ans += dfs(arr, child, node)
-                return ans
-    
-            for node in range(1, n + 1):
-                if primes[node]:
-                    cur = 0
-                    for child in nodes[node]:
-                        if not primes[child]:
-                            if child not in cache:
-                                arr = []
-                                res = dfs(arr, child, node)
-                                for item in arr: cache[item] = res
-                            ans += cache[child] * cur
-                            cur += cache[child]
-                    ans += cur
+            time: 233,
+            memory: 45.47,
+            desc: 'dfs时记录左右的节点的值，进行遍历和平衡',
+            code: `class Solution:
+    def minIncrements(self, n: int, cost: List[int]) -> int:
+        ans = 0
+        def dfs(node: int) -> int:
+            nonlocal ans
+            if node * 2 > n: return cost[node - 1]
+            l, r = dfs(node * 2), dfs(node * 2 + 1)
+            maxn = max(l, r)
+            ans += 2 * maxn - l - r
+            return maxn + cost[node - 1]
+        dfs(1)
         return ans`,
         },
 
