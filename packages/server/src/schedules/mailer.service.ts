37eb2a6dc6d1a639f2b628217930e711eb44c144
liyigang <bestlyg@foxmail.com>
2024-03-14 10:39:20 +0800

    feat: 🎸 update weights

--- packages/server/src/schedules/mailer.service.ts
+++ packages/server/src/schedules/mailer.service.ts
@@ -5,6 +5,7 @@ import * as MarkdownIt from 'markdown-it';
 import * as dayjs from 'dayjs';
 
 const weights = {
+  '2024/3/14': 65,
   '2024/3/13': 64.8,
   '2024/3/12': 65.25,
   '2024/3/11': 65.45,
