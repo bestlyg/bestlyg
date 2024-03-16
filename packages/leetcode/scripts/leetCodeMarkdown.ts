8c149ee354b6b0e672cf1d7e9659c3479d12a20e
liyigang <bestlyg@foxmail.com>
2024-03-16 16:26:12 +0800

    feat: 🎸 add leetcode no.2684

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2312. 卖木头块',
-    url: 'https://leetcode.cn/problems/selling-pieces-of-wood',
+    name: '2684. 矩阵中移动的最大次数',
+    url: 'https://leetcode.cn/problems/maximum-number-of-moves-in-a-grid',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `请你返回切割一块大小为 m x n 的木块后，能得到的 最多 钱数。`,
+    desc: `返回你在矩阵中能够 移动 的 最大 次数。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -28,27 +28,23 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 6046,
-            memory: 38.27,
+            time: 226,
+            memory: 41.75,
             desc: 'dfs',
-            code: `class Solution:
-    def sellingWood(self, m: int, n: int, prices: List[List[int]]) -> int:
-        price_map = {}
-        for k1, k2, price in prices:
-            if k1 not in price_map: price_map[k1] = {}
-            item_map = price_map[k1]
-            if k2 not in item_map: item_map[k2] = price
-        @cache
-        def dfs(m: int, n: int) -> int:
-            ans = 0
-            if m in price_map and n in price_map[m]:
-                ans += price_map[m][n]
-            for i in range(1, m):
-                ans = max(ans, dfs(i, n) + dfs(m - i, n))
-            for i in range(1, n):
-                ans = max(ans, dfs(m, i) + dfs(m, n - i))
-            return ans
-        return dfs(m, n)`,
+            code: `dirs = [(1, 1), (0, 1), (-1, 1)]
+
+    class Solution:
+        def maxMoves(self, grid: List[List[int]]) -> int:
+            n, m = len(grid), len(grid[0])
+            @cache
+            def dfs(row: int, col: int) -> int:
+                ans = 0
+                for x, y in dirs:
+                    nrow, ncol = row + x, col + y
+                    if 0 <= nrow < n and 0 <= ncol < m and grid[row][col] < grid[nrow][ncol]:
+                        ans = max(ans, 1 + dfs(nrow, ncol))
+                return ans
+            return max(dfs(row, 0) for row in range(n))`,
         },
 
         //         {
