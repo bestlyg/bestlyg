628a40782d9a5f43161f2fb468b36ac6cb76c1af
liyigang <bestlyg@foxmail.com>
2024-03-25 18:38:55 +0800

    feat: 🎸 update site

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link className="button button--secondary button--lg" to="/docs/intro">
-                        Start a journey - 10min ⏱️
+                        Start a journey - 5min ⏱️
                     </Link>
                 </div>
             </div>
--- scripts/site-build.mjs
+++ scripts/site-build.mjs
@@ -2,5 +2,5 @@ import 'zx/globals';
 
 await $`pnpm nx build site`;
 await $`zip -vr ./packages/site/build/site.zip ./packages/site/build`;
-await $`scp -r ./packages/site/build/site.zip ubuntu@106.54.220.193:~/site.zip `;
+await $`scp -r ./packages/site/build/site.zip ubuntu@106.54.220.193:~/site.zip`;
 await $`# sudo unzip -o -d /root/bestlyg /home/ubuntu/site.zip`;
