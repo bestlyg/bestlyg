38cbc8f9908e13c24bd6eae77be322d3e097f494
liyigang <bestlyg@foxmail.com>
2024-03-25 10:53:07 +0800

    feat: 🎸 update scripts

--- scripts/site-build.mjs
+++ scripts/site-build.mjs
@@ -1,9 +1,6 @@
 import 'zx/globals';
 
-await $`pnpm --filter leetcode build`;
-await $`rm -rf ./packages/site/docs/leetcode`;
-await $`cp -rf ./packages/leetcode/dist ./packages/site/docs/leetcode`;
-await $`pnpm --filter site build`;
+await $`pnpm nx build site`;
 await $`zip -vr ./packages/site/build/site.zip ./packages/site/build`;
 await $`scp -r ./packages/site/build/site.zip ubuntu@106.54.220.193:~/site.zip `;
 await $`# sudo unzip -o -d /root/bestlyg /home/ubuntu/site.zip`;
