60a77dc4d0999a93878cd022c2bb2c32796a9264
liyigang <bestlyg@foxmail.com>
2024-03-25 17:38:58 +0800

    feat: 🎸 update server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,6 +2,8 @@ import { RequestMethod } from '@nestjs/common/enums';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters';
+import chokidar from 'chokidar';
+import { $, fs } from 'zx';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,3 +19,10 @@ async function bootstrap() {
   await app.listen(50000);
 }
 bootstrap();
+
+const sitePath = '/home/ubuntu/site.zip';
+chokidar.watch(sitePath).on('add', async (event, path) => {
+  console.log('ADD', event, path);
+  await $`sudo unzip -o -d /root/bestlyg /home/ubuntu/site.zip`;
+  await fs.remove(sitePath);
+});
