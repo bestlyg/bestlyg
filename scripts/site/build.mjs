4ec756f7c389046f577e9d0f543ba1ae82b85a7b
liyigang <bestlyg@foxmail.com>
2024-04-03 10:38:40 +0800

    feat: 🎸 update script

--- scripts/site/build.mjs
+++ scripts/site/build.mjs
@@ -2,5 +2,4 @@ import 'zx/globals';
 
 await $`pnpm nx build site`;
 await $`zip -vr ./packages/site/build/site.zip ./packages/site/build`;
-await $`scp -r ./packages/site/build/site.zip ubuntu@106.54.220.193:~/site.zip`;
-await $`zx script site/send`;
+await $`pnpm script site/send`;
