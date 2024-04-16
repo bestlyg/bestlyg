e0f65ee83aa00b91d2dabb680a088990245c2d1d
liyigang <bestlyg@foxmail.com>
2024-04-16 08:13:39 +0800

    feat: 🎸 add leetcode no.924

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '706. 设计哈希映射',
-    url: 'https://leetcode.cn/problems/find-champion-ii',
+    exist: !true,
+    name: '924. 尽量减少恶意软件的传播',
+    url: 'https://leetcode.cn/problems/minimize-malware-spread',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `如果这场比赛存在 唯一 一个冠军，则返回将会成为冠军的队伍。否则，返回 -1 。`,
+    desc: `如果从 initial 中移除某一节点能够最小化 M(initial)， 返回该节点。如果有多个节点满足条件，就返回索引最小的节点。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -28,20 +28,53 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 422,
-            memory: 64.12,
-            desc: '利用双数组存键值对',
-            code: `class MyHashMap:
-    def __init__(self):
-        self.iarr = [False] * (10 ** 6 + 1)
-        self.varr = [-1] * (10 ** 6 + 1)
-    def put(self, key: int, value: int) -> None:
-        self.iarr[key] = True
-        self.varr[key] = value
-    def get(self, key: int) -> int:
-        return self.varr[key] if self.iarr[key] else -1
-    def remove(self, key: int) -> None:
-        self.iarr[key] = False`,
+            time: 163,
+            memory: 19.87,
+            desc: '并查集',
+            code: `class UnionFind:
+    def __init__(self, n) -> None:
+        self.n = n
+        self.data = [i for i in range(0, n)]
+        self.sizes = [1] * n
+        self.cnt = n
+    def size(self, v: int) -> int:
+        return self.sizes[self.find(v)]
+    def find(self, v: int) -> int:
+        if self.data[v] != v:
+            self.data[v] = self.find(self.data[v])
+        return self.data[v]
+    def uni(self, v1: int, v2: int):
+        p1 = self.find(v1)
+        p2 = self.find(v2)
+        if p1 != p2:
+            self.sizes[p1] += self.sizes[p2]
+            self.cnt -= self.sizes[p2]
+            self.data[p2] = p1
+    def same(self, v1: int, v2: int):
+        return self.find(v1) == self.find(v2)
+class Solution:
+    def minMalwareSpread(self, graph: List[List[int]], initial: List[int]) -> int:
+        n = len(graph)
+        uf = UnionFind(n)
+        resIndex = -1
+        resVal = inf
+        for i in range(n):
+            for j in range(n):
+                if graph[i][j]:
+                    uf.uni(i, j)
+        for i in initial:
+            sum = 0
+            used = set()
+            for j in initial:
+                if i != j:
+                    parent = uf.find(j)
+                    if parent not in used:
+                        used.add(parent)
+                        sum += uf.size(parent)
+            if sum < resVal or (sum == resVal and i < resIndex):
+                resVal = sum
+                resIndex = i
+        return resIndex`,
         },
 
         //         {
