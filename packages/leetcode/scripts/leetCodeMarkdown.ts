fb56f625ef8cd1c93e9c10fa30993ee82f92f51f
liyigang <bestlyg@foxmail.com>
2024-04-18 10:17:42 +0800

    feat: 🎸 add leetcode no.2007

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '928. 尽量减少恶意软件的传播 II',
-    url: 'https://leetcode.cn/problems/minimize-malware-spread-ii/',
+    name: '2007. 从双倍数组中还原原数组',
+    url: 'https://leetcode.cn/problems/find-original-array-from-doubled-array/',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `我们可以从 initial 中删除一个节点，并完全移除该节点以及从该节点到任何其他节点的任何连接。请返回移除后能够使 M(initial) 最小化的节点。如果有多个节点满足条件，返回索引 最小的节点 。`,
+    desc: `给你一个数组 changed ，如果 change 是 双倍 数组，那么请你返回 original数组，否则请返回空数组。original 的元素可以以 任意 顺序返回。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -28,49 +28,24 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 601,
-            memory: 19.4,
-            desc: '并查集',
-            code: `class UnionFind:
-    def __init__(self, n) -> None:
-        self.n = n
-        self.data = [i for i in range(0, n)]
-        self.sizes = [1] * n
-        self.cnt = n
-    def size(self, v: int) -> int:
-        return self.sizes[self.find(v)]
-    def find(self, v: int) -> int:
-        if self.data[v] != v:
-            self.data[v] = self.find(self.data[v])
-        return self.data[v]
-    def uni(self, v1: int, v2: int):
-        p1 = self.find(v1)
-        p2 = self.find(v2)
-        if p1 != p2:
-            self.sizes[p1] += self.sizes[p2]
-            self.cnt -= self.sizes[p2]
-            self.data[p2] = p1
-    def same(self, v1: int, v2: int):
-        return self.find(v1) == self.find(v2)
-class Solution:
-    def minMalwareSpread(self, graph: List[List[int]], initial: List[int]) -> int:
-        n = len(graph)
-        def get(ignore_index: int) -> int:
-            uf = UnionFind(n)
-            res = 0
-            for i in range(n):
-                for j in range(n):
-                    if i == ignore_index or j == ignore_index: continue
-                    if graph[i][j]: uf.uni(i, j)
-            used = set()
-            for i in initial:
-                if i != ignore_index:
-                    parent = uf.find(i)
-                    if parent not in used:
-                        used.add(parent)
-                        res += uf.size(parent)
-            return (res, ignore_index)
-        return min(get(i) for i in initial)[1]`,
+            time: 194,
+            memory: 32.71,
+            desc: '哈希存储后遍历',
+            code: `class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        map = Counter(changed)
+        arr = sorted(map.keys())
+        res = []
+        for num in arr:
+            if map[num] == 0: continue
+            if num == 0:
+                if map[num] % 2 != 0: return []
+                res += [0] * (map[num] // 2)
+                continue
+            if map[num] > map[num * 2]: return []
+            map[num * 2] -= map[num]
+            res += [num] * map[num]
+        return res`,
         },
 
         //         {
