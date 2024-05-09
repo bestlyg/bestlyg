880b97a831da79a265b1788624138416ba4ff45b
liyigang <bestlyg@foxmail.com>
2024-05-09 11:25:30 +0800

    feat: 🎸 add getEnv

--- packages/npm/less-plugins/packages/get-env/src/index.ts
+++ packages/npm/less-plugins/packages/get-env/src/index.ts
@@ -1,6 +1,15 @@
+import { addFunctions } from '@less-plugins/shared';
+
 export default class LessPluginsGetEnv {
     constructor() {}
     setOptions(args) {}
     printUsage() {}
-    install(less, pluginMenager, functions) {}
+    install(less, pluginMenager, functions) {
+        addFunctions(functions, [
+            function getEnv(key) {
+                key = key?.value;
+                return process.env[key];
+            },
+        ]);
+    }
 }
--- packages/npm/less-plugins/packages/template/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-## 0.0.5
-
-- 修复没有 index.js 的报错问题
-
-## 0.0.4
-
-- Update dependencies @less-plugins/shared@0.0.7
-
