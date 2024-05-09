d0826ba82e54ccd662246b43e954a33a5811413b
liyigang <bestlyg@foxmail.com>
2024-05-09 09:53:19 +0800

    feat: 🎸 add less plugin get env

---
+++ packages/npm/less-plugins/packages/get-env/index.js
@@ -0,0 +1,1 @@
+module.exports = require('@less-plugins/shared').loadPlugin.apply(null, arguments).default;
---
+++ packages/npm/less-plugins/packages/get-env/src/index.ts
@@ -0,0 +1,6 @@
+export default class LessPluginsGetEnv {
+    constructor() {}
+    setOptions(args) {}
+    printUsage() {}
+    install(less, pluginMenager, functions) {}
+}
