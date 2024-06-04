54211c51cf96550d203284901d1140b84932f3a7
liyigang <bestlyg@foxmail.com>
2024-06-04 10:52:26 +0800

    feat: 🎸 add leetcode no.3067

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '1103. 分糖果 II',
-    url: 'https://leetcode.cn/problems/distribute-candies-to-people',
+    name: '3067. 在带权树网络中统计可连接服务器对数目',
+    url: 'https://leetcode.cn/problems/count-pairs-of-connectable-servers-in-a-weighted-tree-network',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `返回一个长度为 num_people、元素之和为 candies 的数组，以表示糖果的最终分发情况（即 ans[i] 表示第 i 个小朋友分到的糖果数）。`,
+    desc: `请你返回一个长度为 n 的整数数组 count ，其中 count[i] 表示通过服务器 i 可连接 的服务器对的 数目 。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -26,42 +26,43 @@ const leetCodeMarkdown: Markdown = {
         //     code: ``,
         // },
 
-        {
-            date: new Date('2022.03.28').getTime(),
-            script: Script.CPP,
-            time: 0,
-            memory: 6.32,
-            desc: '模拟',
-            code: `class Solution {
-public:
-    vector<int> distributeCandies(int candies, int num_people) {
-        vector<int> ans(num_people,0);
-        for (int i = 0, cnt = 1; candies > 0; i = (i + 1) % num_people,cnt++) {
-            ans[i] += min(candies, cnt);
-            candies -= cnt;
-        }
-        return ans;
-    }
-};`,
-        },
+        // {
+        //     date: new Date('2022.03.28').getTime(),
+        //     script: Script.CPP,
+        //     time: 0,
+        //     memory: 6.32,
+        //     desc: '模拟',
+        //     code: ``,
+        // },
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 37,
-            memory: 16.5,
+            time: 797,
+            memory: 18.23,
             desc: '模拟',
             code: `class Solution:
-    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        res = [0] * num_people
-        cur = 1
-        idx = 0
-        while candies:
-            v = min(cur, candies)
-            res[idx] += v
-            candies -= v
-            cur += 1
-            idx = (idx + 1) % num_people
-        return res`,
+    def countPairsOfConnectableServers(self, edges: List[List[int]], signalSpeed: int) -> List[int]:
+        nodes = [[] for _ in range(len(edges) + 1)]
+        for n1, n2, w in edges:
+            nodes[n1].append((n2, w))
+            nodes[n2].append((n1, w))
+        def dfs(cur: int, prev: int, sum: int) -> int:
+            res = 0
+            if sum % signalSpeed == 0: res += 1
+            for next, w in nodes[cur]:
+                if next != prev:
+                    res += dfs(next, cur, sum + w)
+            return res
+        def get_cnt(cur: int) -> int:
+            if len(nodes[cur]) == 1: return 0
+            arr = [dfs(next, cur, w) for next, w in nodes[cur]]
+            vsum = sum(arr)
+            res = 0
+            for v in arr:
+                vsum -= v
+                res += v * vsum
+            return res
+        return [get_cnt(i) for i in range(len(nodes))]`,
         },
         // {
         //     script: Script.RUST,
