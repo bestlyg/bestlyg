680431128a8a97af2baefa346cacd7bb0c5720a0
liyigang <bestlyg@foxmail.com>
2024-06-08 11:41:05 +0800

    feat: 🎸 add leetcode no.3040

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,8 +3,8 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '3038. 相同分数的最大操作数目 I',
-    url: 'https://leetcode.cn/problems/maximum-number-of-operations-with-the-same-score-i',
+    name: '3040. 相同分数的最大操作数目 II',
+    url: 'https://leetcode.cn/problems/maximum-number-of-operations-with-the-same-score-ii',
     difficulty: Difficulty.简单,
     tag: [],
     desc: `在确保 所有操作分数相同 的前提下，请你求出 最多 能进行多少次操作。请你返回按照上述要求 最多 可以进行的操作次数。`,
@@ -37,19 +37,28 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 33,
-            memory: 16.42,
-            desc: '遍历',
+            time: 2313,
+            memory: 377.62,
+            desc: 'dfs',
             code: `class Solution:
     def maxOperations(self, nums: List[int]) -> int:
-        prev = nums[0] + nums[1]
-        res = 1
-        for i in range(3, len(nums), 2):
-            if nums[i] + nums[i - 1] == prev:
-                res += 1
-            else:
-                break
-        return res`,
+        n = len(nums)
+        @cache
+        def dfs(l: int, r: int, score: int) -> int:
+            if r - l + 1 < 2: return 0
+            res = 0
+            if nums[l] + nums[l + 1] == score:
+                res = max(res, 1 + dfs(l + 2, r, score))
+            if nums[r] + nums[r - 1] == score:
+                res = max(res, 1 + dfs(l, r - 2, score))
+            if nums[l] + nums[r] == score:
+                res = max(res, 1 + dfs(l + 1, r - 1, score))
+            return res
+        return 1 + max(
+            dfs(2, n - 1, nums[0] + nums[1]),
+            dfs(0, n - 1 - 2, nums[n - 1] + nums[n - 2]),
+            dfs(1, n - 1 - 1, nums[0] + nums[n - 1])
+        )`,
         },
         // {
         //     script: Script.RUST,
