c87a1ec80479fdf56cbb852982d460dce4c2ee1c
bestlyg <1057966749@qq.com>
2024-06-08 23:36:35 +0800

    feat: 🎸 update package-json-extension

---
+++ packages/npm/package-json-extension/modern.config.ts
@@ -0,0 +1,50 @@
+import path from 'node:path';
+import { moduleTools, defineConfig } from '@modern-js/module-tools';
+
+const CWD = process.cwd();
+function resolve(...p: string[]) {
+    return path.resolve(__dirname, ...new Array(3).fill('..'), ...p);
+}
+export default defineConfig({
+    plugins: [moduleTools()],
+    buildConfig: [
+        {
+            sourceMap: true,
+            buildType: 'bundleless',
+            shims: true,
+            platform: 'node',
+            input: [resolve(CWD, 'src', '**', '*.ts'), resolve(CWD, 'src', '**', '*.tsx')],
+            target: 'esnext',
+            format: 'esm',
+            autoExtension: true,
+            outDir: resolve(CWD, 'dist', 'esm'),
+            esbuildOptions: options => {
+                options.outExtension = { '.js': '.js' };
+                return options;
+            },
+        },
+        {
+            sourceMap: true,
+            buildType: 'bundleless',
+            shims: true,
+            platform: 'node',
+            input: [resolve(CWD, 'src', '**', '*.ts'), resolve(CWD, 'src', '**', '*.tsx')],
+            target: 'esnext',
+            format: 'cjs',
+            autoExtension: true,
+            outDir: resolve(CWD, 'dist', 'lib'),
+            esbuildOptions: options => {
+                options.outExtension = { '.js': '.cjs' };
+                return options;
+            },
+        },
+        {
+            buildType: 'bundleless',
+            platform: 'browser',
+            dts: {
+                only: true,
+            },
+            outDir: resolve(CWD, 'dist', 'types'),
+        },
+    ],
+});
--- packages/npm/package-json-extension/src/require-json.ts
+++ packages/npm/package-json-extension/src/require-json.ts
@@ -4,6 +4,7 @@ import { resolve } from './functions';
 import { dirname } from 'path';
 import { PackageJsonExtend } from './types';
 import { error } from './print';
+import { fs } from 'zx';
 
 function removeNullKeys(obj) {
     if (_.isArray(obj)) {
@@ -22,18 +23,43 @@ export interface RequireJsonOptions {
     fieldName?: string;
 }
 
+export const cachedJson: Record<string, Record<string, any>> = {};
+
+export function loadFile(path: string, pathSet: Set<string>, options: RequireJsonOptions) {
+    const { fieldName = FIELD_NAME } = options;
+    if (pathSet.has(path)) error('Cyclic Path.');
+    if (cachedJson[path]) return;
+    const json = fs.readJSONSync(path);
+    cachedJson[path] = json;
+    pathSet.add(path);
+    for (const p of (json?.[fieldName] as PackageJsonExtend)?.extends ?? []) {
+        loadFile(resolve(dirname(path), p), pathSet, options);
+    }
+    pathSet.delete(path);
+}
+
+export function load<K extends keyof PackageJsonExtend>(
+    path: string,
+    key: K,
+    options: RequireJsonOptions
+) {
+    const { fieldName = FIELD_NAME } = options;
+    const json: PackageJsonExtend = cachedJson[path]?.[fieldName] ?? {};
+    return _.merge(
+        {},
+        ...(json.extends ?? []).map(p => load(resolve(dirname(path), p), key, options)),
+        json[key] ?? {}
+    );
+}
+
 export function requireJson<R = any>(path: string, options: RequireJsonOptions = {}): R {
     const { fieldName = FIELD_NAME } = options;
-    const map: Record<string, Record<string, any>> = {};
     try {
-        loadFile(path);
-        const vars: Record<string, any> = load(path, 'vars');
-        const metas: Record<string, any> = load(path, 'meta');
-        // console.log('map', map);
-        // console.log('vars', vars);
-        // console.log('metas', metas);
-        const mergedObj = _.merge({}, map[path], metas);
-        mergedObj[fieldName] = map[path][fieldName];
+        loadFile(path, new Set(), options);
+        const vars: Record<string, any> = load(path, 'vars', options);
+        const metas: Record<string, any> = load(path, 'meta', options);
+        const mergedObj = _.merge({}, cachedJson[path], metas);
+        mergedObj[fieldName] = cachedJson[path][fieldName];
         return removeNullKeys(
             _.cloneDeepWith(mergedObj, value => {
                 if (_.isString(value)) {
@@ -43,26 +69,5 @@ export function requireJson<R = any>(path: string, options: RequireJsonOptions =
         );
     } catch (err) {
         error('Update package json error.', err);
-    }
-
-    function loadFile(path: string, pathSet = new Set<string>()) {
-        if (pathSet.has(path)) error('Cyclic Path.');
-        if (map[path]) return;
-        const json = require(path);
-        map[path] = json;
-        pathSet.add(path);
-        for (const p of (json?.[fieldName] as PackageJsonExtend)?.extends ?? []) {
-            loadFile(resolve(dirname(path), p), pathSet);
-        }
-        pathSet.delete(path);
-    }
-
-    function load<K extends keyof PackageJsonExtend>(path: string, key: K) {
-        const json: PackageJsonExtend = map[path]?.[fieldName] ?? {};
-        return _.merge(
-            {},
-            ...(json.extends ?? []).map(p => load(resolve(dirname(path), p), key)),
-            json[key] ?? {}
-        );
     }
 }
