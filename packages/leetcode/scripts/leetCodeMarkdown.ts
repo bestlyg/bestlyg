eb2fda391700d6fa0d3311c398f6b82f23b50041
liyigang <bestlyg@foxmail.com>
2024-06-12 10:35:49 +0800

    feat: 🎸 add leetcode no.2806

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,12 +2,12 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: true,
-    name: '419. 甲板上的战舰',
-    url: 'https://leetcode.cn/problems/maximum-number-of-operations-with-the-same-score-ii',
+    exist: !true,
+    name: '2806. 取整购买后的账户余额',
+    url: 'https://leetcode.cn/problems/account-balance-after-rounded-purchase',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `在确保 所有操作分数相同 的前提下，请你求出 最多 能进行多少次操作。请你返回按照上述要求 最多 可以进行的操作次数。`,
+    desc: `请你返回一个整数，表示你在愿意支出金额为 purchaseAmount 块钱的前提下，购买之后剩下的余额。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -37,29 +37,17 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 37,
-            memory: 18.76,
-            desc: 'dfs',
-            code: `dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-class Solution:
-    def countBattleships(self, board: List[List[str]]) -> int:
-        n = len(board)
-        m = len(board[0])
-        res = 0
-        def check(i: int, j: int) -> int:
-            board[i][j] = '.'
-            for dir in dirs:
-                ni = i + dir[0]
-                nj = j + dir[1]
-                if 0 <= ni < n and 0 <= nj < m and board[ni][nj] == 'X':
-                    check(ni, nj)
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == 'X':
-                    res += 1
-                    check(i, j)
-        return res`,
+            time: 24,
+            memory: 16.45,
+            desc: '遍历',
+            code: `class Solution:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
+        cnt = 0
+        while (cnt + 1) * 10 < purchaseAmount: cnt += 1
+        if abs(cnt * 10 - purchaseAmount) < abs((cnt + 1) * 10 - purchaseAmount):
+            return 100 - 10 * cnt
+        else:
+            return 100 - 10 * (cnt + 1)`,
         },
         // {
         //     script: Script.RUST,
