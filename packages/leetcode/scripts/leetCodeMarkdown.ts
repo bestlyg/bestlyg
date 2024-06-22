d063761254e5689f0123c7132277703a7c38af39
bestlyg <1057966749@qq.com>
2024-06-23 00:38:20 +0800

    feat: 🎸 add leetcode no.520

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -2,9 +2,9 @@ import { Markdown, Difficulty, Tag, Script } from '@/base';
 import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
-    exist: !true,
-    name: '2663. 字典序最小的美丽字符串',
-    url: 'https://leetcode.cn/problems/lexicographically-smallest-beautiful-string',
+    exist: true,
+    name: '520. 检测大写字母',
+    url: 'https://leetcode.cn/problems/detect-capital',
     difficulty: Difficulty.简单,
     tag: [],
     desc: `请你找出并返回一个长度为 n 的美丽字符串，该字符串还满足：在字典序大于 s 的所有美丽字符串中字典序最小。如果不存在这样的字符串，则返回一个空字符串。`,
@@ -37,38 +37,12 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 205,
-            memory: 18.18,
-            desc: '贪心遍历',
+            time: 31,
+            memory: 16.41,
+            desc: '直接判断',
             code: `class Solution:
-    def smallestBeautifulString(self, s: str, k: int) -> str:
-        arr = list(s)
-        ord0 = ord('a')
-        def get_next(c: str) -> str:
-            ordc = ord(c)
-            if ordc - ord0 + 1 == k:
-                return None
-            return chr(ordc + 1)
-        for i in range(len(arr) - 1, -1, -1):
-            next_ord = get_next(arr[i])
-            while next_ord and (i > 0 and next_ord == arr[i - 1] or i > 1 and next_ord == arr[i - 2]):
-                next_ord = get_next(next_ord)
-            if next_ord:
-                arr[i] = next_ord
-                starti = i + 1
-                if 0 < i < len(arr) - 1 and arr[i - 1] == 'a':
-                    arr[starti] = 'b'
-                    starti += 1
-                for j in range(starti, len(arr)):
-                    cur = 0
-                    ch = chr(cur + ord0)
-                    while ch and (j > 0 and ch == arr[j - 1] or j > 1 and ch == arr[j - 2]):
-                        cur = (cur + 1) % k
-                        ch = chr(cur + ord0)
-                    arr[j] = ch
-                break
-        res = ''.join(arr)
-        return res if res > s else ''`,
+    def detectCapitalUse(self, word: str) -> bool:
+        return word.isupper() or word.islower() or len(word) > 1 and word[0].isupper() and word[1:].islower()`,
         },
         // {
         //     script: Script.RUST,
