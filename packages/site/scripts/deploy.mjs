a9e22f7c37b8ed467ec67a5eb2b8ba85e0b0f825
liyigang <bestlyg@foxmail.com>
2024-06-30 10:20:02 +0800

    feat: 🎸 update server

--- packages/site/scripts/deploy.mjs
+++ packages/site/scripts/deploy.mjs
@@ -2,6 +2,6 @@ import '@bestlyg/cli/globals';
 import { ssh, server } from '@bestlyg/config';
 
 const resolve = utils.getResolveFunction(import.meta, 1);
-$`scp -r ${resolve('dist')} ${ssh.username}@${ssh.ip}:${
+$`scp -r ${resolve('build')} ${ssh.username}@${ssh.ip}:${
     server.projectPath
-}/packages/npm/md2resume/dist`.stdio('inherit', 'inherit');
+}/packages/site/build`.stdio('inherit', 'inherit');
