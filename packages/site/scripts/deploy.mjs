e662d9250214087198d5ab560d50e1f827ec8b4f
liyigang <bestlyg@foxmail.com>
2024-06-30 15:24:51 +0800

    feat: 🎸 update site

--- packages/site/scripts/deploy.mjs
+++ packages/site/scripts/deploy.mjs
@@ -2,6 +2,6 @@ import '@bestlyg/cli/globals';
 import { ssh, server } from '@bestlyg/config';
 
 const resolve = utils.getResolveFunction(import.meta, 1);
-$`scp -r ${resolve('build')} ${ssh.username}@${ssh.ip}:${
+$`scp -r ${resolve('dist')} ${ssh.username}@${ssh.ip}:${
     server.webPath
 }/site`.stdio('inherit', 'inherit');
