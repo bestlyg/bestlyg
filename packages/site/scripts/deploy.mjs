570fdc452206c18900da41f485c2c93caed2942b
liyigang <bestlyg@foxmail.com>
2024-07-05 11:04:52 +0800

    feat: 🎸 update site

--- packages/site/scripts/deploy.mjs
+++ packages/site/scripts/deploy.mjs
@@ -2,7 +2,12 @@ import '@bestlyg/cli/globals';
 import { ssh, server } from '@bestlyg/config';
 
 const resolve = utils.getResolveFunction(import.meta, 1);
-$`scp -r ${resolve('dist', 'site.zip')} ${ssh.username}@${ssh.ip}:${server.webPath}/site/site.zip`.stdio(
-    'inherit',
-    'inherit'
-);
+
+const dirPath = resolve('dist');
+const zip = new AdmZip();
+zip.addLocalFolder(dirPath);
+await zip.writeZipPromise(resolve(dirPath, 'site.zip'));
+
+await $`scp -r ${resolve('dist', 'site.zip')} ${ssh.username}@${ssh.ip}:${
+    server.webPath
+}/site/site.zip`.stdio('inherit', 'inherit');
