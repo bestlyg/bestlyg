4ee342f2f4244eafe25153656a57ce0fd7b58788
bestlyg <bestlyg@foxmail.com>
2024-07-12 10:40:03 +0800

    feat: 🎸 add leetcode no.2974

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -7,7 +7,7 @@ const leetCodeMarkdown: Markdown = {
     url: 'https://leetcode.cn/problems/minimum-number-game',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个下标从 0 开始的 正 整数数组 nums 。如果 nums 的一个子数组满足：移除这个子数组后剩余元素 严格递增 ，那么我们称这个子数组为 移除递增 子数组。比方说，[5, 3, 4, 6, 7] 中的 [3, 4] 是一个移除递增子数组，因为移除该子数组后，[5, 3, 4, 6, 7] 变为 [5, 6, 7] ，是严格递增的。请你返回 nums 中 移除递增 子数组的总数目。`,
+    desc: `返回结果数组 arr 。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
