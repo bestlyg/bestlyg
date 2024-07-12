1a1a93cdf1d22aa39087c1eb122635a0bd7856fd
bestlyg <bestlyg@foxmail.com>
2024-07-12 10:36:03 +0800

    feat: 🎸 add leetcode no.2974

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,8 +3,8 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '2970. 统计移除递增子数组的数目 I',
-    url: 'https://leetcode.cn/problems/count-the-number-of-incremovable-subarrays-i',
+    name: '2974. 最小数字游戏',
+    url: 'https://leetcode.cn/problems/minimum-number-game',
     difficulty: Difficulty.简单,
     tag: [],
     desc: `给你一个下标从 0 开始的 正 整数数组 nums 。如果 nums 的一个子数组满足：移除这个子数组后剩余元素 严格递增 ，那么我们称这个子数组为 移除递增 子数组。比方说，[5, 3, 4, 6, 7] 中的 [3, 4] 是一个移除递增子数组，因为移除该子数组后，[5, 3, 4, 6, 7] 变为 [5, 6, 7] ，是严格递增的。请你返回 nums 中 移除递增 子数组的总数目。`,
@@ -37,16 +37,19 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 261,
-            memory: 16.5,
+            time: 49,
+            memory: 16.31,
             desc: '遍历',
             code: `class Solution:
-    def incremovableSubarrayCount(self, nums: List[int]) -> int:
-        def check(nums: List[int]) -> int:
-            for i in range(1, len(nums)):
-                if nums[i - 1] >= nums[i]: return 0
-            return 1
-        return sum(check(nums[0:j] + nums[i:]) for i in range(len(nums) + 1) for j in range(i))`,
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr = []
+        nums.sort()
+        i = 0
+        while i < len(nums):
+            arr.append(nums[i + 1])
+            arr.append(nums[i])
+            i += 2
+        return arr`,
         },
         // {
         //     script: Script.RUST,
