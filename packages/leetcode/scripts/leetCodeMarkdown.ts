9434a18391dfa50f43aa1804c01ae340bf4fbea7
bestlyg <bestlyg@foxmail.com>
2024-07-15 10:57:15 +0800

    feat: 🎸 add leetcode no.721

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,7 +3,7 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: true,
-    name: '807. 保持城市天际线',
+    name: '721. 账户合并',
     url: 'https://leetcode.cn/problems/max-increase-to-keep-city-skyline',
     difficulty: Difficulty.简单,
     tag: [],
@@ -37,19 +37,55 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 54,
-            memory: 16.57,
-            desc: '遍历',
-            code: `class Solution:
-    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        rows = [0] * n
-        cols = [0] * n
+            time: 1584,
+            memory: 19.45,
+            desc: '并查集合并数据',
+            code: `class UnionFind:
+    def __init__(self, n) -> None:
+        self.n = n
+        self.data = [i for i in range(0, n)]
+        self.sizes = [1] * n
+        self.cnt = n
+    def size(self, v: int) -> int:
+        return self.sizes[self.find(v)]
+    def find(self, v: int) -> int:
+        if self.data[v] != v:
+            self.data[v] = self.find(self.data[v])
+        return self.data[v]
+    def uni(self, v1: int, v2: int):
+        p1 = self.find(v1)
+        p2 = self.find(v2)
+        if p1 != p2:
+            self.sizes[p1] += self.sizes[p2]
+            self.cnt -= self.sizes[p2]
+            self.data[p2] = p1
+    def same(self, v1: int, v2: int):
+        return self.find(v1) == self.find(v2)
+    def get_items(self) -> List[List[int]]:
+        map = defaultdict(list)
+        for i in range(self.n): map[self.find(i)].append(i)
+        return map.values()
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        n = len(accounts)
+        uf = UnionFind(n)
         for i in range(n):
-            for j in range(n):
-                rows[i] = max(rows[i], grid[i][j])
-                cols[j] = max(cols[j], grid[i][j])
-        return sum(min(rows[i], cols[j]) - grid[i][j] for i in range(n) for j in range(n))`,
+            name1 = accounts[i][0]
+            emails1 = set(accounts[i][1:])
+            for j in range(i):
+                name2 = accounts[j][0]
+                emails2 = set(accounts[j][1:])
+                if name1 == name2 and not emails1.isdisjoint(emails2):
+                    uf.uni(i, j)
+        res = []
+        for arr in uf.get_items():
+            item = []
+            res.append(item)
+            item.append(accounts[arr[0]][0])
+            emails = []
+            for idx in arr: emails += accounts[idx][1:]
+            item += sorted(set(emails))
+        return res`,
         },
         // {
         //     script: Script.RUST,
