7fea8c5d9242a13416d504deedbed34cb375d556
bestlyg <bestlyg@foxmail.com>
2024-08-03 09:11:55 +0800

    feat: 🎸 add leetcode no.3143

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '3128. 直角三角形',
-    url: 'https://leetcode.cn/problems/right-triangles',
+    name: '3143. 正方形中的最多点数',
+    url: 'https://leetcode.cn/problems/maximum-points-inside-the-square',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你一个二维 boolean 矩阵 grid 。请你返回使用 grid 中的 3 个元素可以构建的 直角三角形 数目，且满足 3 个元素值 都 为 1 。`,
+    desc: `请你返回 合法 正方形中可以包含的 最多 点数。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -37,21 +37,28 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 476,
-            memory: 44.82,
-            desc: '遍历每一个1，查找垂直和水平方向上的所有的其他1的个数',
+            time: 154,
+            memory: 47.63,
+            desc: '排序后遍历',
             code: `class Solution:
-    def numberOfRightTriangles(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        rows = [sum(grid[i][j] for j in range(m)) for i in range(n)]
-        cols = [sum(grid[i][j] for i in range(n)) for j in range(m)]
-        return sum(
-            (rows[i] - 1) * (cols[j] - 1)
-            for i in range(n)
-            for j in range(m)
-            if grid[i][j]
-        )`,
+    def maxPointsInsideSquare(self, points: List[List[int]], s: str) -> int:
+        get_edge = lambda i: max(abs(points[i][0]), abs(points[i][1]))
+        idxs = sorted([i for i in range(len(points))], key = get_edge)
+        used = set()
+        res = i = 0
+        while i < len(idxs):
+            edge = get_edge(idxs[i])
+            cnt = 1
+            if s[idxs[i]] in used: break
+            used.add(s[idxs[i]])
+            while i + 1 < len(idxs) and get_edge(idxs[i + 1]) == edge:
+                i += 1
+                if s[idxs[i]] in used: return res
+                used.add(s[idxs[i]])
+                cnt += 1
+            res += cnt
+            i += 1
+        return res`,
         },
         // {
         //     script: Script.RUST,
