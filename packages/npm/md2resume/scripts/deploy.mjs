d250fd9f7d4ad1947b0701f4aeaa1f4c4ec44f9a
bestlyg <bestlyg@foxmail.com>
2024-08-04 17:36:07 +0800

    feat: 🎸 update md2resume

--- packages/npm/md2resume/scripts/deploy.mjs
+++ packages/npm/md2resume/scripts/deploy.mjs
@@ -1,7 +1,7 @@
 import '@bestlyg/cli/globals';
 import { ssh, server } from '@bestlyg/config';
 
-const resolve = utils.getResolveFunction(import.meta, 1);
+const resolve = best.utils.getResolveFunction(import.meta, 1);
 
 echo`scp -r ${resolve('dist')} ${ssh.username}@${ssh.ip}:${server.webPath}/resume`;
 
