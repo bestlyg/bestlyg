2fa609fd7fe43398b7ba43d780d53adc487ee944
bestlyg <bestlyg@foxmail.com>
2024-08-09 10:14:04 +0800

    feat: 🎸 add leetcode no.3132

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '3131. 找出与数组相加的整数 I',
-    url: 'https://leetcode.cn/problems/find-the-integer-added-to-array-i',
+    name: '3132. 找出与数组相加的整数 II',
+    url: 'https://leetcode.cn/problems/find-the-integer-added-to-array-ii/',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你两个长度相等的数组 nums1 和 nums2。数组 nums1 中的每个元素都与变量 x 所表示的整数相加。如果 x 为负数，则表现为元素值的减少。在与 x 相加后，nums1 和 nums2 相等 。当两个数组中包含相同的整数，并且这些整数出现的频次相同时，两个数组 相等 。返回整数 x 。`,
+    desc: `给你两个整数数组 nums1 和 nums2。从 nums1 中移除两个元素，并且所有其他元素都与变量 x 所表示的整数相加。如果 x 为负数，则表现为元素值的减少。执行上述操作后，nums1 和 nums2 相等 。当两个数组中包含相同的整数，并且这些整数出现的频次相同时，两个数组 相等 。返回能够实现数组相等的 最小 整数 x 。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -37,12 +37,32 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 33,
-            memory: 16.32,
-            desc: '一定存在x使其相等，则直接找对应位置的数字进行求差值',
+            time: 562,
+            memory: 16.57,
+            desc: '排序后暴力枚举',
             code: `class Solution:
-    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        return max(nums2) - max(nums1)`,
+    def minimumAddedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+        nums2.sort()
+        n = len(nums1)
+        res = inf
+        for i in range(n):
+            for j in range(i):
+                i1 = 0
+                while i1 < n and (i1 == i or i1 == j): i1 += 1
+                i2 = 0
+                diff = nums2[i2] - nums1[i1]
+                need_skip = False
+                while i1 < n:
+                    if nums2[i2] - nums1[i1] != diff:
+                        need_skip = True
+                        break
+                    i1 += 1
+                    while i1 < n and (i1 == i or i1 == j): i1 += 1
+                    i2 += 1
+                if not need_skip:
+                    res = min(res, diff)
+        return res`,
         },
         // {
         //     script: Script.RUST,
