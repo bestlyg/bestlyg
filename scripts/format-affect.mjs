daf9db97c14192964adafb97ef32b9ecbedf5dd0
bestlyg <bestlyg@foxmail.com>
2024-08-09 10:33:36 +0800

    feat: 🎸 format

--- scripts/format-affect.mjs
+++ scripts/format-affect.mjs
@@ -1,5 +1,13 @@
 import '@bestlyg/cli/globals';
 
-const diff = await $`git diff-index --name-only HEAD --`;
-console.log(diff.stdout);
-// await $`pnpm prettier --write ${diff}`;
+const CWD = best.utils.CWD;
+const resolve = best.utils.getResolveFunction(import.meta, 1);
+
+const diffPath = await $`git diff HEAD --name-only`;
+const diffRealPath = diffPath.stdout
+    .trim()
+    .split('\n')
+    .map(v => resolve(decodeURIComponent(v)));
+
+console.log(diffRealPath);
+await $`pnpm prettier --write ${diffRealPath}`;
