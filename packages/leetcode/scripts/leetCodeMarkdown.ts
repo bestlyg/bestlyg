c97d99ba311b0259ec82da5c46f9c13b81c183e8
bestlyg <bestlyg@bestlygdeMacBook-Pro.local>
2024-08-10 14:15:43 +0800

    feat: 🎸 add leetcode no.2940

--- packages/leetcode/scripts/leetCodeMarkdown.ts
+++ packages/leetcode/scripts/leetCodeMarkdown.ts
@@ -3,11 +3,11 @@ import { backquote } from '@/utils';
 
 const leetCodeMarkdown: Markdown = {
     exist: !true,
-    name: '3132. 找出与数组相加的整数 II',
-    url: 'https://leetcode.cn/problems/find-the-integer-added-to-array-ii/',
+    name: '2940. 找到 Alice 和 Bob 可以相遇的建筑',
+    url: 'https://leetcode.cn/problems/find-building-where-alice-and-bob-can-meet',
     difficulty: Difficulty.简单,
     tag: [],
-    desc: `给你两个整数数组 nums1 和 nums2。从 nums1 中移除两个元素，并且所有其他元素都与变量 x 所表示的整数相加。如果 x 为负数，则表现为元素值的减少。执行上述操作后，nums1 和 nums2 相等 。当两个数组中包含相同的整数，并且这些整数出现的频次相同时，两个数组 相等 。返回能够实现数组相等的 最小 整数 x 。`,
+    desc: `请你能返回一个数组 ans ，其中 ans[i] 是第 i 个查询中，Alice 和 Bob 可以相遇的 最左边的建筑 。如果对于查询 i ，Alice 和 Bob 不能相遇，令 ans[i] 为 -1 。`,
     solutions: [
         // {
         //     date: new Date('2020.11.11').getTime(),
@@ -37,31 +37,25 @@ const leetCodeMarkdown: Markdown = {
         {
             script: Script.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 562,
-            memory: 16.57,
-            desc: '排序后暴力枚举',
+            time: 324,
+            memory: 39.23,
+            desc: '离线处理queries，过滤能立即得出答案的，剩余的一定是h[i] > h[j]，此时从左往右遍历h，维护当前下标之前的所有需求最小堆',
             code: `class Solution:
-    def minimumAddedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort()
-        n = len(nums1)
-        res = inf
-        for i in range(n):
-            for j in range(i):
-                i1 = 0
-                while i1 < n and (i1 == i or i1 == j): i1 += 1
-                i2 = 0
-                diff = nums2[i2] - nums1[i1]
-                need_skip = False
-                while i1 < n:
-                    if nums2[i2] - nums1[i1] != diff:
-                        need_skip = True
-                        break
-                    i1 += 1
-                    while i1 < n and (i1 == i or i1 == j): i1 += 1
-                    i2 += 1
-                if not need_skip:
-                    res = min(res, diff)
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        res = [-1 for _ in range(len(queries))]
+        arr = [[] for _ in range(len(heights))]
+        for idx, (i, j) in enumerate(queries):
+            if i > j: i, j = j, i
+            if i == j or heights[i] < heights[j]: res[idx] = j
+            else: arr[j].append((heights[i], idx))
+
+        q = []
+        for cur_idx, h in enumerate(heights):
+            while q and q[0][0] < h:
+                idx = heappop(q)[1]
+                res[idx] = cur_idx
+            for v in arr[cur_idx]:
+                heappush(q, v)
         return res`,
         },
         // {
