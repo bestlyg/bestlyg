598b4b3db24a8d94b21218c367f21affb22dd08a
bestlyg <bestlyg@foxmail.com>
2024-08-17 09:41:05 +0800

    feat: 🎸 add leetcode no.3137

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3117. 划分数组得到最小的值之和',
-    url: 'https://leetcode.cn/problems/minimum-sum-of-values-by-dividing-array',
+    name: '3137. K 周期字符串需要的最少操作次数',
+    url: 'https://leetcode.cn/problems/minimum-number-of-operations-to-make-word-k-periodic',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回将 nums 划分为 m 个子数组所能得到的可能的 最小 子数组 值 之和。如果无法完成这样的划分，则返回 -1 。`,
+    desc: `返回使 word 成为 K 周期字符串 所需的 最少 操作次数。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 1329,
-            memory: 419.88,
-            desc: 'dfs记录当前遍历到的两个数组下标和当前记录的与值',
+            time: 102,
+            memory: 19.83,
+            desc: '遍历后统计频率最高的子串',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
