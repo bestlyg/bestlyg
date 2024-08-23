b0a75d52ed0a08e0413a3b2baf4be6a42f437d30
bestlyg <bestlyg@foxmail.com>
2024-08-23 08:21:11 +0800

    feat: 🎸 add leetcode no.3007

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3133. 数组最后一个元素的最小值',
-    url: 'https://leetcode.cn/problems/minimum-array-end',
+    name: '3007. 价值和小于等于 K 的最大数字',
+    url: 'https://leetcode.cn/problems/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回 nums[n - 1] 可能的 最小 值。`,
+    desc: `num 的 累加价值 是从 1 到 num 的数字的 总 价值。如果 num 的累加价值小于或等于 k 则被认为是 廉价 的。请你返回 最大 的廉价数字。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 31,
-            memory: 16.3,
-            desc: '把两个字符串进行拼接，在保留原有x的二进制位的基础上增加n的遍历',
+            time: 387,
+            memory: 24.11,
+            desc: '二分快速计算，利用数位dp求一个数的累加值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
