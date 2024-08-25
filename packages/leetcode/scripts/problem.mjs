2671864446b47497e28c8bdb3ff7df5c83a7501d
bestlyg <bestlyg@foxmail.com>
2024-08-25 09:39:05 +0800

    feat: 🎸 add leetcode no.698

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -7,19 +7,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {LeetCodeProblem} */
 export const problem = {
-    exist: !true,
-    name: '3146. 两个字符串的排列差',
-    url: 'https://leetcode.cn/problems/permutation-difference-between-two-strings',
+    exist: true,
+    name: '698. 划分为k个相等的子集',
+    url: 'https://leetcode.cn/problems/partition-to-k-equal-sum-subsets',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你两个字符串 s 和 t，每个字符串中的字符都不重复，且 t 是 s 的一个排列。排列差 定义为 s 和 t 中每个字符在两个字符串中位置的绝对差值之和。返回 s 和 t 之间的 排列差 。`,
+    desc: `给定一个整数数组  nums 和一个正整数 k，找出是否有可能把这个数组分成 k 个非空子集，其总和都相等。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 44,
-            memory: 16.5,
-            desc: '遍历',
+            time: 521,
+            memory: 48.65,
+            desc: '记忆化递归，同时利用二进制记录是否被使用',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
