20a973e353d71327420638ca77ec6740cab98fd9
bestlyg <bestlyg@foxmail.com>
2024-08-29 10:11:31 +0800

    feat: 🎸 add leetcode no.3142

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3144. 分割字符频率相等的最少子字符串',
-    url: 'https://leetcode.cn/problems/minimum-substring-partition-of-equal-character-frequency',
+    name: '3142. 判断矩阵是否满足条件',
+    url: 'https://leetcode.cn/problems/check-if-grid-satisfies-conditions',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回 s 最少 能分割成多少个平衡子字符串。`,
+    desc: `如果 所有 格子都满足以上条件，那么返回 true ，否则返回 false 。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 9615,
-            memory: 16.46,
-            desc: 'dp[i]表示以i字符结尾时的最少分割数',
+            time: 38,
+            memory: 16.24,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
