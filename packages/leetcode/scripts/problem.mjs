3c6b8f313b19f1b01c8772ad682e6e1183c2b281
bestlyg <bestlyg@foxmail.com>
2024-08-30 14:04:24 +0800

    feat: 🎸 add leetcode no.3153

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3142. 判断矩阵是否满足条件',
-    url: 'https://leetcode.cn/problems/check-if-grid-satisfies-conditions',
+    name: '3153. 所有数对中数位不同之和',
+    url: 'https://leetcode.cn/problems/sum-of-digit-differences-of-all-pairs',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `如果 所有 格子都满足以上条件，那么返回 true ，否则返回 false 。`,
+    desc: `请你返回 nums 中 所有 整数对里，数位不同之和。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 38,
-            memory: 16.24,
-            desc: '遍历',
+            time: 645,
+            memory: 30.79,
+            desc: '遍历时记录前面每一个数位存在的不同字符数',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
