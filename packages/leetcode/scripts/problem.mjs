a4f6a160b7e96dce453f2a81603f27e439a578e3
bestlyg <bestlyg@foxmail.com>
2024-08-31 08:10:48 +0800

    feat: 🎸 add leetcode no.3127

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3153. 所有数对中数位不同之和',
-    url: 'https://leetcode.cn/problems/sum-of-digit-differences-of-all-pairs',
+    name: '3127. 构造相同颜色的正方形',
+    url: 'https://leetcode.cn/problems/make-a-square-with-the-same-color',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回 nums 中 所有 整数对里，数位不同之和。`,
+    desc: `你的任务是改变 至多一个 格子的颜色，使得矩阵中存在一个 2 x 2 颜色完全相同的正方形。如果可以得到一个相同颜色的 2 x 2 正方形，那么返回 true ，否则返回 false 。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 645,
-            memory: 30.79,
-            desc: '遍历时记录前面每一个数位存在的不同字符数',
+            time: 37,
+            memory: 16.36,
+            desc: '遍历所有可能，判断以它为左上角点的四个方格是否存在3个相同颜色',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
