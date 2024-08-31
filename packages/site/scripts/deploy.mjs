22f9cc2d58f66393808a72893e67540f98f93437
bestlyg <bestlyg@foxmail.com>
2024-08-31 23:20:59 +0800

    feat: 🎸 update deploy of site

--- packages/site/scripts/deploy.mjs
+++ packages/site/scripts/deploy.mjs
@@ -1,13 +1,22 @@
 import '@bestlyg/cli/globals';
+import { execSync } from 'child_process';
 import { ssh, server } from '@bestlyg/config';
 
 const resolve = best.utils.getResolveFunction(import.meta, 1);
-
+const fileName = 'site.zip';
 const dirPath = resolve('dist');
 const zip = new best.AdmZip();
 zip.addLocalFolder(dirPath);
-await zip.writeZipPromise(resolve(dirPath, 'site.zip'));
+await zip.writeZipPromise(resolve(dirPath, fileName));
 
-await $`scp -r ${resolve('dist', 'site.zip')} ${ssh.username}@${ssh.ip}:${
-    server.webPath
-}/site/site.zip`.stdio('inherit', 'inherit');
+const distPath = `${server.webPath}/site/${fileName}`;
+
+await $`scp -r ${resolve(dirPath, fileName)} ${ssh.username}@${ssh.ip}:${distPath}`.stdio(
+    'inherit',
+    'inherit',
+);
+
+const commands = [`unzip -o ${distPath}`];
+execSync(`ssh -T ${ssh.username}@${ssh.ip} "${commands.join('; ')}"`, {
+    stdio: 'inherit',
+});
