a8d1145eb06524f413e7ba4221a36e1baa567182
bestlyg <bestlyg@foxmail.com>
2024-09-03 09:29:52 +0800

    feat: 🎸 add leetcode no.2708

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -7,19 +7,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '2024. 考试的最大困扰度',
-    url: 'https://leetcode.cn/problems/maximize-the-confusion-of-an-exam',
+    exist: !true,
+    name: '2708. 一个小组的最大实力值',
+    url: 'https://leetcode.cn/problems/maximum-strength-of-a-group',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `你的任务是改变 至多一个 格子的颜色，使得矩阵中存在一个 2 x 2 颜色完全相同的正方形。如果可以得到一个相同颜色的 2 x 2 正方形，那么返回 true ，否则返回 false 。`,
+    desc: `请你返回老师创建的小组能得到的最大实力值为多少。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 204,
-            memory: 16.68,
-            desc: '滑动窗口',
+            time: 46,
+            memory: 16.51,
+            desc: '排序后拿所有的正书和成对拿负数',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
