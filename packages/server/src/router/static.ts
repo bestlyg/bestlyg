1c19dde41ab9d70cc4b975bd41a64d8d7738a65b
bestlyg <bestlyg@foxmail.com>
2024-09-04 16:24:49 +0800

    feat: 🎸 update server

--- packages/server/src/router/static.ts
+++ packages/server/src/router/static.ts
@@ -1,11 +1,16 @@
-import { resolve } from '@/utils/index';
+import { logger, resolve } from '@/utils/index';
 import Router from '@koa/router';
 import send from 'koa-send';
 
 const router = new Router();
 
-router.get('/(.*)', async ctx => {
-    await send(ctx, '/' + ctx.params['0'], { root: resolve('..', '..', 'static') });
+router.get('/(.*)', async (ctx, next) => {
+    try {
+        await send(ctx, '/' + ctx.params['0'], { root: resolve('..', '..', 'static') });
+    } catch (err) {
+        logger.error('Static Router Error: ', err?.toString());
+        await next();
+    }
 });
 
 export default router;
