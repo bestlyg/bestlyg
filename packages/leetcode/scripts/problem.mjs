138154c8d99032f5909292dac15b8ddb66895865
bestlyg <bestlyg@foxmail.com>
2024-09-04 09:40:00 +0800

    feat: 🎸 add leetcode no.2860

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2708. 一个小组的最大实力值',
-    url: 'https://leetcode.cn/problems/maximum-strength-of-a-group',
+    name: '2860. 让所有学生保持开心的分组方法数',
+    url: 'https://leetcode.cn/problems/happy-students',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回老师创建的小组能得到的最大实力值为多少。`,
+    desc: `返回能够满足让所有学生保持开心的分组方法的数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 46,
-            memory: 16.51,
-            desc: '排序后拿所有的正书和成对拿负数',
+            time: 100,
+            memory: 26.37,
+            desc: '排序后遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
