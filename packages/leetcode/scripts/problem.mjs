02bb7873b4bb0493fb8adccf40ffddcf23c08361
bestlyg <bestlyg@foxmail.com>
2024-09-09 10:31:47 +0800

    feat: 🎸 add leetcode no.2181

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -7,18 +7,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '977. 有序数组的平方',
-    url: 'https://leetcode.cn/problems/squares-of-a-sorted-array',
+    exist: !true,
+    name: '2181. 合并零之间的节点',
+    url: 'https://leetcode.cn/problems/merge-nodes-in-between-zeros',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。`,
+    desc: `对于每两个相邻的 0 ，请你将它们之间的所有节点合并成一个节点，其值是所有已合并节点的值之和。然后将所有 0 移除，修改后的链表不应该含有任何 0 。返回修改后链表的头节点 head 。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 44,
-            memory: 18.36,
+            time: 1094,
+            memory: 79.6,
             desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
