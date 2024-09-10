7c3dbbf33995ce9714be2f4a3f81fa948491769d
bestlyg <bestlyg@foxmail.com>
2024-09-10 13:24:22 +0800

    feat: 🎸 add leetcode no.2552

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -7,19 +7,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {LeetCodeProblem} */
 export const problem = {
-    exist: !true,
-    name: '2181. 合并零之间的节点',
-    url: 'https://leetcode.cn/problems/merge-nodes-in-between-zeros',
+    exist: true,
+    name: '2552. 统计上升四元组',
+    url: 'https://leetcode.cn/problems/count-increasing-quadruplets',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `对于每两个相邻的 0 ，请你将它们之间的所有节点合并成一个节点，其值是所有已合并节点的值之和。然后将所有 0 移除，修改后的链表不应该含有任何 0 。返回修改后链表的头节点 head 。`,
+    desc: `给你一个长度为 n 下标从 0 开始的整数数组 nums ，它包含 1 到 n 的所有数字，请你返回上升四元组的数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 1094,
-            memory: 79.6,
-            desc: '遍历',
+            time: 2666,
+            memory: 16.88,
+            desc: '定义三元组为i<j<k&&v[i]<v[k]<v[j]，遍历时同时记录前面的三元组数量，以及当前节点当k位置时的数量',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
