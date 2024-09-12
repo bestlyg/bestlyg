0cab1f39688963b25aff243b00ec4d2a9871263d
bestlyg <bestlyg@foxmail.com>
2024-09-12 08:28:48 +0800

    feat: 🎸 add leetcode no.2576

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2555. 两个线段获得的最多奖品',
-    url: 'https://leetcode.cn/problems/maximize-win-from-two-segments',
+    name: '2576. 求出最多标记下标',
+    url: 'https://leetcode.cn/problems/find-the-maximum-number-of-marked-indices',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回在选择两个最优线段的前提下，可以获得的 最多 奖品数目。`,
+    desc: `请你执行上述操作任意次，返回 nums 中最多可以标记的下标数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
             time: 614,
             memory: 28.86,
-            desc: '遍历所有区间，并且统计非区间内的最大值',
+            desc: '贪心，排序后从0和n//2两个位置依次向后匹配',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
