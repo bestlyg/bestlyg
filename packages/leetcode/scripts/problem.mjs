cdf5281d3e1afeb38ce2b34a85de897f5034fb16
bestlyg <bestlyg@foxmail.com>
2024-09-12 08:25:56 +0800

    feat: 🎸 add leetcode no.2555

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -7,19 +7,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '2552. 统计上升四元组',
-    url: 'https://leetcode.cn/problems/count-increasing-quadruplets',
+    exist: !true,
+    name: '2555. 两个线段获得的最多奖品',
+    url: 'https://leetcode.cn/problems/maximize-win-from-two-segments',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个长度为 n 下标从 0 开始的整数数组 nums ，它包含 1 到 n 的所有数字，请你返回上升四元组的数目。`,
+    desc: `请你返回在选择两个最优线段的前提下，可以获得的 最多 奖品数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 2666,
-            memory: 16.88,
-            desc: '定义三元组为i<j<k&&v[i]<v[k]<v[j]，遍历时同时记录前面的三元组数量，以及当前节点当k位置时的数量',
+            time: 614,
+            memory: 28.86,
+            desc: '遍历所有区间，并且统计非区间内的最大值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
