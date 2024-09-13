7cf377f08720cc781ac2cc58a8e3eb5edfd01a4c
bestlyg <bestlyg@foxmail.com>
2024-09-13 10:12:14 +0800

    feat: 🎸 add leetcode no.2398

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2576. 求出最多标记下标',
-    url: 'https://leetcode.cn/problems/find-the-maximum-number-of-marked-indices',
+    name: '2398. 预算内的最多机器人数目',
+    url: 'https://leetcode.cn/problems/maximum-number-of-robots-within-budget',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你执行上述操作任意次，返回 nums 中最多可以标记的下标数目。`,
+    desc: `请你返回在 不超过 budget 的前提下，你 最多 可以 连续 运行的机器人数目为多少。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: new Date('2024.02.07').getTime(),
-            time: 614,
-            memory: 28.86,
-            desc: '贪心，排序后从0和n//2两个位置依次向后匹配',
+            time: 1296,
+            memory: 24.51,
+            desc: '由于连续，遍历所有可行区间，并记录当前区间内的最大值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
