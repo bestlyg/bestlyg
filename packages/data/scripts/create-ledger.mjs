da654a771c850dd0deafd670cde190fec93c32e6
bestlyg <bestlyg@foxmail.com>
2024-09-17 14:16:19 +0800

    feat: 🎸 update data

--- packages/data/scripts/create-ledger.mjs
+++ packages/data/scripts/create-ledger.mjs
@@ -12,129 +12,133 @@ import {
 
 const dataPath = resolve(decryptPath, 'ledger', 'data');
 
-const date = best.dayjs(argv._);
+async function main(formattedDate) {
+    const date = best.dayjs(formattedDate);
 
-if (!date.isValid()) {
-    echo`Invalid Date ${JSON.stringify(argv)}`;
-    process.exit(-1);
-}
+    if (!date.isValid()) {
+        echo`Invalid Date ${JSON.stringify(argv)}`;
+        process.exit(-1);
+    }
 
-const ledgerPath = resolve(dataPath, 'index.ts');
-const yearPath = resolve(dataPath, date.format(LEDGER_FORMAT_YEAR), 'index.ts');
-const monthPath = resolve(
-    dataPath,
-    date.format(LEDGER_FORMAT_YEAR),
-    date.format(LEDGER_FORMAT_MONTH),
-    'index.ts'
-);
-const dayPath = resolve(
-    dataPath,
-    date.format(LEDGER_FORMAT_YEAR),
-    date.format(LEDGER_FORMAT_MONTH),
-    date.format(LEDGER_FORMAT_DAY) + '.ts'
-);
-
-async function insertYearToLedger() {
-    const varName = toYearVarName(date);
-    let fileData = (await fs.readFile(ledgerPath)).toString();
-    if (fileData.includes(varName)) return;
-    fileData = insertString(fileData, fileData.indexOf('[') + 1, `\n${varName},`);
-    fileData = insertString(
-        fileData,
-        fileData.indexOf('\n') + 1,
-        `import ${varName} from './${date.format(LEDGER_FORMAT_YEAR)}/index';\n`
+    const ledgerPath = resolve(dataPath, 'index.ts');
+    const yearPath = resolve(dataPath, date.format(LEDGER_FORMAT_YEAR), 'index.ts');
+    const monthPath = resolve(
+        dataPath,
+        date.format(LEDGER_FORMAT_YEAR),
+        date.format(LEDGER_FORMAT_MONTH),
+        'index.ts',
     );
-    await fs.writeFile(ledgerPath, fileData);
-}
+    const dayPath = resolve(
+        dataPath,
+        date.format(LEDGER_FORMAT_YEAR),
+        date.format(LEDGER_FORMAT_MONTH),
+        date.format(LEDGER_FORMAT_DAY) + '.ts',
+    );
 
-async function createYear() {
-    await insertYearToLedger();
-    await fs.ensureDir(path.dirname(yearPath));
-    await fs.writeFile(
-        yearPath,
-        `
+    async function insertYearToLedger() {
+        const varName = toYearVarName(date);
+        let fileData = (await fs.readFile(ledgerPath)).toString();
+        if (fileData.includes(varName)) return;
+        fileData = insertString(fileData, fileData.indexOf('[') + 1, `\n${varName},`);
+        fileData = insertString(
+            fileData,
+            fileData.indexOf('\n') + 1,
+            `import ${varName} from './${date.format(LEDGER_FORMAT_YEAR)}/index';\n`,
+        );
+        await fs.writeFile(ledgerPath, fileData);
+    }
+
+    async function createYear() {
+        await insertYearToLedger();
+        await fs.ensureDir(path.dirname(yearPath));
+        await fs.writeFile(
+            yearPath,
+            `
 import { LedgerYearRecord } from '../../types';
 const ledgerYearRecord: LedgerYearRecord = {
     date: '${date.format(LEDGER_FORMAT_YEAR)}',
     record: [],
 };
 export default ledgerYearRecord;
-`.trim()
-    );
-}
+`.trim(),
+        );
+    }
 
-async function insertMonthToYear() {
-    const varName = toMonthVarName(date);
-    let fileData = (await fs.readFile(yearPath)).toString();
-    if (fileData.includes(varName)) return;
-    fileData = insertString(fileData, fileData.indexOf('[') + 1, `\n${varName},`);
-    fileData = insertString(
-        fileData,
-        fileData.indexOf('\n') + 1,
-        `import ${varName} from './${date.format(LEDGER_FORMAT_MONTH)}/index';\n`
-    );
-    await fs.writeFile(yearPath, fileData);
-}
+    async function insertMonthToYear() {
+        const varName = toMonthVarName(date);
+        let fileData = (await fs.readFile(yearPath)).toString();
+        if (fileData.includes(varName)) return;
+        fileData = insertString(fileData, fileData.indexOf('[') + 1, `\n${varName},`);
+        fileData = insertString(
+            fileData,
+            fileData.indexOf('\n') + 1,
+            `import ${varName} from './${date.format(LEDGER_FORMAT_MONTH)}/index';\n`,
+        );
+        await fs.writeFile(yearPath, fileData);
+    }
 
-async function createMonth() {
-    if (!(await fs.exists(yearPath))) await createYear();
-    await insertMonthToYear();
-    await fs.ensureDir(path.dirname(monthPath));
-    await fs.writeFile(
-        monthPath,
-        `
+    async function createMonth() {
+        if (!(await fs.exists(yearPath))) await createYear();
+        await insertMonthToYear();
+        await fs.ensureDir(path.dirname(monthPath));
+        await fs.writeFile(
+            monthPath,
+            `
 import { LedgerMonthRecord } from '../../../types';
 const ledgerMonthRecord: LedgerMonthRecord = {
     date: '${date.format(LEDGER_FORMAT_MONTH)}',
     record: [],
 };
 export default ledgerMonthRecord;
-`.trim()
-    );
-}
+`.trim(),
+        );
+    }
 
-async function insertDayToMonth() {
-    const varName = toDayVarName(date);
-    let fileData = (await fs.readFile(monthPath)).toString();
-    if (fileData.includes(varName)) return;
-    fileData = insertString(fileData, fileData.indexOf('[') + 1, `\n${varName},`);
-    fileData = insertString(
-        fileData,
-        fileData.indexOf('\n') + 1,
-        `import ${varName} from './${date.format(LEDGER_FORMAT_DAY)}';\n`
-    );
-    await fs.writeFile(monthPath, fileData);
-}
+    async function insertDayToMonth() {
+        const varName = toDayVarName(date);
+        let fileData = (await fs.readFile(monthPath)).toString();
+        if (fileData.includes(varName)) return;
+        fileData = insertString(fileData, fileData.indexOf('[') + 1, `\n${varName},`);
+        fileData = insertString(
+            fileData,
+            fileData.indexOf('\n') + 1,
+            `import ${varName} from './${date.format(LEDGER_FORMAT_DAY)}';\n`,
+        );
+        await fs.writeFile(monthPath, fileData);
+    }
 
-async function createDay() {
-    if (!(await fs.exists(monthPath))) await createMonth();
-    await insertDayToMonth();
-    await fs.ensureDir(path.dirname(dayPath));
-    await fs.writeFile(
-        dayPath,
-        `
+    async function createDay() {
+        if (!(await fs.exists(monthPath))) await createMonth();
+        await insertDayToMonth();
+        await fs.ensureDir(path.dirname(dayPath));
+        await fs.writeFile(
+            dayPath,
+            `
 import { LedgerRecordItemType, LedgerDayRecord } from '../../../types';
 const ledgerDayRecord: LedgerDayRecord = {
     date: '${date.format(LEDGER_FORMAT_DAY)}',
     record: [],
 };
 export default ledgerDayRecord;
-`.trim()
-    );
-}
+`.trim(),
+        );
+    }
 
-/**
- * @param {string} str1
- * @param {number} idx
- * @param {string} str2
- */
-function insertString(str1, idx, str2) {
-    return str1.substring(0, idx) + str2 + str1.substring(idx);
+    /**
+     * @param {string} str1
+     * @param {number} idx
+     * @param {string} str2
+     */
+    function insertString(str1, idx, str2) {
+        return str1.substring(0, idx) + str2 + str1.substring(idx);
+    }
+
+    await createDay();
+    echo`Create ledger success for ${argv._}.`;
 }
 
 try {
-    await createDay();
-    echo`Create ledger success for ${argv._}.`
+    main(argv._);
 } catch (err) {
     echo`Error : ${err?.toString()}`;
 }
