90c59cfe2ab89e91055749542730c7c47117b0f7
bestlyg <bestlyg@foxmail.com>
2024-09-19 14:18:39 +0800

    feat: 🎸 add leetcode no.2414

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -7,19 +7,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '815. 公交路线',
-    url: 'https://leetcode.cn/problems/points-that-intersect-with-cars',
+    exist: !true,
+    name: '2414. 最长的字母序连续子字符串的长度',
+    url: 'https://leetcode.cn/problems/length-of-the-longest-alphabetical-continuous-substring',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个下标从 0 开始的二维整数数组 nums 表示汽车停放在数轴上的坐标。对于任意下标 i，nums[i] = [starti, endi] ，其中 starti 是第 i 辆车的起点，endi 是第 i 辆车的终点。返回数轴上被车 任意部分 覆盖的整数点的数目。`,
+    desc: `给你一个仅由小写英文字母组成的字符串 s ，返回其 最长 的 字母序连续子字符串 的长度。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 193,
-            memory: 51.18,
-            desc: '利用set存储数据，bfs时对遍历过的值进行O1的删除',
+            time: 460,
+            memory: 16.81,
+            desc: '一次遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
