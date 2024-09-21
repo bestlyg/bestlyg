473fd0b2b079ec0a2d183da288584530ee1db425
bestlyg <bestlyg@foxmail.com>
2024-09-21 12:44:02 +0800

    feat: 🎸 add leetcode no.2374

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -8,18 +8,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2376. 统计特殊整数',
-    url: 'https://leetcode.cn/problems/count-special-integers',
+    name: '2374. 边积分最高的节点',
+    url: 'https://leetcode.cn/problems/node-with-highest-edge-score',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `如果一个正整数每一个数位都是 互不相同 的，我们称它是 特殊整数 。给你一个 正 整数 n ，请你返回区间 [1, n] 之间特殊整数的数目。`,
+    desc: `返回 边积分 最高的节点。如果多个节点的 边积分 相同，返回编号 最小 的那个。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 349,
-            memory: 35.45,
-            desc: '数位DP' ,
+            time: 196,
+            memory: 27.92,
+            desc: '遍历' ,
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
