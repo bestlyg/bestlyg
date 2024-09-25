d46653b011d2373c82524ba87bb7fec0a9d7d851
bestlyg <bestlyg@foxmail.com>
2024-09-25 18:52:23 +0800

    feat: 🎸 add leetcode no.2306

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2207. 字符串中最多数目的子序列',
-    url: 'https://leetcode.cn/problems/maximize-number-of-subsequences-in-a-string',
+    name: '2306. 公司命名',
+    url: 'https://leetcode.cn/problems/naming-a-company',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回插入一个字符后，text 中最多包含多少个等于 pattern 的 子序列 。`,
+    desc: `返回 不同 且有效的公司名字的数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 162,
-            memory:16.99,
-            desc: '遍历时统计两个字符的数量',
+            time: 409,
+            memory: 28.9,
+            desc: '把首字符做key存储数据，遍历时取差集',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
