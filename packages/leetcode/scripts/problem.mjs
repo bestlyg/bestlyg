2ab86d0ee362855a210319245f424c1e7316cf6a
bestlyg <bestlyg@foxmail.com>
2024-09-26 18:00:08 +0800

    feat: 🎸 add leetcode no.2535

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: !true,
-    name: '2306. 公司命名',
-    url: 'https://leetcode.cn/problems/naming-a-company',
+    exist: true,
+    name: '2535. 数组元素和与数字和的绝对差',
+    url: 'https://leetcode.cn/problems/difference-between-element-sum-and-digit-sum-of-an-array',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回 不同 且有效的公司名字的数目。`,
+    desc: `返回 元素和 与 数字和 的绝对差。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 409,
-            memory: 28.9,
-            desc: '把首字符做key存储数据，遍历时取差集',
+            time: 82,
+            memory: 16.44,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
