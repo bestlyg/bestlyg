a6a35bd79ea7efe0ce4e7d6e4f2e0436ce5e9bc7
bestlyg <bestlyg@foxmail.com>
2024-09-29 09:33:35 +0800

    feat: 🎸 add leetcode no.2073

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist:true,
-    name: '2516. 每种字符至少取 K 个',
-    url: 'https://leetcode.cn/problems/take-k-of-each-character-from-left-and-right',
+    exist:!true,
+    name: '2073. 买票需要的时间',
+    url: 'https://leetcode.cn/problems/time-needed-to-buy-tickets',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个由字符 'a'、'b'、'c' 组成的字符串 s 和一个非负整数 k 。每分钟，你可以选择取走 s 最左侧 还是 最右侧 的那个字符。你必须取走每种字符 至少 k 个，返回需要的 最少 分钟数；如果无法取到，则返回 -1 。`,
+    desc: `返回位于位置 k（下标从 0 开始）的人完成买票需要的时间（以秒为单位）。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 1110,
-            memory: 16.91,
-            desc: '双指针，先假设全部从左侧取值的最大下标，再依次增加右侧取值',
+            time: 48,
+            memory: 16.57,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
