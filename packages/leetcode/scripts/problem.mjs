192919e6f6dd89477831dc8461fdc999299bae9d
bestlyg <bestlyg@foxmail.com>
2024-10-02 10:17:16 +0800

    feat: 🎸 add leetcode no.1870

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '983. 最低票价',
-    url: 'https://leetcode.cn/problems/minimum-cost-for-tickets',
+    exist: !true,
+    name: '1870. 准时到达的列车最小时速',
+    url: 'https://leetcode.cn/problems/minimum-speed-to-arrive-on-time',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回 你想要完成在给定的列表 days 中列出的每一天的旅行所需要的最低消费 。`,
+    desc: `返回能满足你准时到达办公室所要求全部列车的 最小正整数 时速（单位：千米每小时），如果无法准时到达，则返回 -1 。生成的测试用例保证答案不超过 107 ，且 hour 的 小数点后最多存在两位数字 。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 47,
-            memory: 18.82,
-            desc: 'dfs',
+            time: 1579,
+            memory: 27.52,
+            desc: '二分答案',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
