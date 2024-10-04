b61c8493cca61cd78ade4ee76276719b55d03441
bestlyg <bestlyg@foxmail.com>
2024-10-04 11:11:22 +0800

    feat: 🎸 add leetcode no.1227

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '1928. 规定时间内到达终点的最小花费',
-    url: 'https://leetcode.cn/problems/minimum-cost-to-reach-destination-in-time',
+    name: '1227. 飞机座位分配概率',
+    url: 'https://leetcode.cn/problems/airplane-seat-assignment-probability',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你 maxTime，edges 和 passingFees ，请你返回完成旅行的 最小费用 ，如果无法在 maxTime 分钟以内完成旅行，请你返回 -1 。`,
+    desc: `第 n 位乘客坐在自己的座位上的概率是多少？`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 1579,
-            memory: 27.52,
-            desc: '按费用进行优先队列',
+            time: 37,
+            memory: 16.34,
+            desc: '数学',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
