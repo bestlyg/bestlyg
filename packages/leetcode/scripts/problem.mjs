5b937358e4e4a8a58e200ee73d0e7127d95bc98d
bestlyg <bestlyg@foxmail.com>
2024-10-05 12:24:14 +0800

    feat: 🎸 add leetcode no.2187

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '1227. 飞机座位分配概率',
-    url: 'https://leetcode.cn/problems/airplane-seat-assignment-probability',
+    name: '2187. 完成旅途的最少时间',
+    url: 'https://leetcode.cn/problems/minimum-time-to-complete-trips',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `第 n 位乘客坐在自己的座位上的概率是多少？`,
+    desc: `给你一个整数 totalTrips ，表示所有公交车 总共 需要完成的旅途数目。请你返回完成 至少 totalTrips 趟旅途需要花费的 最少 时间。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 37,
-            memory: 16.34,
-            desc: '数学',
+            time: 2504,
+            memory: 28.99,
+            desc: '二分答案',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
