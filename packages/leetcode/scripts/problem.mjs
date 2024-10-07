5dd6fa36040a963cdce648c9dcbf2bfdca7dee83
bestlyg <bestlyg@foxmail.com>
2024-10-07 11:24:30 +0800

    feat: 🎸 add leetcode no.871

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '134. 加油站',
-    url: 'https://leetcode.cn/problems/minimum-time-to-complete-trips',
+    exist: !true,
+    name: '871. 最低加油次数',
+    url: 'https://leetcode.cn/problems/minimum-number-of-refueling-stops',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个整数 totalTrips ，表示所有公交车 总共 需要完成的旅途数目。请你返回完成 至少 totalTrips 趟旅途需要花费的 最少 时间。`,
+    desc: `为了到达目的地，汽车所必要的最低加油次数是多少？如果无法到达目的地，则返回 -1 。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 95,
-            memory: 22.05,
-            desc: '对于每一个下标进行尝试，如果尝试失败则从下一个失败点进行尝试',
+            time: 52,
+            memory: 16.74,
+            desc: '贪心，每次走到最大距离时，加一次最多的加油站的油',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
