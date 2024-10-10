af7b1a8c3dcc89e72560647f06e886c56c26bc15
bestlyg <bestlyg@foxmail.com>
2024-10-10 08:30:36 +0800

    feat: 🎸 add leetcode no.3162

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '1436. 旅行终点站',
-    url: 'https://leetcode.cn/problems/minimum-number-of-refueling-stops',
+    exist: !true,
+    name: '3162. 优质数对的总数 I',
+    url: 'https://leetcode.cn/problems/find-the-number-of-good-pairs-i',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `为了到达目的地，汽车所必要的最低加油次数是多少？如果无法到达目的地，则返回 -1 。`,
+    desc: `返回 优质数对 的总数。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 43,
-            memory: 16.3,
-            desc: '哈希存储',
+            time: 45,
+            memory: 16.57,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
