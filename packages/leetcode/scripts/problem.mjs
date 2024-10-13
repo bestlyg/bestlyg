e495b8e17140e582c39c3131666c6785d73f0f17
bestlyg <bestlyg@foxmail.com>
2024-10-13 12:23:02 +0800

    feat: 🎸 add leetcode no.1884

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3158. 求出出现两次数字的 XOR 值',
-    url: 'https://leetcode.cn/problems/find-the-xor-of-numbers-which-appear-twice',
+    name: '1884. 鸡蛋掉落-两枚鸡蛋',
+    url: 'https://leetcode.cn/problems/egg-drop-with-2-eggs-and-n-floors',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个数组 nums ，数组中的数字 要么 出现一次，要么 出现两次。请你返回数组中所有出现两次数字的按位 XOR 值，如果没有数字出现过两次，返回 0 。`,
+    desc: `请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少？`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 38,
-            memory: 16.48,
-            desc: '遍历',
+            time: 2719,
+            memory: 21.09,
+            desc: '遍历,每次尝试从一个楼层开始掉落时，统计碎了和没碎的情况',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
