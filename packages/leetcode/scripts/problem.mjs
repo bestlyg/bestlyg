b48aaae56ed57bc106f5f6fda824febc2c90cea8
bestlyg <bestlyg@foxmail.com>
2024-10-15 09:54:17 +0800

    feat: 🎸 add leetcode no.3200

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '175. 组合两个表',
-    url: 'https://leetcode.cn/problems/combine-two-tables',
+    name: '3200. 三角形的最大高度',
+    url: 'https://leetcode.cn/problems/maximum-height-of-a-triangle',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `编写解决方案，报告 Person 表中每个人的姓、名、城市和州。如果 personId 的地址不在 Address 表中，则报告为 null 。`,
+    desc: `给你两个整数 red 和 blue，分别表示红色球和蓝色球的数量。你需要使用这些球来组成一个三角形，满足第 1 行有 1 个球，第 2 行有 2 个球，第 3 行有 3 个球，依此类推。每一行的球必须是 相同 颜色，且相邻行的颜色必须 不同。返回可以实现的三角形的 最大 高度。`,
     solutions: [
         {
-            script: LeetCodeScript.MYSQL,
+            script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 547,
-            // memory: 21.09,
-            desc: 'left join',
+            time: 30,
+            memory: 16.58,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
