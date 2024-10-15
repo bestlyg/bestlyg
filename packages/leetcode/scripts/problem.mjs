882bd78a3d5a0152e066d2b1fe0c921a262f0967
bestlyg <bestlyg@foxmail.com>
2024-10-15 10:36:20 +0800

    feat: 🎸 add leetcode no.177

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '176. 第二高的薪水',
-    url: 'https://leetcode.cn/problems/second-highest-salary',
+    name: '177. 第N高的薪水',
+    url: 'https://leetcode.cn/problems/nth-highest-salary',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `查询并返回 Employee 表中第二高的 不同 薪水 。如果不存在第二高的薪水，查询应该返回 null(Pandas 则返回 None) 。`,
+    desc: `查询 Employee 表中第 n 高的工资。如果没有第 n 个最高工资，查询结果应该为 null 。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 3662,
+            time: 1052,
             // memory: 16.58,
-            desc: '遍历',
+            desc: '通过子查询查到当前超过当前薪水人数的总数',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
