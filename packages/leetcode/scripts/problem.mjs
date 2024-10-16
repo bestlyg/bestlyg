efcd1c40153d74bfd1c243a8cb7213ca49d0ff59
bestlyg <bestlyg@foxmail.com>
2024-10-16 17:32:41 +0800

    feat: 🎸 add leetcode no.262

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '185. 部门工资前三高的所有员工',
-    url: 'https://leetcode.cn/problems/department-top-three-salaries',
+    name: '262. 行程和用户',
+    url: 'https://leetcode.cn/problems/trips-and-users',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `编写解决方案，找出每个部门中 收入高的员工 。`,
+    desc: `编写解决方案找出 "2013-10-01" 至 "2013-10-03" 期间非禁止用户（乘客和司机都必须未被禁止）的取消率。非禁止用户即 banned 为 No 的用户，禁止用户即 banned 为 Yes 的用户。其中取消率 Cancellation Rate 需要四舍五入保留 两位小数 。`,
     solutions: [
         {
             script: LeetCodeScript.SQL,
             // date: '2024-09-15',
-            time: 1431,
+            time: 767,
             // memory: 16.45,
-            desc: '利用子查询查找比当前薪水大的人数，如果小于3那就说明当前人是前三',
+            desc: '先用子查询查找到每个时间段的符合条件的取消次数，再在外部查询中相加求结果',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
