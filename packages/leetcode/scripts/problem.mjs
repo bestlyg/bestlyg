ace6e03bef50272680c7a51ab06f567eec814fae
bestlyg <bestlyg@foxmail.com>
2024-10-16 16:29:06 +0800

    feat: 🎸 add leetcode no.184

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3194. 最小元素和最大元素的最小平均值',
-    url: 'https://leetcode.cn/problems/minimum-average-of-smallest-and-largest-elements',
+    name: '184. 部门工资最高的员工',
+    url: 'https://leetcode.cn/problems/department-highest-salary',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回 averages 中的 最小 元素。`,
+    desc: `查找出每个部门中薪资最高的员工。`,
     solutions: [
         {
-            script: LeetCodeScript.PY,
+            script: LeetCodeScript.SQL,
             // date: '2024-09-15',
-            time: 37,
-            memory: 16.45,
-            desc: '查找score时同时查找大于当前分数的数量',
+            time: 1327,
+            // memory: 16.45,
+            desc: '利用子查询查找比当前薪水大的同部门的人为0的人',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
