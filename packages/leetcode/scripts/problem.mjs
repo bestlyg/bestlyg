8d63daa4685e601f9ff3e3d0b36ddcc0aa40a349
bestlyg <bestlyg@foxmail.com>
2024-10-16 10:05:24 +0800

    feat: 🎸 add leetcode no.3194

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,17 +4,17 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '178. 分数排名',
-    url: 'https://leetcode.cn/problems/rank-scores',
+    name: '3194. 最小元素和最大元素的最小平均值',
+    url: 'https://leetcode.cn/problems/minimum-average-of-smallest-and-largest-elements',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `编写一个解决方案来查询分数的排名。按 score 降序返回结果表。`,
+    desc: `返回 averages 中的 最小 元素。`,
     solutions: [
         {
-            script: LeetCodeScript.SQL,
+            script: LeetCodeScript.PY,
             // date: '2024-09-15',
-            time: 805,
-            // memory: 16.58,
+            time: 37,
+            memory: 16.45,
             desc: '查找score时同时查找大于当前分数的数量',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
