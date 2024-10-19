2d6c461332ef900260199ac1b60c4e589f8ca7a1
bestlyg <bestlyg@foxmail.com>
2024-10-19 10:38:36 +0800

    feat: 🎸 add leetcode no.3192

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3191. 使二进制数组全部等于 1 的最少操作次数 I',
-    url: 'https://leetcode.cn/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i',
+    name: '3192. 使二进制数组全部等于 1 的最少操作次数 II',
+    url: 'https://leetcode.cn/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-ii',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回将 nums 中所有元素变为 1 的 最少 操作次数。如果无法全部变成 1 ，返回 -1 。`,
+    desc: `请你返回将 nums 中所有元素变为 1 的 最少 操作次数。`,
     solutions: [
         {
             script: LeetCodeScript.SQL,
             // date: '2024-09-15',
-            time: 158,
-            memory: 20.34,
-            desc: '模拟',
+            time: 62,
+            memory: 20.55,
+            desc: '从前往后依次翻转0',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
