228eca033a58bb587b36c82f6a99a753ee197f60
bestlyg <bestlyg@foxmail.com>
2024-10-23 08:32:26 +0800

    feat: 🎸 add leetcode no.3185

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,9 +3,9 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist:! true,
-    name: '3184. 构成整天的下标对数目 I',
-    url: 'https://leetcode.cn/problems/count-pairs-that-form-a-complete-day-i',
+    exist: !true,
+    name: '3185. 构成整天的下标对数目 II',
+    url: 'https://leetcode.cn/problems/count-pairs-that-form-a-complete-day-ii',
     level: LeetCodeLevel.Easy,
     tagList: [],
     desc: `给你一个整数数组 hours，表示以 小时 为单位的时间，返回一个整数，表示满足 i < j 且 hours[i] + hours[j] 构成 整天 的下标对 i, j 的数目。`,
@@ -13,9 +13,9 @@ export const problem = {
         {
             script: LeetCodeScript.PY3,
             // date: '2024-09-15',
-            time: 14,
-            memory: 16.59,
-            desc: '遍历',
+            time: 75,
+            memory: 60.75,
+            desc: '取模后遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
