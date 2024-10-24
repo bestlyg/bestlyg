29508f2d612245425d263d13fd54e7f2d004ddc0
bestlyg <bestlyg@foxmail.com>
2024-10-24 08:20:22 +0800

    feat: 🎸 add leetcode no.3175

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3185. 构成整天的下标对数目 II',
-    url: 'https://leetcode.cn/problems/count-pairs-that-form-a-complete-day-ii',
+    name: '3175. 找到连续赢 K 场比赛的第一位玩家',
+    url: 'https://leetcode.cn/problems/find-the-first-player-to-win-k-games-in-a-row',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个整数数组 hours，表示以 小时 为单位的时间，返回一个整数，表示满足 i < j 且 hours[i] + hours[j] 构成 整天 的下标对 i, j 的数目。`,
+    desc: `这个比赛的赢家是 第一位连续 赢下 k 场比赛的玩家。请你返回这个比赛的赢家编号。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-09-15',
-            time: 75,
-            memory: 60.75,
-            desc: '取模后遍历',
+            time: 43,
+            memory: 28.94,
+            desc: '遍历，只要取到最大数就直接返回',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
