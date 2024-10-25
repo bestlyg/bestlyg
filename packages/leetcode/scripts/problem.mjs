01f0335df8a83239f55eb3e9a59a480c9967488b
bestlyg <bestlyg@foxmail.com>
2024-10-25 09:50:41 +0800

    feat: 🎸 add leetcode no.3180

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3175. 找到连续赢 K 场比赛的第一位玩家',
-    url: 'https://leetcode.cn/problems/find-the-first-player-to-win-k-games-in-a-row',
+    name: '3180. 执行操作可获得的最大总奖励 I',
+    url: 'https://leetcode.cn/problems/maximum-total-reward-using-operations-i',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `这个比赛的赢家是 第一位连续 赢下 k 场比赛的玩家。请你返回这个比赛的赢家编号。`,
+    desc: `以整数形式返回执行最优操作能够获得的 最大 总奖励。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-09-15',
-            time: 43,
-            memory: 28.94,
-            desc: '遍历，只要取到最大数就直接返回',
+            time: 4026,
+            memory: 763.09,
+            desc: '去重后dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
