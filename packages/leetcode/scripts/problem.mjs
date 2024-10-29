e5a9bf13623257959a4db9694ce9041f9c63583c
bestlyg <bestlyg@foxmail.com>
2024-10-30 00:13:54 +0800

    feat: 🎸 add leetcode no.3216

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3211. 生成不含相邻零的二进制字符串',
-    url: 'https://leetcode.cn/problems/generate-binary-strings-without-adjacent-zeros',
+    name: '3216. 交换后字典序最小的字符串',
+    url: 'https://leetcode.cn/problems/lexicographically-smallest-string-after-a-swap',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回所有长度为 n 的 有效 字符串，可以以任意顺序排列。`,
+    desc: `给你一个仅由数字组成的字符串 s，在最多交换一次 相邻 且具有相同 奇偶性 的数字后，返回可以得到的字典序最小的字符串。如果两个数字都是奇数或都是偶数，则它们具有相同的奇偶性。例如，5 和 9、2 和 4 奇偶性相同，而 6 和 9 奇偶性不同。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-09-15',
-            time: 52,
-            memory:17.8,
-            desc: 'dfs',
+            time: 3,
+            memory: 16.28,
+            desc: '遍历，找到一个可以交换的字符位置',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
