c2e3de12128cc43fc2a38e2b5ea94ca27989a5b3
bestlyg <bestlyg@foxmail.com>
2024-11-02 13:20:24 +0800

    feat: 🎸 add leetcode no.3259

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3216. 交换后字典序最小的字符串',
-    url: 'https://leetcode.cn/problems/lexicographically-smallest-string-after-a-swap',
+    name: '3259. 超级饮料的最大强化能量',
+    url: 'https://leetcode.cn/problems/maximum-energy-boost-from-two-drinks',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个仅由数字组成的字符串 s，在最多交换一次 相邻 且具有相同 奇偶性 的数字后，返回可以得到的字典序最小的字符串。如果两个数字都是奇数或都是偶数，则它们具有相同的奇偶性。例如，5 和 9、2 和 4 奇偶性相同，而 6 和 9 奇偶性不同。`,
+    desc: `返回在接下来的 n 小时内你能获得的 最大 总强化能量。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
-            // date: '2024-09-15',
-            time: 3,
-            memory: 16.28,
-            desc: '遍历，找到一个可以交换的字符位置',
+            date: '2024-11-01',
+            time: 1121,
+            memory: 426.5,
+            desc: 'dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
