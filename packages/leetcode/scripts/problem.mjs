5d85f53b23c24de1ef6bfcbafc8fa49e5f8ce3a1
bestlyg <bestlyg@foxmail.com>
2024-11-06 08:34:34 +0800

    feat: 🎸 add leetcode no.3254

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3222. 求出硬币游戏的赢家',
-    url: 'https://leetcode.cn/problems/find-the-winning-player-in-coin-game',
+    name: '3254. 长度为 K 的子数组的能量值 I',
+    url: 'https://leetcode.cn/problems/find-the-power-of-k-size-subarrays-i',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `两名玩家都采取 最优 策略，请你返回游戏的赢家。`,
+    desc: `请你返回一个长度为 n - k + 1 的整数数组 results ，其中 results[i] 是子数组 nums[i..(i + k - 1)] 的能量值。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
             time: 0,
-            memory: 16.56,
-            desc: '只有1个75和4个10才能组成115的价格',
+            memory: 16.63,
+            desc: '遍历时同时记录当前值与前面值是否符合要求',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
