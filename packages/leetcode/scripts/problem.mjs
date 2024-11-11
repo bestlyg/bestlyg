ed4f6c12ac790c4028bf69053dd9098200220fd2
bestlyg <bestlyg@foxmail.com>
2024-11-11 10:40:14 +0800

    feat: 🎸 add leetcode no.1547

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '540. 有序数组中的单一元素',
-    url: 'https://leetcode.cn/problems/design-neighbor-sum-service',
+    exist: !true,
+    name: '1547. 切棍子的最小成本',
+    url: 'https://leetcode.cn/problems/minimum-cost-to-cut-a-stick',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `给你一个 n x n 的二维数组 grid，它包含范围 [0, n2 - 1] 内的不重复元素。实现 neighborSum 类。`,
+    desc: `返回切棍子的 最小总成本 。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 4,
-            memory: 23.52,
-            desc: '遍历时用异或去重',
+            time: 942,
+            memory: 19.98,
+            desc: 'dfs尝试在每一个点进行切割',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
