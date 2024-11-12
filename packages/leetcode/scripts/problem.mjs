03f4021f43df76deec11f8a43be9393a366b0f6d
bestlyg <bestlyg@foxmail.com>
2024-11-12 09:18:46 +0800

    feat: 🎸 add leetcode no.3258

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '1547. 切棍子的最小成本',
-    url: 'https://leetcode.cn/problems/minimum-cost-to-cut-a-stick',
+    name: '3258. 统计满足 K 约束的子字符串数量 I',
+    url: 'https://leetcode.cn/problems/count-substrings-that-satisfy-k-constraint-i',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回切棍子的 最小总成本 。`,
+    desc: `返回一个整数，表示 s 的所有满足 k 约束 的子字符串的数量。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 942,
-            memory: 19.98,
-            desc: 'dfs尝试在每一个点进行切割',
+            time: 124,
+            memory: 16.62,
+            desc: '遍历每个子串',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
