50000430cdd507acad03391bc34efbdb32191ff8
bestlyg <bestlyg@foxmail.com>
2024-11-19 09:59:24 +0800

    feat: 🎸 add leetcode no.3243

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '661. 图片平滑器',
-    url: 'https://leetcode.cn/problems/friends-of-appropriate-ages',
+    exist: !true,
+    name: '3243. 新增道路查询后的最短距离 I',
+    url: 'https://leetcode.cn/problems/shortest-distance-after-road-addition-queries-i',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回 最少 翻转次数，使得矩阵中 所有 行和列都是 回文的 ，且矩阵中 1 的数目可以被 4 整除 。`,
+    desc: `返回一个数组 answer，对于范围 [0, queries.length - 1] 中的每个 i，answer[i] 是处理完前 i + 1 个查询后，从城市 0 到城市 n - 1 的最短路径的长度。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 127,
-            memory: 17.56,
-            desc: '遍历',
+            time: 421,
+            memory: 16.93,
+            desc: 'bfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
