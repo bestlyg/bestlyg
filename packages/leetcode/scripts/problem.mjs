ab7b7900754eec65a08a9a15e99bc9536d46a357
bestlyg <bestlyg@foxmail.com>
2024-11-20 09:50:20 +0800

    feat: 🎸 add leetcode no.3244

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3243. 新增道路查询后的最短距离 I',
-    url: 'https://leetcode.cn/problems/shortest-distance-after-road-addition-queries-i',
+    name: '3244. 新增道路查询后的最短距离 II',
+    url: 'https://leetcode.cn/problems/shortest-distance-after-road-addition-queries-ii',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回一个数组 answer，对于范围 [0, queries.length - 1] 中的每个 i，answer[i] 是处理完前 i + 1 个查询后，从城市 0 到城市 n - 1 的最短路径的长度。`,
+    desc: `返返回一个数组 answer，对于范围 [0, queries.length - 1] 中的每个 i，answer[i] 是处理完前 i + 1 个查询后，从城市 0 到城市 n - 1 的最短路径的长度。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 421,
-            memory: 16.93,
-            desc: 'bfs',
+            time: 79,
+            memory: 44.22,
+            desc: '贪心，考虑到不会交叉的路径，尽可能选择新连接的路径即可',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
