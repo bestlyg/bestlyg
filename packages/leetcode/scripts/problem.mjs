674ba637436eb77e5b8159de5c26b52b61c813d5
bestlyg <bestlyg@foxmail.com>
2024-11-22 10:11:48 +0800

    feat: 🎸 add leetcode no.3233

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3248. 矩阵中的蛇',
-    url: 'https://leetcode.cn/problems/snake-in-matrix',
+    name: '3233. 统计不是特殊数字的数字数量',
+    url: 'https://leetcode.cn/problems/find-the-count-of-numbers-which-are-not-special',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回执行 commands 后蛇所停留的最终单元格的位置。`,
+    desc: `返回区间 [l, r] 内 不是 特殊数字 的数字数量。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 7,
-            memory: 16.38,
-            desc: '模拟',
+            time: 253,
+            memory: 16.86,
+            desc: '枚举素数，只有素数的乘积才是特殊数字',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
