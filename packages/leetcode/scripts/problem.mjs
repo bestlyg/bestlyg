3121a3d7bec71cf28447ae47f2c80ce90d723327
bestlyg <bestlyg@foxmail.com>
2024-11-23 10:01:54 +0800

    feat: 🎸 add leetcode no.3238

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3233. 统计不是特殊数字的数字数量',
-    url: 'https://leetcode.cn/problems/find-the-count-of-numbers-which-are-not-special',
+    name: '3238. 求出胜利玩家的数目',
+    url: 'https://leetcode.cn/problems/find-the-number-of-winning-players',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回区间 [l, r] 内 不是 特殊数字 的数字数量。`,
+    desc: `请你返回游戏中 胜利玩家 的数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 253,
-            memory: 16.86,
-            desc: '枚举素数，只有素数的乘积才是特殊数字',
+            time: 4,
+            memory: 16.44,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
