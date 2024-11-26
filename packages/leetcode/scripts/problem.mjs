a2651c1048b4ad826660b715e161d5eeedf82c71
bestlyg <bestlyg@foxmail.com>
2024-11-26 08:53:42 +0800

    feat: 🎸 add leetcode no.3206

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,18 +3,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '743. 网络延迟时间',
-    url: 'https://leetcode.cn/problems/find-the-number-of-winning-players',
+    exist: !true,
+    name: '3206. 交替组 I',
+    url: 'https://leetcode.cn/problems/alternating-groups-i',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回游戏中 胜利玩家 的数目。`,
+    desc: `请你返回 交替 组的数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 6,
-            memory: 18.07,
+            time: 0,
+            memory:17.32,
             desc: 'bfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
