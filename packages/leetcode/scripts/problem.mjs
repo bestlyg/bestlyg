0a5d675184df94b8d7459805395116de0f4477dc
bestlyg <bestlyg@foxmail.com>
2024-11-27 09:38:15 +0800

    feat: 🎸 add leetcode no.3208

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,8 +4,8 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3206. 交替组 I',
-    url: 'https://leetcode.cn/problems/alternating-groups-i',
+    name: '3208. 交替组 II',
+    url: 'https://leetcode.cn/problems/alternating-groups-ii',
     level: LeetCodeLevel.Easy,
     tagList: [],
     desc: `请你返回 交替 组的数目。`,
@@ -13,9 +13,9 @@ export const problem = {
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 0,
-            memory:17.32,
-            desc: 'bfs',
+            time: 183,
+            memory: 20.6,
+            desc: '记录以r为最后一个元素的滑动窗口',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
