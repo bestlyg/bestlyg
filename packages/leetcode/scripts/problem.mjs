c99fb7c9e1bafd8bcfc19e8138407d1ae62aa2fd
bestlyg <bestlyg@foxmail.com>
2024-11-30 21:03:54 +0800

    feat: 🎸 add leetcode no.3232

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3250. 单调数组对的数目 I',
-    url: 'https://leetcode.cn/problems/find-the-count-of-monotonic-pairs-i',
+    name: '3232. 判断是否可以赢得数字游戏',
+    url: 'https://leetcode.cn/problems/find-if-digit-game-can-be-won',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回所有 单调 数组对的数目。`,
+    desc: `如果 Alice 能赢得这场游戏，返回 true；否则，返回 false。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 2817,
-            memory: 123.61,
-            desc: 'dfs',
+            time: 1,
+            memory: 16.98,
+            desc: '只要两者不等，就一定Alice赢',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
