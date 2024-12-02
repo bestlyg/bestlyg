f0cbebc4d4b15539c6961c2b97e4652ba501ea86
bestlyg <bestlyg@foxmail.com>
2024-12-02 09:46:51 +0800

    feat: 🎸 add leetcode no.52

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,7 +4,7 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: true,
-    name: '51. N 皇后',
+    name: '52. N 皇后 II',
     url: 'https://leetcode.cn/problems/find-if-digit-game-can-be-won',
     level: LeetCodeLevel.Easy,
     tagList: [],
@@ -13,8 +13,8 @@ export const problem = {
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 1707,
-            memory: 17.51,
+            time: 2329,
+            memory: 17.29,
             desc: 'dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
