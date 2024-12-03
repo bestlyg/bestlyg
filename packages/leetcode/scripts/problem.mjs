ed5441295f4a6157db272fe18602d5f225936a81
bestlyg <bestlyg@foxmail.com>
2024-12-03 10:05:05 +0800

    feat: 🎸 add leetcode no.3274

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '52. N 皇后 II',
-    url: 'https://leetcode.cn/problems/find-if-digit-game-can-be-won',
+    exist: !true,
+    name: '3274. 检查棋盘方格颜色是否相同',
+    url: 'https://leetcode.cn/problems/check-if-two-chessboard-squares-have-the-same-color',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `如果 Alice 能赢得这场游戏，返回 true；否则，返回 false。`,
+    desc: `如果这两个方格颜色相同，返回 true，否则返回 false。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 2329,
-            memory: 17.29,
-            desc: 'dfs',
+            time: 38,
+            memory:16.98,
+            desc: '获取坐标值直接判断',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
