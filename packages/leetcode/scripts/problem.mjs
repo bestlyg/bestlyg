76d7a604f74576bc99cb12bdb3934856c2768d1a
bestlyg <bestlyg@foxmail.com>
2024-12-06 10:17:40 +0800

    feat: 🎸 add leetcode no.999

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: !true,
-    name: '3274. 检查棋盘方格颜色是否相同',
-    url: 'https://leetcode.cn/problems/check-if-two-chessboard-squares-have-the-same-color',
+    exist: true,
+    name: '999. 可以被一步捕获的棋子数',
+    url: 'https://leetcode.cn/problems/available-captures-for-rook',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `如果这两个方格颜色相同，返回 true，否则返回 false。`,
+    desc: `返回白车将能 吃掉 的 卒的数量。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 38,
-            memory:16.98,
-            desc: '获取坐标值直接判断',
+            time: 0,
+            memory:17.23,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
