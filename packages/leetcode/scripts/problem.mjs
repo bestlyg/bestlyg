08d4d0134ce8e610ef907ae68c91cff443655db7
bestlyg <bestlyg@foxmail.com>
2024-12-10 10:16:09 +0800

    feat: 🎸 add leetcode no.935

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -3,19 +3,19 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
-    exist: true,
-    name: '1812. 判断国际象棋棋盘中一个格子的颜色',
-    url: 'https://leetcode.cn/problems/available-captures-for-rook',
+    exist: !true,
+    name: '935. 骑士拨号器',
+    url: 'https://leetcode.cn/problems/knight-dialer',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回白车将能 吃掉 的 卒的数量。`,
+    desc: `你可以将骑士放置在任何数字单元格上，然后你应该执行 n - 1 次移动来获得长度为 n 的号码。所有的跳跃应该是有效的骑士跳跃。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 0,
-            memory:16.96,
-            desc: '遍历',
+            time: 59,
+            memory: 27.67,
+            desc: 'dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
