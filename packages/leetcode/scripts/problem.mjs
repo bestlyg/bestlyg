b271055cdee3dc6ca280f420e37b7ef946a6b5c5
bestlyg <bestlyg@foxmail.com>
2024-12-11 09:52:47 +0800

    feat: 🎸 add leetcode no.2717

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '935. 骑士拨号器',
-    url: 'https://leetcode.cn/problems/knight-dialer',
+    name: '2717. 半有序排列',
+    url: 'https://leetcode.cn/problems/semi-ordered-permutation',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `你可以将骑士放置在任何数字单元格上，然后你应该执行 n - 1 次移动来获得长度为 n 的号码。所有的跳跃应该是有效的骑士跳跃。`,
+    desc: `返回使 nums 变成 半有序排列 所需的最小操作次数。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 59,
-            memory: 27.67,
-            desc: 'dfs',
+            time: 0,
+            memory: 16.88,
+            desc: '遍历找到最大最小值后直接交换',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
