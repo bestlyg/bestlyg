66dbe5fee9e07a10160af97435592d857a393560
bestlyg <bestlyg@foxmail.com>
2024-12-12 09:35:29 +0800

    feat: 🎸 add leetcode no.2931

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2717. 半有序排列',
-    url: 'https://leetcode.cn/problems/semi-ordered-permutation',
+    name: '2931. 购买物品的最大开销',
+    url: 'https://leetcode.cn/problems/maximum-spending-after-buying-items',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回使 nums 变成 半有序排列 所需的最小操作次数。`,
+    desc: `请你返回购买所有 m * n 件物品需要的 最大开销 。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 0,
-            memory: 16.88,
-            desc: '遍历找到最大最小值后直接交换',
+            time:175,
+            memory: 27.46,
+            desc: '贪心，每次找最便宜的',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
