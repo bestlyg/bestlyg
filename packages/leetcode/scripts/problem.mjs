efad587cbcb635e0097bdcc9bc59babd557c4faf
bestlyg <bestlyg@foxmail.com>
2024-12-13 08:39:46 +0800

    feat: 🎸 add leetcode no.3264

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '2931. 购买物品的最大开销',
+    name: '3264. K 次乘运算后的最终数组 I',
     url: 'https://leetcode.cn/problems/maximum-spending-after-buying-items',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回购买所有 m * n 件物品需要的 最大开销 。`,
+    desc: `请你返回执行完 k 次乘运算之后，最终的 nums 数组。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time:175,
-            memory: 27.46,
-            desc: '贪心，每次找最便宜的',
+            time: 3,
+            memory: 16.95,
+            desc: '堆每次取出最小值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
