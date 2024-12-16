ab52e3dbdadf1e792920a1c2a2be722b2a07db1b
bestlyg <bestlyg@foxmail.com>
2024-12-16 09:57:27 +0800

    feat: 🎸 add leetcode no.1847

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '1338. 数组大小减半',
-    url: 'https://leetcode.cn/problems/reduce-array-size-to-the-half',
+    name: '1847. 最近的房间',
+    url: 'https://leetcode.cn/problems/closest-room',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `返回 至少 能删除数组中的一半整数的整数集合的最小大小。`,
+    desc: `请你返回长度为 k 的数组 answer ，其中 answer[j] 为第 j 个查询的结果。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 65,
-            memory: 39.97,
-            desc: '计数',
+            time: 233,
+            memory: 58.32,
+            desc: '有序数组',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
