5a61dfe3422c701793c7d28a1ef4eb55a6976b3a
bestlyg <bestlyg@foxmail.com>
2024-12-19 10:07:45 +0800

    feat: 🎸 add leetcode no.3285

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,18 +4,18 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '1847. 最近的房间',
-    url: 'https://leetcode.cn/problems/closest-room',
+    name: '3285. 找到稳定山的下标',
+    url: 'https://leetcode.cn/problems/find-indices-of-stable-mountains',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回长度为 k 的数组 answer ，其中 answer[j] 为第 j 个查询的结果。`,
+    desc: `请你返回一个数组，包含所有 稳定 山的下标，你可以以 任意 顺序返回下标数组。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 233,
-            memory: 58.32,
-            desc: '有序数组',
+            time: 0,
+            memory: 17.44,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
