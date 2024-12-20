03b7b5c88b60874313a1a473b547c056d10457df
bestlyg <bestlyg@foxmail.com>
2024-12-20 10:22:40 +0800

    feat: 🎸 add leetcode no.3138

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,17 +4,17 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: !true,
-    name: '3285. 找到稳定山的下标',
-    url: 'https://leetcode.cn/problems/find-indices-of-stable-mountains',
+    name: '3138. 同位字符串连接的最小长度',
+    url: 'https://leetcode.cn/problems/minimum-length-of-anagram-concatenation',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回一个数组，包含所有 稳定 山的下标，你可以以 任意 顺序返回下标数组。`,
+    desc: `请你返回字符串 t 的 最小 可能长度。`,
     solutions: [
         {
             script: LeetCodeScript.PY3,
             // date: '2024-11-01',
-            time: 0,
-            memory: 17.44,
+            time: 1586,
+            memory: 18.31,
             desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
