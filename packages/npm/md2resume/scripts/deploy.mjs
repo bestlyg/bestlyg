28f73f212b20c345b82cffc778bb67dc4dd74099
bestlyg <bestlyg@foxmail.com>
2024-12-24 20:59:24 +0800

    feat: 🎸 update md2resume

--- packages/npm/md2resume/scripts/deploy.mjs
+++ packages/npm/md2resume/scripts/deploy.mjs
@@ -1,11 +1,17 @@
 import '@bestlyg/cli/globals';
+import { execSync } from 'child_process';
 import { ssh, server } from '@bestlyg/config';
 
 const resolve = best.utils.getResolveFunction(import.meta, 1);
+
+const commands = [`rm -rf ${server.webPath}/resume`];
+execSync(`ssh -T ${ssh.username}@${ssh.ip} "${commands.join('; ')}"`, {
+    stdio: 'inherit',
+});
 
 echo`scp -r ${resolve('dist')} ${ssh.username}@${ssh.ip}:${server.webPath}/resume`;
 
 $`scp -r ${resolve('dist')} ${ssh.username}@${ssh.ip}:${server.webPath}/resume`.stdio(
     'inherit',
-    'inherit'
+    'inherit',
 );
