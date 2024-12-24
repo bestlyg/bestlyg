217e84148a4233c3495998fb54bab365cf4a6dd9
bestlyg <bestlyg@foxmail.com>
2024-12-24 11:05:51 +0800

    feat: 🎸 update problem

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,17 +4,17 @@ import { LeetCodeLevel, LeetCodeScript, PATH_CODE } from '@bestlyg/leetcode';
 /** @type {import("../dist/types").LeetCodeProblem} */
 export const problem = {
     exist: true,
-    name: '855. 考场就座',
-    url: 'https://leetcode.cn/problems/exam-room',
+    name: '1705. 吃苹果的最大数目',
+    url: 'https://leetcode.cn/problems/maximum-number-of-eaten-apples',
     level: LeetCodeLevel.Easy,
     tagList: [],
-    desc: `请你返回区间 [lo, hi] 之间的整数按权重排序后的第 k 个数。`,
+    desc: `给你两个长度为 n 的整数数组 days 和 apples ，返回你可以吃掉的苹果的最大数目。`,
     solutions: [
         {
             script: LeetCodeScript.PY,
             // date: '2024-11-01',
-            time: 96,
-            memory: 21.22,
+            time: 981,
+            memory: 22.36,
             
             desc: '有序数组遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
