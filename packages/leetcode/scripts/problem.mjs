fcf2f65d6264e9707b86f465241387e73cf2a13d
bestlyg <bestlyg@foxmail.com>
2024-12-27 10:00:53 +0800

    feat: 🎸 add leetcode no.3159

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -1,27 +1,27 @@
+/**
+ * @typedef {import("@bestlyg/data").prismaClient.LeetcodeScriptType}
+ */
 import '@bestlyg/cli/globals';
 import { PATH_CODE } from '@bestlyg/leetcode';
 import { prismaClient } from '@bestlyg/data';
 
-/** @type {prismaClient.LeetcodeSolution[]} */
-const solutions = [
-    {
-        script: 'python',
-        // date: '2024-11-01',
-        time: 0,
-        memory: 17.58,
-        desc: '遍历',
-        code: await fs.readFile(PATH_CODE, 'utf8'),
-    },
-];
-
 // /** @type {import("../dist/types").LeetCodeProblem} */
-/** @type {prismaClient.LeetcodeProblem} */
+/** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 export const problem = {
     exist: !true,
-    name: '3083. 字符串及其反转中是否存在同一子字符串',
-    url: 'https://leetcode.cn/problems/existence-of-a-substring-in-a-string-and-its-reverse',
+    name: '3159. 查询数组中元素的出现位置',
+    url: 'https://leetcode.cn/problems/find-occurrences-of-an-element-in-an-array',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个字符串 s ，请你判断字符串 s 是否存在一个长度为 2 的子字符串，在其反转后的字符串中也出现。如果存在这样的子字符串，返回 true；如果不存在，返回 false 。`,
-    solutions,
+    desc: `请你返回一个整数数组 answer ，包含所有查询的答案。`,
+    solutions: [
+        {
+            script: 'python',
+            // date: '2024-11-01',
+            time: 49,
+            memory: 31.65,
+            desc: '遍历',
+            code: await fs.readFile(PATH_CODE, 'utf8'),
+        },
+    ],
 };
