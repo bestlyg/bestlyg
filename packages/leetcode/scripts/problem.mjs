84f884952e0b5e94fc5499cfee93cf5423fcab52
bestlyg <bestlyg@foxmail.com>
2024-12-28 09:19:35 +0800

    feat: 🎸 add leetcode no.3046

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,17 +9,17 @@ import { prismaClient } from '@bestlyg/data';
 /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 export const problem = {
     exist: !true,
-    name: '3159. 查询数组中元素的出现位置',
-    url: 'https://leetcode.cn/problems/find-occurrences-of-an-element-in-an-array',
+    name: '3046. 分割数组',
+    url: 'https://leetcode.cn/problems/split-the-array',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回一个整数数组 answer ，包含所有查询的答案。`,
+    desc: `如果能够分割数组就返回 true ，否则返回 false 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 49,
-            memory: 31.65,
+            time: 4,
+            memory: 17.7,
             desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
