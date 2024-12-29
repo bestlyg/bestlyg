5f10c7876e7edd431094ec2706a3dde496923522
bestlyg <bestlyg@foxmail.com>
2024-12-29 12:47:41 +0800

    feat: 🎸 add leetcode no.1366

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -1,26 +1,28 @@
 /**
- * @typedef {import("@bestlyg/data").prismaClient.LeetcodeScriptType}
+ * @typedef {import("@bestlyg/data").prismaClient.LeetcodeProblem} Problem
+ * @typedef {import("@bestlyg/data").prismaClient.LeetcodeSolution} Solution
  */
 import '@bestlyg/cli/globals';
 import { PATH_CODE } from '@bestlyg/leetcode';
-import { prismaClient } from '@bestlyg/data';
+// import { prismaClient } from '@bestlyg/data';
 
 // /** @type {import("../dist/types").LeetCodeProblem} */
-/** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
+// /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
+/** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '3046. 分割数组',
-    url: 'https://leetcode.cn/problems/split-the-array',
+    name: '1366. 通过投票对团队排名',
+    url: 'https://leetcode.cn/problems/rank-teams-by-votes',
     level: 'Easy',
     tagList: [],
-    desc: `如果能够分割数组就返回 true ，否则返回 false 。`,
+    desc: `请你返回能表示按排名系统 排序后 的所有团队排名的字符串。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 4,
-            memory: 17.7,
-            desc: '遍历',
+            time: 20,
+            memory: 18.21,
+            desc: '遍历后dfs比较',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
