89f9c37d3467cfb61094f4639df16a7766a9af4e
bestlyg <bestlyg@foxmail.com>
2025-01-03 13:52:41 +0800

    feat: 🎸 add leetcode no.731

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,8 +11,8 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: true,
-    name: '729. 我的日程安排表 I',
-    url: 'https://leetcode.cn/problems/my-calendar-i',
+    name: '731. 我的日程安排表 II',
+    url: 'https://leetcode.cn/problems/my-calendar-ii',
     level: 'Easy',
     tagList: [],
     desc: `实现 MyCalendar 类。`,
@@ -20,9 +20,9 @@ export const problem = {
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 73,
-            memory: 18.5,
-            desc: '有序遍历',
+            time: 2079,
+            memory: 18.68,
+            desc: '差分记录区间内已经订阅的课程数量',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
