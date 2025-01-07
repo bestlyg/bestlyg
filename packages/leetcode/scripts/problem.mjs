1a16a00512f2467bd26e945e2d90850267c3032f
bestlyg <bestlyg@foxmail.com>
2025-01-07 08:43:28 +0800

    feat: 🎸 add leetcode no.3019

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,17 +11,17 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '2274. 不含特殊楼层的最大连续楼层数',
-    url: 'https://leetcode.cn/problems/maximum-consecutive-floors-without-special-floors',
+    name: '3019. 按键变更的次数',
+    url: 'https://leetcode.cn/problems/number-of-changing-keys',
     level: 'Easy',
     tagList: [],
-    desc: `返回不含特殊楼层的 最大 连续楼层数。`,
+    desc: `返回用户输入过程中按键变更的次数。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 107,
-            memory: 31.85,
+            time: 3,
+            memory: 17.33,
             desc: '两两结对遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
