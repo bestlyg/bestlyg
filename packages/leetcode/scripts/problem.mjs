c1a9a56624039e2b0746b1a66152ae994a2f0c5d
bestlyg <bestlyg@foxmail.com>
2025-01-08 10:03:55 +0800

    feat: 🎸 add leetcode no.2264

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '3019. 按键变更的次数',
-    url: 'https://leetcode.cn/problems/number-of-changing-keys',
+    name: '2264. 字符串中最大的 3 位相同数字',
+    url: 'https://leetcode.cn/problems/largest-3-same-digit-number-in-string',
     level: 'Easy',
     tagList: [],
-    desc: `返回用户输入过程中按键变更的次数。`,
+    desc: `以字符串形式返回 最大的优质整数 。如果不存在满足要求的整数，则返回一个空字符串 "" 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
             time: 3,
-            memory: 17.33,
-            desc: '两两结对遍历',
+            memory: 17.36,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
