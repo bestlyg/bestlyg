9e84d1bccf894ebb896697b3f063bb33eee1e016
bestlyg <bestlyg@foxmail.com>
2025-01-11 14:05:46 +0800

    feat: 🎸 update scripts

--- packages/server/ecosystem.config.cjs
+++ packages/server/ecosystem.config.cjs
@@ -2,16 +2,19 @@ module.exports = {
     apps: [
         {
             name: 'bestlyg-server',
-            // script: './scripts/deploy-pm2.js',
-            // interpreter: 'node',
+            script: './scripts/deploy-pm2.js',
+            interpreter: 'node',
             // interpreter_args: '--input-type=module',
+            env: {
+                NODE_ENV: 'production',
+            },
             // env_production: {
             //     NODE_ENV: 'production',
             // },
             // env_development: {
             //     NODE_ENV: 'development',
             // },
-            script: 'pnpm start:prod',
+            // script: 'pnpm start:prod',
         },
     ],
 };
