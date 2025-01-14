6c3a8125df0bdcf61215064072f5f9261f61ea69
bestlyg <bestlyg@foxmail.com>
2025-01-14 08:59:10 +0800

    feat: 🎸 add leetcode no.3065

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '2270. 分割数组的方案数',
-    url: 'https://leetcode.cn/problems/number-of-ways-to-split-array',
+    name: '3065. 超过阈值的最少操作数 I',
+    url: 'https://leetcode.cn/problems/minimum-operations-to-exceed-threshold-value-i',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回 nums 中的 合法分割 方案数。`,
+    desc: `你需要使数组中的所有元素都大于或等于 k ，请你返回需要的 最少 操作次数。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 57,
-            memory: 31.80,
-            desc: '前缀和遍历',
+            time: 0,
+            memory: 17.72,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
