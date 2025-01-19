3323405bcf99cb9fb87da2232b1511e419b6120f
bestlyg <bestlyg@foxmail.com>
2025-01-19 08:25:10 +0800

    feat: 🎸 add leetcode no.2266

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '3097. 或值至少为 K 的最短子数组 II',
-    url: 'https://leetcode.cn/problems/shortest-subarray-with-or-at-least-k-ii',
+    name: '2266. 统计打字方案数',
+    url: 'https://leetcode.cn/problems/count-number-of-texts',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回 nums 中 最短特别非空 子数组的长度，如果特别子数组不存在，那么返回 -1 。`,
+    desc: `给你一个字符串 pressedKeys ，表示 Bob 收到的字符串，请你返回 Alice 总共可能发出多少种文字信息 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 3548,
-            memory: 35.96,
-            desc: '滑动窗口记录区间内各位值的数量',
+            time: 2411,
+            memory: 561.98,
+            desc: 'dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
