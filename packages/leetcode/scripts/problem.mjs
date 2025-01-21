3efbabbfd24af9565f863f85d8adc2c5d1ae2f01
bestlyg <bestlyg@foxmail.com>
2025-01-21 10:25:12 +0800

    feat: 🎸 add leetcode no.2218

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '2239. 找到最接近 0 的数字',
-    url: 'https://leetcode.cn/problems/find-closest-number-to-zero',
+    name: '2218. 从栈中取出 K 个硬币的最大面值和',
+    url: 'https://leetcode.cn/problems/maximum-value-of-k-coins-from-piles',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个长度为 n 的整数数组 nums ，请你返回 nums 中最 接近 0 的数字。如果有多个答案，请你返回它们中的 最大值 。`,
+    desc: `给你一个列表 piles ，其中 piles[i] 是一个整数数组，分别表示第 i 个栈里 从顶到底 的硬币面值。同时给你一个正整数 k ，请你返回在 恰好 进行 k 次操作的前提下，你钱包里硬币面值之和 最大为多少 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 0,
-            memory: 17.68,
-            desc: '遍历',
+            time: 5239,
+            memory: 308.18,
+            desc: 'dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
