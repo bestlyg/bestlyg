589c732f6e63d997830b45cf136d4d3e690d26a7
bestlyg <bestlyg@foxmail.com>
2025-01-22 09:56:03 +0800

    feat: 🎸 add leetcode no.1561

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '2218. 从栈中取出 K 个硬币的最大面值和',
-    url: 'https://leetcode.cn/problems/maximum-value-of-k-coins-from-piles',
+    name: '1561. 你可以获得的最大硬币数目',
+    url: 'https://leetcode.cn/problems/maximum-number-of-coins-you-can-get',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个列表 piles ，其中 piles[i] 是一个整数数组，分别表示第 i 个栈里 从顶到底 的硬币面值。同时给你一个正整数 k ，请你返回在 恰好 进行 k 次操作的前提下，你钱包里硬币面值之和 最大为多少 。`,
+    desc: `返回你可以获得的最大硬币数目。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 5239,
-            memory: 308.18,
-            desc: 'dfs',
+            time: 83,
+            memory: 27.00,
+            desc: '排序后贪心，每次拿最大的两个和最小的一个',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
