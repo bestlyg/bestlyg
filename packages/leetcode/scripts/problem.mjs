3535466fd112a4d62efbb43829a6b09c4cfacc07
bestlyg <bestlyg@foxmail.com>
2025-01-23 10:14:11 +0800

    feat: 🎸 add leetcode no.2920

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: !true,
-    name: '1561. 你可以获得的最大硬币数目',
-    url: 'https://leetcode.cn/problems/maximum-number-of-coins-you-can-get',
+    name: '2920. 收集所有金币可获得的最大积分',
+    url: 'https://leetcode.cn/problems/maximum-points-after-collecting-coins-from-all-nodes',
     level: 'Easy',
     tagList: [],
-    desc: `返回你可以获得的最大硬币数目。`,
+    desc: `返回收集 所有 树节点的金币之后可以获得的最大积分。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 83,
-            memory: 27.00,
-            desc: '排序后贪心，每次拿最大的两个和最小的一个',
+            time: 2657,
+            memory: 370.45,
+            desc: '记忆化搜索',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
