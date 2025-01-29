6c17d456bd23ec6302b3d88dc9d0ed6dbf9d8e33
bestlyg <bestlyg@foxmail.com>
2025-01-29 11:01:11 +0800

    feat: 🎸 add leetcode no.219

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,18 +11,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem & {solutions:Solution[]}} */
 export const problem = {
     exist: true,
-    name: '119. 杨辉三角 II',
-    url: 'https://leetcode.cn/problems/pascals-triangle-ii',
+    name: '219. 存在重复元素 II',
+    url: 'https://leetcode.cn/problems/contains-duplicate-ii',
     level: 'Easy',
     tagList: [],
-    desc: `给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。`,
+    desc: `给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 4,
-            memory: 17.33,
-            desc: 'dp',
+            time: 19,
+            memory: 30.23,
+            desc: '遍历，用哈希表记录前一个相同值的下标',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
