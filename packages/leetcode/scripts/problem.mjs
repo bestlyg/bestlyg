19774a90c6ecfe6a3875c8dd2931549fc8964040
bestlyg <bestlyg@foxmail.com>
2025-02-05 14:30:11 +0800

    feat: 🎸 add leetcode no.90

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '922. 按奇偶排序数组 II',
-    url: 'https://leetcode.cn/problems/sort-array-by-parity-ii',
+    name: '90. 子集 II',
+    url: 'https://leetcode.cn/problems/subsets-ii',
     level: 'Easy',
     tagList: [],
-    desc: `给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数 。`,
+    desc: `给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的 子集（幂集）。解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 12,
-            memory: 18.84,
-            desc: '遍历',
+            time: 0,
+            memory:17.98,
+            desc: 'dfs所有的可能并去重',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
