d097e83597e68b9bc6eb10d9ed845c3ee718ef2c
bestlyg <bestlyg@foxmail.com>
2025-02-07 08:38:16 +0800

    feat: 🎸 add leetcode no.59

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '47. 全排列 II',
-    url: 'https://leetcode.cn/problems/permutations-ii',
+    name: '59. 螺旋矩阵 II',
+    url: 'https://leetcode.cn/problems/spiral-matrix-ii',
     level: 'Easy',
     tagList: [],
-    desc: `给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。`,
+    desc: `给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 12,
-            memory: 17.82,
-            desc: '内置全排列',
+            time: 4,
+            memory: 17.42,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
