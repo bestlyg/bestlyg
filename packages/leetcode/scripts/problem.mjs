50651e5e6e93e728d9a5bfd8b21bd2ab42eaec5d
bestlyg <bestlyg@foxmail.com>
2025-02-08 10:17:06 +0800

    feat: 🎸 add leetcode no.63

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '59. 螺旋矩阵 II',
-    url: 'https://leetcode.cn/problems/spiral-matrix-ii',
+    name: '63. 不同路径 II',
+    url: 'https://leetcode.cn/problems/unique-paths-ii',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。`,
+    desc: `返回机器人能够到达右下角的不同路径数量。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 4,
-            memory: 17.42,
-            desc: '遍历',
+            time: 0,
+            memory: 17.56,
+            desc: 'dp[i][j]表示ij下标的路径数量',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
