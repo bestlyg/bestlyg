a2dc4f33830425e6bbfc4582b3fda3629c150fd7
bestlyg <bestlyg@foxmail.com>
2025-02-09 15:51:28 +0800

    feat: 🎸 add leetcode no.80

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '63. 不同路径 II',
-    url: 'https://leetcode.cn/problems/unique-paths-ii',
+    name: '80. 删除有序数组中的重复项 II',
+    url: 'https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii',
     level: 'Easy',
     tagList: [],
-    desc: `返回机器人能够到达右下角的不同路径数量。`,
+    desc: `给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 0,
-            memory: 17.56,
-            desc: 'dp[i][j]表示ij下标的路径数量',
+            time: 94,
+            memory: 20.31,
+            desc: '双指针遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
