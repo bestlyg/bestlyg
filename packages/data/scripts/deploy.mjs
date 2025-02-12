af5fba769a810f29f1f1ff766fe4407342a31d75
bestlyg <bestlyg@foxmail.com>
2025-02-12 09:52:55 +0800

    feat: 🎸 update scripts

--- packages/data/scripts/deploy.mjs
+++ packages/data/scripts/deploy.mjs
@@ -1,17 +1,18 @@
 import '@bestlyg/cli/globals';
 import { execSync } from 'child_process';
-import { ssh, server } from '@bestlyg/common';
+import { ConfigurationSchema, getConfiguration } from '@bestlyg/common';
 
 const resolve = best.utils.getResolveFunction(import.meta, 1);
 best.dotenv.config({
     path: resolve('node_modules', '@bestlyg', 'config', '.env.local'),
 });
+const config = ConfigurationSchema.parse(getConfiguration(process.env));
 const fileName = '.env.local';
 const dbName = 'best_data';
 
-const envDistPath = resolve(server.projectPath, 'packages', 'config', fileName);
+const envDistPath = resolve(config.ssh.projectPath, 'packages', 'config', fileName);
 
-const sqlDistPath = resolve(server.projectPath, 'packages', 'data', 'dist', dbName + '.sql');
+const sqlDistPath = resolve(config.ssh.projectPath, 'packages', 'data', 'dist', dbName + '.sql');
 
 // await `psql -d best_data -U root -h localhost -p 5432 < ~/Desktop/best_data.sql`
 const dumpPath = resolve('dist', dbName + '.sql');
@@ -22,25 +23,25 @@ await $`pg_dump -h localhost -p 5432 -U root -f ${dumpPath} ${dbName} -c`.stdio(
     'inherit',
 );
 
-await $`scp -r ${dumpPath} ${ssh.username}@${ssh.ip}:${sqlDistPath}`.stdio(
+await $`scp -r ${dumpPath} ${config.ssh.username}@${config.ssh.ip}:${sqlDistPath}`.stdio(
     'inherit',
     'inherit',
     'inherit',
 );
 
-await $`scp -r ${resolve('node_modules', '@bestlyg', 'config', fileName)} ${ssh.username}@${ssh.ip}:${envDistPath}`.stdio(
+await $`scp -r ${resolve('node_modules', '@bestlyg', 'config', fileName)} ${config.ssh.username}@${config.ssh.ip}:${envDistPath}`.stdio(
     'inherit',
     'inherit',
     'inherit',
 );
 
 const commands = [
-    `cd ${server.projectPath}/packages/data`,
+    `cd ${config.config.ssh.projectPath}/packages/data`,
     `pnpm prisma generate`,
     `pnpm --filter @bestlyg/data run build`,
-    `PGPASSWORD=root psql -d best_data -U root -h localhost -p 5432 < ${sqlDistPath}`
+    `PGPASSWORD=root psql -d best_data -U root -h localhost -p 5432 < ${sqlDistPath}`,
 ];
 
-execSync(`ssh -T ${ssh.username}@${ssh.ip} "${commands.join('; ')}"`, {
+execSync(`ssh -T ${config.ssh.username}@${config.ssh.ip} "${commands.join('; ')}"`, {
     stdio: 'inherit',
 });
--- packages/data/scripts/generate.mjs
+++ packages/data/scripts/generate.mjs
@@ -3,7 +3,7 @@ import '@bestlyg/cli/globals';
 const resolve = best.utils.getResolveFunction(import.meta, 1);
 
 best.dotenv.config({
-    path: resolve('node_modules', '@bestlyg', 'config', '.env.local'),
+    path: resolve('node_modules', '@bestlyg', 'common', '.env'),
 });
 
 await $`prisma generate`.stdio('inherit', 'inherit', 'inherit');
