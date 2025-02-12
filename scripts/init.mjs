8b8a2eb256abbe1a05c2c09518577f6f5f323419
bestlyg <bestlyg@foxmail.com>
2025-02-12 21:38:37 +0800

    feat: 🎸 update scripts

--- scripts/init.mjs
+++ scripts/init.mjs
@@ -2,3 +2,5 @@ import 'zx/globals';
 
 await $`git remote add gitee https://github.com/bestlyg/bestlyg.git`;
 await $`git remote add github https://github.com/bestlyg/bestlyg.git`;
+await $`git config --global user.email "bestlyg@foxmail.com"`
+await $`git config --global user.name bestlyg`
