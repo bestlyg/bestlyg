27b60eaa9eb33da1168855be2d4dadfecf2bb853
bestlyg <bestlyg@foxmail.com>
2025-02-14 10:15:07 +0800

    feat: 🎸 add leetcode no.1552

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '1742. 盒子中小球的最大数量',
-    url: 'https://leetcode.cn/problems/maximum-number-of-balls-in-a-box',
+    exist: !true,
+    name: '1552. 两球之间的磁力',
+    url: 'https://leetcode.cn/problems/magnetic-force-between-two-balls',
     level: 'Easy',
     tagList: [],
-    desc: `给你两个整数 lowLimit 和 highLimit ，返回放有最多小球的盒子中的小球数量。如果有多个盒子都满足放有最多小球，只需返回其中任一盒子的小球数量。`,
+    desc: `给你一个整数数组 position 和一个整数 m ，请你返回最大化的最小磁力。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 567,
-            memory: 17.59,
-            desc: '遍历时存储',
+            time: 429,
+            memory: 30.03,
+            desc: '二分答案',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
