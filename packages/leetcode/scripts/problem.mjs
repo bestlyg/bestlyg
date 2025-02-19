27bd0b0e2c8118450ff8631685910760769f0cb1
bestlyg <bestlyg@foxmail.com>
2025-02-19 10:04:21 +0800

    feat: 🎸 add leetcode no.624

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2080. 区间内查询数字的频率',
-    url: 'https://leetcode.cn/problems/range-frequency-queries',
+    name: '624. 数组列表中的最大距离',
+    url: 'https://leetcode.cn/problems/maximum-distance-in-arrays',
     level: 'Easy',
     tagList: [],
-    desc: `请你设计一个数据结构，它能求出给定子数组内一个给定值的 频率 。`,
+    desc: `给定 m 个数组，每个数组都已经按照升序排好序了。现在你需要从两个不同的数组中选择两个整数（每个数组选一个）并且计算它们的距离。两个整数 a 和 b 之间的距离定义为它们差的绝对值 |a-b| 。返回最大距离。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 123,
-            memory: 57.92,
-            desc: '把相同的数字归拢后查询',
+            time: 97,
+            memory: 33.13,
+            desc: '记录前面的最小值和最大值，与当前的做比较',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
