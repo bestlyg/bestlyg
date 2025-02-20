ac8a4e95c31c345b157458f1f7a345d1c228a7fa
bestlyg <bestlyg@foxmail.com>
2025-02-20 18:15:16 +0800

    feat: 🎸 update prisma

--- packages/common/scripts/generate.mjs
+++
@@ -1,9 +0,0 @@
-import '@bestlyg/cli/globals';
-
-const resolve = best.utils.getResolveFunction(import.meta, 1);
-
-best.dotenv.config({
-    path: resolve('node_modules', '@bestlyg', 'common', '.env'),
-});
-
-await $`prisma generate`.stdio('inherit', 'inherit', 'inherit');
