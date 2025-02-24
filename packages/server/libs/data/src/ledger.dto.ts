f06d6e62b020e02e85b7c471b67be446a7573f73
bestlyg <bestlyg@foxmail.com>
2025-02-24 13:55:02 +0800

    feat: 🎸 update server

--- packages/server/libs/data/src/ledger.dto.ts
+++ packages/server/libs/data/src/ledger.dto.ts
@@ -3,16 +3,26 @@ import { createZodDto } from '@anatine/zod-nestjs';
 import { PageParam } from '@bestlyg/common';
 import { extendApi } from '@anatine/zod-openapi';
 
-export const SelectLedgerPageSchema = extendApi(
-    z
-        .object({
-            date: z.string().date().optional(),
-        })
-        .merge(PageParam.Schema),
-    {
+// export const SelectLedgerPageSchema = extendApi(
+//     z
+//         .object({
+//             date: z.string().date().optional(),
+//         })
+//         .merge(PageParam.Schema),
+//     {
+//         title: '分页账本',
+//         description: '获取用户分页账本',
+//     },
+// );
+
+export const SelectLedgerPageSchema = z
+    .object({
+        date: z.string().date().optional(),
+    })
+    .merge(PageParam.Schema)
+    .openapi({
         title: '分页账本',
         description: '获取用户分页账本',
-    },
-);
+    });
 
 export class SelectLedgerPageDto extends createZodDto(SelectLedgerPageSchema) {}
