e08207f4ba164a65d906d2fb34d265df51288ca9
bestlyg <bestlyg@foxmail.com>
2025-02-24 10:50:10 +0800

    feat: 🎸 update catalog

--- packages/server/libs/data/src/ledger.dto.ts
+++ packages/server/libs/data/src/ledger.dto.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 import { createZodDto } from '@anatine/zod-nestjs';
+import { PageParam } from '@bestlyg/common';
 
 export const SelectLedgerPageSchema = z
     .object({
-        current: z.coerce.number().readonly(),
-        pageSize: z.coerce.number().readonly(),
         date: z.string().date().optional(),
     })
+    .merge(PageParam.Schema)
     .openapi({
         title: '分页账本',
         description: '获取用户分页账本',
-    })
+    });
 
 export class SelectLedgerPageDto extends createZodDto(SelectLedgerPageSchema) {}
