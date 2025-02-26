5865a21fc43bc166d0d690b913cc03ceeb8597b9
bestlyg <bestlyg@foxmail.com>
2025-02-26 08:38:52 +0800

    feat: 🎸 add leetcode no.1472

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -5,23 +5,22 @@ import '@bestlyg/cli/globals';
 import { PATH_CODE } from '@bestlyg/leetcode';
 // import { prismaClient } from '@bestlyg/common';
 
-
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '2502. 设计内存分配器',
-    url: 'https://leetcode.cn/problems/design-memory-allocator',
+    exist: !true,
+    name: '1472. 设计浏览器历史记录',
+    url: 'https://leetcode.cn/problems/design-browser-history',
     level: 'Easy',
     tagList: [],
-    desc: `请你设计一个具备以下功能的内存分配器。`,
+    desc: `请你实现 BrowserHistory 类。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 1157,
-            memory: 18.13,
+            time: 25,
+            memory: 19.75,
             desc: '模拟',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
