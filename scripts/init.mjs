1cac7b51432544dccca7de6f04ea3f73b403f255
bestlyg <bestlyg@foxmail.com>
2025-03-01 23:18:06 +0800

    feat: 🎸 update init file

--- scripts/init.mjs
+++ scripts/init.mjs
@@ -4,9 +4,11 @@ import { fileURLToPath } from 'node:url';
 
 const run = cmd => {
     try {
+        console.log(`\n\n==>[${cmd}]`)
         execSync(cmd, { stdio: 'inherit' });
     } catch (err) {
-        console.log('===>', err?.toString());
+        console.log(`\n\n==>Error [${cmd}]`)
+        console.error(err);
     }
 };
 const CWD = process.cwd();
@@ -20,12 +22,13 @@ run(`git remote add gitee https://gitee.com/bestlyg/bestlyg.git`);
 run(`git remote add github https://github.com/bestlyg/bestlyg.git`);
 run(`git config --global user.email "bestlyg@foxmail.com"`);
 run(`git config --global user.name bestlyg`);
-run(
-    `scp root@106.54.220.193:/root/bestlyg/packages/common/dist/best_data.sql ${resolve('databases', 'bestlyg-data', `best_data.sql`)}`,
-);
-run(
-    `scp root@106.54.220.193:/root/bestlyg/packages/common/.env ${resolve('packages', 'common', '.env')}`,
-);
+run(`git pull origin master`)
+// run(
+//     `scp root@106.54.220.193:/root/best_data.sql ${resolve('databases', 'bestlyg-data', `best_data.sql`)}`,
+// );
+// run(
+//     `scp root@106.54.220.193:/root/bestlyg/packages/common/.env ${resolve('packages', 'common', '.env')}`,
+// );
 const dumpSql = `psql -U root -d best_data -h localhost -p 5432 < /bestlyg-data/best_data.sql`;
 run(dumpSql);
 run(`docker exec bestlyg-postgres sh -c "${dumpSql}`);
