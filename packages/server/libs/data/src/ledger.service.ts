f21adff3cef693d6e14148896b26dd6ebebd5355
bestlyg <bestlyg@foxmail.com>
2025-03-01 23:01:28 +0800

    feat: 🎸 update server

--- packages/server/libs/data/src/ledger.service.ts
+++ packages/server/libs/data/src/ledger.service.ts
@@ -2,7 +2,7 @@ import { PrismaService } from '@bestlyg-server/common';
 import { Injectable, Logger } from '@nestjs/common';
 import { dayjs } from '@bestlyg/cli';
 import { PageParam, PageData, SelectLedgerPageDto } from '@bestlyg/common';
-import { Ledger } from '@bestlyg/common/prisma-client';
+import { Ledger, LedgerType } from '@bestlyg/common/prisma-client';
 
 @Injectable()
 export class LedgerService {
@@ -46,73 +46,87 @@ export class LedgerService {
             lte: now.endOf('month').toDate(),
             gte: now.startOf('month').toDate(),
         };
+        const typeList: LedgerType[] = [
+            'BreakfastWallet',
+            'LunchWallet',
+            'DinnerWallet',
+            'FamilyWallet',
+            'TravelWallet',
+        ];
 
-        const breakfastWalletCost = await this.prismaService.ledger.aggregate({
-            _sum: { balance: true },
-            where: {
-                type: 'BreakfastWallet',
-                date: currentMonth,
-                io: false,
-            },
-        });
-        const breakfastWalletTotal = await this.prismaService.ledger.aggregate({
-            _sum: { balance: true },
-            where: {
-                type: 'BreakfastWallet',
-                date: currentMonth,
-                io: true,
-            },
-        });
+        type SummaryRecord = {
+            balance: Record<
+                | 'BreakfastWallet'
+                | 'LunchWallet'
+                | 'DinnerWallet'
+                | 'FamilyWallet'
+                | 'TravelWallet',
+                { total: number; cost: number }
+            >;
+        };
 
-        const lunchWalletCost = await this.prismaService.ledger.aggregate({
-            _sum: { balance: true },
-            where: {
-                type: 'LunchWallet',
-                date: currentMonth,
-                io: false,
-            },
-        });
-        const lunchWalletTotal = await this.prismaService.ledger.aggregate({
-            _sum: { balance: true },
-            where: {
-                type: 'LunchWallet',
-                date: currentMonth,
-                io: true,
-            },
-        });
-
-        const dinnerWalletCost = await this.prismaService.ledger.aggregate({
-            _sum: { balance: true },
-            where: {
-                type: 'DinnerWallet',
-                date: currentMonth,
-                io: false,
-            },
-        });
-        const dinnerWalletTotal = await this.prismaService.ledger.aggregate({
-            _sum: { balance: true },
-            where: {
-                type: 'DinnerWallet',
-                date: currentMonth,
-                io: true,
-            },
-        });
-
-        return {
-            balance: {
-                breakfastWallet: {
-                    total: breakfastWalletTotal._sum.balance ?? 0,
-                    cost: breakfastWalletCost._sum.balance ?? 0,
+        const res: {
+            currentMonth: SummaryRecord;
+            all: SummaryRecord;
+        } = {
+            currentMonth: {
+                balance: {
+                    BreakfastWallet: { total: 0, cost: 0 },
+                    LunchWallet: { total: 0, cost: 0 },
+                    DinnerWallet: { total: 0, cost: 0 },
+                    FamilyWallet: { total: 0, cost: 0 },
+                    TravelWallet: { total: 0, cost: 0 },
                 },
-                lunchWallet: {
-                    total: lunchWalletTotal._sum.balance ?? 0,
-                    cost: lunchWalletCost._sum.balance ?? 0,
-                },
-                dinnerWallet: {
-                    total: dinnerWalletTotal._sum.balance ?? 0,
-                    cost: dinnerWalletCost._sum.balance ?? 0,
+            },
+            all: {
+                balance: {
+                    BreakfastWallet: { total: 0, cost: 0 },
+                    LunchWallet: { total: 0, cost: 0 },
+                    DinnerWallet: { total: 0, cost: 0 },
+                    FamilyWallet: { total: 0, cost: 0 },
+                    TravelWallet: { total: 0, cost: 0 },
                 },
             },
         };
+
+        for (const ty of typeList) {
+            const currentMonthCost = await this.prismaService.ledger.aggregate({
+                _sum: { balance: true },
+                where: {
+                    type: ty,
+                    date: currentMonth,
+                    io: false,
+                },
+            });
+            const currentMonthTotal = await this.prismaService.ledger.aggregate({
+                _sum: { balance: true },
+                where: {
+                    type: ty,
+                    date: currentMonth,
+                    io: true,
+                },
+            });
+            res.currentMonth.balance[ty].total = currentMonthTotal._sum.balance;
+            res.currentMonth.balance[ty].cost = currentMonthCost._sum.balance;
+
+            const allCost = await this.prismaService.ledger.aggregate({
+                _sum: { balance: true },
+                where: {
+                    type: ty,
+                    io: false,
+                },
+            });
+            const allTotal = await this.prismaService.ledger.aggregate({
+                _sum: { balance: true },
+                where: {
+                    type: ty,
+                    io: true,
+                },
+            });
+            res.all.balance[ty].total = allTotal._sum.balance;
+            res.all.balance[ty].cost = allCost._sum.balance;
+        }
+
+        return res;
     }
 }
