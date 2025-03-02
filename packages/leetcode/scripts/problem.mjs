f9b8db6d921ee500c6af06318eb74196187f864e
bestlyg <bestlyg@foxmail.com>
2025-03-02 15:34:02 +0800

    feat: 🎸 add leetcode no.132

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,8 +10,8 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '131. 分割回文串',
-    url: 'https://leetcode.cn/problems/palindrome-partitioning',
+    name: '132. 分割回文串 II',
+    url: 'https://leetcode.cn/problems/palindrome-partitioning-ii',
     level: 'Easy',
     tagList: [],
     desc: `给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。`,
@@ -19,8 +19,8 @@ export const problem = {
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 28,
-            memory: 39.18,
+            time: 4179,
+            memory: 322.74,
             desc: 'dfs判断每一个子串',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
