21235eecdec18a7b6cc6df7d87a181a1fc4552a0
bestlyg <bestlyg@foxmail.com>
2025-03-05 09:30:26 +0800

    feat: 🎸 add leetcode no.1328

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '1745. 分割回文串 IV',
-    url: 'https://leetcode.cn/problems/palindrome-partitioning-iv',
+    name: '1328. 破坏回文串',
+    url: 'https://leetcode.cn/problems/break-a-palindrome',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个字符串 s ，如果可以将它分割成三个 非空 回文子字符串，那么返回 true ，否则返回 false 。`,
+    desc: `给你一个由小写英文字母组成的回文字符串 palindrome ，请你将其中 一个 字符用任意小写英文字母替换，使得结果字符串的 字典序最小 ，且 不是 回文串。请你返回结果字符串。如果无法做到，则返回一个 空串 。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 4928,
-            memory: 324.87,
-            desc: 'dfs判断每一个位置是否需要切割',
+            time: 0,
+            memory: 17.57,
+            desc: '贪心是不是都是a，不是a就找第一个非a换成a，否则最后一个字符换成b',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
