90b9d76e30357b8076c1cf328fdaf263b321029e
bestlyg <bestlyg@foxmail.com>
2025-03-06 17:10:58 +0800

    feat: 🎸 update server

--- packages/server/src/interceptors/logging.interceptor.ts
+++ packages/server/src/interceptors/logging.interceptor.ts
@@ -1,5 +1,4 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
-import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -8,11 +7,19 @@ export class LoggingInterceptor implements NestInterceptor {
     private readonly logger = new Logger(LoggingInterceptor.name);
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<void> {
-        const request: Request = context.switchToHttp().getRequest();
+        const request = context.switchToHttp().getRequest();
         const now = Date.now();
         return next.handle().pipe(
             tap(() => {
-                this.logger.log(`${request.method} ${request.url} ${Date.now() - now}ms`);
+                const info = [
+                    request.method,
+                    request.url,
+                    `${Date.now() - now}ms`,
+                    request.body && JSON.stringify(request.body),
+                ]
+                    .filter(Boolean)
+                    .join(' ');
+                this.logger.log(info);
             }),
         );
     }
