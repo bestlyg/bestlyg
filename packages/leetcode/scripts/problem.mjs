1b6becb12a5db9598a4a9d8316f035d3b9068fbc
bestlyg <bestlyg@foxmail.com>
2025-03-13 10:44:48 +0800

    feat: 🎸 add leetcode no.3306

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,8 +10,8 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3305. 元音辅音字符串计数 I',
-    url: 'https://leetcode.cn/problems/count-of-substrings-containing-every-vowel-and-k-consonants-i',
+    name: '3306. 元音辅音字符串计数 II',
+    url: 'https://leetcode.cn/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii',
     level: 'Easy',
     tagList: [],
     desc: `返回 word 的 子字符串 中，每个元音字母（'a'、'e'、'i'、'o'、'u'）至少 出现一次，并且 恰好 包含 k 个辅音字母的子字符串的总数。`,
@@ -19,9 +19,9 @@ export const problem = {
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 9256,
-            memory: 17.55,
-            desc: '枚举',
+            time: 2283,
+            memory: 18.3,
+            desc: '计算大于等于K的和大于等于K+1的值进行相减',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
