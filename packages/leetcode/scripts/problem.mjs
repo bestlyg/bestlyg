d63195cc9cc87cd5ca7dcb27abc4cb50df1afc69
bestlyg <bestlyg@foxmail.com>
2025-03-19 08:53:17 +0800

    feat: 🎸 add leetcode no.2610

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '2614. 对角线上的质数',
-    url: 'https://leetcode.cn/problems/prime-in-diagonal',
+    name: '2610. 转换二维数组',
+    url: 'https://leetcode.cn/problems/convert-an-array-into-a-2d-array-with-conditions',
     level: 'Easy',
     tagList: [],
-    desc: `返回位于 nums 至少一条 对角线 上的最大 质数 。如果任一对角线上均不存在质数，返回 0 。`,
+    desc: `给你一个整数数组 nums 。请你创建一个满足以下条件的二维数组：二维数组应该 只 包含数组 nums 中的元素。二维数组中的每一行都包含 不同 的整数。二维数组的行数应尽可能 少 。返回结果数组。`,
     solutions: [
         {
             script: 'python',
             // date: '2024-11-01',
-            time: 0,
-            memory: 59.69,
-            desc: '素数筛+遍历',
+            time: 3,
+            memory: 17.52,
+            desc: '计数后遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
