5b6f7bc965b45326363924c534a3bfac527d00a9
bestlyg <bestlyg@foxmail.com>
2025-03-25 13:25:49 +0800

    feat: 🎸 update leetcode

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: !true,
-    name: '2680. 最大或值',
-    url: 'https://leetcode.cn/problems/maximum-or',
+    exist: true,
+    name: '2711. 对角线上不同值的数量差',
+    url: 'https://leetcode.cn/problems/difference-of-number-of-distinct-values-on-diagonals',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k 。每一次操作中，你可以选择一个数并将它乘 2 。你最多可以进行 k 次操作，请你返回 nums[0] | nums[1] | ... | nums[n - 1] 的最大值。a | b 表示两个整数 a 和 b 的 按位或 运算。`,
+    desc: `返回矩阵 answer 。`,
     solutions: [
         {
             script: 'python',
-            // date: '2024-11-01',
-            time: 136,
-            memory: 30.93,
-            desc: '贪心对每个值进行乘K次2，判断最大值',
+            // date: '2025.03.24',
+            time: 131,
+            memory: 17.81,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
