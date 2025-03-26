c3dfe5f321ec65122fd2992c50a9710e5355349c
bestlyg <bestlyg@foxmail.com>
2025-03-26 15:15:07 +0800

    feat: 🎸 add leetcode no.2829

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '2711. 对角线上不同值的数量差',
-    url: 'https://leetcode.cn/problems/difference-of-number-of-distinct-values-on-diagonals',
+    exist: !true,
+    name: '2829. k-avoiding 数组的最小总和',
+    url: 'https://leetcode.cn/problems/determine-the-minimum-sum-of-a-k-avoiding-array',
     level: 'Easy',
     tagList: [],
-    desc: `返回矩阵 answer 。`,
+    desc: `返回长度为 n 的 k-avoiding 数组的可能的最小总和。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 131,
-            memory: 17.81,
-            desc: '遍历',
+            time: 3,
+            memory: 17.35,
+            desc: '遍历，记录哪个数被用过了',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
