9328032d5cf029878f82dbe4a36048e80f8eb446
bestlyg <bestlyg@foxmail.com>
2025-03-28 19:51:45 +0800

    feat: 🎸 update common

--- packages/common/src/base-model.ts
+++ packages/common/src/base-model.ts
@@ -1,4 +1,9 @@
-export class BaseModel {
+import EventEmitter from 'eventemitter3';
+
+export class BaseModel<
+    EventTypes extends EventEmitter.ValidEventTypes = string | symbol,
+    Context extends any = any,
+> extends EventEmitter<EventTypes, Context> {
     set<K extends keyof this, V extends this[K]>(key: K, val: V) {
         this[key] = val;
         return this;
--- packages/common/src/client/react-base-model.ts
+++ packages/common/src/client/react-base-model.ts
@@ -1,7 +1,11 @@
+import EventEmitter from 'eventemitter3';
 import { BaseModel } from '@/base-model';
 import { useState } from 'react';
 
-export class ReactBaseModel extends BaseModel {
+export class ReactBaseModel<
+    EventTypes extends EventEmitter.ValidEventTypes = string | symbol,
+    Context extends any = any,
+> extends BaseModel<EventTypes, Context> {
     useState<K extends keyof this>(key: K) {
         const stateConfig = useState(this[key]);
         this.set(key, stateConfig[0]);
