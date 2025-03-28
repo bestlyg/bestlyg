9c91baf6f6807df78bf525df07523e38f9ec3153
bestlyg <bestlyg@foxmail.com>
2025-03-28 20:00:36 +0800

    feat: 🎸 add leetcode no.2716

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2829. k-avoiding 数组的最小总和',
-    url: 'https://leetcode.cn/problems/determine-the-minimum-sum-of-a-k-avoiding-array',
+    name: '2716. 最小化字符串长度',
+    url: 'https://leetcode.cn/problems/minimize-string-length',
     level: 'Easy',
     tagList: [],
-    desc: `返回长度为 n 的 k-avoiding 数组的可能的最小总和。`,
+    desc: `返回一个表示 最小化 字符串的长度的整数。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 3,
-            memory: 17.35,
-            desc: '遍历，记录哪个数被用过了',
+            time: 20,
+            memory: 17.39,
+            desc: '理论上处理N次之后，只会留下所有的不同的字符，只需要计算不同字符的数量即可',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
