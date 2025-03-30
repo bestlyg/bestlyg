bfd5dd72467095dcf25b3cfc03d5b97459c8cd9a
bestlyg <bestlyg@foxmail.com>
2025-03-30 12:45:22 +0800

    feat: 🎸 add leetcode no.2109

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2360. 图中的最长环',
-    url: 'https://leetcode.cn/problems/longest-cycle-in-a-graph',
+    name: '2109. 向字符串添加空格',
+    url: 'https://leetcode.cn/problems/adding-spaces-to-a-string',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回图中的 最长 环，如果没有任何环，请返回 -1 。`,
+    desc: `请你添加空格，并返回修改后的字符串。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 835,
-            memory: 73.39,
-            desc: '剪枝所有的没有入边的节点，直到没有节点或者所有节点都存在入边',
+            time: 135,
+            memory: 45.33,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
