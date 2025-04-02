40b8ab8a20f16eaf358537b3b2f69fddc26e26fe
bestlyg <bestlyg@foxmail.com>
2025-04-02 13:15:01 +0800

    feat: 🎸 add leetcode no.2873

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,17 +10,17 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2140. 解决智力问题',
-    url: 'https://leetcode.cn/problems/solving-questions-with-brainpower',
+    name: '2873. 有序三元组中的最大值 I',
+    url: 'https://leetcode.cn/problems/maximum-value-of-an-ordered-triplet-i',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回这场考试里你能获得的 最高 分数。`,
+    desc: `给你一个下标从 0 开始的整数数组 nums 。请你从所有满足 i < j < k 的下标三元组 (i, j, k) 中，找出并返回下标三元组的最大值。如果所有满足条件的三元组的值都是负数，则返回 0 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 259,
-            memory: 126,
+            time: 107,
+            memory: 17.45,
             desc: 'dfs',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
