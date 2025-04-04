209aa4f5546aacf58afca182be0333fe9c5e60bb
bestlyg <bestlyg@foxmail.com>
2025-04-04 16:34:38 +0800

    feat: 🎸 add leetcode no.1123

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: !true,
-    name: '2874. 有序三元组中的最大值 II',
-    url: 'https://leetcode.cn/problems/maximum-value-of-an-ordered-triplet-ii',
+    exist: true,
+    name: '1123. 最深叶节点的最近公共祖先',
+    url: 'https://leetcode.cn/problems/lowest-common-ancestor-of-deepest-leaves',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个下标从 0 开始的整数数组 nums 。请你从所有满足 i < j < k 的下标三元组 (i, j, k) 中，找出并返回下标三元组的最大值。如果所有满足条件的三元组的值都是负数，则返回 0 。`,
+    desc: `给你一个有根节点 root 的二叉树，返回它 最深的叶节点的最近公共祖先 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 1527,
-            memory: 49.89,
-            desc: '遍历j值，记录左侧最大值和最小值，以及右侧最大值和最小值',
+            time: 8,
+            memory: 18.02,
+            desc: '记录所有最底层的节点，向上遍历直到节点相同',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
