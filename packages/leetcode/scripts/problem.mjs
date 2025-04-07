30e8206709d54e4664866d94c0ccd9a1243571ee
bestlyg <bestlyg@foxmail.com>
2025-04-07 09:54:44 +0800

    feat: 🎸 add leetcode no.416

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '368. 最大整除子集',
-    url: 'https://leetcode.cn/problems/largest-divisible-subset',
+    name: '416. 分割等和子集',
+    url: 'https://leetcode.cn/problems/partition-equal-subset-sum',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个由 无重复 正整数组成的集合 nums ，请你找出并返回其中最大的整除子集 answer。`,
+    desc: `给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 179,
-            memory: 17.59,
-            desc: 'dp记录每个点的最大长度，并同时记录 最大长度时的前一个数字下标',
+            time: 1736,
+            memory: 485.61,
+            desc: '遍历所有的数组，判断是否能凑成nsum/2的数值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
