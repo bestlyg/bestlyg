b5ad85aa40a854fd982f1999925b366d4a30c9bf
bestlyg <bestlyg@foxmail.com>
2025-04-11 10:17:47 +0800

    feat: 🎸 add leetcode no.3375

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -11,17 +11,17 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 export const problem = {
     exist: !true,
     name: '3375. 使数组的值全部为 K 的最少操作次数',
-    url: 'https://leetcode.cn/problems/minimum-operations-to-make-array-values-equal-to-k',
+    url: 'https://leetcode.cn/problems/count-symmetric-integers',
     level: 'Easy',
     tagList: [],
-    desc: `你的目标是将 nums 中的所有元素都变为 k ，请你返回 最少 操作次数。如果无法将所有元素都变 k ，那么返回 -1 。`,
+    desc: `返回在 [low, high] 范围内的 对称整数的数目 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 75,
-            memory: 17.75,
-            desc: '计数',
+            time: 869,
+            memory: 17.57,
+            desc: '遍历每一个数',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
