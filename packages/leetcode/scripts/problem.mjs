63d1a25ee748114d8fa5a6e51d869c106591e8ce
bestlyg <bestlyg@foxmail.com>
2025-04-13 15:21:38 +0800

    feat: 🎸 add leetcode no.1922

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2999. 统计强大整数的数目',
-    url: 'https://leetcode.cn/problems/count-the-number-of-powerful-integers',
+    name: '1922. 统计好数字的数目',
+    url: 'https://leetcode.cn/problems/count-good-numbers',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回区间 [start..finish] 内强大整数的 总数目 。`,
+    desc: `给你一个整数 n ，请你返回长度为 n 且为好数字的数字字符串 总数 。由于答案可能会很大，请你将它对 109 + 7 取余后返回 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 35,
-            memory: 17.90,
-            desc: '数位dp',
+            time: 0,
+            memory: 17.72,
+            desc: '快速幂',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
