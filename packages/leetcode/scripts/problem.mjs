48c34a6376ee1c76913ca77d19c65709c48466bb
bestlyg <bestlyg@foxmail.com>
2025-04-18 09:41:29 +0800

    feat: 🎸 add leetcode no.2364

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2176. 统计数组中相等且可以被整除的数对',
-    url: 'https://leetcode.cn/problems/count-equal-and-divisible-pairs-in-an-array',
+    name: '2364. 统计坏数对的数目',
+    url: 'https://leetcode.cn/problems/count-number-of-bad-pairs',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k ，请你返回满足 0 <= i < j < n ，nums[i] == nums[j] 且 (i * j) 能被 k 整除的数对 (i, j) 的 数目 。`,
+    desc: `给你一个下标从 0 开始的整数数组 nums 。如果 i < j 且 j - i != nums[j] - nums[i] ，那么我们称 (i, j) 是一个 坏数对 。请你返回 nums 中 坏数对 的总数目。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 39,
-            memory: 17.49,
-            desc: '遍历',
+            time: 151,
+            memory: 37.98,
+            desc: '遍历时记录nums[i] - i的值，只有相等才不是坏整数对，所以不想等的数量即当前下标为j时的坏整数对数量',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
