9b27acd1f3eb2d2ef80563e8eac746a4dbd6b7ff
bestlyg <bestlyg@foxmail.com>
2025-04-20 10:30:05 +0800

    feat: 🎸 add leetcode no.781

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '2563. 统计公平数对的数目',
-    url: 'https://leetcode.cn/problems/count-the-number-of-fair-pairs',
+    name: '781. 森林中的兔子',
+    url: 'https://leetcode.cn/problems/rabbits-in-forest',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个下标从 0 开始、长度为 n 的整数数组 nums ，和两个整数 lower 和 upper ，返回 公平数对的数目 。`,
+    desc: `给你数组 answers ，返回森林中兔子的最少数量。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 243,
-            memory: 31.02,
-            desc: '排序后用二分记录符合当前节点的另一个值的范围',
+            time: 0,
+            memory: 17.7,
+            desc: '记录说每个数量的兔子有多少个，贪心的获取最大值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
