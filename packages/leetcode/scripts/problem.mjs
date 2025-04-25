fb41803a5a12ddf0115ac7f6577fcc41f905c8b0
bestlyg <bestlyg@foxmail.com>
2025-04-25 09:16:46 +0800

    feat: 🎸 add leetcode no.2845

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2799. 统计完全子数组的数目',
-    url: 'https://leetcode.cn/problems/count-complete-subarrays-in-an-array',
+    name: '2845. 统计趣味子数组的数目',
+    url: 'https://leetcode.cn/problems/count-of-interesting-subarrays',
     level: 'Easy',
     tagList: [],
-    desc: `返回数组中 完全子数组 的数目。`,
+    desc: `请你找出并统计数组中 趣味子数组 的数目。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 39,
-            memory: 17.86,
-            desc: '计算大于等于cnt的数量和大于等于cnt+1的数量做相减，得到等于cnt的数量',
+            time: 247,
+            memory: 40.54,
+            desc: '前缀和遍历区间内所有的符合规则的值，遍历前缀和数组利用模上modulo来找出相同倍数的值',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
