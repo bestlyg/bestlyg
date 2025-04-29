d09abf760f0c7836c8f596f86b57edc5070bd74c
bestlyg <bestlyg@foxmail.com>
2025-04-29 09:08:24 +0800

    feat: 🎸 add leetcode no.2962

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,17 +10,17 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2302. 统计得分小于 K 的子数组数目',
-    url: 'https://leetcode.cn/problems/count-subarrays-with-score-less-than-k',
+    name: '2962. 统计最大元素出现至少 K 次的子数组',
+    url: 'https://leetcode.cn/problems/count-subarrays-where-max-element-appears-at-least-k-times',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个正整数数组 nums 和一个整数 k ，请你返回 nums 中分数 严格小于 k 的 非空整数子数组数目。`,
+    desc: `给你一个整数数组 nums 和一个 正整数 k 。请你统计有多少满足 「 nums 中的 最大 元素」至少出现 k 次的子数组，并返回满足这一条件的子数组的数目。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 160,
-            memory: 29.73,
+            time: 82,
+            memory: 29.4,
             desc: '滑动窗口，遍历右端点，记录最大符合条件的左侧端点',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
