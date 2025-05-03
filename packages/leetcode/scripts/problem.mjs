28bacae211db1d160b5780cb03c9ebe3e6953442
bestlyg <bestlyg@foxmail.com>
2025-05-03 09:14:34 +0800

    feat: 🎸 add leetcode no.1007

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '838. 推多米诺',
-    url: 'https://leetcode.cn/problems/push-dominoes',
+    exist: !true,
+    name: '1007. 行相等的最少多米诺旋转',
+    url: 'https://leetcode.cn/problems/minimum-domino-rotations-for-equal-row',
     level: 'Easy',
     tagList: [],
-    desc: `返回表示最终状态的字符串。`,
+    desc: `返回能使 tops 中所有值或者 bottoms 中所有值都相同的最小旋转次数。如果无法做到，返回 -1.`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 187,
-            memory: 21.69,
-            desc: 'bfs',
+            time: 19,
+            memory: 18.21,
+            desc: '只有6个可能性，依次遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
