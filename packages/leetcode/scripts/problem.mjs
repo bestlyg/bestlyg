ad319c29bbe2ec97e98f32a9fecdd3a85b0078c5
bestlyg <bestlyg@foxmail.com>
2025-05-07 09:04:52 +0800

    feat: 🎸 add leetcode no.3341

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '1920. 基于排列构建数组',
-    url: 'https://leetcode.cn/problems/build-array-from-permutation',
+    name: '3341. 到达最后一个房间的最少时间 I',
+    url: 'https://leetcode.cn/problems/find-minimum-time-to-reach-last-room-i',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个 从 0 开始的排列 nums（下标也从 0 开始）。请你构建一个 同样长度 的数组 ans ，其中，对于每个 i（0 <= i < nums.length），都满足 ans[i] = nums[nums[i]] 。返回构建好的数组 ans 。`,
+    desc: `请你返回到达房间 (n - 1, m - 1) 所需要的 最少 时间。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 0,
-            memory: 17.78,
-            desc: '遍历',
+            time: 191,
+            memory: 18.18,
+            desc: '优先队列',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
