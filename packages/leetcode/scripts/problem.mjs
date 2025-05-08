c6c87681164f177be57c55fdbe05600919c8b9b8
bestlyg <bestlyg@foxmail.com>
2025-05-08 08:55:13 +0800

    feat: 🎸 add leetcode no.3342

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,8 +10,8 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3341. 到达最后一个房间的最少时间 I',
-    url: 'https://leetcode.cn/problems/find-minimum-time-to-reach-last-room-i',
+    name: '3342. 到达最后一个房间的最少时间 II',
+    url: 'https://leetcode.cn/problems/find-minimum-time-to-reach-last-room-ii',
     level: 'Easy',
     tagList: [],
     desc: `请你返回到达房间 (n - 1, m - 1) 所需要的 最少 时间。`,
@@ -19,9 +19,9 @@ export const problem = {
         {
             script: 'python',
             // date: '2025.03.24',
-            time: 191,
-            memory: 18.18,
-            desc: '优先队列',
+            time: 2397,
+            memory: 161.84,
+            desc: '优先队列，记录下一步要走的步长',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
