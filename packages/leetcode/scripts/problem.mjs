72c3ebac69d83ca57e7198d49b598c31dd6f3371
bestlyg <bestlyg@foxmail.com>
2025-05-13 16:32:50 +0800

    feat: 🎸 add leetcode no.3335

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2094. 找出 3 位偶数',
-    url: 'https://leetcode.cn/problems/finding-3-digit-even-numbers',
+    name: '3335. 字符串转换后的长度 I',
+    url: 'https://leetcode.cn/problems/total-characters-in-string-after-transformations-i',
     level: 'Easy',
     tagList: [],
-    desc: `将找出的所有互不相同的整数按 递增顺序 排列，并以数组形式返回。`,
+    desc: `返回 恰好 执行 t 次转换后得到的字符串的 长度。`,
     solutions: [
         {
             script: 'python',
-            date: '2025.05.10',
-            time: 2079,
-            memory: 17.86,
-            desc: '遍历',
+            // date: '2025.05.10',
+            time: 410,
+            memory: 18.05,
+            desc: '记录每一个字母的数量，对z做特殊处理，用偏移值表示移动',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
