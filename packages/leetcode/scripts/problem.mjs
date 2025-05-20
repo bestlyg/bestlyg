f764fbaf3be3524709458fff03af554d01595349
bestlyg <bestlyg@foxmail.com>
2025-05-20 08:51:19 +0800

    feat: 🎸 add leetcode no.3355

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3024. 三角形类型',
-    url: 'https://leetcode.cn/problems/type-of-triangle',
+    name: '3355. 零数组变换 I',
+    url: 'https://leetcode.cn/problems/zero-array-transformation-i',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个下标从 0 开始长度为 3 的整数数组 nums ，需要用它们来构造三角形。如果一个三角形的所有边长度相等，那么这个三角形称为 equilateral 。如果一个三角形恰好有两条边长度相等，那么这个三角形称为 isosceles 。如果一个三角形三条边的长度互不相同，那么这个三角形称为 scalene 。如果这个数组无法构成一个三角形，请你返回字符串 "none" ，否则返回一个字符串表示这个三角形的类型。`,
+    desc: `如果在按顺序处理所有查询后，可以将 nums 转换为 零数组 ，则返回 true，否则返回 false。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.10',
-            time: 0,
-            memory: 17.34,
-            desc: '直接判断',
+            time: 59,
+            memory: 54.06,
+            desc: '差分数组记录每一个下标最多可以被处理多少次',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
