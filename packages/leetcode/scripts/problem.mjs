c21c0f6d825a252a681cda287519dc38d1870cf4
bestlyg <bestlyg@foxmail.com>
2025-05-22 10:02:30 +0800

    feat: 🎸 add leetcode no.3356

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3356. 零数组变换 II',
-    url: 'https://leetcode.cn/problems/zero-array-transformation-ii',
+    name: '3356. 零数组变换 III',
+    url: 'https://leetcode.cn/problems/zero-array-transformation-iii',
     level: 'Easy',
     tagList: [],
-    desc: `返回 k 可以取到的 最小非负 值，使得在 顺序 处理前 k 个查询后，nums 变成 零数组。如果不存在这样的 k，则返回 -1。`,
+    desc: `请你返回 最多 可以从 queries 中删除多少个元素，使得 queries 中剩下的元素仍然能将 nums 变为一个 零数组 。如果无法将 nums 变为一个 零数组 ，返回 -1 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.10',
-            time: 975,
-            memory: 63.3,
-            desc: '二分query，判断最小能零数组的大小',
+            time: 456,
+            memory: 57.63,
+            desc: '对queries排序，遍历每一个nums下标，用堆记录所有起始点小于等于该下标的query，并按照尾端点排序',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
