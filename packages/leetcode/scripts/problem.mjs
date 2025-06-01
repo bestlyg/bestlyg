e66446e31a99c5bb950b82982eb0871c7188d381
bestlyg <bestlyg@foxmail.com>
2025-06-01 08:12:50 +0800

    feat: 🎸 add leetcode no.2929

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '909. 蛇梯棋',
-    url: 'https://leetcode.cn/problems/snakes-and-ladders',
+    exist: !true,
+    name: '2929. 给小朋友们分糖果 II',
+    url: 'https://leetcode.cn/problems/distribute-candies-among-children-ii',
     level: 'Easy',
     tagList: [],
-    desc: `返回达到编号为 n2 的方格所需的最少掷骰次数，如果不可能，则返回 -1。`,
+    desc: `给你两个正整数 n 和 limit 。请你将 n 颗糖果分给 3 位小朋友，确保没有任何小朋友得到超过 limit 颗糖果，请你返回满足此条件下的 总方案数 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 47,
-            memory: 17.64,
-            desc: 'bfs',
+            time: 5297,
+            memory: 17.41,
+            desc: '只有三个人，遍历第一个人获取到的数量，求出第二个人最多和最少的数量',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
