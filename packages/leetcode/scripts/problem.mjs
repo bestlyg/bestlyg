a76289f75e71f095cfbab8c83f4c66f38f86a776
bestlyg <bestlyg@foxmail.com>
2025-06-03 09:13:10 +0800

    feat: 🎸 add leetcode no.1298

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '135. 分发糖果',
-    url: 'https://leetcode.cn/problems/candy',
+    exist: !true,
+    name: '1298. 你能从盒子里获得的最大糖果数',
+    url: 'https://leetcode.cn/problems/maximum-candies-you-can-get-from-boxes',
     level: 'Easy',
     tagList: [],
-    desc: `请你给每个孩子分发糖果，计算并返回需要准备的 最少糖果数目 。`,
+    desc: `给你一个 initialBoxes 数组，表示你现在得到的盒子，你可以获得里面的糖果，也可以用盒子里的钥匙打开新的盒子，还可以继续探索从这个盒子里找到的其他盒子。请你按照上述规则，返回可以获得糖果的 最大数目 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
             time: 23,
-            memory: 19.8,
-            desc: '贪心从左到右和从右到左',
+            memory: 27.72,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
