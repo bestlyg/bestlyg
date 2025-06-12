c832bd42cc27db796024cebc5b2d6c3c3b2da9e8
bestlyg <bestlyg@foxmail.com>
2025-06-12 09:58:03 +0800

    feat: 🎸 add leetcode no.3423

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,17 +10,17 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3442. 奇偶频次间的最大差值 I',
-    url: 'https://leetcode.cn/problems/maximum-difference-between-even-and-odd-frequency-i',
+    name: '3423. 循环数组中相邻元素的最大差值',
+    url: 'https://leetcode.cn/problems/maximum-difference-between-adjacent-elements-in-a-circular-array',
     level: 'Easy',
     tagList: [],
-    desc: `返回 最大 差值。`,
+    desc: `给你一个 循环 数组 nums ，请你找出相邻元素之间的 最大 绝对差值。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
             time: 0,
-            memory:17.4,
+            memory:17.68,
             desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
