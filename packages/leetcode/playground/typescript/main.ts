fa328ce3e965a0ba3d8a5bc3ee3e658ffce6be28
bestlyg <bestlyg@foxmail.com>
2025-06-24 09:04:32 +0800

    feat: 🎸 add leetcode no.2200

---
+++ packages/leetcode/playground/typescript/main.ts
@@ -0,0 +1,45 @@
+function check(s: string) {
+    for (let i = 0; i < s.length / 2; i++) {
+        if (s[i] !== s[s.length - 1 - i]) return false
+    }
+    return true
+}
+function kMirror(k: number, n: number): number[] {
+    const arr: number[] = []
+    for (let num = 1; arr.length < 30; num++) {
+        if (check(num.toString()) && check(num.toString(k))) {
+            arr.push(num)
+        }
+    }
+    return arr;
+};
+
+const arr:any[] = []
+for (let k = 2; k <= 9; k++) {
+    const res = kMirror(k, 10)
+    arr.push(res)
+    console.log(res)
+}
+console.log(JSON.stringify(arr))
+/**
+ [
+          1,         3,        5,         7,
+          9,        33,       99,       313,
+        585,       717,     7447,      9009,
+      15351,     32223,    39993,     53235,
+      53835,     73737,   585585,   1758571,
+    1934391,   1979791,  3129213,   5071705,
+    5259525,   5841485, 13500531, 719848917,
+  910373019, 939474939
+]
+[
+         1,        2,       4,       8,
+       121,      151,     212,     242,
+       484,      656,     757,   29092,
+     48884,    74647,   75457,   76267,
+     92929,    93739,  848848, 1521251,
+   2985892,  4022204, 4219124, 4251524,
+   4287824,  5737375, 7875787, 7949497,
+  27711772, 83155138
+]
+ */
--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2138. 将字符串拆分为若干长度为 k 的组',
-    url: 'https://leetcode.cn/problems/divide-a-string-into-groups-of-size-k',
+    name: '2200. 找出数组中的所有 K 近邻下标',
+    url: 'https://leetcode.cn/problems/find-all-k-distant-indices-in-an-array',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个字符串 s ，以及每组的长度 k 和一个用于填充的字符 fill ，按上述步骤处理之后，返回一个字符串数组，该数组表示 s 分组后 每个组的组成情况 。`,
+    desc: `以列表形式返回按 递增顺序 排序的所有 K 近邻下标。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 0,
-            memory:17.79,
-            desc: '先补齐再遍历',
+            time: 179,
+            memory:17.83,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
