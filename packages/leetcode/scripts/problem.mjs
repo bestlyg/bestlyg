7f7859587772a47d5dc04c792f9b8e387ef7ad34
bestlyg <bestlyg@foxmail.com>
2025-06-26 09:55:11 +0800

    feat: 🎸 add leetcode no.2311

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '2200. 找出数组中的所有 K 近邻下标',
-    url: 'https://leetcode.cn/problems/find-all-k-distant-indices-in-an-array',
+    name: '2311. 小于等于 K 的最长二进制子序列',
+    url: 'https://leetcode.cn/problems/longest-binary-subsequence-less-than-or-equal-to-k',
     level: 'Easy',
     tagList: [],
-    desc: `以列表形式返回按 递增顺序 排序的所有 K 近邻下标。`,
+    desc: `给你一个二进制字符串 s 和一个正整数 k 。请你返回 s 的 最长 子序列的长度，且该子序列对应的 二进制 数字小于等于 k 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 179,
-            memory:17.83,
-            desc: '遍历',
+            time: 34,
+            memory: 17.83,
+            desc: '贪心的记录最小前缀',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
