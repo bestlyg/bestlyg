846bfec90c7e8329c65b796c576e4bd3c92016c2
bestlyg <bestlyg@foxmail.com>
2025-07-01 09:51:32 +0800

    feat: 🎸 add leetcode no.3330

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,18 +9,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '594. 最长和谐子序列',
-    url: 'https://leetcode.cn/problems/longest-harmonious-subsequence',
+    exist: !true,
+    name: '3330. 找到初始输入字符串 I',
+    url: 'https://leetcode.cn/problems/find-the-original-typed-string-i',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个整数数组 nums ，请你在所有可能的 子序列 中找到最长的和谐子序列的长度。`,
+    desc: `给你一个字符串 word ，它表示 最终 显示在 Alice 显示屏上的结果。请你返回 Alice 一开始可能想要输入字符串的总方案数。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 59,
-            memory: 18.80,
+            time: 47,
+            memory: 17.43,
             desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
