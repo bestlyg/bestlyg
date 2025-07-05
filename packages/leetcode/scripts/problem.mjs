f8940c99f6d9ab20005ffe7e2437176b483dc098
bestlyg <bestlyg@foxmail.com>
2025-07-05 08:24:57 +0800

    feat: 🎸 add leetcode no.1394

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3304. 找出第 K 个字符 I',
-    url: 'https://leetcode.cn/problems/find-the-k-th-character-in-string-game-i',
+    name: '1394. 找出数组中的幸运数',
+    url: 'https://leetcode.cn/problems/find-lucky-integer-in-an-array',
     level: 'Easy',
     tagList: [],
-    desc: `在执行足够多的操作后， word 中 至少 存在 k 个字符，此时返回 word 中第 k 个字符的值。`,
+    desc: `给你一个整数数组 arr，请你从中找出并返回一个幸运数。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 11,
-            memory: 17.50,
-            desc: '模拟',
+            time:0,
+            memory: 17.82,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
