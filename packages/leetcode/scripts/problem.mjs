d5bb944c77f2ee86bc514b2cf9faccae53c81bea
bestlyg <bestlyg@foxmail.com>
2025-07-13 10:19:53 +0800

    feat: 🎸 add leetcode no.2410

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3169. 无需开会的工作日',
-    url: 'https://leetcode.cn/problems/count-days-without-meetings',
+    name: '2410. 运动员和训练师的最大匹配数',
+    url: 'https://leetcode.cn/problems/maximum-matching-of-players-with-trainers',
     level: 'Easy',
     tagList: [],
-    desc: `返回员工可工作且没有安排会议的天数。`,
+    desc: `请你返回满足上述要求 players 和 trainers 的 最大 匹配数。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 237,
-            memory: 51.89,
-            desc: '合并区间',
+            time: 92,
+            memory: 31.13,
+            desc: '贪心排序',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
