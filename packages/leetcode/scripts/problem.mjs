e7ef25eaa969aba236f1a8ba09730e662482b724
bestlyg <bestlyg@foxmail.com>
2025-07-16 22:15:25 +0800

    feat: 🎸 add leetcode no.3201

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '1290. 二进制链表转整数',
-    url: 'https://leetcode.cn/problems/convert-binary-number-in-a-linked-list-to-integer',
+    name: '3201. 找出有效子序列的最大长度 I',
+    url: 'https://leetcode.cn/problems/find-the-maximum-length-of-valid-subsequence-i',
     level: 'Easy',
     tagList: [],
-    desc: `请你返回该链表所表示数字的 十进制值 。`,
+    desc: `返回 nums 的 最长的有效子序列 的长度。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 4,
-            memory: 17.38,
-            desc: '遍历',
+            time: 538,
+            memory: 65.02,
+            desc: '分别计算，都是奇数，都是偶数，一奇一偶',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
