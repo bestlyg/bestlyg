4c12407b8c2564579426514e95abc51a616bde2f
bestlyg <bestlyg@foxmail.com>
2025-07-18 14:19:52 +0800

    feat: 🎸 add leetcode no.2163

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -10,18 +10,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3202. 找出有效子序列的最大长度 II',
-    url: 'https://leetcode.cn/problems/find-the-maximum-length-of-valid-subsequence-ii',
+    name: '2163. 删除元素后和的最小差值',
+    url: 'https://leetcode.cn/problems/minimum-difference-in-sums-after-removal-of-elements',
     level: 'Easy',
     tagList: [],
-    desc: `返回 nums 的 最长有效子序列 的长度。`,
+    desc: `请你返回删除 n 个元素之后，剩下两部分和的 差值的最小值 是多少。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 2525,
-            memory: 26.13,
-            desc: '取模，遍历每一种余数，dp[cur][prev] = dp[prev][cur] + 1',
+            time: 519,
+            memory: 52.51,
+            desc: '遍历前2/3个的最小值，和后2/3个的最大值，进行枚举',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
