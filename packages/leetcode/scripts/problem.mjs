f30086755767a9da6f3656aec7fa85eb5cf2e8a1
bestlyg <bestlyg@foxmail.com>
2025-07-28 09:17:44 +0800

    feat: 🎸 add leetcode no.2044

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -9,19 +9,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {prismaClient.LeetcodeProblem & {solutions:prismaClient.LeetcodeSolution[]}} */
 /** @type {Problem} */
 export const problem = {
-    exist: !true,
-    name: '2210. 统计数组中峰和谷的数量',
-    url: 'https://leetcode.cn/problems/count-hills-and-valleys-in-an-array',
+    exist: true,
+    name: '2044. 统计按位或能得到最大值的子集数目',
+    url: 'https://leetcode.cn/problems/count-number-of-maximum-bitwise-or-subsets',
     level: 'Easy',
     tagList: [],
-    desc: `返回 nums 中峰和谷的数量。`,
+    desc: `对数组 a 执行 按位或 ，结果等于 a[0] OR a[1] OR ... OR a[a.length - 1]（下标从 0 开始）。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 0,
-            memory: 17.7,
-            desc: '遍历',
+            time: 1461,
+            memory: 17.45,
+            desc: '二进制枚举所有可能',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
