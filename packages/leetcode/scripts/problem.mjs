6f4b848d05eeb67dcdfda3cde50d884bbfcea31d
bestlyg <bestlyg@foxmail.com>
2025-08-02 12:15:25 +0800

    feat: 🎸 add leetcode no.2561

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -5,17 +5,17 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '118. 杨辉三角',
-    url: 'https://leetcode.cn/problems/pascals-triangle',
+    name: '2561. 重排水果',
+    url: 'https://leetcode.cn/problems/rearranging-fruits',
     level: 'Easy',
     tagList: [],
-    desc: `给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。`,
+    desc: `返回使两个果篮相等的最小交换成本，如果无法使两个果篮相等，则返回 -1 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 0,
-            memory:17.46,
+            time: 236,
+            memory: 53.92,
             desc: '枚举',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
