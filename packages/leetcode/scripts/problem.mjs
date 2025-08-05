714e98b76a0ac0d144e5210947155ef34e6cf3c4
bestlyg <bestlyg@foxmail.com>
2025-08-05 09:56:39 +0800

    feat: 🎸 add leetcode no.3477

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,19 +4,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '904. 水果成篮',
-    url: 'https://leetcode.cn/problems/fruit-into-baskets',
+    exist: !true,
+    name: '3477. 水果成篮 II',
+    url: 'https://leetcode.cn/problems/fruits-into-baskets-ii',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个整数数组 fruits ，返回你可以收集的水果的 最大 数目。`,
+    desc: `返回所有可能分配完成后，剩余未放置的水果种类的数量。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 346,
-            memory: 23.5,
-            desc: '双指针，枚举右端点，移动左端点',
+            time: 40,
+            memory: 17.8,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
