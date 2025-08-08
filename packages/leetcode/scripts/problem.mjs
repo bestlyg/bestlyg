680f07e07f983583f5cb3c19fbf39cdd0519f7e4
bestlyg <bestlyg@foxmail.com>
2025-08-08 10:10:16 +0800

    feat: 🎸 add leetcode no.3363

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,19 +4,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '808. 分汤',
-    url: 'https://leetcode.cn/problems/soup-servings',
+    exist: !true,
+    name: '3363. 最多可收集的水果数目',
+    url: 'https://leetcode.cn/problems/find-the-maximum-number-of-fruits-collected',
     level: 'Easy',
     tagList: [],
-    desc: `返回汤 A 在 B 前耗尽的概率，加上两种汤在 同一回合 耗尽概率的一半。返回值在正确答案 10-5 的范围内将被认为是正确的。`,
+    desc: `请你返回三个小朋友总共 最多 可以收集多少个水果。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 4,
-            memory: 9.3,
-            desc: 'dfs',
+            time: 2531,
+            memory: 396.37,
+            desc: '中间的人只能走一条线，下面的人只能走下半区，上面的人只能走上半区，进行DFS',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
