3e390ed5bc52a039a42093a955f30d6f0dc7bf0e
bestlyg <bestlyg@foxmail.com>
2025-08-10 10:15:06 +0800

    feat: 🎸 add leetcode no.869

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -5,8 +5,8 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: true,
-    name: '231. 2 的幂',
-    url: 'https://leetcode.cn/problems/power-of-two',
+    name: '869. 重新排序得到 2 的幂',
+    url: 'https://leetcode.cn/problems/reordered-power-of-2',
     level: 'Easy',
     tagList: [],
     desc: `如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。`,
@@ -15,8 +15,8 @@ export const problem = {
             script: 'python',
             // date: '2025.05.24',
             time: 0,
-            memory: 17.55,
-            desc: '如果是2的幂次方，那么二进制只有一个1',
+            memory: 17.42,
+            desc: '计数比较',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
