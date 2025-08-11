2eba071933c8fdadf744ec1155caaae873ea60ea
bestlyg <bestlyg@foxmail.com>
2025-08-11 09:02:41 +0800

    feat: 🎸 add leetcode no.2438

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,19 +4,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 /** @type {Problem} */
 export const problem = {
-    exist: true,
-    name: '869. 重新排序得到 2 的幂',
-    url: 'https://leetcode.cn/problems/reordered-power-of-2',
+    exist: !true,
+    name: '2438. 二的幂数组中查询范围内的乘积',
+    url: 'https://leetcode.cn/problems/range-product-queries-of-powers',
     level: 'Easy',
     tagList: [],
-    desc: `如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。`,
+    desc: `请你返回一个数组 answers ，长度与 queries 的长度相同，其中 answers[i]是第 i 个查询的答案。由于查询的结果可能非常大，请你将每个 answers[i] 都对 109 + 7 取余 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 0,
-            memory: 17.42,
-            desc: '计数比较',
+            time: 47,
+            memory: 47.24,
+            desc: '对n的每一个1位进行记录',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
