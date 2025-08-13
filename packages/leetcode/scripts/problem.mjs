cf8c12c2b9df8e0590b6e2d28adf1bcd668c3984
bestlyg <bestlyg@foxmail.com>
2025-08-13 08:55:17 +0800

    feat: 🎸 add leetcode no.326

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -4,19 +4,19 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 /** @type {Problem} */
 export const problem = {
-    exist: !true,
-    name: '2787. 将一个数字表示成幂的和的方案数',
-    url: 'https://leetcode.cn/problems/ways-to-express-an-integer-as-sum-of-powers',
+    exist: true,
+    name: '326. 3 的幂',
+    url: 'https://leetcode.cn/problems/power-of-three',
     level: 'Easy',
     tagList: [],
-    desc: `给你两个 正 整数 n 和 x 。请你返回将 n 表示成一些 互不相同 正整数的 x 次幂之和的方案数。换句话说，你需要返回互不相同整数 [n1, n2, ..., nk] 的集合数目，满足 n = n1x + n2x + ... + nkx 。`,
+    desc: `给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 2072,
-            memory: 664.86,
-            desc: 'dfs',
+            time: 19,
+            memory:17.44,
+            desc: '遍历',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
