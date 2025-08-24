6a02f58ea5c778779a9cc62ec637737a91bf5e2e
bestlyg <bestlyg@foxmail.com>
2025-08-24 14:43:13 +0800

    feat: 🎸 add leetcode no.1493

--- packages/leetcode/scripts/problem.mjs
+++ packages/leetcode/scripts/problem.mjs
@@ -5,18 +5,18 @@ import { PATH_CODE } from '@bestlyg/leetcode';
 /** @type {Problem} */
 export const problem = {
     exist: !true,
-    name: '3195. 包含所有 1 的最小矩形面积 I',
-    url: 'https://leetcode.cn/problems/find-the-minimum-area-to-cover-all-ones-i',
+    name: '1493. 删掉一个元素以后全为 1 的最长子数组',
+    url: 'https://leetcode.cn/problems/longest-subarray-of-1s-after-deleting-one-element',
     level: 'Easy',
     tagList: [],
-    desc: `给你一个二维 二进制 数组 grid。请你找出一个边在水平方向和竖直方向上、面积 最小 的矩形，并且满足 grid 中所有的 1 都在矩形的内部。返回这个矩形可能的 最小 面积。`,
+    desc: `给你一个二进制数组 nums ，你需要从中删掉一个元素。请你在删掉元素的结果数组中，返回最长的且只包含 1 的非空子数组的长度。如果不存在这样的子数组，请返回 0 。`,
     solutions: [
         {
             script: 'python',
             // date: '2025.05.24',
-            time: 549,
-            memory: 53.89,
-            desc: '记录存在1的最大边界',
+            time: 74,
+            memory: 21.20,
+            desc: '滑动窗口',
             code: await fs.readFile(PATH_CODE, 'utf8'),
         },
     ],
