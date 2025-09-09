622e4ae86a56695e9be18d6c768e22246ab9099d
bestlyg <bestlyg@foxmail.com>
2025-09-09 10:19:09 +0800

    feat: 🎸 add leetcode no.2327

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '1317. 将整数转换为两个无零整数的和',
-    url: 'https://leetcode.cn/problems/convert-integer-to-the-sum-of-two-no-zero-integers',
+    name: '2327. 知道秘密的人数',
+    url: 'https://leetcode.cn/problems/number-of-people-aware-of-a-secret',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你一个整数 n，请你返回一个 由两个整数组成的列表 [a, b]，满足：a 和 b 都是无零整数a + b = n题目数据保证至少有一个有效的解决方案。`,
+    desc: `给你一个整数 n ，请你返回在第 n 天结束时，知道秘密的人数。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 0,
-            memory: 17.55,
-            desc: '直接计算',
+            time: 3,
+            memory: 17.60,
+            desc: '存储遗忘队列和等待队列',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
