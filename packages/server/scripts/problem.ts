bd5dddcd895b0cbbeeaadd24aea82804eac3132d
bestlyg <bestlyg@foxmail.com>
2025-09-11 10:02:42 +0800

    feat: 🎸 add leetcode no.2785

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '1733. 需要教语言的最少人数',
-    url: 'https://leetcode.cn/problems/minimum-number-of-people-to-teach',
+    name: '2785. 将字符串中的元音字母排序',
+    url: 'https://leetcode.cn/problems/sort-vowels-in-a-string',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `你可以选择 一门 语言并教会一些用户，使得所有好友之间都可以相互沟通。请返回你 最少 需要教会多少名用户。`,
+    desc: `给你一个下标从 0 开始的字符串 s ，将 s 中的元素重新 排列 得到新的字符串 t 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 237,
-            memory: 26.87,
-            desc: '枚举每一个语言要学会的人数',
+            time: 127,
+            memory: 20.23,
+            desc: '获取所有元音进行排序',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
