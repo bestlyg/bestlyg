8251373474188cd03e0c99e252803775e8769b86
bestlyg <bestlyg@foxmail.com>
2025-09-14 13:52:20 +0800

    feat: 🎸 add leetcode no.966

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3227. 字符串元音游戏',
-    url: 'https://leetcode.cn/problems/vowels-game-in-a-string',
+    name: '966. 元音拼写检查器',
+    url: 'https://leetcode.cn/problems/vowel-spellchecker',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `如果小红赢得游戏，返回 true，否则返回 false。`,
+    desc: `给出一些查询 queries，返回一个单词列表 answer，其中 answer[i] 是由查询 query = queries[i] 得到的正确单词。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 36,
-            memory: 17.79,
-            desc: '只要存在元音，alice必赢',
+            time: 47,
+            memory: 20.13,
+            desc: '预处理所有的可能',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
