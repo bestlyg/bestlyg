1b7fa74ba4e0ea1dba77809ad8607b3ad78af295
bestlyg <bestlyg@foxmail.com>
2025-09-16 09:09:57 +0800

    feat: 🎸 add leetcode no.2197

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '1935. 可以输入的最大单词数',
-    url: 'https://leetcode.cn/problems/maximum-number-of-words-you-can-type',
+    name: '2197. 替换数组中的非互质数',
+    url: 'https://leetcode.cn/problems/replace-non-coprime-numbers-in-array',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `键盘出现了一些故障，有些字母键无法正常工作。而键盘上所有其他键都能够正常工作。`,
+    desc: `给你一个整数数组 nums 。请你对数组执行下述操作：返回修改后得到的 最终 数组。可以证明的是，以 任意 顺序替换相邻的非互质数都可以得到相同的结果。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 2,
-            memory: 17.81,
-            desc: '遍历',
+            time: 166,
+            memory: 31.51,
+            desc: '遍历时用栈存储',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
