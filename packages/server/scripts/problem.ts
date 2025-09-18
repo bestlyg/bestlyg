64be8781183fff0f00a9917dd328dce316a6c262
bestlyg <bestlyg@foxmail.com>
2025-09-18 09:50:51 +0800

    feat: 🎸 add leetcode no.3408

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '2349. 设计数字容器系统',
-    url: 'https://leetcode.cn/problems/design-a-number-container-system',
+    name: '3408. 设计任务管理器',
+    url: 'https://leetcode.cn/problems/design-task-manager',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `设计一个数字容器系统，可以实现以下功能：在系统中给定下标处 插入 或者 替换 一个数字。返回 系统中给定数字的最小下标。`,
+    desc: `请你设计一个 TaskManager 类。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 195,
-            memory: 80.1,
-            desc: '用堆存储每个数字下最小的下标',
+            time: 753,
+            memory:135.35,
+            desc: '懒堆记录所有事件用户的优先级',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
