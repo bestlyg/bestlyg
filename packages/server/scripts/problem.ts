f32e09fa69cd1cb51126dc08094bf55f8340b10b
bestlyg <bestlyg@foxmail.com>
2025-09-21 15:06:19 +0800

    feat: 🎸 add leetcode no.1912

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3508. 设计路由器',
-    url: 'https://leetcode.cn/problems/implement-router',
+    name: '1912. 设计电影租借系统',
+    url: 'https://leetcode.cn/problems/design-movie-rental-system',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你设计一个数据结构来高效管理网络路由器中的数据包。`,
+    desc: `你有一个电影租借公司和 n 个电影商店。你想要实现一个电影租借系统，它支持查询、预订和返还电影的操作。同时系统还能生成一份当前被借出电影的报告。所有电影用二维整数数组 entries 表示，其中 entries[i] = [shopi, moviei, pricei] 表示商店 shopi 有一份电影 moviei 的拷贝，租借价格为 pricei 。每个商店有 至多一份 编号为 moviei 的电影拷贝。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 331,
-            memory: 81.59,
-            desc: '哈希记录列表，二分查找时间段',
+            time: 792,
+            memory: 128.48,
+            desc: '有序数组',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
