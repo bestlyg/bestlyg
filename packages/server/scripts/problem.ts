0de890b79730a26248716b7a4db0cdfb8d5217a5
bestlyg <bestlyg@foxmail.com>
2025-09-25 09:48:35 +0800

    feat: 🎸 add leetcode no.120

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '165. 比较版本号',
-    url: 'https://leetcode.cn/problems/compare-version-numbers',
+    name: '120. 三角形最小路径和',
+    url: 'https://leetcode.cn/problems/triangle',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你两个 版本号字符串 version1 和 version2 ，请你比较它们。版本号由被点 '.' 分开的修订号组成。修订号的值 是它 转换为整数 并忽略前导零。`,
+    desc: `给定一个三角形 triangle ，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 0,
-            memory: 17.57,
-            desc: '分割后遍历',
+            time: 15,
+            memory: 21.11,
+            desc: 'dfs',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
