e14d7fd5cf208f539860bb42e1573e062a09960a
bestlyg <bestlyg@foxmail.com>
2025-09-26 10:14:05 +0800

    feat: 🎸 add leetcode no.611

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,17 +15,17 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '120. 三角形最小路径和',
-    url: 'https://leetcode.cn/problems/triangle',
+    name: '611. 有效三角形的个数',
+    url: 'https://leetcode.cn/problems/valid-triangle-number',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给定一个三角形 triangle ，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。`,
+    desc: `给定一个包含非负整数的数组 nums ，返回其中可以组成三角形三条边的三元组个数。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 15,
-            memory: 21.11,
+            time: 3033,
+            memory: 17.63,
             desc: 'dfs',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
