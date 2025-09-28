cc39dd866e7adc90d5218998e2efd2f63010eabe
bestlyg <bestlyg@foxmail.com>
2025-09-28 10:37:33 +0800

    feat: 🎸 add leetcode no.976

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '611. 有效三角形的个数',
-    url: 'https://leetcode.cn/problems/valid-triangle-number',
+    name: '976. 三角形的最大周长',
+    url: 'https://leetcode.cn/problems/largest-perimeter-triangle',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给定一个包含非负整数的数组 nums ，返回其中可以组成三角形三条边的三元组个数。`,
+    desc: `给定由一些正数（代表长度）组成的数组 nums ，返回 由其中三个长度组成的、面积不为零的三角形的最大周长 。如果不能形成任何面积不为零的三角形，返回 0。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 3033,
-            memory: 17.63,
-            desc: 'dfs',
+            time: 2,
+            memory: 18.38,
+            desc: '排序后遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
