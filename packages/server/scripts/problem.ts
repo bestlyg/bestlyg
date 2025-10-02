41f83c8441466dfcb65b95c4208457cf038da9d3
bestlyg <bestlyg@foxmail.com>
2025-10-02 13:44:47 +0800

    feat: 🎸 update leetcode

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '2221. 数组的三角和',
-    url: 'https://leetcode.cn/problems/find-triangular-sum-of-an-array',
+    name: '1518. 换水问题',
+    url: 'https://leetcode.cn/problems/water-bottles',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你返回 nums 的三角和。`,
+    desc: `给你两个整数 numBottles 和 numExchange ，返回你 最多 可以喝到多少瓶水。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 1356,
-            memory: 21.93,
-            desc: 'dfs',
+            time: 0,
+            memory: 17.43,
+            desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
