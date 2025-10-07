e4ebeb8f57a8f7eb28e68ff95a37cac17f548871
bestlyg <bestlyg@foxmail.com>
2025-10-07 22:35:09 +0800

    feat: 🎸 add leetcode no.1488

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,19 +14,19 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: !true,
-    name: '1518. 换水问题',
-    url: 'https://leetcode.cn/problems/water-bottles',
+    exist: true,
+    name: '1488. 避免洪水泛滥',
+    url: 'https://leetcode.cn/problems/avoid-flood-in-the-city',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你两个整数 numBottles 和 numExchange ，返回你 最多 可以喝到多少瓶水。`,
+    desc: `你的国家有无数个湖泊，所有湖泊一开始都是空的。当第 n 个湖泊下雨前是空的，那么它就会装满水。如果第 n 个湖泊下雨前是 满的 ，这个湖泊会发生 洪水 。你的目标是避免任意一个湖泊发生洪水。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 0,
-            memory: 17.43,
-            desc: '遍历',
+            time: 331,
+            memory: 92.38,
+            desc: '用堆记录最先应该被清空的水库',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
