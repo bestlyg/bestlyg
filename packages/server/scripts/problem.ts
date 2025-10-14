83557df14ae8a14e8e449937830cdc8bd672df6a
bestlyg <bestlyg@foxmail.com>
2025-10-14 09:59:25 +0800

    feat: 🎸 add leetcode no.3349

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,17 +15,17 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '2273. 移除字母异位词后的结果数组',
-    url: 'https://leetcode.cn/problems/find-resultant-array-after-removing-anagrams',
+    name: '3349. 检测相邻递增子数组 I',
+    url: 'https://leetcode.cn/problems/adjacent-increasing-subarrays-detection-i',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `在执行所有操作后，返回 words 。可以证明，按任意顺序为每步操作选择下标都会得到相同的结果。`,
+    desc: `如果可以找到这样的 两个 子数组，请返回 true；否则返回 false。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.05.24',
-            time: 35,
-            memory: 17.50,
+            time: 123,
+            memory: 17.41,
             desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
