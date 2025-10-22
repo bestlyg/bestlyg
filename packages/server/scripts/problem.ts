94c7de9bf4c11de3e797865309c6e44bd39d58ea
bestlyg <bestlyg@foxmail.com>
2025-10-22 09:59:22 +0800

    feat: 🎸 add leetcode no.3347

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,8 +15,8 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3346. 执行操作后元素的最高频率 I',
-    url: 'https://leetcode.cn/problems/maximum-frequency-of-an-element-after-performing-operations-i',
+    name: '3347. 执行操作后元素的最高频率 II',
+    url: 'https://leetcode.cn/problems/maximum-frequency-of-an-element-after-performing-operations-ii',
     level: LeetcodeLevelType.Easy,
     tags: [],
     desc: `在执行完所有操作以后，请你返回 nums 中出现 频率最高 元素的出现次数。一个元素 x 的 频率 指的是它在数组中出现的次数。`,
@@ -24,9 +24,9 @@ export const problem: LeetcodeProblem & { exist: boolean } = {
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.15',
-            time: 767,
-            memory: 32.78,
-            desc: '二分查找区间',
+            time: 1625,
+            memory: 53.82,
+            desc: '离散差分',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
