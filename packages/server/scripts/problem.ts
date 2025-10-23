ef8a6ef9a58cb0be6f8b6b807cb0ee78867af38c
bestlyg <bestlyg@foxmail.com>
2025-10-23 08:04:37 +0800

    feat: 🎸 update server

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3347. 执行操作后元素的最高频率 II',
-    url: 'https://leetcode.cn/problems/maximum-frequency-of-an-element-after-performing-operations-ii',
+    name: '3461. 判断操作后字符串中的数字是否相等 I',
+    url: 'https://leetcode.cn/problems/check-if-digits-are-equal-in-string-after-operations-i',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `在执行完所有操作以后，请你返回 nums 中出现 频率最高 元素的出现次数。一个元素 x 的 频率 指的是它在数组中出现的次数。`,
+    desc: `如果 s 最后剩下的两个数字 相同 ，返回 true 。否则，返回 false。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.15',
-            time: 1625,
-            memory: 53.82,
-            desc: '离散差分',
+            time: 91,
+            memory:17.70,
+            desc: '递归',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
