549c73e46034de6524211815750659c258b1f690
bestlyg <bestlyg@foxmail.com>
2025-10-25 13:44:05 +0800

    feat: 🎸 add leetcode no.3461

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -16,17 +16,17 @@ import path from 'path';
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
     name: '3461. 判断操作后字符串中的数字是否相等 I',
-    url: 'https://leetcode.cn/problems/check-if-digits-are-equal-in-string-after-operations-i',
+    url: 'https://leetcode.cn/problems/calculate-money-in-leetcode-bank',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `如果 s 最后剩下的两个数字 相同 ，返回 true 。否则，返回 false。`,
+    desc: `给你 n ，请你返回在第 n 天结束的时候他在力扣银行总共存了多少块钱。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.15',
-            time: 91,
-            memory:17.70,
-            desc: '递归',
+            time: 3,
+            memory: 17.71,
+            desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
