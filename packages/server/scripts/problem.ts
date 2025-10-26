9f6999072ba774f727cdcd758d6f35fb51f68d6d
bestlyg <bestlyg@foxmail.com>
2025-10-26 16:12:32 +0800

    feat: 🎸 add leetcode no.2043

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3461. 判断操作后字符串中的数字是否相等 I',
-    url: 'https://leetcode.cn/problems/calculate-money-in-leetcode-bank',
+    name: '2043. 简易银行系统',
+    url: 'https://leetcode.cn/problems/simple-bank-system',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你 n ，请你返回在第 n 天结束的时候他在力扣银行总共存了多少块钱。`,
+    desc: `实现 Bank 类。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.15',
-            time: 3,
-            memory: 17.71,
-            desc: '遍历',
+            time: 41,
+            memory: 47.22,
+            desc: '模拟',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
