1b815ed5f9760c147d37a482761673f96d707d52
bestlyg <bestlyg@foxmail.com>
2025-10-27 13:11:42 +0800

    feat: 🎸 add leetcode no.2125

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,17 +15,17 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '2043. 简易银行系统',
-    url: 'https://leetcode.cn/problems/simple-bank-system',
+    name: '2125. 银行中的激光束数量',
+    url: 'https://leetcode.cn/problems/number-of-laser-beams-in-a-bank',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `实现 Bank 类。`,
+    desc: `返回银行中激光束的总数量。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.15',
-            time: 41,
-            memory: 47.22,
+            time: 13,
+            memory: 19.45,
             desc: '模拟',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
