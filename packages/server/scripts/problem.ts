cb8f25f29d349f43623855f9ff16c66a6ca048fe
bestlyg <bestlyg@foxmail.com>
2025-10-29 10:38:55 +0800

    feat: 🎸 add leetcode no.3370

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3354. 使数组元素等于零',
-    url: 'https://leetcode.cn/problems/make-array-elements-equal-to-zero',
+    name: '3370. 仅含置位位的最小整数',
+    url: 'https://leetcode.cn/problems/smallest-number-with-all-set-bits',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `返回可能的有效选择方案数目。`,
+    desc: `返回 大于等于 n 且二进制表示仅包含 置位 位的 最小 整数 x 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.15',
-            time: 41,
-            memory: 17.73,
-            desc: '判断0的左右数据总和',
+            time: 0,
+            memory: 17.62,
+            desc: '找最高位1',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
