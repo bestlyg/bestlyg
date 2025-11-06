5ab7353bf5ea5ca6d22b6ad0382f579099d36ce8
bestlyg <bestlyg@foxmail.com>
2025-11-06 15:52:20 +0800

    feat: 🎸 add leetcode no.3607

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3318. 计算子数组的 x-sum I',
-    url: 'https://leetcode.cn/problems/find-x-sum-of-all-k-long-subarrays-i',
+    name: '3607. 电网维护',
+    url: 'https://leetcode.cn/problems/power-grid-maintenance',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `返回一个长度为 n - k + 1 的整数数组 answer，其中 answer[i] 是 子数组 nums[i..i + k - 1] 的 x-sum。`,
+    desc: `返回一个整数数组，表示按照查询中出现的顺序，所有类型为 [1, x] 的查询结果。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.31',
-            time: 31,
-            memory: 17.49,
-            desc: '遍历每个子数组',
+            time: 1187,
+            memory: 109.22,
+            desc: '并查集记录每一个网，然后有序数组记录每一个组里最小的',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
