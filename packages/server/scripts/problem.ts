75c92d2607f37fff5c0bac8d932be961dca235b1
bestlyg <bestlyg@foxmail.com>
2025-11-07 13:24:35 +0800

    feat: 🎸 add leetcode no.2528

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3607. 电网维护',
-    url: 'https://leetcode.cn/problems/power-grid-maintenance',
+    name: '2528. 最大化城市的最小电量',
+    url: 'https://leetcode.cn/problems/maximize-the-minimum-powered-city',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `返回一个整数数组，表示按照查询中出现的顺序，所有类型为 [1, x] 的查询结果。`,
+    desc: `这 k 座供电站可以建在多个城市。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.10.31',
-            time: 1187,
-            memory: 109.22,
-            desc: '并查集记录每一个网，然后有序数组记录每一个组里最小的',
+            time: 7158,
+            memory: 29.67,
+            desc: '二分答案遍历最小值为power时的情况，用差分纪录当前电厂的数量',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
