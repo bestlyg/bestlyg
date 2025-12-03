65f712335e7fa6746cda7e4395e99bd1e05cd0cd
bestlyg <bestlyg@foxmail.com>
2025-12-03 22:56:10 +0800

    feat: 🎸 add leetcode no.3623

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '1590. 使数组和能被 P 整除',
-    url: 'https://leetcode.cn/problems/make-sum-divisible-by-p',
+    name: '3623. 统计梯形的数目 I',
+    url: 'https://leetcode.cn/problems/count-number-of-trapezoids-i',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你返回你需要移除的最短子数组的长度，如果无法满足题目要求，返回 -1 。`,
+    desc: `返回可以从 points 中任意选择四个不同点组成的 水平梯形 数量。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.11.21',
-            time: 91,
-            memory: 37.45,
-            desc: '遍历每一个数字，记录前缀和取模P后的结果',
+            time: 143,
+            memory: 62.29,
+            desc: '记录每条线存在多少个节点',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
