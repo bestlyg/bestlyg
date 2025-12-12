558d261dcd572d60ccb19f9c1a45727be06fbb18
bestlyg <bestlyg@foxmail.com>
2025-12-12 09:22:48 +0800

    feat: 🎸 add leetcode no.3433

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '3531. 统计被覆盖的建筑',
-    url: 'https://leetcode.cn/problems/count-covered-buildings',
+    name: '3433. 统计用户被提及情况',
+    url: 'https://leetcode.cn/problems/count-mentions-per-user',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `返回 被覆盖 的建筑数量。`,
+    desc: `返回数组 mentions ，其中 mentions[i] 表示  id 为  i 的用户在所有 MESSAGE 事件中被提及的次数。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.11.21',
-            time: 347,
-            memory: 59.02,
-            desc: '遍历每个存在建筑的行，再依次遍历每个建筑',
+            time: 38,
+            memory: 17.83,
+            desc: '模拟',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
