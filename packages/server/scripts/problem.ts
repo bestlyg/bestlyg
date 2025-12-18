657b6c02f7b5248f741bdc901c5a0d4e8e075d3f
bestlyg <bestlyg@foxmail.com>
2025-12-18 22:21:43 +0800

    feat: 🎸 update leetcode

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '2110. 股票平滑下跌阶段的数目',
-    url: 'https://leetcode.cn/problems/number-of-smooth-descent-periods-of-a-stock',
+    name: '3573. 买卖股票的最佳时机 V',
+    url: 'https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-v',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你返回 平滑下降阶段 的数目。`,
+    desc: `通过进行 最多 k 笔交易，返回你可以获得的最大总利润。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.11.21',
-            time: 67,
-            memory: 29.44,
-            desc: '遍历时记录上一个最小平滑下降下标',
+            time: 5579,
+            memory: 194.18,
+            desc: 'dfs',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
