5f7c25b124125adb7a8e7ae27ab45ee60ad34179
bestlyg <bestlyg@foxmail.com>
2025-12-24 21:41:20 +0800

    feat: 🎸 add leetcode no.3074

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,18 +14,18 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: true,
-    name: '944. 删列造序',
-    url: 'https://leetcode.cn/problems/delete-columns-to-make-sorted',
+    exist: !true,
+    name: '3074. 重新分装苹果',
+    url: 'https://leetcode.cn/problems/apple-redistribution-into-boxes',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `返回你需要删除的列数。`,
+    desc: `请你选择一些箱子来将这 n 个包裹中的苹果重新分装到箱子中，返回你需要选择的箱子的 最小 数量。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.11.21',
-            time: 107,
-            memory: 18,
+            time: 0,
+            memory: 17.27,
             desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
