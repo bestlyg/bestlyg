23f41ee747e641c6eac7ef2f4d75fa56f2979c82
bestlyg <bestlyg@foxmail.com>
2026-01-25 11:32:35 +0800

    feat: 🎸 update leetcode

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,19 +14,19 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: !true,
-    name: '3314. 构造最小位运算数组 I',
-    url: 'https://leetcode.cn/problems/construct-the-minimum-bitwise-array-i',
+    exist: true,
+    name: '1877. 数组中最大数对和的最小值',
+    url: 'https://leetcode.cn/problems/minimize-maximum-pair-sum-in-array',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你一个长度为 n 的 质数 数组 nums 。你的任务是返回一个长度为 n 的数组 ans ，对于每个下标 i ，以下 条件 均成立：ans[i] OR (ans[i] + 1) == nums[i]除此以外，你需要 最小化 结果数组里每一个 ans[i] 。`,
+    desc: `请你在最优数对划分的方案下，返回最小的 最大数对和 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2025.11.21',
-            time: 127,
-            memory: 19.04,
-            desc: '遍历',
+            time: 191,
+            memory: 31.24,
+            desc: '排序后，头尾相加',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
