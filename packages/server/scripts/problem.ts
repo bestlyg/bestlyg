0a80204891024b1f52efc456afc254528ae49041
bestlyg <bestlyg@foxmail.com>
2026-01-27 20:01:14 +0800

    feat: 🎸 update leetcode

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '1877. 数组中最大数对和的最小值',
-    url: 'https://leetcode.cn/problems/minimize-maximum-pair-sum-in-array',
+    name: '1200. 最小绝对差',
+    url: 'https://leetcode.cn/problems/minimum-absolute-difference',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你在最优数对划分的方案下，返回最小的 最大数对和 。`,
+    desc: `请你找到所有具有最小绝对差的元素对，并且按升序的顺序返回。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
-            // date: '2025.11.21',
-            time: 191,
-            memory: 31.24,
-            desc: '排序后，头尾相加',
+            date: '2026.01.27',
+            time: 63,
+            memory: 30.7,
+            desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
