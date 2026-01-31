2187ce9dbfed40e6566dca74ad7e1a69551203fa
bestlyg <bestlyg@foxmail.com>
2026-01-31 23:17:29 +0800

    feat: 🎸 add leetcode no.744

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '1200. 最小绝对差',
-    url: 'https://leetcode.cn/problems/minimum-absolute-difference',
+    name: '744. 寻找比目标字母大的最小字母',
+    url: 'https://leetcode.cn/problems/find-smallest-letter-greater-than-target',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你找到所有具有最小绝对差的元素对，并且按升序的顺序返回。`,
+    desc: `返回 letters 中大于 target 的最小的字符。如果不存在这样的字符，则返回 letters 的第一个字符。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
-            date: '2026.01.27',
-            time: 63,
-            memory: 30.7,
-            desc: '遍历',
+            // date: '2026.01.27',
+            time: 0,
+            memory:20.68,
+            desc: '二分查找',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
