519a9b2af65c57b65d26f57adbf2d48e9aab6e14
bestlyg <bestlyg@foxmail.com>
2026-02-18 12:49:44 +0800

    feat: 🎸 add leetcode no.693

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '190. 颠倒二进制位',
-    url: 'https://leetcode.cn/problems/reverse-bits',
+    name: '693. 交替位二进制数',
+    url: 'https://leetcode.cn/problems/binary-number-with-alternating-bits',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: ``,
+    desc: `给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.02.07',
-            time: 59,
-            memory: 18.90,
-            desc: '全排列',
+            time: 0,
+            memory: 19.24,
+            desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
