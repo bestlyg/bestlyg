e8691cf557152d212bd6e3b81af6ac0768db7937
bestlyg <bestlyg@foxmail.com>
2026-02-19 20:00:57 +0800

    feat: 🎸 add leetcode no.696

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '693. 交替位二进制数',
-    url: 'https://leetcode.cn/problems/binary-number-with-alternating-bits',
+    name: '696. 计数二进制子串',
+    url: 'https://leetcode.cn/problems/count-binary-substrings',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。`,
+    desc: `给定一个字符串 s，统计并返回具有相同数量 0 和 1 的非空（连续）子字符串的数量，并且这些子字符串中的所有 0 和所有 1 都是成组连续的。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.02.07',
-            time: 0,
-            memory: 19.24,
-            desc: '遍历',
+            time: 79,
+            memory: 19.83,
+            desc: '计数相邻的1和0',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
