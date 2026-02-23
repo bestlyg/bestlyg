511ea568283e94c3f8265adc129c70ace867cb38
bestlyg <bestlyg@foxmail.com>
2026-02-23 12:42:04 +0800

    feat: 🎸 add leetcode no.762

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '696. 计数二进制子串',
-    url: 'https://leetcode.cn/problems/count-binary-substrings',
+    name: '762. 二进制表示中质数个计算置位',
+    url: 'https://leetcode.cn/problems/prime-number-of-set-bits-in-binary-representation',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给定一个字符串 s，统计并返回具有相同数量 0 和 1 的非空（连续）子字符串的数量，并且这些子字符串中的所有 0 和所有 1 都是成组连续的。`,
+    desc: `给你两个整数 left 和 right ，在闭区间 [left, right] 范围内，统计并返回 计算置位位数为质数 的整数个数。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.02.07',
-            time: 79,
-            memory: 19.83,
-            desc: '计数相邻的1和0',
+            time: 115,
+            memory: 19.21,
+            desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
