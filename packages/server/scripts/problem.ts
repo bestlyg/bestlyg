58ec5d8a1d9fdad96828feaf9336cf960595bc2f
bestlyg <bestlyg@foxmail.com>
2026-02-24 18:33:19 +0800

    feat: 🎸 add leetcode no.1022

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,19 +14,19 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: true,
-    name: '762. 二进制表示中质数个计算置位',
-    url: 'https://leetcode.cn/problems/prime-number-of-set-bits-in-binary-representation',
+    exist: !true,
+    name: '1022. 从根到叶的二进制数之和',
+    url: 'https://leetcode.cn/problems/sum-of-root-to-leaf-binary-numbers',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你两个整数 left 和 right ，在闭区间 [left, right] 范围内，统计并返回 计算置位位数为质数 的整数个数。`,
+    desc: `对树上的每一片叶子，我们都要找出从根到该叶子的路径所表示的数字。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.02.07',
-            time: 115,
+            time: 3,
             memory: 19.21,
-            desc: '遍历',
+            desc: 'dfs',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
