37eab17bff589def4f14e1ccea3cdb0fd089d807
bestlyg <bestlyg@foxmail.com>
2026-02-25 10:09:40 +0800

    feat: 🎸 add leetcode no.1356

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,19 +14,19 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: !true,
-    name: '1022. 从根到叶的二进制数之和',
-    url: 'https://leetcode.cn/problems/sum-of-root-to-leaf-binary-numbers',
+    exist: true,
+    name: '1356. 根据数字二进制下 1 的数目排序',
+    url: 'https://leetcode.cn/problems/sort-integers-by-the-number-of-1-bits',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `对树上的每一片叶子，我们都要找出从根到该叶子的路径所表示的数字。`,
+    desc: `给你一个整数数组 arr 。请你将数组中的元素按照其二进制表示中数字 1 的数目升序排序。如果存在多个数字二进制中 1 的数目相同，则必须将它们按照数值大小升序排列。请你返回排序后的数组。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.02.07',
-            time: 3,
-            memory: 19.21,
-            desc: 'dfs',
+            time: 4,
+            memory: 19.20,
+            desc: '排序',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
