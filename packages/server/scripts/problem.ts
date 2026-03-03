e63ad3245d39f505b7f5eac60b1e61981f68ed90
bestlyg <bestlyg@foxmail.com>
2026-03-03 13:51:40 +0800

    feat: 🎸 add leetcode no.1545

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: !true,
-    name: '1689. 十-二进制数的最少数目',
-    url: 'https://leetcode.cn/problems/partitioning-into-minimum-number-of-deci-binary-numbers',
+    name: '1545. 找出第 N 个二进制字符串中的第 K 位',
+    url: 'https://leetcode.cn/problems/find-kth-bit-in-nth-binary-string',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你一个表示十进制整数的字符串 n ，返回和为 n 的 十-二进制数 的最少数目。`,
+    desc: `请你返回  Sn 的 第 k 位字符 ，题目数据保证 k 一定在 Sn 长度范围以内。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.02.07',
-            time: 89,
-            memory: 19.75,
-            desc: '贪心，取str中最大的数字',
+            time: 3,
+            memory: 19.03,
+            desc: 'dfs判断k在左边还是右边还是中间',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
