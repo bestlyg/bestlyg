d32c761d8ae0ee7c1927743aff65e228ed0fad22
bestlyg <bestlyg@foxmail.com>
2026-03-06 19:06:26 +0800

    feat: 🎸 add leetcode no.1784

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,17 +15,17 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist:! true,
-    name: '1582. 二进制矩阵中的特殊位置',
-    url: 'https://leetcode.cn/problems/special-positions-in-a-binary-matrix',
+    name: '1784. 检查二进制字符串字段',
+    url: 'https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给定一个 m x n 的二进制矩阵 mat，返回矩阵 mat 中特殊位置的数量。`,
+    desc: `给你一个二进制字符串 s ，该字符串 不含前导零 。如果 s 包含 零个或一个由连续的 '1' 组成的字段 ，返回 true​​​ 。否则，返回 false 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
-            date: '2026.03.05',
-            time: 115,
-            memory: 19.58,
+            // date: '2026.03.05',
+            time: 3,
+            memory: 19.00,
             desc: '遍历',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
