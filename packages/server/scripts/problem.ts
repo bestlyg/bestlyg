3739cf3cb7eacc641ea45d746e1f24646fd621a8
bestlyg <bestlyg@foxmail.com>
2026-03-18 13:57:54 +0800

    feat: 🎸 update leetcode

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist:! true,
-    name: '1415. 长度为 n 的开心字符串中字典序第 k 小的字符串',
-    url: 'https://leetcode.cn/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n',
+    name: '1727. 重新排列后的最大子矩阵',
+    url:'https://leetcode.cn/problems/largest-submatrix-with-rearrangements',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你返回排序后的第 k 个开心字符串，如果长度为 n 的开心字符串少于 k 个，那么请你返回 空字符串 。`,
+    desc: `请你返回最优方案下将 matrix 重新排列后，全是 1 的最大子矩阵面积。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.03.05',
-            time: 3,
-            memory: 19.00,
-            desc: 'dfs,数量是3*2*2...是个可明确的数量，可以批量计算',
+            time: 123,
+            memory: 33.29,
+            desc: '遍历每行，记录从当前值往上的最大连续的1',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
