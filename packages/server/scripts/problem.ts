fa51a85c135f8dd459fd603e32bd94cb031cd7dd
bestlyg <bestlyg@foxmail.com>
2026-04-24 22:42:23 +0800

    feat: 🎸 add leetcode no.2833

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,18 +14,18 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: true,
-    name: '2615. 等值距离和',
-    url:'https://leetcode.cn/problems/sum-of-distances',
+    exist: !true,
+    name: '2833. 距离原点最远的点',
+    url:'https://leetcode.cn/problems/furthest-point-from-origin',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给你一个下标从 0 开始的整数数组 nums 。现有一个长度等于 nums.length 的数组 arr 。对于满足 nums[j] == nums[i] 且 j != i 的所有 j ，arr[i] 等于所有 |i - j| 之和。如果不存在这样的 j ，则令 arr[i] 等于 0 。返回数组 arr 。`,
+    desc: `移动 n 次之后，请你找出可以到达的距离原点 最远 的点，并返回 从原点到这一点的距离 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.03.19',
-            time: 116,
-            memory: 53.25,
+            time: 3,
+            memory: 18.96,
             desc: '记录所有相同的下标的数组，遍历下标的时候记录左右的sum数量',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
