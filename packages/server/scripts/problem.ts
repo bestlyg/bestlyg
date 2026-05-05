b596b81bc8beb6e4dcfa80e6cee6a0ee1671e734
bestlyg <bestlyg@foxmail.com>
2026-05-05 21:01:44 +0800

    feat: 🎸 add leetcode no.61

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -15,18 +15,18 @@ import path from 'path';
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
     exist: true,
-    name: '796. 旋转字符串',
-    url:'https://leetcode.cn/problems/rotate-string',
+    name: '61. 旋转链表',
+    url:'https://leetcode.cn/problems/rotate-list',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `给定两个字符串, s 和 goal。如果在若干次旋转操作之后，s 能变成 goal ，那么返回 true 。`,
+    desc: `给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
             // date: '2026.03.19',
-            time: 0,
-            memory: 19.14,
-            desc: '遍历',
+            time: 3,
+            memory: 19.04,
+            desc: '记录新头节点进行重新组装',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
