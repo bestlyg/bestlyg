b8ddd76a95e00f1eb5e5be9db67c4e893c45f3e2
bestlyg <bestlyg@foxmail.com>
2026-05-20 22:26:56 +0800

    feat: 🎸 add leetcode no.2657

--- packages/server/scripts/problem.ts
+++ packages/server/scripts/problem.ts
@@ -14,19 +14,19 @@ import path from 'path';
 // /** @type {import("../dist/types").LeetCodeProblem} */
 // /** @type {Problem} */
 export const problem: LeetcodeProblem & { exist: boolean } = {
-    exist: true,
-    name: '1345. 跳跃游戏 IV',
-    url: 'https://leetcode.cn/problems/jump-game-iv',
+    exist: !true,
+    name: '2657. 找到两个数组的前缀公共数组',
+    url: 'https://leetcode.cn/problems/find-the-prefix-common-array-of-two-arrays',
     level: LeetcodeLevelType.Easy,
     tags: [],
-    desc: `请你返回到达数组最后一个元素的下标处所需的 最少操作次数 。`,
+    desc: `请你返回 A 和 B 的 前缀公共数组 。`,
     solutions: [
         {
             script: LeetcodeScriptType.python,
-            date: '2026.05.18',
-            time: 24,
-            memory: 44.82,
-            desc: '取交集后排序',
+            // date: '2026.05.18',
+            time: 7,
+            memory: 19.2,
+            desc: '二进制存储',
             code: fs.readFileSync(path.resolve(__dirname, 'code'), 'utf8'),
         },
     ],
